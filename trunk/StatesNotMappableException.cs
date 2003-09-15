8bbf8826de16c8351fcb6f62a628c468563d30a6
yvette <yvette>
2003-09-15 07:04:12 +0000

    *** empty log message ***
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@1217 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ trunk/StatesNotMappableException.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace FSM
+{
+	/// <summary>
+	/// Zusammenfassung für StatesNotMappableException.
+	/// </summary>
+	public class StatesNotMappableException : Exception
+	{
+		public StatesNotMappableException()
+		{
+			//
+			// TODO: Fügen Sie hier die Konstruktorlogik hinzu
+			//
+		}
+	}
+}
