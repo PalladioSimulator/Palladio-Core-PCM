2c8634ca06d02b6be6a4dc58c6cd1757cec6b175
sliver <sliver>
2004-05-07 15:07:31 +0000

    example prints protocols and components now
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@173 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- trunk/Example.cs
+++ trunk/Example.cs
@@ -6,101 +6,144 @@ using Palladio.ComponentModel;
 namespace Example
 {
 	/// <summary>
+	/// Example for the usage of the ComponentModel.
 	/// </summary>
 	public class ComponentExample
 	{
+		private static FSMFactory factory = new FSMFactory();
 
 		public static Component BuildComponentA()
 		{
-			FSMFactory factory = new FSMFactory();
-
 			IState[] states = new State[5];
 			states[0] = new State("start",true,false);
 			states[1] = new State("final",false,true);
 
-			Input[] inputs = new Input[5];
-			inputs[2] = new Input("e1");
-			inputs[3] = new Input("e2");
-			inputs[4] = new Input("e3");
+			// Input symbols of the protocol of the ProvidesInterface
+			Input[] provInputs = new Input[2];
+			provInputs[0] = new Input("d1");
+			provInputs[1] = new Input("d2");
 
-			// Provides Signaturen
-			IList prvSigList = new ArrayList();
-			prvSigList.Add( new SimpleSignature("d1") );
-			prvSigList.Add( new SimpleSignature("d2") );
+			IList provTransList = new ArrayList();
+			//									start	     d2			start
+			provTransList.Add( new Transition (states[0],provInputs[1],states[0]) );
+			//									start	     d1			final
+			provTransList.Add( new Transition (states[0],provInputs[0],states[1]) );
+			//									final	     d2			final
+			provTransList.Add( new Transition (states[1],provInputs[1],states[1]) );
 
-			// Provides InterfaceModel
-			IInterfaceModel prvIModel = new SignatureListIModel( prvSigList );
+			IFiniteStateMachine provFsm = factory.CreateFSM(provTransList);
+			
+			// Provides signatures
+			IList provSigList = new ArrayList();
+			provSigList.Add( new SimpleSignature("d1") );
+			provSigList.Add( new SimpleSignature("d2") );
+			IInterfaceModel provIModel = new ProtocolIModel( provFsm, provSigList );
+
+			// Input symbols of the protocol of the RequiresInterface
+			Input[] reqInputs = new Input[3];
+			reqInputs[0] = new Input("e1");
+			reqInputs[1] = new Input("e2");
+			reqInputs[2] = new Input("e3");
 
 			// Service Effect Spec d1
 			IList d1TransList = new ArrayList();
-			d1TransList.Add( new Transition (states[0],inputs[2],states[1]) );
-			d1TransList.Add( new Transition (states[1],inputs[3],states[0]) );
+			//								  start		  e1	  final
+			d1TransList.Add( new Transition (states[0],reqInputs[0],states[1]) );
+			//								  final		  e2	  start
+			d1TransList.Add( new Transition (states[1],reqInputs[1],states[0]) );
 			IFiniteStateMachine d1Fsm = factory.CreateFSM(d1TransList);
 
 			IList d1SigList = new ArrayList();
 			d1SigList.Add( new SimpleSignature("e1") );
 			d1SigList.Add( new SimpleSignature("e2") );
-
 			IInterfaceModel d1IModel = new ProtocolIModel( d1Fsm, d1SigList );
 
 			// Service Effect Spec d2
 			IList d2TransList = new ArrayList();
-			d2TransList.Add( new Transition (states[0],inputs[3],states[1]) );
+			//								  start		  e3	  final
+			d2TransList.Add( new Transition (states[0],reqInputs[2],states[1]) );
 			IFiniteStateMachine d2Fsm = factory.CreateFSM(d2TransList);
 
 			IList d2SigList = new ArrayList();
-			d2SigList.Add( new SimpleSignature("e1") );
-			d2SigList.Add( new SimpleSignature("e2") );
-
+			d2SigList.Add( new SimpleSignature("e3") );
 			IInterfaceModel d2IModel = new ProtocolIModel( d2Fsm, d2SigList );
 
 			// ServiceList
-			IList prvSrvList = new ArrayList();
-			prvSrvList.Add( new Service(d1IModel, new SimpleSignature("d1")) );
-			prvSrvList.Add( new Service(d2IModel, new SimpleSignature("d2")) );
+			IList provSrvList = new ArrayList();
+			provSrvList.Add( new Service(d1IModel, new SimpleSignature("d1")) );
+			provSrvList.Add( new Service(d2IModel, new SimpleSignature("d2")) );
 
 			// ProvidesInterface
-			ProvidesInterface prvIFace = new ProvidesInterface( prvIModel, prvSrvList, "provides");
+			ProvidesInterface provIFace = new ProvidesInterface( provIModel, provSrvList, "provides");
 
 			// Requires IModel
-			IList rqSigList = new ArrayList();
-			rqSigList.Add( new SimpleSignature("e1") );
-			rqSigList.Add( new SimpleSignature("e2") );
-			rqSigList.Add( new SimpleSignature("e3") );
-			IInterfaceModel rqIModel = new SignatureListIModel(rqSigList);
+			IList reqSigList = new ArrayList();
+			reqSigList.Add( new SimpleSignature("e1") );
+			reqSigList.Add( new SimpleSignature("e2") );
+			reqSigList.Add( new SimpleSignature("e3") );
+			IInterfaceModel reqIModel = new SignatureListIModel(reqSigList);
 
 			// RequiresInterface
-			RequiresInterface rqIFace = new RequiresInterface(rqIModel,"extern");
+			RequiresInterface reqIFace = new RequiresInterface(reqIModel,"extern");
 
 			// Component
-			IList prvList = new ArrayList();
-			prvList.Add(prvIFace);
-			IList rqList = new ArrayList();
-			rqList.Add(rqIFace);
+			IList provList = new ArrayList();
+			provList.Add(provIFace);
+			IList reqList = new ArrayList();
+			reqList.Add(reqIFace);
 
-			return new Component(prvList,rqList);		
+			return new Component(provList,reqList);		
 		}
 
 		public static Component BuildComponentB()
 		{
-			IList prvSigList = new ArrayList();
-			prvSigList.Add( new SimpleSignature("e1") );
-			prvSigList.Add( new SimpleSignature("e2") );
-			prvSigList.Add( new SimpleSignature("e3") );
+			IList provSigList = new ArrayList();
+			provSigList.Add( new SimpleSignature("e1") );
+			provSigList.Add( new SimpleSignature("e2") );
+			provSigList.Add( new SimpleSignature("e3") );
+			provSigList.Add( new SimpleSignature("e4") );
 
-			IInterfaceModel prvIModel = new SignatureListIModel( prvSigList );
-			ProvidesInterface prvIFace = new ProvidesInterface( prvIModel, "extern" );
+			IInterfaceModel provIModel = new SignatureListIModel( provSigList );
+			ProvidesInterface provIFace = new ProvidesInterface( provIModel, "extern" );
 
-			IList prvList = new ArrayList();
-			prvList.Add( prvIFace );
-			return new Component( prvList );
+			IList provList = new ArrayList();
+			provList.Add( provIFace );
+			return new Component( provList );
 		}
+
 
 		public static void Main()
 		{
 			Component compA = BuildComponentA();
 			Component compB = BuildComponentB();
-			CompBinding binding = new CompBinding(compA,compB);
+			ComponentBinding binding = new ComponentBinding(compA,compB);
+
+			Console.WriteLine("\n------------------------------------------------------");
+			Console.WriteLine(binding.IfaceBindings[0].ProvIface);
+
+			Console.WriteLine("\n------------------------------------------------------");
+			Console.WriteLine(binding.IfaceBindings[0].ReqIface);
+
+			Console.WriteLine("\n------------------------------------------------------");
+			IInterfaceModel modelIntersection = binding.IfaceBindings[0].GetIntersection();
+			Console.WriteLine(modelIntersection);
+
+			Console.WriteLine("\n------------------------------------------------------");
+			
+			IList provIfaceList = binding.ReqComponent.GetProvidesIfaceList();
+			foreach (ProvidesInterface provIface in provIfaceList)
+			{
+				Console.WriteLine(provIface);
+				Console.WriteLine("\n------------------------------------------------------");
+			}
+
+			IList reqIfaceList = binding.ReqComponent.GetRequiresIfaceList();
+			foreach (RequiresInterface reqIface in reqIfaceList)
+			{
+				Console.WriteLine(reqIface);
+				Console.WriteLine("\n------------------------------------------------------");
+			}
+
 		}
 	}
 }
