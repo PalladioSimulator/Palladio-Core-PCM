60cc1fe46ff55bc458c04da04f2baee915e39742
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2004-05-12 13:28:04 +0000

    Added documentation and CVS log
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@1174 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- trunk/src/InputSymbolHash.cs
+++ trunk/src/InputSymbolHash.cs
@@ -4,13 +4,34 @@ using System.Collections;
 namespace Palladio.FiniteStateMachines
 {
 	/// <summary>
-	/// Zusammenfassung für InputSymbolHash.
+	/// A helper class for editing FSMs. An array of <see cref="IInput"/> is loaded
+	/// into this hash and the single <see cref="IInput"/> can be accessed by their
+	/// IDs from the hashmap. 
 	/// </summary>
 	/// <remarks>
+	/// An usage example:
+	/// <code>
+	///	IEditableFiniteStateMachine fsm = FSMFactory.CreateEmptyFSM();
+	///	StateHash states = FSMFactory.CreateStatesFromList("1","2","3");
+	///	InputSymbolHash inputs = FSMFactory.CreateInputFromList("a","b","c","eps");
+	///	fsm.AddStates(states.StoredStates);
+	///	fsm.AddInputSymbols(inputs.StoredInputs);
+	///	fsm.AddTransitions (FSMFactory.CreateDefaultTransition(states["1"],inputs["a"],states["1"]));
+	///	fsm.AddTransitions (FSMFactory.CreateDefaultTransition(states["1"],inputs["b"],states["2"]));
+	///	fsm.AddTransitions (FSMFactory.CreateDefaultTransition(states["2"],inputs["c"],states["3"]));
+	///	fsm.AddTransitions (FSMFactory.CreateDefaultTransition(states["3"],inputs["eps"],states["1"]));
+	///	Assert.IsTrue(fsm.Transitions.Length == 4);
+	///	states = FSMFactory.CreateStatesFromList("1","2","3");
+	///	fsm.FinalStates = new IState[] { states["1"], states["3"] };
+	///	fsm.StartState = states["1"];
+	/// </code>
 	/// <pre>
 	/// Version history:
 	///
 	/// $Log$
+	/// Revision 1.4  2004/05/12 13:28:04  sbecker
+	/// Added documentation and CVS log
+	///
 	/// Revision 1.3  2004/05/12 08:12:59  sbecker
 	/// Fixed small typo
 	///
@@ -23,8 +44,14 @@ namespace Palladio.FiniteStateMachines
 	/// </remarks>	
 	public class InputSymbolHash
 	{
+		/// <summary>
+		/// The internal used Hashtable to store the input symbols and their IDs
+		/// </summary>
 		protected Hashtable inputHash = new Hashtable();
 
+		/// <summary>
+		/// Get the input symbol with the given ID
+		/// </summary>
 		public IInput this[object key]
 		{
 			get
@@ -33,11 +60,18 @@ namespace Palladio.FiniteStateMachines
 			}
 		}
 
-		public void Add(object key, object val)
+		/// <summary>
+		/// Add an input symbol to this Hashtable
+		/// </summary>
+		/// <param name="anInput">An IInput to be added to the Hashtable</param>
+		public void Add(IInput anInput)
 		{
-			inputHash.Add(key,val);
+			inputHash.Add(anInput.ID,anInput);
 		}
 
+		/// <summary>
+		/// Return the stored IInputs as an array of IInput
+		/// </summary>
 		public IInput[] StoredInputs
 		{
 			get
@@ -48,18 +82,29 @@ namespace Palladio.FiniteStateMachines
 			}
 		}
 
+		/// <summary>
+		/// Default constructor - create an empty input symbol hash
+		/// </summary>
 		public InputSymbolHash()
 		{
 		}
 
+		/// <summary>
+		/// Create an input symbol hash from an array of IInput
+		/// </summary>
+		/// <param name="inputs">The IInput array to create the hashmap from</param>
 		public InputSymbolHash(IInput[] inputs)
 		{
 			foreach (IInput i in inputs)
 			{
-				this.Add(i.ID,i);
+				this.Add(i);
 			}
 		}
 
+		/// <summary>
+		/// A ToString to format the entries of this hashmap
+		/// </summary>
+		/// <returns>A string representation of the hashmap</returns>
 		public override string ToString()
 		{
 			string result = "";
