19a6bcdd1d184578d4f360a4937b2fc2b2003c54
joemal <dummy>
2004-05-20 14:20:15 +0000

    xxx
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@1622 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- trunk/src/ComponentNetworkSimulation/src/testapplication/TestApp.cs
+++ trunk/src/ComponentNetworkSimulation/src/testapplication/TestApp.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections;
 using ComponentNetworkSimulation;
-using Palladio.Attributes;
+using ComponentNetworkSimulation.Structure;
+using ComponentNetworkSimulation.Structure.Services;
+using ComponentNetworkSimulation.Structure.Visitor;
 using Palladio.FiniteStateMachines;
-using ReflectionBasedVisitor;
+
 namespace testapplication
 {
 
@@ -13,218 +15,55 @@ namespace testapplication
 
 		public static void Main()
 		{
-	/*		try 
-			{
-				ISimulationEnvironment env = new SimpleTestSimulationEnvironment();
-				env.Simulate();
-			}
-			catch(Exception exc)
-			{
-				Console.Out.WriteLine(""+exc.GetType()+exc.StackTrace);
+			/*		try 
+					{
+						ISimulationEnvironment env = new SimpleTestSimulationEnvironment();
+						env.Simulate();
+					}
+					catch(Exception exc)
+					{
+						Console.Out.WriteLine(""+exc.GetType()+exc.StackTrace);
 				
-			}
-*/
+					}
+		*/			
 			IFiniteStateMachine fsm = createFSM();
-			MyVisitor visitor = new MyVisitor();
-			visitor.setStartService(fsm);
-			int cntr = 0;
-			while(visitor.VisitNext() && cntr++<20);
+
+			IComponentArchitectureVisitor visitor = new DefaultComponentArchitectureVisitor();
+			visitor.SetStart(fsm);
+            
+			int a=0;
+
+			while(visitor.CurrentTimeConsumer != null && a++ <20)
+			{
+				Console.WriteLine("Enter TC: "+visitor.CurrentTimeConsumer+", time to wait: "+
+					visitor.CurrentTimeConsumer.ThreadEntered());
+				visitor.NextTimeConsumer();
+			}
 			Console.ReadLine();
-		}
-
-		private static IFiniteStateMachine createFSM()
-		{
-			IEditableFiniteStateMachine fsm = FSMFactory.GetEditableFSM(FSMFactory.CreateEmptyFSM());		
-			InputSymbolHash inps = FSMFactory.CreateInputFromList("e0","e1","e2");
-			fsm.AddInputSymbols(inps.StoredInputs);
-
-			StateHash states = FSMFactory.CreateStatesFromList("0","1","2","3");
-			fsm.AddStates(states.StoredStates);
-			fsm.StartState = states["0"];
-			fsm.FinalStates = new IState[] {states["3"]};
 			
-			fsm.AddTransition("0",inps["e0"].ID,"1");
-			fsm.AddTransition("1",inps["e1"].ID,"2");
-			fsm.AddTransition("1",inps["e2"].ID,"3");
-			fsm.AddTransition("2",inps["e2"].ID,"3");
-
-			return fsm;
 		}
 
-		public class MyVisitor : ReflectionBasedVisitor.ReflectionBasedVisitor
+		public static IFiniteStateMachine createFSM() 
 		{
-			private ArchitectureStack stack = new ArchitectureStack();
+			IServiceBuilder builder = new DefaultServiceBuilder();
 
-			public MyVisitor() : base(false){}
+			IControlFlowStrategy strategy = builder.GetControlFlowStrategy(DefaultServiceBuilder.STRATEGY_RANDOM);
 
-			//Todo: later add service, IFaceModel, Component
-			public void setStartService(IFiniteStateMachine fsm)
-			{
-				stack.Clear();
-				stack.Push(fsm);
-			}
+			builder.AddState(new StaticTimeStateParams("0",strategy,LoggingType_t.NO_LOG,5));
+			builder.AddState(new StaticTimeStateParams("1",strategy,LoggingType_t.NO_LOG,7));
+			builder.AddState(new StaticTimeStateParams("2",strategy,LoggingType_t.NO_LOG,3));
+			builder.AddState(new StaticTimeStateParams("3",strategy,LoggingType_t.NO_LOG,8));
 
-			public bool VisitNext()
-			{
-				Console.WriteLine("Next Step");
-				if (stack.Count != 0) Visit(stack.Pop());
+			builder.SetFinalStates(new String[]{"3"});
+			builder.SetStartState("0");
 
-				return (stack.Count != 0);
-			}
+			builder.AddTransition("0","e0","1");
+			builder.AddTransition("1","e1","2");
+			builder.AddTransition("1","e2","3");
+			builder.AddTransition("2","e2","3");
 
-			public override void VisitObject(object o)
-			{
-				Console.WriteLine("Visit unknown obj");
-			}
-
-			public void VisitIState(IState state)
-			{
-				Console.WriteLine("Visit state: "+state);
-				IFiniteStateMachine fsm = (IFiniteStateMachine)stack.Peek();
-
-				ITransition[] outTrans = fsm.GetOutgoingTransitions(state);
-				int count = outTrans.Length;
-
-				if (fsm.IsFinalState(state)) count ++;
-                int next = TestApp.random.Next(count);				
-
-				if (next < outTrans.Length)	
-					stack.Push(outTrans[next]);
-				else 
-					stack.Pop();
-			}
-
-			public void VisitITransition(ITransition transition)
-			{
-				Console.WriteLine("Visit transition: "+transition.InputSymbol);
-				stack.Push(transition.DestinationState);
-			}
+			return builder.BuildService();
 		}
 
-		public class StaticTimeState : IState
-		{
-			protected string id;
-			protected IAttributeHash attributes;
-
-			/// <summary>
-			/// If true, this state is an error state in which the FSM can drop if
-			/// running along an invalid transition
-			/// </summary>
-			public bool IsErrorState
-			{
-				get
-				{
-					return false;
-				}
-
-				set
-				{
-				}
-			}
-		
-			/// <summary>
-			/// Return a string representation of this state, e.g. for graphical output.
-			/// Here implemented as payload.ToString();
-			/// </summary>
-			public string ID
-			{
-				get
-				{
-					return id;
-				}
-			}
-
-			/// <summary>
-			/// Information describing this state. Any type of information object can be
-			/// associated to a state.
-			/// </summary>
-			public virtual IAttributeHash Attributes
-			{
-				get
-				{
-					return attributes;
-				}
-			}
-
-			/// <summary>
-			///		Default implementation of GetHashCode(). 
-			///		Refers to the hashcode of name.
-			/// </summary>
-			/// <returns>
-			///		Hashcode of Name
-			///	</returns>
-			public override int GetHashCode() 
-			{
-				return id.GetHashCode();
-			}
-		
-			/// <summary>
-			///		Default implementation of Equals.
-			/// </summary>
-			/// <param name="obj">
-			///		The other object.
-			///	</param>
-			/// <returns>
-			///		True, if Name, IsStartState and
-			///		IsFinalState are Equal; false otherwise.
-			///	</returns>
-			public override bool Equals(object obj) 
-			{
-				if (obj is IState) 
-				{ 
-					IState state = (IState)obj;
-					return id.Equals(state.ID);
-				}
-				return false;
-			}
-
-			/// <summary>
-			/// Overriden ToString for debug purposes
-			/// </summary>
-			/// <returns>The label of this state</returns>
-			public override string ToString()
-			{
-				return ID;
-			}
-
-			/// <summary>
-			/// Implementation of the data structure driven visitor movement. It is
-			/// not implemented here!
-			/// </summary>
-			/// <param name="v">The visitor visiting this object</param>
-			public virtual void AcceptVisitor(IVisitor v) {}
-		}
- 
-
-		public class ArchitectureStack : Stack
-		{
-			public void Push(IState state)
-			{
-                base.Push(state);
-			}
-
-			public void Push(ITransition transition)
-			{
-				base.Push(transition);
-			}
-
-			public void Push(IFiniteStateMachine fsm)
-			{
-				base.Push(fsm);
-				this.Push(fsm.StartState);
-			}
-			
-			//todo: später mal weiter Push methoden für Komponenten, IFaces und Services
-
-			public override void Push(object obj)
-			{
-				throw(new NotImplementedException("This method is not supported in this implementation of stack."));
-			}
-
-			public Type PeekType() 
-			{
-				return this.Peek().GetType();
-			}
-		}
 	}
 }
