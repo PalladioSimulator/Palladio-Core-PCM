996e94ce510e662ef3e6d45f462acb7f20f3048e
joemal <dummy>
2004-05-24 16:00:37 +0000

    xxx
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@1624 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- trunk/src/ComponentNetworkSimulation/src/testapplication/TestApp.cs
+++ trunk/src/ComponentNetworkSimulation/src/testapplication/TestApp.cs
@@ -1,10 +1,7 @@
 using System;
-using System.Collections;
-using ComponentNetworkSimulation;
-using ComponentNetworkSimulation.Structure;
-using ComponentNetworkSimulation.Structure.Services;
-using ComponentNetworkSimulation.Structure.Visitor;
 using Palladio.FiniteStateMachines;
+using Palladio.Attributes;
+using ComponentNetworkSimulation.Structure.Services;
 
 namespace testapplication
 {
@@ -26,44 +23,13 @@ namespace testapplication
 				
 					}
 		*/			
-			IFiniteStateMachine fsm = createFSM();
 
-			IComponentArchitectureVisitor visitor = new DefaultComponentArchitectureVisitor();
-			visitor.SetStart(fsm);
-            
-			int a=0;
-
-			while(visitor.CurrentTimeConsumer != null && a++ <20)
-			{
-				Console.WriteLine("Enter TC: "+visitor.CurrentTimeConsumer+", time to wait: "+
-					visitor.CurrentTimeConsumer.ThreadEntered());
-				visitor.NextTimeConsumer();
-			}
+			IState state = FSMFactory.CreateDefaultState("State");
+			IAttributeType type = new AttributesFactory().Default.CreateAttributeType(Guid.NewGuid(),"Controlflowstrategie",typeof(IControlFlowStrategy));
+			state.Attributes.Add(type, new DefaultServiceBuilder().GetControlFlowStrategy(DefaultServiceBuilder.STRATEGY_RANDOM));
+            Console.WriteLine("Attr: "+state.Attributes[type]);
 			Console.ReadLine();
 			
 		}
-
-		public static IFiniteStateMachine createFSM() 
-		{
-			IServiceBuilder builder = new DefaultServiceBuilder();
-
-			IControlFlowStrategy strategy = builder.GetControlFlowStrategy(DefaultServiceBuilder.STRATEGY_RANDOM);
-
-			builder.AddState(new StaticTimeStateParams("0",strategy,LoggingType_t.NO_LOG,5));
-			builder.AddState(new StaticTimeStateParams("1",strategy,LoggingType_t.NO_LOG,7));
-			builder.AddState(new StaticTimeStateParams("2",strategy,LoggingType_t.NO_LOG,3));
-			builder.AddState(new StaticTimeStateParams("3",strategy,LoggingType_t.NO_LOG,8));
-
-			builder.SetFinalStates(new String[]{"3"});
-			builder.SetStartState("0");
-
-			builder.AddTransition("0","e0","1");
-			builder.AddTransition("1","e1","2");
-			builder.AddTransition("1","e2","3");
-			builder.AddTransition("2","e2","3");
-
-			return builder.BuildService();
-		}
-
 	}
 }
