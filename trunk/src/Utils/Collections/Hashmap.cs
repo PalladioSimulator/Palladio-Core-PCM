b44482263d826ec91c1a90f9c9815e6e17a3dc42
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2004-06-02 14:53:08 +0000

    Added Constructors
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@1756 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- trunk/src/Utils/Collections/Hashmap.cs
+++ trunk/src/Utils/Collections/Hashmap.cs
@@ -11,6 +11,9 @@ namespace Palladio.Utils.Collections
 	/// Version history:
 	///
 	/// $Log$
+	/// Revision 1.2  2004/06/02 14:53:08  sbecker
+	/// Added Constructors
+	///
 	/// Revision 1.1  2004/05/24 09:18:49  sliver
 	/// added Hashmap
 	///
@@ -80,5 +83,271 @@ namespace Palladio.Utils.Collections
 			}
 			return result;
 		}
-		}
+
+		#region Hashmap()
+		/// <overloads>
+		/// Initializes a new instance of the <see cref="Hashmap"/> class.
+		/// </overloads>
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Hashmap"/> class
+		/// that is empty and has the default initial capacity, the default load factor,
+		/// the default hash code provider and the default comparer.
+		/// </summary>
+		/// <remarks>Please refer to <see cref="Hashmap()"/> for details.</remarks>
+
+		public Hashmap() : base() {}
+
+		#endregion
+		#region Hashmap(IHashCodeProvider, IComparer)
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Hashmap"/> class
+		/// that is empty and has the default initial capacity, the default load factor,
+		/// the specified hash code provider and the specified comparer.
+		/// </summary>
+		/// <param name="provider">
+		/// <para>The <see cref="IHashCodeProvider"/> that supplies the hash codes
+		/// for all keys in the <see cref="Hashmap"/>.</para>
+		/// <para>-or-</para>
+		/// <para>A null reference to use the default hash code provider, which is each key's
+		/// implementation of <see cref="Object.GetHashCode"/>.</para></param>
+		/// <param name="comparer">
+		/// <para>The <see cref="IComparer"/> to use to determine whether two keys are equal.</para>
+		/// <para>-or-</para>
+		/// <para>A null reference to use the default comparer, which is each key's
+		/// implementation of <see cref="Object.Equals"/>.</para></param>
+		/// <remarks>
+		/// Please refer to <see cref="Hashmap(IHashCodeProvider, IComparer)"/> for details.
+		/// </remarks>
+
+		public Hashmap(IHashCodeProvider provider, IComparer comparer) : base (provider,comparer){}
+
+		#endregion
+		#region Hashmap(Int32)
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Hashmap"/> class
+		/// that is empty and has the specified initial capacity, the default load factor,
+		/// the default hash code provider and the default comparer.
+		/// </summary>
+		/// <param name="capacity">The approximate number of elements that the new
+		/// <see cref="Hashmap"/> is initially capable of storing.</param>
+		/// <exception cref="ArgumentOutOfRangeException">
+		/// <paramref name="capacity"/> is less than zero.</exception>
+		/// <remarks>Please refer to <see cref="Hashmap(Int32)"/> for details.</remarks>
+
+		public Hashmap(int capacity) : base (capacity) {}
+
+		#endregion
+		#region Hashmap(Int32, IHashCodeProvider, IComparer)
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Hashmap"/> class
+		/// that is empty and has the specified initial capacity, the default load factor,
+		/// the specified hash code provider and the specified comparer.
+		/// </summary>
+		/// <param name="capacity">The approximate number of elements that the new
+		/// <see cref="Hashmap"/> is initially capable of storing.</param>
+		/// <param name="provider">
+		/// <para>The <see cref="IHashCodeProvider"/> that supplies the hash codes
+		/// for all keys in the <see cref="Hashmap"/>.</para>
+		/// <para>-or-</para>
+		/// <para>A null reference to use the default hash code provider, which is each key's
+		/// implementation of <see cref="Object.GetHashCode"/>.</para></param>
+		/// <param name="comparer">
+		/// <para>The <see cref="IComparer"/> to use to determine whether two keys are equal.</para>
+		/// <para>-or-</para>
+		/// <para>A null reference to use the default comparer, which is each key's
+		/// implementation of <see cref="Object.Equals"/>.</para></param>
+		/// <exception cref="ArgumentOutOfRangeException">
+		/// <paramref name="capacity"/> is less than zero.</exception>
+		/// <remarks>
+		/// Please refer to <see cref="Hashmap(Int32, IHashCodeProvider, IComparer)"/> for details.
+		/// </remarks>
+
+		public Hashmap(int capacity,
+			IHashCodeProvider provider, IComparer comparer) : base (capacity,provider,comparer){}
+
+		#endregion
+		#region Hashmap(Int32, Single)
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Hashmap"/> class
+		/// that is empty and has the specified initial capacity, the specified load factor,
+		/// the default hash code provider and the default comparer.
+		/// </summary>
+		/// <param name="capacity">The approximate number of elements that the new
+		/// <see cref="Hashmap"/> is initially capable of storing.</param>
+		/// <param name="loadFactor">A number in the range from 0.1 through 1.0
+		/// indicating the maximum ratio of elements to buckets.</param>
+		/// <exception cref="ArgumentOutOfRangeException">
+		/// <para><paramref name="capacity"/> is less than zero.</para>
+		/// <para>-or-</para>
+		/// <para><paramref name="loadFactor"/> is less than 0.1.</para>
+		/// <para>-or-</para>
+		/// <para><paramref name="loadFactor"/> is greater than 1.0.</para></exception>
+		/// <remarks>Please refer to <see cref="Hashmap(Int32, Single)"/> for details.</remarks>
+		public Hashmap(int capacity, float loadFactor) : base (capacity,loadFactor) {}
+
+		#endregion
+		#region Hashmap(Int32, Single, IHashCodeProvider, IComparer)
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Hashmap"/> class
+		/// that is empty and has the specified initial capacity, the specified load factor,
+		/// the specified hash code provider and the specified comparer.
+		/// </summary>
+		/// <param name="capacity">The approximate number of elements that the new
+		/// <see cref="Hashmap"/> is initially capable of storing.</param>
+		/// <param name="loadFactor">A number in the range from 0.1 through 1.0
+		/// indicating the maximum ratio of elements to buckets.</param>
+		/// <param name="provider">
+		/// <para>The <see cref="IHashCodeProvider"/> that supplies the hash codes
+		/// for all keys in the <see cref="Hashmap"/>.</para>
+		/// <para>-or-</para>
+		/// <para>A null reference to use the default hash code provider, which is each key's
+		/// implementation of <see cref="Object.GetHashCode"/>.</para></param>
+		/// <param name="comparer">
+		/// <para>The <see cref="IComparer"/> to use to determine whether two keys are equal.</para>
+		/// <para>-or-</para>
+		/// <para>A null reference to use the default comparer, which is each key's
+		/// implementation of <see cref="Object.Equals"/>.</para></param>
+		/// <exception cref="ArgumentOutOfRangeException">
+		/// <para><paramref name="capacity"/> is less than zero.</para>
+		/// <para>-or-</para>
+		/// <para><paramref name="loadFactor"/> is less than 0.1.</para>
+		/// <para>-or-</para>
+		/// <para><paramref name="loadFactor"/> is greater than 1.0.</para></exception>
+		/// <remarks>
+		/// Please refer to <see cref="Hashmap(Int32, Single, IHashCodeProvider, IComparer)"/>
+		/// for details.</remarks>
+		public Hashmap(int capacity, float loadFactor,
+			IHashCodeProvider provider, IComparer comparer) : base (capacity,loadFactor,provider,comparer){}
+
+		#endregion
+		#region Hashmap(Hashmap)
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Hashmap"/> class
+		/// that contains elements copied from the specified dictionary and
+		/// that has the same initial capacity as the number of elements copied, the
+		/// default load factor, the default hash code provider and the default comparer.
+		/// </summary>
+		/// <param name="dictionary">The <see cref="Hashmap"/>
+		/// whose elements are copied to the new collection.</param>
+		/// <exception cref="ArgumentNullException">
+		/// <paramref name="dictionary"/> is a null reference.</exception>
+		/// <remarks>Please refer to <see cref="Hashmap(IDictionary)"/> for details.</remarks>
+
+		public Hashmap(Hashmap dictionary) : base(dictionary) {}
+
+		#endregion
+		#region Hashmap(IDictionary)
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Hashmap"/> class
+		/// that contains elements copied from the specified <see cref="IDictionary"/>
+		/// and that has the same initial capacity as the number of elements copied, the
+		/// default load factor, the default hash code provider and the default comparer.
+		/// </summary>
+		/// <param name="dictionary">The <see cref="IDictionary"/>
+		/// whose elements are copied to the new collection.</param>
+		/// <exception cref="ArgumentNullException">
+		/// <paramref name="dictionary"/> is a null reference.</exception>
+		/// <remarks>Please refer to <see cref="Hashmap(IDictionary)"/> for details.</remarks>
+
+		public Hashmap(IDictionary dictionary) : base (dictionary){}
+
+		#endregion
+		#region Hashmap(IDictionary, IHashCodeProvider, IComparer)
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Hashmap"/> class
+		/// that contains elements copied from the specified <see cref="IDictionary"/>
+		/// and that has the same initial capacity as the number of elements copied, the
+		/// default load factor, the specified hash code provider and the specified comparer.
+		/// </summary>
+		/// <param name="dictionary">The <see cref="IDictionary"/>
+		/// whose elements are copied to the new collection.</param>
+		/// <param name="provider">
+		/// <para>The <see cref="IHashCodeProvider"/> that supplies the hash codes
+		/// for all keys in the <see cref="Hashmap"/>.</para>
+		/// <para>-or-</para>
+		/// <para>A null reference to use the default hash code provider, which is each key's
+		/// implementation of <see cref="Object.GetHashCode"/>.</para></param>
+		/// <param name="comparer">
+		/// <para>The <see cref="IComparer"/> to use to determine whether two keys are equal.</para>
+		/// <para>-or-</para>
+		/// <para>A null reference to use the default comparer, which is each key's
+		/// implementation of <see cref="Object.Equals"/>.</para></param>
+		/// <exception cref="ArgumentNullException">
+		/// <paramref name="dictionary"/> is a null reference.</exception>
+		/// <remarks>
+		/// Please refer to <see cref="Hashmap(IDictionary, IHashCodeProvider, IComparer)"/>
+		/// for details.</remarks>
+
+		public Hashmap(IDictionary dictionary,
+			IHashCodeProvider provider, IComparer comparer) : base (dictionary,provider,comparer){}
+
+		#endregion
+		#region Hashmap(IDictionary, Single)
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Hashmap"/> class
+		/// that contains elements copied from the specified <see cref="IDictionary"/>
+		/// and that has the same initial capacity as the number of elements copied, the
+		/// specified load factor, the default hash code provider and the default comparer.
+		/// </summary>
+		/// <param name="dictionary">The <see cref="IDictionary"/>
+		/// whose elements are copied to the new collection.</param>
+		/// <param name="loadFactor">A number in the range from 0.1 through 1.0
+		/// indicating the maximum ratio of elements to buckets.</param>
+		/// <exception cref="ArgumentNullException">
+		/// <paramref name="dictionary"/> is a null reference.</exception>
+		/// <exception cref="ArgumentOutOfRangeException">
+		/// <para><paramref name="loadFactor"/> is less than 0.1.</para>
+		/// <para>-or-</para>
+		/// <para><paramref name="loadFactor"/> is greater than 1.0.</para></exception>
+		/// <remarks>
+		/// Please refer to <see cref="Hashmap(IDictionary, Single)"/> for details.
+		/// </remarks>
+		public Hashmap(IDictionary dictionary, float loadFactor) : base (dictionary,loadFactor){}
+
+		#endregion
+		#region Hashmap(IDictionary, Single, IHashCodeProvider, IComparer)
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Hashmap"/> class
+		/// that contains elements copied from the specified <see cref="IDictionary"/>
+		/// and that has the same initial capacity as the number of elements copied, the
+		/// specified load factor, the specified hash code provider and the specified comparer.
+		/// </summary>
+		/// <param name="dictionary">The <see cref="IDictionary"/>
+		/// whose elements are copied to the new collection.</param>
+		/// <param name="loadFactor">A number in the range from 0.1 through 1.0
+		/// indicating the maximum ratio of elements to buckets.</param>
+		/// <param name="provider">
+		/// <para>The <see cref="IHashCodeProvider"/> that supplies the hash codes
+		/// for all keys in the <see cref="Hashmap"/>.</para>
+		/// <para>-or-</para>
+		/// <para>A null reference to use the default hash code provider, which is each key's
+		/// implementation of <see cref="Object.GetHashCode"/>.</para></param>
+		/// <param name="comparer">
+		/// <para>The <see cref="IComparer"/> to use to determine whether two keys are equal.</para>
+		/// <para>-or-</para>
+		/// <para>A null reference to use the default comparer, which is each key's
+		/// implementation of <see cref="Object.Equals"/>.</para></param>
+		/// <exception cref="ArgumentNullException">
+		/// <paramref name="dictionary"/> is a null reference.</exception>
+		/// <exception cref="ArgumentOutOfRangeException">
+		/// <para><paramref name="loadFactor"/> is less than 0.1.</para>
+		/// <para>-or-</para>
+		/// <para><paramref name="loadFactor"/> is greater than 1.0.</para></exception>
+		/// <remarks>
+		/// Please refer to <see cref="Hashmap(IDictionary, Single, IHashCodeProvider, IComparer)"/>
+		/// for details.</remarks>
+		public Hashmap(IDictionary dictionary,
+			float loadFactor, IHashCodeProvider provider, IComparer comparer) : base(dictionary,loadFactor,provider,comparer){}
+
+		#endregion
+	}
 }
