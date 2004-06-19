b2d05a4e55aece0ded757a355724fcd8b83c5354
joemal <dummy>
2004-06-19 13:49:44 +0000

    tests
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@1649 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- trunk/src/ComponentNetworkSimulation/src/testapplication/TestApp.cs
+++ trunk/src/ComponentNetworkSimulation/src/testapplication/TestApp.cs
@@ -1,35 +1,81 @@
 using System;
 using Palladio.FiniteStateMachines;
 using Palladio.Attributes;
-using ComponentNetworkSimulation.Structure.Services;
+using ComponentNetworkSimulation.Simulation;
+using ComponentNetworkSimulation.Structure;
+using ComponentNetworkSimulation.Structure.Visitor;
+using Palladio.ComponentModel;
+using Palladio.Identifier;
 
 namespace testapplication
 {
 
-	public class TestApp
+	public class TestApp : IThreadObserver
 	{
 		public static Random random = new Random();
 
 		public static void Main()
 		{
-			/*		try 
-					{
-						ISimulationEnvironment env = new SimpleTestSimulationEnvironment();
-						env.Simulate();
-					}
-					catch(Exception exc)
-					{
-						Console.Out.WriteLine(""+exc.GetType()+exc.StackTrace);
-				
-					}
-		*/			
+			for (int a=0;a<5;a++)
+			{
+				Console.Out.WriteLine("Neuer lauf: ");
+				IComponentArchitecture architecture = new SimpleComponentArchitecture();
 
-			IState state = FSMFactory.CreateDefaultState("State");
-			IAttributeType type = new AttributesFactory().Default.CreateAttributeType(Guid.NewGuid(),"Controlflowstrategie",typeof(IControlFlowStrategy));
-			state.Attributes.Add(type, new DefaultServiceBuilder().GetControlFlowStrategy(DefaultServiceBuilder.STRATEGY_RANDOM));
-            Console.WriteLine("Attr: "+state.Attributes[type]);
-			Console.ReadLine();
-			
+				IThreadStartingPoint sp = new DefaultThreadStartingPoint(ID("CompCom"),ID("P1"),ID("d1")); 
+				IComponentVisitor visitor = architecture.CreateVisitor(sp);
+				DefaultSimulationThread thread = new DefaultSimulationThread(0,visitor,SimulationThreadType.TYPE_LOG_NOTHING,new TestApp());
+
+				int round = 0;
+
+				while(thread.IsAlive && round <50)
+				{
+					round ++;
+					long timeInFuture = thread.TimeInFuture;
+					Console.WriteLine("CurrentTC: "+thread.CurrentTimeConsumer);
+					Console.WriteLine("Time in future: "+thread.TimeInFuture);
+					thread.TimeMoved(timeInFuture);
+					Console.ReadLine();
+				}
+
+			}
+			Console.ReadLine();			
 		}
+
+		public static IIdentifier ID(string id)
+		{
+			return IdentifiableFactory.CreateStringID(id);
+		}
+
+		#region IThreadObserver Member
+
+		public void NotifyThreadReachedEnd(ISimulationThread sender)
+		{
+			Console.WriteLine("Thread reached end ...");
+		}
+
+		public void NotifyThreadChangedTimeConsumer(ISimulationThread sender, ITimeConsumer previous)
+		{
+			Console.WriteLine("Thread changed TC from "+previous+" to "+sender.CurrentTimeConsumer);
+		}
+
+		public void NotifyTimeStep(ISimulationThread sender, long timeStep)
+		{
+			Console.WriteLine("Thread makes a timestep: "+timeStep+" ms");
+		}
+
+		public void NotifyUnknownElementFound()
+		{
+			Console.WriteLine("Thread found and unknown element !");
+		}
+
+		public void NotifyUnboundExternalCall(IComponent c, IExternalSignature externalSignature)
+		{
+			Console.WriteLine("Thread found the unbound external call: ");
+			Console.WriteLine("Component: "+c.ID);
+			Console.WriteLine("Interface: "+externalSignature.RoleID);
+			Console.WriteLine("Signature: "+externalSignature.Signature.ID);
+		}
+
+		#endregion
 	}
 }
