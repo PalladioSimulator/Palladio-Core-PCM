0f520fb32a36a31d18969d6be90427b280806e9e
hkoziolek <heiko.koziolek@gmail.com>
2004-08-07 10:23:38 +0000

    Neue Scheduler-Komponente zur Verteilung der Anfragen auf mehrere Server
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@1893 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ trunk/Scheduler/SchedulerThread.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Scheduler
+{
+	/// <summary>
+	/// Summary description for SchedulerThread.
+	/// </summary>
+	public class SchedulerThread
+	{
+		private TcpClient tcpClient;
+		private IPEndPoint serverEndPoint;
+		
+		public SchedulerThread(TcpClient aClient, IPEndPoint aEndPoint)
+		{
+			this.tcpClient = aClient;
+			this.serverEndPoint = aEndPoint;
+			
+			Thread clientThread = new Thread(new ThreadStart(ForwardRequest));
+			clientThread.Start();
+		}
+
+		protected void ForwardRequest()
+		{
+			// open server connection
+			TcpClient schedulerToServer = new TcpClient();
+			schedulerToServer.Connect(this.serverEndPoint);
+			NetworkStream schedulerToServerStream = schedulerToServer.GetStream();
+			
+			// transmit client request to web server
+			NetworkStream clientStream = this.tcpClient.GetStream();			
+			int i=0;
+			Byte[] bytes = new Byte[256];
+			while(clientStream.DataAvailable) 
+			{   
+				i=clientStream.Read(bytes,0,bytes.Length);	// receive from client
+				schedulerToServerStream.Write(bytes, 0, i); // send to server
+			}
+			
+			// wait for answer
+			while (!schedulerToServerStream.DataAvailable); 
+			
+			// transmit web server response to client
+			i=0;
+			Byte[] receiveBuffer = new Byte[256];
+			while (schedulerToServerStream.DataAvailable)
+			{
+				i=schedulerToServerStream.Read(receiveBuffer,0,receiveBuffer.Length); // receive from server
+				clientStream.Write(receiveBuffer, 0, i);	// send to client
+			}
+
+			clientStream.Close();
+			schedulerToServer.Close();
+			
+		}
+	}
+}
