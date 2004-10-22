54c9a8dda692717af275ff14cef90997fba7f199
kelsaka <krogmann@fzi.de>
2004-10-22 09:37:19 +0000

    minor changes
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@1771 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ trunk/Webserver/src/DefaultWebserver.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace Palladio.Webserver
+{
+	/// <summary>
+	///DefaultWebserver.
+	/// </summary>
+	/// 
+	/// <remarks>
+	/// <pre>
+	/// Version history:
+	///
+	/// $Log$
+	/// Revision 1.1  2004/10/22 09:37:19  kelsaka
+	/// minor changes
+	///
+	///
+	/// </pre>
+	/// </remarks>
+	public class DefaultWebserver
+	{
+		public DefaultWebserver()
+		{
+
+
+		}
+
+
+		/// <summary>
+		/// Starts a running webserver.
+		/// </summary>
+		public void Run()
+		{
+			
+		}
+
+	}
+}
