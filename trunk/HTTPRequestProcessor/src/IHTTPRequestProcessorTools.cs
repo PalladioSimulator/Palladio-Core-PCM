94fa4650cd3b851d771c040225927b91f19edddf
kelsaka <krogmann@fzi.de>
2004-10-29 16:41:51 +0000

    tools to process a http request
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@1787 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ trunk/HTTPRequestProcessor/src/IHTTPRequestProcessorTools.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Palladio.Webserver.HTTPRequestProcessor
+{
+	/// <summary>
+	/// IHTTPRequestProcessorTools. Standard utilities used to process a IHTTPRequest.
+	/// </summary>
+	public interface IHTTPRequestProcessorTools
+	{
+
+
+	}
+}
