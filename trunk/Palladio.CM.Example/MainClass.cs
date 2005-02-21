03f53337ab2f2332c84effc7f8e4793197a0c69a
joemal <dummy>
2005-02-21 14:04:15 +0000

    initial import
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@519 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ trunk/Palladio.CM.Example/MainClass.cs
@@ -0,0 +1,32 @@
+using System;
+using Palladio.ComponentModel;
+using Palladio.Identifier;
+
+namespace Palladio.CM.Example
+{
+	/// <summary>
+	/// Zusammenfassung für Class1.
+	/// </summary>
+	/// <remarks>
+	/// <pre>
+	/// Version history:
+	///
+	/// $Log$
+	/// Revision 1.1  2005/02/21 14:04:15  joemal
+	/// initial import
+	///
+	///
+	///
+	/// </pre>
+	/// </remarks>
+	class MainClass
+	{
+		/// <summary>
+		/// Der Haupteinstiegspunkt für die Anwendung.
+		/// </summary>
+		[STAThread]
+		static void Main(string[] args)
+		{
+		}
+	}
+}
