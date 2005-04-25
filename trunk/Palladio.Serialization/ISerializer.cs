fe0b62e95aae363df01ef13c9bf48afec11266a0
joemal <dummy>
2005-04-25 16:34:34 +0000

    initial creation
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@629 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ trunk/Palladio.Serialization/ISerializer.cs
@@ -0,0 +1,62 @@
+using System;
+
+namespace Palladio.Serialization
+{
+	/// <summary>
+	/// This interface defines methods that must be implemented by a serializer. Serializers are registered in 
+	/// the SerializationManager to support several methods to load or store a model. 
+	/// </summary>
+	/// <remarks>
+	/// <pre>
+	/// Version history:
+	///
+	/// $Log$
+	/// Revision 1.1  2005/04/25 16:34:34  joemal
+	/// initial creation
+	///
+	///
+	/// </pre>
+	/// </remarks>
+	public interface ISerializer
+	{
+		/// <summary>
+		/// called by the serialization manager when the serializer has been registered. This method can be used to 
+		/// inilize the serializer.
+		/// </summary>
+		/// <exception cref="ModelSerializationException">thrown if an error occurs while inilizing the serializer.</exception>
+		void Registered();
+
+		/// <summary>
+		/// called to load a model from the given location. The location is from one of the supported location types, returned
+		/// by the properties. The serializer can throw the two specified exceptions.
+		/// </summary>
+		/// <param name="location">the location</param>
+		/// <exception cref="OpenLocationException">thrown if the location can't be opened.</exception>
+		/// <exception cref="ModelSerializationException">thrown if an error occurs while reading the model.</exception>
+		void Load(ISerializationLocation location);
+
+		/// <summary>
+		/// called to store a model to the given location. The location is from one of the supported location types, returned
+		/// by the properties. The serializer can throw the two specified exceptions.
+		/// </summary>
+		/// <param name="location">the location</param>
+ 		/// <exception cref="OpenLocationException">thrown if the location can't be opened.</exception>
+		/// <exception cref="ModelSerializationException">thrown if an error occurs while writing the model.</exception>
+		void Store(ISerializationLocation location);
+
+		/// <summary>
+		/// called by the serialization manager after the serializer has been unregistered. This method can be used to 
+		/// tidy up and free resources the serializer has used.
+		/// </summary>
+		void UnRegistered();
+
+		/// <summary>
+		/// the property has to return an array of supported location types. The load and store methods must support the
+		/// serialization and deserialization from this localtion types.
+		/// </summary>
+		Type[] SupportedLocationTypes
+		{
+			get;
+		}
+	}
+}
---
+++ trunk/Palladio.Serialization/ModelSerializationException.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Palladio.Serialization
+{
+	/// <summary>
+	/// This exception is thrown by the serialization manager, when errors occur while store or load the model. 
+	/// </summary>
+	/// <remarks>
+	/// <pre>
+	/// Version history:
+	///
+	/// $Log$
+	/// Revision 1.1  2005/04/25 16:34:34  joemal
+	/// initial creation
+	///
+	///
+	///
+	/// </pre>
+	/// </remarks>
+	public class ModelSerializationException : Exception
+	{
+		/// <summary>
+		/// Create a new ModelSerializationException.
+		/// </summary>
+		/// <param name="message">Message delivered by the exception.</param>
+		/// <param name="cause">the exception that is the cause of this one</param>
+		public ModelSerializationException(string message, Exception cause) : base(message,cause) {}
+	}
+}
