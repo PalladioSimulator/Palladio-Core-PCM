e89a5c56eee74d17e467bf8c803f2275b573918c
kelsaka <krogmann@fzi.de>
2005-08-15 06:55:34 +0000

    - initial creation
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@724 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ trunk/Palladio.FiniteStateMachines.Serializer/src/AttributeNotSupportedException.cs
@@ -0,0 +1,47 @@
+using System;
+using Palladio.Attributes;
+
+namespace Palladio.FiniteStateMachines.Serializer
+{
+	/// <summary>
+	/// Exception that is rised if a <see cref="IAttribute" /> is not supported.
+	/// </summary>
+	/// <remarks>
+	/// <code>
+	/// Version history:
+	///
+	/// $Log$
+	/// Revision 1.1  2005/08/15 06:55:34  kelsaka
+	/// - initial creation
+	///
+	/// </code>
+	/// </remarks>
+	public class AttributeNotSupportedException : ApplicationException
+	{
+		private string errorMessage;
+
+		/// <summary>
+		/// Constructor supporting error message.
+		/// </summary>
+		/// <param name="errorMessage">The message of the error.</param>
+		public AttributeNotSupportedException(string errorMessage)
+		{
+			this.errorMessage = errorMessage;
+		}
+
+		public override string Message
+		{
+			get
+			{
+				return base.Message + " " + errorMessage;
+			}
+		}
+
+		public override string ToString()
+		{
+			return base.ToString () + " " + errorMessage;;
+		}
+
+
+	}
+}
