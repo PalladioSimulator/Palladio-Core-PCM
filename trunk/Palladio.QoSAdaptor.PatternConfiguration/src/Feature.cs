692c68d8373d876d1c9f8603c98be5b3f94a9c12
streekm <dummy>
2005-10-05 09:13:17 +0000

    Further implmentation of the feature diagram parser and initial check-in of the Configuration form.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@1364 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- trunk/Palladio.QoSAdaptor.PatternConfiguration/src/Feature.cs
+++ trunk/Palladio.QoSAdaptor.PatternConfiguration/src/Feature.cs
@@ -1,4 +1,24 @@
-using System;
+#region info
+///////////////////////////////////////////////////////////////////////////////
+// This software has been developed as a part of the diploma thesis 
+// "Einfluss von Generatorkonfiguration auf die QoS-Vorhersage für 
+// Komponentenadapter" ("Influence of the configuration of a generator on the 
+// prediction of the QoS of component adaptors")
+// at the 
+// University of Oldenburg
+// Department of Computing Science
+// Software Engineering Group
+// Palladio Research Group
+// (http://se.informatik.uni-oldenburg.de/research/projects/Palladio)
+// 
+// Development period: July 2005 - January 2006
+// 
+// Author: Niels Streekmann
+// E-mail: niels.streekmann@informatik.uni-oldenburg.de
+///////////////////////////////////////////////////////////////////////////////
+#endregion
+
+using System.Collections;
 
 namespace Palladio.QoSAdaptor.PatternConfiguration
 {
@@ -7,11 +27,236 @@ namespace Palladio.QoSAdaptor.PatternConfiguration
 	/// </summary>
 	public class Feature
 	{
-		public Feature()
+		#region data
+		/// <summary>
+		/// Indicates that this feature is the root node of the feature tree.
+		/// </summary>
+		private bool root;
+
+		/// <summary>
+		/// The name of this feature
+		/// </summary>
+		private string name;
+
+		/// <summary>
+		/// Detailed description of this feature.
+		/// </summary>
+		private string description;
+
+		/// <summary>
+		/// The guid of this feature in the feature diagram. 
+		/// </summary>
+		private string guid;
+
+		/// <summary>
+		/// The type of this feature if is represents a variable. If it does 
+		/// not, the type is null;
+		/// </summary>
+		private string type;
+
+		// TODO: Find another word for "instances"
+		/// <summary>
+		/// The minimum number of instances of this feature
+		/// </summary>
+		private int minCardinality;
+
+		/// <summary>
+		/// The maximum number of instances of this feature
+		/// </summary>
+		private int maxCardinality;
+
+		/// <summary>
+		/// The parent feature of this feature in the feature tree. Null, if 
+		/// this is the root feature.
+		/// </summary>
+		private Feature parent;
+
+		/// <summary>
+		/// A list of child features of this feature in the feature tree. 
+		/// Empty, if this is a leaf feature.
+		/// </summary>
+		private ArrayList children;
+		#endregion
+
+		#region properties
+		/// <summary>
+		/// Returns the name of this feature
+		/// </summary>
+		public string Name
 		{
-			//
-			// TODO: Fügen Sie hier die Konstruktorlogik hinzu
-			//
+			get
+			{
+				return this.name;
+			}
 		}
+
+		/// <summary>
+		/// Returns the description of this feature
+		/// </summary>
+		public string Description
+		{
+			get
+			{
+				return this.description;
+			}
+		}
+
+		/// <summary>
+		/// Returns the guid of this feature
+		/// </summary>
+		public string Guid
+		{
+			get
+			{
+				return this.guid;
+			}
+		}
+
+		/// <summary>
+		/// Indicates if this feature is the root node of the feature tree
+		/// </summary>
+		public bool IsRoot
+		{
+			get
+			{
+				return this.root;
+			}
+			set
+			{
+				this.root = value;
+			}
+		}
+
+		/// <summary>
+		/// Indicates if this feature is a leaf in the feature tree.
+		/// </summary>
+		public bool isLeaf
+		{
+			get
+			{
+				if (this.children.Count == 0)
+					return true;
+				else 
+					return false;
+			}
+		}
+
+		/// <summary>
+		/// The type of the feature if it represents a variable. If it does not
+		/// the type is null.
+		/// </summary>
+		public string Type
+		{
+			get
+			{
+				return this.type;
+			}
+			set
+			{
+				this.type = value;
+			}
+		}
+
+		/// <summary>
+		/// The minimum number of instances of the feature in the feature tree.
+		/// </summary>
+		public int MinCardinality
+		{
+			get
+			{
+				return this.minCardinality;
+			}
+			set
+			{
+				this.minCardinality = value;
+			}
+		}
+
+		/// <summary>
+		/// The maximum number of instances of the feature in the feature tree.
+		/// </summary>
+		public int MaxCardinality
+		{
+			get
+			{
+				return this.maxCardinality;
+			}
+			set
+			{
+				this.maxCardinality = value;
+			}
+		}
+
+		/// <summary>
+		/// The parent feature of this feature in the feature tree.
+		/// Null, if this is the root feature.
+		/// </summary>
+		public Feature Parent
+		{
+			get
+			{
+				return this.parent;
+			}
+			set
+			{
+				this.parent = value;
+			}
+		}
+
+		/// <summary>
+		/// Returns a list of children of this feature in the feature node.
+		/// Changes to this list will not change the inner state of this 
+		/// feature.
+		/// </summary>
+		public IList Children
+		{
+			get
+			{
+				return (IList)this.children.Clone();
+			}
+		}
+		#endregion
+
+		#region constructor
+		public Feature(string name, string description, string guid)
+		{
+			this.name = name;
+			this.description = description;
+			this.guid = guid;
+			this.type = null;
+			this.root = false;
+			// TODO: Change default values to 0 or -1?
+			this.minCardinality = 1;
+			this.maxCardinality = 1;
+			this.parent = null;
+			this.children = new ArrayList();
+		}
+		#endregion
+
+		#region public methods
+		/// <summary>
+		/// Adds a child to the list of children of this feature.
+		/// </summary>
+		/// <param name="child">A feature in the same feature tree which is a 
+		/// child of this feature.</param>
+		public void AddChild(Feature child)
+		{
+			this.children.Add(child);
+		}
+
+		/// <summary>
+		/// Returns a string representation of this feature.
+		/// </summary>
+		/// <returns></returns>
+		public override string ToString()
+		{
+			string s =  "FEATURE "+this.name+". ";
+			if (this.root) s+= "Is root. ";
+			if (this.type != null) s+= "Type = "+this.type+". ";
+			s+= "guid = "+this.guid+". Description: "+this.description;
+			return s;
+		}
+
+		#endregion
 	}
 }
--- trunk/Palladio.QoSAdaptor.PatternConfiguration/src/FeatureRepositoryParser.cs
+++ trunk/Palladio.QoSAdaptor.PatternConfiguration/src/FeatureRepositoryParser.cs
@@ -1,26 +1,28 @@
 #region info
 ///////////////////////////////////////////////////////////////////////////////
-/// This software has been developed as a part of the diploma thesis 
-/// "Einfluss von Generatorkonfiguration auf die QoS-Vorhersage für 
-/// Komponentenadapter" ("Influence of the configuration of a generator on the 
-/// prediction of the QoS of component adaptors")
-/// at the 
-/// University of Oldenburg
-/// Department of Computing Science
-/// Software Engineering Group
-/// Palladio Research Group
-/// (http://se.informatik.uni-oldenburg.de/research/projects/Palladio)
-/// 
-/// Development period: July 2005 - January 2006
-/// 
-/// Author: Niels Streekmann
-/// E-mail: niels.streekmann@informatik.uni-oldenburg.de
+// This software has been developed as a part of the diploma thesis 
+// "Einfluss von Generatorkonfiguration auf die QoS-Vorhersage für 
+// Komponentenadapter" ("Influence of the configuration of a generator on the 
+// prediction of the QoS of component adaptors")
+// at the 
+// University of Oldenburg
+// Department of Computing Science
+// Software Engineering Group
+// Palladio Research Group
+// (http://se.informatik.uni-oldenburg.de/research/projects/Palladio)
+// 
+// Development period: July 2005 - January 2006
+// 
+// Author: Niels Streekmann
+// E-mail: niels.streekmann@informatik.uni-oldenburg.de
 ///////////////////////////////////////////////////////////////////////////////
 #endregion
+
 using System;
 using System.IO;
 using System.Xml;
 using System.Xml.XPath;
+using Palladio.Utils.Collections;
 
 namespace Palladio.QoSAdaptor.PatternConfiguration
 {
@@ -31,20 +33,35 @@ namespace Palladio.QoSAdaptor.PatternConfiguration
 	/// </summary>
 	class FeatureRepositoryParser
 	{
+		#region data
 		/// <summary>
-		/// Standard constructor
+		/// Holds the root node of the feature diagramm, which also is the 
+		/// return value of the parse method.
 		/// </summary>
+		private Feature rootNode;
+
+		/// <summary>
+		/// Holds all features in the diagram. The hash key is the name of the
+		/// feature / node.
+		/// </summary>
+		private Hashmap features;
+		#endregion
+
+		#region constructor
 		public FeatureRepositoryParser()
 		{
-			
+			this.features = new Hashmap();
 		}
+		#endregion
 
+		#region public methods
 		/// <summary>
 		/// Parses the file given by its filename.
 		/// </summary>
 		/// <param name="file">Name of a file containg a CaptainFeature
 		/// repository.</param>
-		public void Parse(string file)
+		/// <returns>The root feature of the feature diagram.</returns>
+		public Feature Parse(string file)
 		{
 			XmlDocument doc;
 			XPathNavigator nav;
@@ -56,17 +73,13 @@ namespace Palladio.QoSAdaptor.PatternConfiguration
 				XPathNodeIterator it;
 				it = nav.Select("/CFRepository/ModelSpace/ModelLevel/FeatureSpace");
 				it.MoveNext();
-				if (it.Current == null) Console.WriteLine("it.Current == null");
-				else Console.WriteLine("TEST: "+it.Current.Name);
-				XPathNodeIterator it2;
-				it2=it.Current.SelectChildren("Feature",it.Current.NamespaceURI);
-				it2.MoveNext();
-				if (it2.Current == null) Console.WriteLine("it2.Current == null");
-				else Console.WriteLine("TEST2: "+it2.Current.Name);
-				it2.Current.MoveToFirstAttribute();
-				Console.WriteLine("HALLO "+it2.Current.Value);
-
 				
+				// Get FeatureSpace attributes
+				XPathNavigator pathNavigator = it.Clone().Current;
+				pathNavigator.MoveToFirstAttribute();
+				Console.WriteLine("FeatureSpace name = "+pathNavigator.Value);
+				//if (pathNavigator.Value.Equals(TODO: PatternName))
+					ParseFeatureSpace(it);
 			}
 			catch (XmlException e) 
 			{
@@ -77,6 +90,120 @@ namespace Palladio.QoSAdaptor.PatternConfiguration
 				Console.WriteLine("File Was not Found!");
 				Console.WriteLine(e);
 			}
+
+			// TODO: return the root node of the feature diagram. The name of 
+			// this node has to correspond to the name of the design pattern in
+			// the PatternDesciption.
+			return null;
 		}
+		#endregion
+
+		#region private methods
+		/// <summary>
+		/// Parses the feature space and creates Feature objects including 
+		/// their connections for all features described in it. 
+		/// </summary>
+		/// <param name="it">A XPathNodeIterator pointing to the FeatureSpace
+		/// node.</param>
+		private void ParseFeatureSpace (XPathNodeIterator it)
+		{
+			XPathNodeIterator it2 = it.Current.SelectChildren
+										("Feature",it.Current.NamespaceURI);
+			while (it2.MoveNext())
+			{
+				ParseFeature(it2);
+			}
+
+			it2 = it.Current.SelectChildren("Diagram",it.Current.NamespaceURI);
+			while (it2.MoveNext())
+			{
+				ParseDiagram(it2);
+			}
+		}
+
+		/// <summary>
+		/// Parses a feature node and creates a Feature object containing the
+		/// same information.
+		/// </summary>
+		/// <param name="it">XPathNodeIterator representing a "feature node"
+		/// of the FeatureSpace.</param>
+		private void ParseFeature(XPathNodeIterator it)
+		{
+			// it2 has to be cloned, because else the other children can
+			// not be handled after reading the attributes.
+			XPathNavigator it3 = it.Clone().Current;
+
+			// Parse attributes
+			it3.MoveToFirstAttribute();
+			Console.WriteLine("Attribute "+it3.Value);
+			string name = it3.Value;
+			it3.MoveToNextAttribute();
+			string description = it3.Value;
+			it3.MoveToNextAttribute(); // premature is not saved in the feature
+			it3.MoveToNextAttribute();
+			string guid = it3.Value;
+			// the constraint id is also not saved in the feature.
+			Feature feature = new Feature(name, description, guid);
+
+			// Get type
+			XPathNodeIterator it4 = it.Current.SelectChildren(
+				"SpecializationParameter", it.Current.NamespaceURI);
+			if (it4.MoveNext())
+			{
+				it4.Current.MoveToFirstAttribute();
+				feature.Type = it4.Current.Value;
+			}
+
+			// DEBUG OUTPUT
+			Console.WriteLine(feature.ToString()+"\n");
+
+			this.features.Add(name, feature);
+		}
+
+		/// <summary>
+		/// Parses the feature diagramm and builds the internal feature tree.
+		/// </summary>
+		/// <param name="it">XPathNodeIterator representing the "diagram node"
+		/// of the FeatureSpace.</param>
+		private void ParseDiagram(XPathNodeIterator it)
+		{
+			// Get the root node
+			XPathNodeIterator it2 = it.Current.SelectChildren(
+				"FeatureNode", it.Current.NamespaceURI);
+			it2.MoveNext();
+			XPathNavigator root = it2.Clone().Current;
+			root.MoveToFirstAttribute(); // guid is not needed
+			root.MoveToNextAttribute(); // name of the root feature
+			this.rootNode = (Feature)this.features[root.Value];
+			Console.WriteLine("ROOT = "+root.Value);
+
+			// Recursively parse all cardinalities
+			XPathNodeIterator it3 = it2.Current.SelectChildren("Cardinality",
+				it2.Current.NamespaceURI);
+			while (it3.MoveNext())
+			{
+				this.rootNode.AddChild(ParseCardinality(it3.Current,rootNode));
+			}
+		}
+
+		/// <summary>
+		/// Parses a cardinality node of a feature diagram description. Adds 
+		/// all necessary information to the Feature object represented in the
+		/// current cardinality node and calls itself recursively for the 
+		/// cardinality nodes that are children of the corresponding
+		/// FeatureNode.
+		/// </summary>
+		/// <param name="it">XPathNavigator representing the current 
+		/// Cardinality node.</param>
+		/// <param name="parent">Feature representing the parent node of the 
+		/// current Cardinality node.</param>
+		/// <returns>Feature representing the current Cardinality node.
+		/// </returns>
+		private Feature ParseCardinality (XPathNavigator it, Feature parent)
+		{
+			Feature thisNode = null;
+			return thisNode;
+		}
+		#endregion
 	}
 }
