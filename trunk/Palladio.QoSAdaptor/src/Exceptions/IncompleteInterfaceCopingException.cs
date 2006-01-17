7d9105e2b79364c4032bbaa9ee2adc4f057d4867
streekm <dummy>
2006-01-17 08:44:14 +0000

    Added comment for constructor.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@1506 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- trunk/Palladio.QoSAdaptor/src/Exceptions/IncompleteInterfaceCopingException.cs
+++ trunk/Palladio.QoSAdaptor/src/Exceptions/IncompleteInterfaceCopingException.cs
@@ -7,6 +7,11 @@ namespace Palladio.QoSAdaptor.Exceptions
 	/// </summary>
 	public class IncompleteInterfaceCopingException :Exception
 	{
+		/// <summary>
+		/// Constructor which calls the parent constructor with the given
+		/// message.
+		/// </summary>
+		/// <param name="message">The exception message.</param>
 		public IncompleteInterfaceCopingException(string message):
 			base(message)
 		{
