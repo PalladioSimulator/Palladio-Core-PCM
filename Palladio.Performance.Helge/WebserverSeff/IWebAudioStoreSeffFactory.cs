77dc1efd4fdb800bf71209ac886b034377f20ccd
hkoziolek <heiko.koziolek@gmail.com>
2006-03-09 16:17:30 +0000

    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@1950 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.Performance.Helge/WebserverSeff/IWebAudioStoreSeffFactory.cs
+++ Palladio.Performance.Helge/WebserverSeff/IWebAudioStoreSeffFactory.cs
@@ -10,9 +10,16 @@ namespace Palladio.Performance.WebserverSeff
 	{
 		IFiniteStateMachine CreateSeff_AudioStore_GetUploadedFileView();
 		IFiniteStateMachine CreateSeff_AudioStore_HandleUpload();
-		IFiniteStateMachine CreateSeff_EncodingAdapter_InsertAudioFile();
-		IFiniteStateMachine CreateSeff_EncodingAdapter_InsertAudioInfo();
+		IFiniteStateMachine CreateSeff_AudioStore_FinalizeUpload();
 		IFiniteStateMachine CreateSeff_OggEncoder_EncodeFile();
 		IFiniteStateMachine CreateSeff_WebGUI_Upload_ServerClick();
+		IFiniteStateMachine CreateSeff_EncodingAdapter_InsertAudioFile();
+		IFiniteStateMachine CreateSeff_EncodingAdapter_InsertAudioInfo();
+		IFiniteStateMachine CreateSeff_EncodingAdapter_FinalizeUpload();
+		IFiniteStateMachine CreateSeff_DBAdapter_InsertAudioFile();
+		IFiniteStateMachine CreateSeff_DBAdapter_InsertAudioInfo();
+		IFiniteStateMachine CreateSeff_DBAdapter_GetAudioFileInfo();
+		IFiniteStateMachine CreateSeff_BufferingDBAdapter_GetAudioFileInfo();
+		IFiniteStateMachine CreateSeff_BufferingDBAdapter_FinalizeUpload();
 	}
 }
--- Palladio.Performance.Helge/WebserverSeff/src/WebAudioStoreSeffFactory.cs
+++ Palladio.Performance.Helge/WebserverSeff/src/WebAudioStoreSeffFactory.cs
@@ -1,6 +1,4 @@
-using System;
 using Palladio.FiniteStateMachines;
-using Palladio.Performance.WebserverSeff;
 
 namespace Palladio.Performance.WebserverSeff
 {
@@ -13,6 +11,9 @@ namespace Palladio.Performance.WebserverSeff
 		{
 		}
 
+		//**********
+		//AudioStore
+		//**********
 		public IFiniteStateMachine CreateSeff_AudioStore_GetUploadedFileView()
 		{
 			IEditableFiniteStateMachine fsm = FSMFactory.CreateEmptyFSM();
@@ -39,6 +40,62 @@ namespace Palladio.Performance.WebserverSeff
 			return fsm;
 		}
 
+
+		public IFiniteStateMachine CreateSeff_AudioStore_FinalizeUpload()
+		{
+			IEditableFiniteStateMachine fsm = FSMFactory.CreateEmptyFSM();
+			IState[] states = WebserverModelHelper.CreateStates(701);
+			fsm.AddStates(states);
+			fsm.StartState = WebserverModelHelper.GetState(701);
+			fsm.FinalStates = new IState[] {WebserverModelHelper.GetState(701)};
+			
+			WebserverModelHelper.AddTransition(702, ref fsm, 701, 701, "IAudioDB_FinalizeUpload");			
+			return fsm;
+		}
+
+
+
+		//**********
+		//OggEncoder
+		//**********
+		public IFiniteStateMachine CreateSeff_OggEncoder_EncodeFile()
+		{
+			IEditableFiniteStateMachine fsm = FSMFactory.CreateEmptyFSM();
+			IState[] states = WebserverModelHelper.CreateStates(501, 502, 503, 504, 505);
+			fsm.AddStates(states);
+			fsm.StartState = WebserverModelHelper.GetState(501);
+			fsm.FinalStates = new IState[] {WebserverModelHelper.GetState(505)};
+			
+			WebserverModelHelper.AddTransition(505, ref fsm, 501, 502, "IEncoder_WriteMp3ToDisk");			
+			WebserverModelHelper.AddTransition(506, ref fsm, 502, 503, "IEncoder_Mp32Wav");			
+			WebserverModelHelper.AddTransition(507, ref fsm, 503, 504, "IEncoder_ExecuteEncoder");			
+			WebserverModelHelper.AddTransition(508, ref fsm, 504, 505, "IEncoder_ReadEncodedFileFromDisk");			
+			return fsm;
+		}
+
+
+
+		//******
+		//WebGUI
+		//******
+		public IFiniteStateMachine CreateSeff_WebGUI_Upload_ServerClick()
+		{
+			IEditableFiniteStateMachine fsm = FSMFactory.CreateEmptyFSM();
+			IState[] states = WebserverModelHelper.CreateStates(601,602);
+			fsm.AddStates(states);
+			fsm.StartState = WebserverModelHelper.GetState(601);
+			fsm.FinalStates = new IState[] {WebserverModelHelper.GetState(602)};
+			
+			WebserverModelHelper.AddTransition(603, ref fsm, 601, 601, "IAudioStore_HandleUpload");			
+			WebserverModelHelper.AddTransition(604, ref fsm, 601, 602, "IAudioStore_FinalizeUpload");
+			return fsm;
+		}
+
+
+		
+		//***************
+		//EncodingAdapter
+		//***************
 		public IFiniteStateMachine CreateSeff_EncodingAdapter_InsertAudioFile()
 		{
 			IEditableFiniteStateMachine fsm = FSMFactory.CreateEmptyFSM();
@@ -63,31 +120,106 @@ namespace Palladio.Performance.WebserverSeff
 			WebserverModelHelper.AddTransition(403, ref fsm, 401, 402, "IAudioDB_InsertAudioInfo");			
 			return fsm;
 		}
-
-		public IFiniteStateMachine CreateSeff_OggEncoder_EncodeFile()
+		public IFiniteStateMachine CreateSeff_EncodingAdapter_FinalizeUpload()
 		{
 			IEditableFiniteStateMachine fsm = FSMFactory.CreateEmptyFSM();
-			IState[] states = WebserverModelHelper.CreateStates(501, 502, 503, 504, 505);
+			IState[] states = WebserverModelHelper.CreateStates(801);
 			fsm.AddStates(states);
-			fsm.StartState = WebserverModelHelper.GetState(501);
-			fsm.FinalStates = new IState[] {WebserverModelHelper.GetState(505)};
+			fsm.StartState = WebserverModelHelper.GetState(801);
+			fsm.FinalStates = new IState[] {WebserverModelHelper.GetState(801)};
 			
-			WebserverModelHelper.AddTransition(505, ref fsm, 501, 502, "IEncoder_WriteMp3ToDisk");			
-			WebserverModelHelper.AddTransition(506, ref fsm, 502, 503, "IEncoder_Mp32Wav");			
-			WebserverModelHelper.AddTransition(507, ref fsm, 503, 504, "IEncoder_ExecuteEncoder");			
-			WebserverModelHelper.AddTransition(508, ref fsm, 504, 505, "IEncoder_ReadEncodedFileFromDisk");			
+			WebserverModelHelper.AddTransition(802, ref fsm, 801, 801, "IAudioDB_FinalizeUpload");			
 			return fsm;
 		}
 
-		public IFiniteStateMachine CreateSeff_WebGUI_Upload_ServerClick()
+
+
+		//*********
+		//DBAdapter
+		//*********
+		public IFiniteStateMachine CreateSeff_DBAdapter_InsertAudioFile()
 		{
 			IEditableFiniteStateMachine fsm = FSMFactory.CreateEmptyFSM();
-			IState[] states = WebserverModelHelper.CreateStates(601);
+			IState[] states = WebserverModelHelper.CreateStates(901,902,903,904,905,906,907);
 			fsm.AddStates(states);
-			fsm.StartState = WebserverModelHelper.GetState(601);
-			fsm.FinalStates = new IState[] {WebserverModelHelper.GetState(601)};
+			fsm.StartState = WebserverModelHelper.GetState(901);
+			fsm.FinalStates = new IState[] {WebserverModelHelper.GetState(907)};
 			
-			WebserverModelHelper.AddTransition(602, ref fsm, 601, 601, "IAudioStore_HandleUpload");			
+			WebserverModelHelper.AddTransition(908, ref fsm, 901, 902, "IConnection_Open");
+			WebserverModelHelper.AddTransition(909, ref fsm, 902, 903, "ICommand_ExecuteNonQuery");
+			WebserverModelHelper.AddTransition(910, ref fsm, 903, 904, "ICommand_ExecuteReader");
+			WebserverModelHelper.AddTransition(911, ref fsm, 904, 905, "IDataReader_Read");
+			WebserverModelHelper.AddTransition(912, ref fsm, 905, 906, "IDataReader_Close");
+			WebserverModelHelper.AddTransition(913, ref fsm, 906, 907, "IConnection_Close");
+			return fsm;
+		}
+
+		public IFiniteStateMachine CreateSeff_DBAdapter_InsertAudioInfo()
+		{
+			IEditableFiniteStateMachine fsm = FSMFactory.CreateEmptyFSM();
+			IState[] states = WebserverModelHelper.CreateStates(1001,1002,1003,1004);
+			fsm.AddStates(states);
+			fsm.StartState = WebserverModelHelper.GetState(1001);
+			fsm.FinalStates = new IState[] {WebserverModelHelper.GetState(1004)};
+			
+			WebserverModelHelper.AddTransition(1005, ref fsm, 1001, 1002, "IConnection_Open");
+			WebserverModelHelper.AddTransition(1006, ref fsm, 1002, 1003, "ICommand.ExecuteNonQuery");
+			WebserverModelHelper.AddTransition(1007, ref fsm, 1003, 1004, "IConnection_Close");
+			return fsm;
+		}
+
+		public IFiniteStateMachine CreateSeff_DBAdapter_GetAudioFileInfo()
+		{
+			IEditableFiniteStateMachine fsm = FSMFactory.CreateEmptyFSM();
+			IState[] states = WebserverModelHelper.CreateStates(1101,1102,1103,1104,1105,1106);
+			fsm.AddStates(states);
+			fsm.StartState = WebserverModelHelper.GetState(1101);
+			fsm.FinalStates = new IState[] {WebserverModelHelper.GetState(1106)};
+			
+			WebserverModelHelper.AddTransition(1107, ref fsm, 1101, 1102, "IConnection_Open");
+			WebserverModelHelper.AddTransition(1108, ref fsm, 1102, 1103, "ICommand_ExecuteReader");
+			WebserverModelHelper.AddTransition(1109, ref fsm, 1103, 1104, "IDataReader_Read");
+			WebserverModelHelper.AddTransition(1110, ref fsm, 1104, 1105, "IDataReader_Close");
+			WebserverModelHelper.AddTransition(1111, ref fsm, 1105, 1106, "IConnection_Close");
+			return fsm;
+		}
+
+
+
+		//*******************
+		//BufferingdDBAdapter
+		//*******************
+		public IFiniteStateMachine CreateSeff_BufferingDBAdapter_GetAudioFileInfo()
+		{
+			IEditableFiniteStateMachine fsm = FSMFactory.CreateEmptyFSM();
+			IState[] states = WebserverModelHelper.CreateStates(1201,1202,1203,1204,1205,1206);
+			fsm.AddStates(states);
+			fsm.StartState = WebserverModelHelper.GetState(1201);
+			fsm.FinalStates = new IState[] {WebserverModelHelper.GetState(1206)};
+			
+			WebserverModelHelper.AddTransition(1207, ref fsm, 1201, 1202, "IConnection_Open");
+			WebserverModelHelper.AddTransition(1208, ref fsm, 1202, 1203, "ICommand_ExecuteReader");
+			WebserverModelHelper.AddTransition(1209, ref fsm, 1203, 1204, "IDataReader_Read");
+			WebserverModelHelper.AddTransition(1210, ref fsm, 1204, 1205, "IDataReader_Close");
+			WebserverModelHelper.AddTransition(1212, ref fsm, 1205, 1206, "IConnection_Close");
+			return fsm;
+		}
+
+		public IFiniteStateMachine CreateSeff_BufferingDBAdapter_FinalizeUpload()
+		{
+			IEditableFiniteStateMachine fsm = FSMFactory.CreateEmptyFSM();
+			IState[] states = WebserverModelHelper.CreateStates(1301,1302,1303,1304,1305,1306,1307);
+			fsm.AddStates(states);
+			fsm.StartState = WebserverModelHelper.GetState(1301);
+			fsm.FinalStates = new IState[] {WebserverModelHelper.GetState(1307)};
+			
+			WebserverModelHelper.AddTransition(1308, ref fsm, 1301, 1302, "IConnection_Open");
+			WebserverModelHelper.AddTransition(1309, ref fsm, 1302, 1303, "ICommand_ExecuteReader");
+			WebserverModelHelper.AddTransition(1310, ref fsm, 1303, 1304, "IDataReader_Read");
+			WebserverModelHelper.AddTransition(1311, ref fsm, 1304, 1305, "IDataReader_Close");
+			WebserverModelHelper.AddTransition(1312, ref fsm, 1305, 1306, "ICommand_ExecuteNonQuery");
+			WebserverModelHelper.AddTransition(1313, ref fsm, 1306, 1305, "ICommand_ExecuteNonQuery");
+			WebserverModelHelper.AddTransition(1314, ref fsm, 1305, 1307, "IConnection_Close");
 			return fsm;
 		}
 	}
