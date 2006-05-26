2d44c8841e120738ace581f1c93b635ae6c15f7f
hkoziolek <heiko.koziolek@gmail.com>
2006-05-26 13:02:08 +0000

    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@2018 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- FACS06/RegExVisitor/src/RegEx/visitor/MainClass.java
+++ FACS06/RegExVisitor/src/RegEx/visitor/MainClass.java
@@ -1,5 +1,6 @@
 package RegEx.visitor;
 
+
 import java.io.File;
 import java.util.Iterator;
 
@@ -18,54 +19,10 @@ import RegEx.DistributionFunction;
 import RegEx.Expression;
 import RegEx.RegExPackage;
 import RegEx.TimeConsumption;
-import RegEx.visualise.Visualization;
+import RegEx.util.Serialization;
+import RegEx.util.Visualization;
 
 public class MainClass {
-
-	@SuppressWarnings("unchecked")
-	public static Expression loadRegEx(String fileName) {
-		// Create a resource set to hold the resources.
-		//
-		ResourceSet resourceSet = new ResourceSetImpl();
-
-		// Register the appropriate resource factory to handle all file
-		// extentions.
-		//
-		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap()
-				.put(Resource.Factory.Registry.DEFAULT_EXTENSION,
-						new XMIResourceFactoryImpl());
-
-		// Register the package to ensure it is available during loading.
-		//
-		resourceSet.getPackageRegistry().put(RegExPackage.eNS_URI,
-				RegExPackage.eINSTANCE);
-
-		// Construct the URI for the instance file.
-		// The argument is treated as a file path only if it denotes an existing
-		// file.
-		// Otherwise, it's directly treated as a URL.
-		//
-		File file = new File(fileName);
-		URI uri = file.isFile() ? URI.createFileURI(file.getAbsolutePath())
-				: URI.createURI(fileName);
-
-		// Demand load resource for this file.
-		//
-		Resource resource = resourceSet.getResource(uri, true);
-		System.out.println("Loaded " + uri);
-
-		// Validate the contents of the loaded resource.
-		//
-		for (Iterator j = resource.getContents().iterator(); j.hasNext();) {
-			EObject eObject = (EObject) j.next();
-			Diagnostic diagnostic = Diagnostician.INSTANCE.validate(eObject);
-			if (diagnostic.getSeverity() != Diagnostic.OK) {
-				printDiagnostic(diagnostic, "");
-			}
-		}
-		EObject eObject = (EObject)resource.getContents().iterator().next();
-		return (Expression) EcoreUtil.getRootContainer(eObject);
-	}
 
 	/**
 	 * <!-- begin-user-doc --> Prints diagnostics with indentation. <!--
@@ -86,7 +43,18 @@ public class MainClass {
 	}
 
 	public static void main(String[] args) {
-		Expression expr = loadRegEx("/home/jens/workspace/Parallel.regex");
+		Expression expr = 
+			(Expression)Serialization.loadFromXMI(
+			"C:\\Dokumente und Einstellungen\\koziolek\\workspace\\RegExVisitor\\Parallel.regex");
+		
+		// get measured distribution function
+		DistributionFunction distFunc = 
+			(DistributionFunction)Serialization.loadFromXMI(
+			"C:\\Dokumente und Einstellungen\\koziolek\\workspace\\Example\\single_thread.xmi");
+		
+		// add measured distribution function to symbol nodes
+		RegExInitVisitor initVisitor = new RegExInitVisitor(expr,distFunc);
+
 		RegExPerformanceVisitor perfVisitor = new RegExPerformanceVisitor(expr,2);
 	
 		TimeConsumption[] cpuTimes = perfVisitor.getResultCpuTimes();
@@ -96,7 +64,8 @@ public class MainClass {
 			printDF(cpuTimes[i].getNormDF());
 			vis.addDistributionFunction(cpuTimes[i].getNormDF(), "CPU"+i);
 		}
-		vis.visualize();	
+		vis.visualize();
+
 	}
 	
 	public static void printDF(DistributionFunction df){
