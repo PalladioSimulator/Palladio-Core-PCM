009c0d98345ce0f6787aae40e04dcf12fddb3963
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2006-10-12 11:22:06 +0000

    Probfunction regenerated
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@2220 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ Palladio.EMFProbfunctionModel/trunk/de.uka.ipd.sdq.probfunction/src/de/uka/ipd/sdq/probfunction/ProbabilityDensityFunction.java
@@ -0,0 +1,28 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.probfunction;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Probability Density Function</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see de.uka.ipd.sdq.probfunction.probfunctionPackage#getProbabilityDensityFunction()
+ * @model
+ * @generated
+ */
+public interface ProbabilityDensityFunction extends ProbabilityFunction {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	String copyright = "Copyright by SDQ U Karlsruhe (TH) 2006";
+
+} // ProbabilityDensityFunction
---
+++ Palladio.EMFProbfunctionModel/trunk/de.uka.ipd.sdq.probfunction/src/de/uka/ipd/sdq/probfunction/Unit.java
@@ -0,0 +1,61 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.probfunction;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Unit</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link de.uka.ipd.sdq.probfunction.Unit#getUnitName <em>Unit Name</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see de.uka.ipd.sdq.probfunction.probfunctionPackage#getUnit()
+ * @model
+ * @generated
+ */
+public interface Unit extends EObject {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	String copyright = "Copyright by SDQ U Karlsruhe (TH) 2006";
+
+	/**
+	 * Returns the value of the '<em><b>Unit Name</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Unit Name</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Unit Name</em>' attribute.
+	 * @see #setUnitName(String)
+	 * @see de.uka.ipd.sdq.probfunction.probfunctionPackage#getUnit_UnitName()
+	 * @model required="true" ordered="false"
+	 * @generated
+	 */
+	String getUnitName();
+
+	/**
+	 * Sets the value of the '{@link de.uka.ipd.sdq.probfunction.Unit#getUnitName <em>Unit Name</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Unit Name</em>' attribute.
+	 * @see #getUnitName()
+	 * @generated
+	 */
+	void setUnitName(String value);
+
+} // Unit
