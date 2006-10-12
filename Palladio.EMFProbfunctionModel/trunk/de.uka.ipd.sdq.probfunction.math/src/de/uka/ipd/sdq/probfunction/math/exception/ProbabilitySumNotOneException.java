463e289572b90c92a52c4f863e55d2895d896528
Ihssane <dummy>
2006-10-12 16:18:31 +0000

    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@2226 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.EMFProbfunctionModel/trunk/de.uka.ipd.sdq.probfunction.math/src/de/uka/ipd/sdq/probfunction/math/exception/ProbabilitySumNotOneException.java
+++ Palladio.EMFProbfunctionModel/trunk/de.uka.ipd.sdq.probfunction.math/src/de/uka/ipd/sdq/probfunction/math/exception/ProbabilitySumNotOneException.java
@@ -1,6 +1,5 @@
 package de.uka.ipd.sdq.probfunction.math.exception;
-//package de.uka.ipd.sdq.math.probfunction.exception;
-//
-//public class ProbabilitySumNotOneException extends Exception {
-//
-//}
+
+public class ProbabilitySumNotOneException extends Exception {
+
+}
