ebcd9c70c2f90b1f0e3572bd45f3a02e1670a7cd
Ihssane <dummy>
2006-10-12 16:22:52 +0000

    some help Methodes added.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@2231 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.EMFProbfunctionModel/trunk/de.uka.ipd.sdq.probfunction.math/src/de/uka/ipd/sdq/probfunction/math/util/MathTools.java
+++ Palladio.EMFProbfunctionModel/trunk/de.uka.ipd.sdq.probfunction.math/src/de/uka/ipd/sdq/probfunction/math/util/MathTools.java
@@ -1,8 +1,10 @@
 package de.uka.ipd.sdq.probfunction.math.util;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import de.uka.ipd.sdq.probfunction.math.IContinuousSample;
 import flanagan.complex.Complex;
 
 /**
@@ -91,14 +93,76 @@ public class MathTools {
 		}
 		return resultList;
 	}
-	
+
 	/**
 	 * Compares two doubles.
+	 * 
 	 * @param d1
 	 * @param d2
-	 * @return True, if the difference between both values is lower than EPSILON_ERROR; false otherwise.
+	 * @return True, if the difference between both values is lower than
+	 *         EPSILON_ERROR; false otherwise.
 	 */
-	public static boolean equalsDouble(double d1, double d2){
+	public static boolean equalsDouble(double d1, double d2) {
 		return (Math.abs(d1 - d2) < EPSILON_ERROR);
+	}
+
+	/**
+	 * Compute the sum of probabilities of an IContinuousSample list.
+	 * 
+	 * @param list
+	 * @return the computed value.
+	 */
+	public static double sumOfCountinuousSamples(List<IContinuousSample> list) {
+		double sum = 0.0;
+		for (IContinuousSample s : list)
+			sum += s.getProbability();
+		return sum;
+	}
+	
+	/**
+	 * Compute the sum of a Double list.
+	 * @param list
+	 * @return the computed value.
+	 */
+	public static double sumOfDoubles(List<Double> list) {
+		double sum = 0.0;
+		for (Double d : list)
+			sum += d;
+		return sum;
+	}
+
+	/**
+	 * @param prob
+	 * @return
+	 */
+	public static List<Double> computeIntervalOfProb(List<Double> prob) {
+		List<Double> probabilities = new ArrayList<Double>();
+		probabilities.add(prob.get(0));
+
+		for (int i = 1; i < probabilities.size(); i++)
+			probabilities.add(probabilities.get(i - 1) + prob.get(i));
+		return probabilities;
+	}
+
+	/**
+	 * @param samples
+	 * @param prob
+	 * @return
+	 */
+	public static HashMap<Double, Line> computeLines(
+			List<IContinuousSample> samples, List<Double> intervals) {
+		HashMap<Double, Line> lines = new HashMap<Double, Line>();
+		lines.put(intervals.get(0), new Line(0, 0, samples.get(0).getValue(),
+				samples.get(0).getProbability()));
+
+		for (int i = 1; i < intervals.size(); i++) {
+			double x1 = samples.get(i - 1).getValue();
+			double y1 = intervals.get(i - 1);
+			double x2 = samples.get(i).getValue();
+			double y2 = intervals.get(i);
+			lines.put(intervals.get(i), new Line(x1, y1, x2, y2));
+		}
+
+		return lines;
 	}
 }
