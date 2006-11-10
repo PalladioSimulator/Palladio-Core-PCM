c185c7a6246be07f2fc276d864d7812c787c3671
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2006-11-10 10:46:17 +0000

    Committed edit/editor again *sigh*
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@2361 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ PalladioCM.DerivedContext/trunk/de.uka.ipd.sdq.context.edit/src/de/uka/ipd/sdq/context/usage/provider/LoopIterationItemProvider.java
@@ -0,0 +1,214 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.context.usage.provider;
+
+
+import de.uka.ipd.sdq.context.usage.LoopIteration;
+import de.uka.ipd.sdq.context.usage.UsagePackage;
+
+import de.uka.ipd.sdq.pcm.core.stochastics.StochasticsFactory;
+
+import de.uka.ipd.sdq.pcm.parameter.ParameterFactory;
+
+import de.uka.ipd.sdq.pcm.seff.SeffFactory;
+
+import de.uka.ipd.sdq.pcm.system.SystemFactory;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.common.util.ResourceLocator;
+
+import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
+import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
+import org.eclipse.emf.edit.provider.IItemLabelProvider;
+import org.eclipse.emf.edit.provider.IItemPropertySource;
+import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
+import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
+import org.eclipse.emf.edit.provider.ItemProviderAdapter;
+import org.eclipse.emf.edit.provider.ViewerNotification;
+
+/**
+ * This is the item provider adapter for a {@link de.uka.ipd.sdq.context.usage.LoopIteration} object.
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class LoopIterationItemProvider
+	extends ItemProviderAdapter
+	implements	
+		IEditingDomainItemProvider,	
+		IStructuredItemContentProvider,	
+		ITreeItemContentProvider,	
+		IItemLabelProvider,	
+		IItemPropertySource {
+	/**
+	 * This constructs an instance from a factory and a notifier.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public LoopIterationItemProvider(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+
+	/**
+	 * This returns the property descriptors for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public List getPropertyDescriptors(Object object) {
+		if (itemPropertyDescriptors == null) {
+			super.getPropertyDescriptors(object);
+
+			addLoopaction_LoopIterationPropertyDescriptor(object);
+		}
+		return itemPropertyDescriptors;
+	}
+
+	/**
+	 * This adds a property descriptor for the Loopaction Loop Iteration feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addLoopaction_LoopIterationPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_LoopIteration_loopaction_LoopIteration_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_LoopIteration_loopaction_LoopIteration_feature", "_UI_LoopIteration_type"),
+				 UsagePackage.Literals.LOOP_ITERATION__LOOPACTION_LOOP_ITERATION,
+				 true,
+				 false,
+				 true,
+				 null,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
+	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
+	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Collection getChildrenFeatures(Object object) {
+		if (childrenFeatures == null) {
+			super.getChildrenFeatures(object);
+			childrenFeatures.add(UsagePackage.Literals.LOOP_ITERATION__ITERATIONS_LOOP_ITERATION);
+		}
+		return childrenFeatures;
+	}
+
+	/**
+	 * This returns LoopIteration.gif.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Object getImage(Object object) {
+		return overlayImage(object, getResourceLocator().getImage("full/obj16/LoopIteration"));
+	}
+
+	/**
+	 * This returns the label text for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getText(Object object) {
+		return getString("_UI_LoopIteration_type");
+	}
+
+	/**
+	 * This handles model notifications by calling {@link #updateChildren} to update any cached
+	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void notifyChanged(Notification notification) {
+		updateChildren(notification);
+
+		switch (notification.getFeatureID(LoopIteration.class)) {
+			case UsagePackage.LOOP_ITERATION__ITERATIONS_LOOP_ITERATION:
+				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
+				return;
+		}
+		super.notifyChanged(notification);
+	}
+
+	/**
+	 * This adds to the collection of {@link org.eclipse.emf.edit.command.CommandParameter}s
+	 * describing all of the children that can be created under this object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void collectNewChildDescriptors(Collection newChildDescriptors, Object object) {
+		super.collectNewChildDescriptors(newChildDescriptors, object);
+
+		newChildDescriptors.add
+			(createChildParameter
+				(UsagePackage.Literals.LOOP_ITERATION__ITERATIONS_LOOP_ITERATION,
+				 StochasticsFactory.eINSTANCE.createRandomVariable()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(UsagePackage.Literals.LOOP_ITERATION__ITERATIONS_LOOP_ITERATION,
+				 SeffFactory.eINSTANCE.createParametricResourceDemand()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(UsagePackage.Literals.LOOP_ITERATION__ITERATIONS_LOOP_ITERATION,
+				 SeffFactory.eINSTANCE.createPrimitiveParametricParameterUsage()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(UsagePackage.Literals.LOOP_ITERATION__ITERATIONS_LOOP_ITERATION,
+				 SeffFactory.eINSTANCE.createIterationCount()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(UsagePackage.Literals.LOOP_ITERATION__ITERATIONS_LOOP_ITERATION,
+				 SeffFactory.eINSTANCE.createCollectionParametricParameterUsage()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(UsagePackage.Literals.LOOP_ITERATION__ITERATIONS_LOOP_ITERATION,
+				 ParameterFactory.eINSTANCE.createPrimitiveParameterCharacterisation()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(UsagePackage.Literals.LOOP_ITERATION__ITERATIONS_LOOP_ITERATION,
+				 ParameterFactory.eINSTANCE.createCollectionParameterCharacterisation()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(UsagePackage.Literals.LOOP_ITERATION__ITERATIONS_LOOP_ITERATION,
+				 SystemFactory.eINSTANCE.createSpecifiedTimeConsumption()));
+	}
+
+	/**
+	 * Return the resource locator for this item provider's resources.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ResourceLocator getResourceLocator() {
+		return ContextEditPlugin.INSTANCE;
+	}
+
+}
---
+++ PalladioCM.DerivedContext/trunk/de.uka.ipd.sdq.context.editor/src/de/uka/ipd/sdq/context/usage/presentation/ContextEditorPlugin.java
@@ -0,0 +1,103 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.context.usage.presentation;
+
+import de.uka.ipd.sdq.identifier.provider.IdentifierEditPlugin;
+
+import de.uka.ipd.sdq.pcm.core.stochastics.provider.PcmEditPlugin;
+
+import de.uka.ipd.sdq.probfunction.provider.ProbabilityFunctionEditPlugin;
+
+import org.eclipse.emf.common.EMFPlugin;
+
+import org.eclipse.emf.common.ui.EclipseUIPlugin;
+
+import org.eclipse.emf.common.util.ResourceLocator;
+
+/**
+ * This is the central singleton for the Context editor plugin.
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public final class ContextEditorPlugin extends EMFPlugin {
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final ContextEditorPlugin INSTANCE = new ContextEditorPlugin();
+
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private static Implementation plugin;
+
+	/**
+	 * Create the instance.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ContextEditorPlugin() {
+		super
+		  (new ResourceLocator [] {
+		     IdentifierEditPlugin.INSTANCE,
+		     PcmEditPlugin.INSTANCE,
+		     ProbabilityFunctionEditPlugin.INSTANCE,
+		   });
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	public ResourceLocator getPluginResourceLocator() {
+		return plugin;
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	public static Implementation getPlugin() {
+		return plugin;
+	}
+
+	/**
+	 * The actual implementation of the Eclipse <b>Plugin</b>.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static class Implementation extends EclipseUIPlugin {
+		/**
+		 * Creates an instance.
+		 * <!-- begin-user-doc -->
+		 * <!-- end-user-doc -->
+		 * @generated
+		 */
+		public Implementation() {
+			super();
+
+			// Remember the static instance.
+			//
+			plugin = this;
+		}
+	}
+
+}
