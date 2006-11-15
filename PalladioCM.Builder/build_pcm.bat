e9bdc25e3276520149cf028b1f507f335f4a8393
hkoziolek <heiko.koziolek@gmail.com>
2006-11-15 16:45:36 +0000

    Added context plugin
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@2378 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- PalladioCM.Builder/build_pcm.bat
+++ PalladioCM.Builder/build_pcm.bat
@@ -1,10 +1,10 @@
 @echo off
 
 rem The install directory of the PDE Build
-set ECLIPSE_HOME=C:\pdebuild\org.eclipse.releng.basebuilder
+set ECLIPSE_HOME=C:\Programme\PDEBuilder\org.eclipse.releng.basebuilder
 
 rem The directory where this script is located
-set BASE_DIR=C:\Temp\testpcm
+set BASE_DIR=D:\Diss\svn\code\PalladioCM.Builder
 
 java -cp %ECLIPSE_HOME%\startup.jar org.eclipse.core.launcher.Main -application org.eclipse.ant.core.antRunner -clean -buildfile "%ECLIPSE_HOME%\plugins\org.eclipse.pde.build\scripts\build.xml" -Dbuilder="%BASE_DIR%\pcm_build_scripts" %1
 	
