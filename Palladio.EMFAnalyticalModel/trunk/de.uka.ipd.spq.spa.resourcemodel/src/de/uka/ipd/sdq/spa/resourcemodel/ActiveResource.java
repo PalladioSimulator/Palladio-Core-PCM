fa566ec44da55f54920151f60cd4e4798d3f3633
JensHappe <jens.happe@avenyou.com>
2006-11-21 10:23:18 +0000

    added changes in the meta model
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@2426 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ Palladio.EMFAnalyticalModel/trunk/de.uka.ipd.spq.spa.resourcemodel/src/de/uka/ipd/sdq/spa/resourcemodel/ActiveResource.java
@@ -0,0 +1,22 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.spa.resourcemodel;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Active Resource</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see de.uka.ipd.sdq.spa.resourcemodel.ResourceModelPackage#getActiveResource()
+ * @model abstract="true"
+ * @generated
+ */
+public interface ActiveResource extends EObject {
+} // ActiveResource
