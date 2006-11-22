e2a42bd477b82b6a534eebbb5a92b08af07fa30b
JensHappe <jens.happe@avenyou.com>
2006-11-22 11:53:20 +0000

    deleted generated code
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@2465 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.EMFAnalyticalModel/trunk/de.uka.ipd.sdq.spa.concurrencysolver/src/de/uka/ipd/sdq/spa/concurrencysolver/ResponseTimeSwitch.java
+++
@@ -1,7 +0,0 @@
-package de.uka.ipd.sdq.spa.concurrencysolver;
-
-import de.uka.ipd.sdq.qnm.util.QnmSwitch;
-
-public class ResponseTimeSwitch extends QnmSwitch {
-
-}
--- Palladio.EMFAnalyticalModel/trunk/de.uka.ipd.sdq.spa.concurrencysolver/src/de/uka/ipd/sdq/spa/concurrencysolver/qnm/solver/SolvServerResult.java
+++
@@ -1,7 +0,0 @@
-package de.uka.ipd.sdq.spa.concurrencysolver.qnm.solver;
-
-import de.uka.ipd.sdq.qnm.qnResult.impl.ServerResultImpl;
-
-public class SolvServerResult extends ServerResultImpl {
-
-}
