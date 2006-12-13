69b863774256213b0800a0b98f61e2eb09208e11
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2006-12-13 06:22:51 +0000

    made a copy
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@2634 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ PalladioCM.CodeGen/branches/FirstWorkingProcessCopy/de.uka.ipd.sdq.simucomframework/src/de/uka/ipd/sdq/simucomframework/usage/IScenarioRunner.java
@@ -0,0 +1,7 @@
+package de.uka.ipd.sdq.simucomframework.usage;
+
+import desmoj.core.simulator.SimProcess;
+
+public interface IScenarioRunner {
+	void scenarioRunner(SimProcess thread);
+}
---
+++ PalladioCM.CodeGen/branches/FirstWorkingProcessCopy/de.uka.ipd.sdq.simucomframework/src/de/uka/ipd/sdq/simucomframework/usage/IUser.java
@@ -0,0 +1,5 @@
+package de.uka.ipd.sdq.simucomframework.usage;
+
+public interface IUser extends IScenarioRunner {
+	void startUserLife();
+}
