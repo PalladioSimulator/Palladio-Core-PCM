dd472ff76c2bd2b8d8c4d36fc4a581ec8fcfbe13
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2007-03-07 19:39:20 +0000

    Fixed the build script to work with basebuilder 3.3
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@2812 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- PalladioCM.Builder/build_pcm.bat
+++ PalladioCM.Builder/build_pcm.bat
@@ -4,7 +4,7 @@ rem The install directory of the PDE Build
 set ECLIPSE_HOME=C:\pdebuild\org.eclipse.releng.basebuilder
 
 rem The directory where this script is located
-set BASE_DIR=C:\Temp\testpcm
+set BASE_DIR=C:\coding\pcm2builder
 
-java -cp %ECLIPSE_HOME%\startup.jar org.eclipse.core.launcher.Main -application org.eclipse.ant.core.antRunner -clean -buildfile "%ECLIPSE_HOME%\plugins\org.eclipse.pde.build\scripts\build.xml" -Dbuilder="%BASE_DIR%\pcm_build_scripts" %1
+java -cp %ECLIPSE_HOME%\plugins\org.eclipse.equinox.launcher.jar org.eclipse.core.launcher.Main -application org.eclipse.ant.core.antRunner -clean -buildfile "%ECLIPSE_HOME%\plugins\org.eclipse.pde.build\scripts\build.xml" -Dbuilder="%BASE_DIR%\pcm_build_scripts" %1
 	
