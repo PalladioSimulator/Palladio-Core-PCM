7ad80c3925fdf5e4fd3a9c770343ae75951cd5d9
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2007-03-07 17:00:46 +0000

    GMF2 version checkin
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@2790 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ Palladio.StoEx/trunk/de.uka.ipd.sdq.stoex/src/de/uka/ipd/sdq/stoex/ProbabilityFunctionLiteral.java
@@ -0,0 +1,54 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.stoex;
+
+import de.uka.ipd.sdq.probfunction.ProbabilityFunction;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Probability Function Literal</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link de.uka.ipd.sdq.stoex.ProbabilityFunctionLiteral#getFunction_ProbabilityFunctionLiteral <em>Function Probability Function Literal</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see de.uka.ipd.sdq.stoex.StoexPackage#getProbabilityFunctionLiteral()
+ * @model
+ * @generated
+ */
+public interface ProbabilityFunctionLiteral extends Atom {
+	/**
+	 * Returns the value of the '<em><b>Function Probability Function Literal</b></em>' containment reference.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Function Probability Function Literal</em>' containment reference isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Function Probability Function Literal</em>' containment reference.
+	 * @see #setFunction_ProbabilityFunctionLiteral(ProbabilityFunction)
+	 * @see de.uka.ipd.sdq.stoex.StoexPackage#getProbabilityFunctionLiteral_Function_ProbabilityFunctionLiteral()
+	 * @model containment="true" required="true" ordered="false"
+	 * @generated
+	 */
+	ProbabilityFunction getFunction_ProbabilityFunctionLiteral();
+
+	/**
+	 * Sets the value of the '{@link de.uka.ipd.sdq.stoex.ProbabilityFunctionLiteral#getFunction_ProbabilityFunctionLiteral <em>Function Probability Function Literal</em>}' containment reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Function Probability Function Literal</em>' containment reference.
+	 * @see #getFunction_ProbabilityFunctionLiteral()
+	 * @generated
+	 */
+	void setFunction_ProbabilityFunctionLiteral(ProbabilityFunction value);
+
+} // ProbabilityFunctionLiteral
---
+++ Palladio.StoEx/trunk/de.uka.ipd.sdq.stoex/src/de/uka/ipd/sdq/stoex/impl/TermImpl.java
@@ -0,0 +1,43 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.stoex.impl;
+
+import de.uka.ipd.sdq.stoex.StoexPackage;
+import de.uka.ipd.sdq.stoex.Term;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Term</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public abstract class TermImpl extends ComparisonImpl implements Term {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected TermImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return StoexPackage.Literals.TERM;
+	}
+
+} //TermImpl
