e94945a4edf40f1ee7824938317c848ec6771593
roman <dummy>
2007-03-15 18:20:59 +0000

    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@2879 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ PalladioCM.CodeGenGUI/trunk/de.uka.ipd.sdq.codegen.simudatavisualization/src/de/uka/ipd/sdq/codegen/simudatavisualization/SimuImages.java
@@ -0,0 +1,50 @@
+package de.uka.ipd.sdq.codegen.simudatavisualization;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
+
+public class SimuImages {
+
+	public static final String RUN = "run_obj";
+	public static final String SENSOR = "sensor_obj";
+	public static final String EXPERIMENT = "experiment_obj";
+	public static final String SENSORS = "sensors_category";
+	public static final String TREEROOT = "tree_root";
+
+	// For the toolbar images
+	public static ImageRegistry imageRegistry = new ImageRegistry();
+
+	
+	/**
+	 * Note: An image registry owns all of the image objects registered with it,
+	 * and automatically disposes of them the SWT Display is disposed.
+	 */ 
+
+	static {
+		String iconPath = "icons/";
+		
+		imageRegistry.put(RUN,
+				 getImageDescriptor(iconPath + RUN + ".gif")
+		);
+		imageRegistry.put(SENSOR,
+				 getImageDescriptor(iconPath + SENSOR + ".gif")
+				);
+		imageRegistry.put(SENSORS,
+				 getImageDescriptor(iconPath + SENSORS + ".gif")
+				);
+		imageRegistry.put(EXPERIMENT,
+				 getImageDescriptor(iconPath + EXPERIMENT + ".gif")
+				);
+		imageRegistry.put(TREEROOT,
+				 getImageDescriptor(iconPath + TREEROOT + ".gif")
+				);
+	}
+	
+	/**
+	 *@param imageFilePath the relative to the root of the plug-in; the path must be legal
+     *@return an image descriptor, or null if no image could be found
+	 */
+	public static ImageDescriptor getImageDescriptor(String imageFilePath) {
+		return Activator.getImageDescriptor(imageFilePath);
+	}
+}
