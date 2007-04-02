68ead55e8de4aba4a8a5b0a284991baeb5e90698
roman <dummy>
2007-04-02 08:32:27 +0000

    add ReportView
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@2951 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- PalladioCM.CodeGenGUI/trunk/de.uka.ipd.sdq.codegen.simudatavisualization/src/de/uka/ipd/sdq/codegen/simudatavisualization/views/ReportView.java
+++ PalladioCM.CodeGenGUI/trunk/de.uka.ipd.sdq.codegen.simudatavisualization/src/de/uka/ipd/sdq/codegen/simudatavisualization/views/ReportView.java
@@ -1,43 +1,23 @@
 package de.uka.ipd.sdq.codegen.simudatavisualization.views;
 
-import java.io.File;
-
 import org.eclipse.birt.chart.device.IDeviceRenderer;
 import org.eclipse.birt.chart.exception.ChartException;
 import org.eclipse.birt.chart.internal.log.DefaultLoggerImpl;
-import org.eclipse.birt.chart.model.ChartWithAxes;
-import org.eclipse.birt.chart.model.ModelPackage;
-import org.eclipse.birt.chart.model.attribute.impl.ColorDefinitionImpl;
-import org.eclipse.birt.chart.model.component.Axis;
-import org.eclipse.birt.chart.model.component.Series;
-import org.eclipse.birt.chart.model.component.impl.SeriesImpl;
-import org.eclipse.birt.chart.model.data.NumberDataSet;
-import org.eclipse.birt.chart.model.data.SeriesDefinition;
-import org.eclipse.birt.chart.model.data.TextDataSet;
-import org.eclipse.birt.chart.model.data.impl.NumberDataSetImpl;
-import org.eclipse.birt.chart.model.data.impl.SeriesDefinitionImpl;
-import org.eclipse.birt.chart.model.data.impl.TextDataSetImpl;
-import org.eclipse.birt.chart.model.layout.Plot;
-import org.eclipse.birt.chart.model.type.LineSeries;
-import org.eclipse.birt.chart.model.type.impl.LineSeriesImpl;
 import org.eclipse.birt.chart.util.PluginSettings;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.swt.dnd.DND;
+import org.eclipse.swt.dnd.DropTarget;
+import org.eclipse.swt.dnd.Transfer;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.part.EditorInputTransfer;
 import org.eclipse.ui.part.EditorPart;
 import org.eclipse.ui.views.properties.IPropertySheetPage;
 import org.eclipse.ui.views.properties.tabbed.ITabbedPropertySheetPageContributor;
@@ -45,52 +25,14 @@ import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
 
 import de.uka.ipd.sdq.codegen.simudatavisualization.actions.ChartSwtViewer;
 import de.uka.ipd.sdq.codegen.simudatavisualization.birt.ChartVisualization;
-import de.uka.ipd.sdq.codegen.simudatavisualization.birt.Histogram;
-import de.uka.ipd.sdq.codegen.simudatavisualization.birt.TimeSpanSensorToDatasetAdapterFactory;
-import de.uka.ipd.sdq.codegen.simudatavisualization.birt.ReportCongiguration;
-import de.uka.ipd.sdq.codegen.simudatavisualization.birt.RunEntry;
-import de.uka.ipd.sdq.sensorfactory.entities.Sensor;
-import de.uka.ipd.sdq.sensorframework.adapter.AdapterRegistry;
 
 public class ReportView extends EditorPart implements
 		ITabbedPropertySheetPageContributor {
-
+	
 	public static String EDITOR_ID = "de.uka.ipd.sdq.codegen.simudatavisualization.views.ReportView";
 
-	private ReportCongiguration configObject;
 	private ChartVisualization visualization;
-
-	@Override
-	public void doSave(IProgressMonitor monitor) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void doSaveAs() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void init(IEditorSite site, IEditorInput input)
-			throws PartInitException {
-		setSite(site);
-		setInput(input);
-
-		// Create configuratio object
-		configObject = new ReportCongiguration();
-	}
-
-	@Override
-	public boolean isDirty() {
-		return false;
-	}
-
-	@Override
-	public boolean isSaveAsAllowed() {
-		return false;
-	}
+	
 
 	@Override
 	public void createPartControl(Composite parent) {
@@ -103,7 +45,7 @@ public class ReportView extends EditorPart implements
 		}
 
 		// create new chart
-		visualization = new ChartVisualization(configObject);
+		visualization = new ChartVisualization();
 		
 		// render chart
 		new ChartSwtViewer(parent, 0, idr, visualization.getChart());
@@ -113,7 +55,6 @@ public class ReportView extends EditorPart implements
 			@Override
 			public void addSelectionChangedListener(
 					ISelectionChangedListener listener) {
-				// TODO Auto-generated method stub
 			}
 
 			@Override
@@ -124,7 +65,6 @@ public class ReportView extends EditorPart implements
 			@Override
 			public void removeSelectionChangedListener(
 					ISelectionChangedListener listener) {
-				// TODO Auto-generated method stub
 			}
 
 			@Override
@@ -133,9 +73,20 @@ public class ReportView extends EditorPart implements
 
 			}
 		});
-
+		
+		addDropSupport(parent);
 	}
-
+	
+	private void addDropSupport(Control control) {
+	
+		int operations = DND.DROP_COPY | DND.DROP_DEFAULT;
+		DropTarget target = new DropTarget(control, operations);
+		
+		Transfer[] transferTypes = new Transfer[] {EditorInputTransfer.getInstance()};
+		target.setTransfer(transferTypes);
+		target.addDropListener(new ViewDropTargetListener());
+	}
+	
 	@Override
 	public Object getAdapter(Class adapter) {
 		if (adapter == IPropertySheetPage.class)
@@ -144,16 +95,8 @@ public class ReportView extends EditorPart implements
 	}
 
 	@Override
-	public void setFocus() {
-	}
-
-	@Override
 	public String getContributorId() {
 		return getSite().getId();
-	}
-
-	public ReportCongiguration getConfigObject() {
-		return configObject;
 	}
 
 	/**
@@ -161,5 +104,38 @@ public class ReportView extends EditorPart implements
 	 */
 	public ChartVisualization getVisualization() {
 		return visualization;
+	}
+
+	@Override
+	public void setFocus() {
+	}
+	
+	@Override
+	public void doSave(IProgressMonitor monitor) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void doSaveAs() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void init(IEditorSite site, IEditorInput input)
+	throws PartInitException {
+		setSite(site);
+		setInput(input);
+	}
+	
+	@Override
+	public boolean isDirty() {
+		return false;
+	}
+	
+	@Override
+	public boolean isSaveAsAllowed() {
+		return false;
 	}
 }
