ae59af243f5bb972b707aa5359c25ebda22fe643
roman <dummy>
2007-04-03 14:46:22 +0000

    create project
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@2974 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.seff.helper/src/de/uka/ipd/sdq/pcm/gmf/seff/helper/SEFFHelperPlugin.java
@@ -0,0 +1,50 @@
+package de.uka.ipd.sdq.pcm.gmf.seff.helper;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class SEFFHelperPlugin extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "de.uka.ipd.sdq.pcm.gmf.seff.helper";
+
+	// The shared instance
+	private static SEFFHelperPlugin plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public SEFFHelperPlugin() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static SEFFHelperPlugin getDefault() {
+		return plugin;
+	}
+
+}
