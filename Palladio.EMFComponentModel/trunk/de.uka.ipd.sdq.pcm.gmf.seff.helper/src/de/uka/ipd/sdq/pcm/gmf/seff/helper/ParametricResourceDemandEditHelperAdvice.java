28e987c265f7da8a5f969b1cd6389f89b2ed9516
roman <dummy>
2007-04-11 07:47:41 +0000

    fix dialog quelle
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@2988 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.seff.helper/src/de/uka/ipd/sdq/pcm/gmf/seff/helper/ParametricResourceDemandEditHelperAdvice.java
+++ Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.seff.helper/src/de/uka/ipd/sdq/pcm/gmf/seff/helper/ParametricResourceDemandEditHelperAdvice.java
@@ -6,7 +6,6 @@ package de.uka.ipd.sdq.pcm.gmf.seff.helper;
 import java.util.ArrayList;
 
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.gmf.runtime.common.core.command.ICommand;
 import org.eclipse.gmf.runtime.emf.type.core.commands.SetValueCommand;
@@ -17,15 +16,8 @@ import org.eclipse.gmf.runtime.emf.type.core.requests.SetRequest;
 import org.eclipse.ui.PlatformUI;
 
 import de.uka.ipd.sdq.dialogs.selection.PalladioSelectEObjectDialog;
-import de.uka.ipd.sdq.pcm.core.entity.Entity;
-import de.uka.ipd.sdq.pcm.gmf.seff.providers.PalladioComponentModelElementTypes;
-import de.uka.ipd.sdq.pcm.repository.Interface;
-import de.uka.ipd.sdq.pcm.repository.RepositoryPackage;
-import de.uka.ipd.sdq.pcm.repository.RequiredRole;
-import de.uka.ipd.sdq.pcm.repository.Signature;
 import de.uka.ipd.sdq.pcm.resourcetype.ProcessingResourceType;
 import de.uka.ipd.sdq.pcm.resourcetype.ResourceRepository;
-import de.uka.ipd.sdq.pcm.resourcetype.ResourceType;
 import de.uka.ipd.sdq.pcm.seff.SeffPackage;
 
 /**
@@ -35,44 +27,41 @@ import de.uka.ipd.sdq.pcm.seff.SeffPackage;
 public class ParametricResourceDemandEditHelperAdvice extends
 		AbstractEditHelperAdvice implements IEditHelperAdvice {
 
+	final String EDITING_DOMAIN_ID = "de.uka.ipd.sdq.PCMBench.editingDomain";
+	/**
+	 * The transactional editing domain which is used to get the commands and alter the model 
+	 */
+	final TransactionalEditingDomain editingDomain = TransactionalEditingDomain.Registry.INSTANCE
+	.getEditingDomain(EDITING_DOMAIN_ID);
+
 	/* (non-Javadoc)
 	 * @see org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelperAdvice#getAfterConfigureCommand(org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest)
 	 */
 	@Override
 	protected ICommand getAfterConfigureCommand(ConfigureRequest request) {
 		
-		final String EDITING_DOMAIN_ID = "de.uka.ipd.sdq.PCMBench.editingDomain";
-		/**
-		 * The transactional editing domain which is used to get the commands and alter the model 
-		 */
-		final TransactionalEditingDomain editingDomain = TransactionalEditingDomain.Registry.INSTANCE
-		.getEditingDomain(EDITING_DOMAIN_ID);
-
-		EObject eOobject = request.getElementToConfigure().eContainer();
 		EObject resource = null;
 		ArrayList<Object> filterList = new ArrayList<Object>();
 		filterList.add(ResourceRepository.class);
-		filterList.add(ResourceType.class);
 		filterList.add(ProcessingResourceType.class);
+
 		ArrayList<Object> additionalReferences = new ArrayList<Object>();
-	//	additionalReferences.add(RepositoryPackage.eINSTANCE.get);
 		PalladioSelectEObjectDialog dialog = new PalladioSelectEObjectDialog(
 				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
 				filterList, 
 				additionalReferences,
-				eOobject);
+				editingDomain.getResourceSet());
 		dialog.open();
 		if (dialog.getResult() == null)
 			return new CanceledCommand();
-		if (!(dialog.getResult() instanceof Signature))
+		if (!(dialog.getResult() instanceof ProcessingResourceType))
 			return new CanceledCommand();
 		resource = (ProcessingResourceType) dialog.getResult();
 		
-		// TODO
 		ICommand cmd = new SetValueCommand(
 				new SetRequest(
 						request.getElementToConfigure(), 
-						SeffPackage.eINSTANCE.getExternalCallAction_CalledService_ExternalService(),
+						SeffPackage.eINSTANCE.getParametricResourceDemand_RequiredResource_ParametricResourceDemand(),
 						resource));
 		return cmd;
 	}
