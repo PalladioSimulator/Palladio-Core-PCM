c3da4f350e359d9b063d8ddc66fb2e8955853b68
roman <dummy>
2007-04-13 13:54:10 +0000

    add AquireAction and ReleaseAction EditHelper
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@3000 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.seff.helper/src/de/uka/ipd/sdq/pcm/gmf/seff/helper/InternalActionEditHelperAdvice.java
+++ Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.seff.helper/src/de/uka/ipd/sdq/pcm/gmf/seff/helper/InternalActionEditHelperAdvice.java
@@ -4,7 +4,6 @@
 package de.uka.ipd.sdq.pcm.gmf.seff.helper;
 
 import org.eclipse.gmf.runtime.common.core.command.ICommand;
-import org.eclipse.gmf.runtime.emf.type.core.ElementTypeRegistry;
 import org.eclipse.gmf.runtime.emf.type.core.IElementType;
 import org.eclipse.gmf.runtime.emf.type.core.commands.CreateElementCommand;
 import org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelperAdvice;
@@ -12,24 +11,26 @@ import org.eclipse.gmf.runtime.emf.type.core.edithelper.IEditHelperAdvice;
 import org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest;
 import org.eclipse.gmf.runtime.emf.type.core.requests.CreateElementRequest;
 
+import de.uka.ipd.sdq.pcm.gmf.seff.providers.PalladioComponentModelElementTypes;
+
 /**
  * @author admin
  *
  */
-public class CreateLoopIterationsEditHelperAdvice extends
-		AbstractEditHelperAdvice implements IEditHelperAdvice {
-
+public class InternalActionEditHelperAdvice extends AbstractEditHelperAdvice
+		implements IEditHelperAdvice {
+	
 	/* (non-Javadoc)
 	 * @see org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelperAdvice#getAfterConfigureCommand(org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest)
 	 */
 	@Override
 	protected ICommand getAfterConfigureCommand(ConfigureRequest request) {
+		IElementType elementType = PalladioComponentModelElementTypes.ParametricResourceDemand_3008;
 
-		IElementType elementType = ElementTypeRegistry.getInstance().getType("de.uka.ipd.sdq.pcm.gmf.seff.LoopIterations_9000");
-		
 		CreateElementRequest createElementRequest = new CreateElementRequest(
 				request.getElementToConfigure(), elementType);
 		return new CreateElementCommand(createElementRequest);
 	}
+
 
 }
