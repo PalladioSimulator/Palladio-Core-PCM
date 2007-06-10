5f1b72d5f89cfc13b2759fe9dca8001ae601558b
roman <dummy>
2007-06-10 18:46:57 +0000

    fix bug 211 (Im Usage Model bei der Erzeugung von Branch und Loop Compartements gleich Start und Stop Action vorgeben)
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@3355 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.usage.helper/src/de/uka/ipd/sdq/pcm/gmf/usage/helper/ScenarioBehaviourEditHelperAdvice.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package de.uka.ipd.sdq.pcm.gmf.usage.helper;
+
+import org.eclipse.gmf.runtime.common.core.command.ICommand;
+import org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelperAdvice;
+import org.eclipse.gmf.runtime.emf.type.core.edithelper.IEditHelperAdvice;
+import org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest;
+
+/** @author roman */
+public class ScenarioBehaviourEditHelperAdvice extends AbstractEditHelperAdvice
+		implements IEditHelperAdvice {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelperAdvice#getAfterConfigureCommand(org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest)
+	 */
+	@Override
+	protected ICommand getAfterConfigureCommand(ConfigureRequest request) {
+		ConfigureMinimalSeffCommand cmd = new ConfigureMinimalSeffCommand(
+				request);
+		return cmd;
+	}
+}
