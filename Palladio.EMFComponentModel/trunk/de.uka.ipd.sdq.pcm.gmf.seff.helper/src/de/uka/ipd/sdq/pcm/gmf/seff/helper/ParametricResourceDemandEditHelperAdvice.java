9b872d9c984a7f3f5988bbc35b8dc8ecf42b96a4
roman <dummy>
2007-06-14 13:46:52 +0000

    fix Bug 205
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@3390 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.seff.helper/src/de/uka/ipd/sdq/pcm/gmf/seff/helper/ParametricResourceDemandEditHelperAdvice.java
+++ Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.seff.helper/src/de/uka/ipd/sdq/pcm/gmf/seff/helper/ParametricResourceDemandEditHelperAdvice.java
@@ -3,28 +3,13 @@
  */
 package de.uka.ipd.sdq.pcm.gmf.seff.helper;
 
-import java.util.ArrayList;
-
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.transaction.TransactionalEditingDomain;
-import org.eclipse.emf.transaction.util.TransactionUtil;
 import org.eclipse.gmf.runtime.common.core.command.ICommand;
-import org.eclipse.gmf.runtime.emf.type.core.commands.SetValueCommand;
 import org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelperAdvice;
 import org.eclipse.gmf.runtime.emf.type.core.edithelper.IEditHelperAdvice;
 import org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest;
-import org.eclipse.gmf.runtime.emf.type.core.requests.SetRequest;
-import org.eclipse.ui.PlatformUI;
 
-import de.uka.ipd.sdq.dialogs.selection.PalladioSelectEObjectDialog;
-import de.uka.ipd.sdq.pcm.resourcetype.ProcessingResourceType;
-import de.uka.ipd.sdq.pcm.resourcetype.ResourceRepository;
-import de.uka.ipd.sdq.pcm.seff.SeffPackage;
 
-/**
- * @author admin
- *
- */
+/** @author roman */
 public class ParametricResourceDemandEditHelperAdvice extends
 		AbstractEditHelperAdvice implements IEditHelperAdvice {
 
@@ -34,32 +19,6 @@ public class ParametricResourceDemandEditHelperAdvice extends
 	 */
 	@Override
 	protected ICommand getAfterConfigureCommand(ConfigureRequest request) {
-		
-		EObject resource = null;
-		ArrayList<Object> filterList = new ArrayList<Object>();
-		filterList.add(ResourceRepository.class);
-		filterList.add(ProcessingResourceType.class);
-
-		ArrayList<Object> additionalReferences = new ArrayList<Object>();
-		PalladioSelectEObjectDialog dialog = new PalladioSelectEObjectDialog(
-				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
-				filterList, 
-				additionalReferences,
-				request.getEditingDomain().getResourceSet());
-		dialog.setProvidedService(ProcessingResourceType.class);
-		dialog.open();
-		if (dialog.getResult() == null)
-			return new CanceledCommand();
-		if (!(dialog.getResult() instanceof ProcessingResourceType))
-			return new CanceledCommand();
-		resource = (ProcessingResourceType) dialog.getResult();
-		
-		ICommand cmd = new SetValueCommand(
-				new SetRequest(
-						request.getElementToConfigure(), 
-						SeffPackage.eINSTANCE.getParametricResourceDemand_RequiredResource_ParametricResourceDemand(),
-						resource));
-		return cmd;
+		return new ParametricResourceDemandConfigureCommand(request);
 	}
-
 }
