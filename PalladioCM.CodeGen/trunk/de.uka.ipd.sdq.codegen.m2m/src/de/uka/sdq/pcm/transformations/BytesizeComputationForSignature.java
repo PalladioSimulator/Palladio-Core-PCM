f823b0d4f5af6dac22bbb8e66101c48248acba5f
hkoziolek <heiko.koziolek@gmail.com>
2007-06-15 16:14:29 +0000

    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@3409 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- PalladioCM.CodeGen/trunk/de.uka.ipd.sdq.codegen.m2m/src/de/uka/sdq/pcm/transformations/BytesizeComputationForSignature.java
+++ PalladioCM.CodeGen/trunk/de.uka.ipd.sdq.codegen.m2m/src/de/uka/sdq/pcm/transformations/BytesizeComputationForSignature.java
@@ -7,24 +7,48 @@ import de.uka.ipd.sdq.pcm.repository.CompositeDataType;
 import de.uka.ipd.sdq.pcm.repository.DataType;
 import de.uka.ipd.sdq.pcm.repository.InnerDeclaration;
 import de.uka.ipd.sdq.pcm.repository.Parameter;
+import de.uka.ipd.sdq.pcm.repository.ParameterModifier;
 import de.uka.ipd.sdq.pcm.repository.PrimitiveDataType;
 import de.uka.ipd.sdq.pcm.repository.Signature;
 
 public class BytesizeComputationForSignature {
-
-	public String getBytesizeForSignature(Signature sig){
-		
+	
+	public enum Modifier{
+		IN,
+		OUT
+	}
+	
+	public String getBytesizeForSignature(Signature sig, Modifier mod){
 		StringBuffer result = new StringBuffer();
 		
 		EList<Parameter> params = sig.getParameters__Signature();
 		for (Parameter param : params){
 			DataType dataType = param.getDatatype__Parameter();
-			result.append(getCharacterisationString(dataType, param.getParameterName()));
+			ParameterModifier parMod = param.getModifier__Parameter();
+			if (mod == Modifier.IN){
+				if (parMod == ParameterModifier.IN || 
+					parMod == ParameterModifier.INOUT || 
+					parMod == ParameterModifier.NONE){
+					result.append(getCharacterisationString(dataType, param.getParameterName()));
+				}
+			} else {
+				if (parMod == ParameterModifier.INOUT || 
+					parMod == ParameterModifier.OUT){
+					result.append(getCharacterisationString(dataType, param.getParameterName()));
+				}
+			}
+		}
+
+		if (mod == Modifier.OUT){
+			DataType returnType = sig.getReturntype__Signature();	
+			if (returnType != null){
+				result.append(getCharacterisationString(returnType, sig.getServiceName()+".RETURN"));	
+			}
 		}
 		
 		int length = result.length();
 		if (length > 0) result.delete(length-3, length); // remove last " + "
-		
+
 		return result.toString();
 	}
 
@@ -34,15 +58,20 @@ public class BytesizeComputationForSignature {
 		if (dataType instanceof PrimitiveDataType){
 			result.append(name+".BYTESIZE + ");
 		} else if (dataType instanceof CollectionDataType){
-			result.append(name+".NUMBER_OF_ELEMENTS * "+
-					name+".INNER.BYTESIZE + ");
+			CollectionDataType collDataType = (CollectionDataType)dataType;
+			DataType innerDataType = collDataType.getInnerType_CollectionDataType();
+			String innerSize = getCharacterisationString(innerDataType, name + ".INNER");
+			result.append(name+".NUMBER_OF_ELEMENTS * "+ innerSize);
 		} else if (dataType instanceof CompositeDataType){
 			CompositeDataType compDataType = (CompositeDataType)dataType;
 			EList<InnerDeclaration> innerList = compDataType.getInnerDeclaration_CompositeDataType();
+			result.append("(");
 			for (InnerDeclaration decl : innerList){
 				DataType innerDataType = decl.getDatatype_InnerDeclaration();
 				result.append(getCharacterisationString(innerDataType, name+"."+decl.getEntityName()));
 			}
+			result.delete(result.length()-3, result.length());
+			result.append(") + ");
 		}
 		return result.toString();
 	}
--- PalladioCM.CodeGen/trunk/de.uka.ipd.sdq.codegen.m2m/src/de/uka/sdq/pcm/transformations/tests/BytesizeComputationForSignatureTest.java
+++ PalladioCM.CodeGen/trunk/de.uka.ipd.sdq.codegen.m2m/src/de/uka/sdq/pcm/transformations/tests/BytesizeComputationForSignatureTest.java
@@ -8,6 +8,7 @@ import de.uka.ipd.sdq.pcm.repository.CollectionDataType;
 import de.uka.ipd.sdq.pcm.repository.CompositeDataType;
 import de.uka.ipd.sdq.pcm.repository.InnerDeclaration;
 import de.uka.ipd.sdq.pcm.repository.Parameter;
+import de.uka.ipd.sdq.pcm.repository.ParameterModifier;
 import de.uka.ipd.sdq.pcm.repository.PrimitiveDataType;
 import de.uka.ipd.sdq.pcm.repository.PrimitiveTypeEnum;
 import de.uka.ipd.sdq.pcm.repository.RepositoryFactory;
@@ -16,27 +17,34 @@ import de.uka.sdq.pcm.transformations.BytesizeComputationForSignature;
 
 public class BytesizeComputationForSignatureTest {
 
-	private Signature testSig;
+	private Signature testSig1;
+	private Signature testSig2;
+	private Signature testSig3;
 	
 	@Before
 	public void setUp(){
 		RepositoryFactory factory = RepositoryFactory.eINSTANCE; 
 		
-		testSig = factory.createSignature();
+		testSig1 = factory.createSignature();
+		
 		
 		Parameter param1 = factory.createParameter();
 		PrimitiveDataType primDT = factory.createPrimitiveDataType();
 		primDT.setType(PrimitiveTypeEnum.INT);
 		param1.setParameterName("param1");
 		param1.setDatatype__Parameter(primDT);
-		testSig.getParameters__Signature().add(param1);
+		testSig1.getParameters__Signature().add(param1);
 		
 		Parameter param2 = factory.createParameter();
 		CollectionDataType collDT = factory.createCollectionDataType();
 		collDT.setEntityName("SomeCollection");
+		collDT.setInnerType_CollectionDataType(primDT);
+		
 		param2.setDatatype__Parameter(collDT);
 		param2.setParameterName("param2");
-		testSig.getParameters__Signature().add(param2);
+		//param2.setModifier__Parameter(ParameterModifier.OUT);
+		testSig1.getParameters__Signature().add(param2);
+		
 		
 		Parameter param3 = factory.createParameter();
 		CompositeDataType compDT = factory.createCompositeDataType();
@@ -73,21 +81,102 @@ public class BytesizeComputationForSignatureTest {
 		compDT.getInnerDeclaration_CompositeDataType().add(innerDecl4);
 		
 		
-		testSig.getParameters__Signature().add(param3);
+		testSig1.getParameters__Signature().add(param3);
 		
+	}
+	
+	@Before
+	public void setUp2(){
+		RepositoryFactory factory = RepositoryFactory.eINSTANCE; 
+		
+		testSig2 = factory.createSignature();
+		
+		Parameter param1 = factory.createParameter();
+		PrimitiveDataType primDT = factory.createPrimitiveDataType();
+		primDT.setType(PrimitiveTypeEnum.INT);
+		param1.setParameterName("param1");
+		param1.setDatatype__Parameter(primDT);
+		testSig2.getParameters__Signature().add(param1);
+		
+		Parameter param2 = factory.createParameter();
+		CollectionDataType collDT = factory.createCollectionDataType();
+		collDT.setEntityName("SomeCollection");
+		param2.setDatatype__Parameter(collDT);
+		param2.setParameterName("param2");
+		testSig2.getParameters__Signature().add(param2);
+		
+		
+		CollectionDataType collDT2 = factory.createCollectionDataType();
+		collDT2.setEntityName("SomeCollection2");
+		collDT2.setInnerType_CollectionDataType(primDT);
+		
+		collDT.setInnerType_CollectionDataType(collDT2);		
+		
+	}
+
+	@Before
+	public void setUp3(){
+		RepositoryFactory factory = RepositoryFactory.eINSTANCE; 
+		
+		testSig3 = factory.createSignature();
+		
+		Parameter param1 = factory.createParameter();
+		PrimitiveDataType primDT = factory.createPrimitiveDataType();
+		primDT.setType(PrimitiveTypeEnum.INT);
+		param1.setParameterName("param1");
+		param1.setDatatype__Parameter(primDT);
+		testSig3.getParameters__Signature().add(param1);
+		
+		Parameter param2 = factory.createParameter();
+		CollectionDataType collDT = factory.createCollectionDataType();
+		collDT.setEntityName("SomeCollection");
+		param2.setDatatype__Parameter(collDT);
+		param2.setParameterName("param2");
+		testSig3.getParameters__Signature().add(param2);
+		
+		CompositeDataType compDT = factory.createCompositeDataType();
+		compDT.setEntityName("SomeComposite");
+		
+		InnerDeclaration innerDecl1 = factory.createInnerDeclaration();
+		innerDecl1.setEntityName("innerParam1");
+		innerDecl1.setDatatype_InnerDeclaration(primDT);
+		compDT.getInnerDeclaration_CompositeDataType().add(innerDecl1);
+		
+		InnerDeclaration innerDecl2 = factory.createInnerDeclaration();
+		innerDecl2.setEntityName("innerParam2");
+		innerDecl2.setDatatype_InnerDeclaration(primDT);
+		compDT.getInnerDeclaration_CompositeDataType().add(innerDecl2);
+		
+		collDT.setInnerType_CollectionDataType(compDT);		
 	}
 	
 	@Test
 	public void testGetByteSize(){
 		BytesizeComputationForSignature blah = new BytesizeComputationForSignature();
-		String actual = blah.getBytesizeForSignature(testSig);
-		
+		String actual = blah.getBytesizeForSignature(testSig1, BytesizeComputationForSignature.Modifier.IN);
 		Assert.assertEquals("param1.BYTESIZE + " +
 				"param2.NUMBER_OF_ELEMENTS * param2.INNER.BYTESIZE + " + 
-				"param3.innerParam1.BYTESIZE + " + 
+				"(param3.innerParam1.BYTESIZE + " + 
 				"param3.innerParam2.BYTESIZE + " + 
 				"param3.innerParam3.NUMBER_OF_ELEMENTS * param3.innerParam3.INNER.BYTESIZE + " +
-				"param3.innerParam4.innerInnerParam1.BYTESIZE", actual);
+				"(param3.innerParam4.innerInnerParam1.BYTESIZE))", actual);
+	}		
+		
+	@Test
+	public void testGetByteSize2(){
+		BytesizeComputationForSignature foo = new BytesizeComputationForSignature();
+		String actual2 = foo.getBytesizeForSignature(testSig2, BytesizeComputationForSignature.Modifier.IN);
+		Assert.assertEquals("param1.BYTESIZE + " +
+				"param2.NUMBER_OF_ELEMENTS * param2.INNER.NUMBER_OF_ELEMENTS * param2.INNER.INNER.BYTESIZE", actual2);
+
 	}
 	
+	@Test
+	public void testGetByteSize3(){
+		BytesizeComputationForSignature foo = new BytesizeComputationForSignature();
+		String actual = foo.getBytesizeForSignature(testSig3, BytesizeComputationForSignature.Modifier.IN);
+		Assert.assertEquals("param1.BYTESIZE + " +
+				"param2.NUMBER_OF_ELEMENTS * (param2.INNER.innerParam1.BYTESIZE + param2.INNER.innerParam2.BYTESIZE)", actual);
+
+	}
 }
