4fbad16683a65382faf440fd7be3d4dddfa59faf
pmerkle <merkle@kit.edu>
2007-07-02 18:07:40 +0000

    SimulationWorkflow and ISimulationJob documentation added
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@3511 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- PalladioCM.CodeGen/trunk/de.uka.ipd.sdq.codegen.simucontroller/src/de/uka/ipd/sdq/codegen/simucontroller/workflow/SimulationWorkflow.java
+++ PalladioCM.CodeGen/trunk/de.uka.ipd.sdq.codegen.simucontroller/src/de/uka/ipd/sdq/codegen/simucontroller/workflow/SimulationWorkflow.java
@@ -9,6 +9,14 @@ import org.eclipse.core.runtime.NullProgressMonitor;
 
 import de.uka.ipd.sdq.codegen.simucontroller.SimuControllerPlugin;
 
+/**
+ * Class that manages the execution and rollback of a number of Jobs.
+ * The jobs are executed in the order they are added to the workflow.
+ * A rollback will affect all Jobs which have been executed.
+ * The given Progress Monitor will receive updates before and after a
+ * job is executed. 
+ */
+
 public class SimulationWorkflow {
 	private LinkedList<ISimulationJob> myJobs;
 	private Stack<ISimulationJob> myExecutedJobs;
@@ -25,6 +33,13 @@ public class SimulationWorkflow {
 		}
 	}
 
+	/**
+	 * Start the workflow. The jobs will be executed in the order
+	 * they were added to the workflow. Exceptions will be logged
+	 * and then thrown again.
+	 * 
+	 * @throws Exception
+	 */
 	public void run() throws Exception {
 		myMonitor.beginTask("Simulation Run", myJobs.size());
 
@@ -48,6 +63,12 @@ public class SimulationWorkflow {
 		myMonitor.done();
 	}
 
+	/**
+	 * Trigger a rollback of all jobs which have been executed.
+	 * Exceptions will be logged and thrown again.
+	 * 
+	 * @throws Exception
+	 */
 	public void rollback() throws Exception {
 		while (!myExecutedJobs.empty()) {
 			try {
@@ -59,6 +80,10 @@ public class SimulationWorkflow {
 		}
 	}
 
+	/**
+	 * adds a job to the workflow
+	 * @param job the job to add to the workflow
+	 */
 	public void addJob(ISimulationJob job) {
 		if (job != null) {
 			myJobs.add(job);
