64247ba8124b9c6e68ab3fee9667f31b15149f62
JensHappe <jens.happe@avenyou.com>
2007-08-09 14:13:04 +0000

    model refactoring
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@3684 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ Palladio.EMFProbfunctionModel/trunk/de.uka.ipd.sdq.probfunction/src/de/uka/ipd/sdq/probfunction/impl/ProbabilityFunctionImpl.java
@@ -0,0 +1,45 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.probfunction.impl;
+
+import de.uka.ipd.sdq.probfunction.ProbabilityFunction;
+import de.uka.ipd.sdq.probfunction.ProbfunctionPackage;
+
+import de.uka.ipd.sdq.units.impl.UnitCarryingElementImpl;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Probability Function</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public abstract class ProbabilityFunctionImpl extends UnitCarryingElementImpl implements ProbabilityFunction {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ProbabilityFunctionImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return ProbfunctionPackage.Literals.PROBABILITY_FUNCTION;
+	}
+
+} //ProbabilityFunctionImpl
