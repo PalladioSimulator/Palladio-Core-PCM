0d6fd17c3d76f49c79d0ad1680f5de7abd3bc610
JensHappe <jens.happe@avenyou.com>
2007-08-14 10:52:37 +0000

    moved Sensorframwork GUI
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@3724 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.SensorFrameworkGUI/tags/Experiment2/de.uka.ipd.sdq.sensorframework.visualisation.rvisualisation/src/de/uka/ipd/sdq/codegen/rvisualisation/reportitems/IReportItem.java
+++
@@ -1,8 +0,0 @@
-package de.uka.ipd.sdq.codegen.rvisualisation.reportitems;
-
-import de.uka.ipd.sdq.codegen.rvisualisation.actions.RInterface;
-
-public interface IReportItem {
-	void executeRCommands(RInterface re);
-	void visit(IReportRenderingVisitor v);
-}
