d7c915f2ec1994ae3e3b3c8ba0249233d7be3294
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2007-08-14 18:56:00 +0000

    Refactored the dialogs plugin into several smaller plugins to reduce the dependency hell caused by the old monolithic dialogs plugin
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@3782 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.usage/src/de/uka/ipd/sdq/pcm/gmf/usage/edit/policies/OpenStoExDialog.java
+++ Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.usage/src/de/uka/ipd/sdq/pcm/gmf/usage/edit/policies/OpenStoExDialog.java
@@ -1,9 +1,6 @@
 package de.uka.ipd.sdq.pcm.gmf.usage.edit.policies;
 
-import org.eclipse.emf.common.command.CommandWrapper;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.gef.EditPart;
-import org.eclipse.gef.EditPolicy;
 import org.eclipse.gef.Request;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gmf.runtime.diagram.ui.commands.ICommandProxy;
@@ -15,11 +12,9 @@ import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.ui.PlatformUI;
 
-import de.uka.ipd.sdq.dialogs.stoex.StochasticExpressionEditDialog;
+import de.uka.ipd.sdq.pcm.dialogs.stoex.StochasticExpressionEditDialog;
 import de.uka.ipd.sdq.pcm.parameter.VariableCharacterisation;
 import de.uka.ipd.sdq.pcm.repository.Parameter;
-import de.uka.ipd.sdq.pcm.seff.AbstractResourceDemandingAction;
-import de.uka.ipd.sdq.pcm.seff.ParametricResourceDemand;
 import de.uka.ipd.sdq.pcm.seff.ResourceDemandingSEFF;
 import de.uka.ipd.sdq.pcm.stochasticexpressions.PCMStoExPrettyPrintVisitor;
 import de.uka.ipd.sdq.stoex.RandomVariable;
