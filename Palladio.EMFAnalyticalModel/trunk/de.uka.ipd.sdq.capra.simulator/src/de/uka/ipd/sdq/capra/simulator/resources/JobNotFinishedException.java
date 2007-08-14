2a25d1dd89d2920632d590f9c4b343c51d8033f7
JensHappe <jens.happe@avenyou.com>
2007-08-14 14:30:08 +0000

    changes to new simulation
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@3760 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.EMFAnalyticalModel/trunk/de.uka.ipd.sdq.capra.simulator/src/de/uka/ipd/sdq/capra/simulator/resources/JobNotFinishedException.java
+++
@@ -1,7 +0,0 @@
-package de.uka.ipd.sdq.capra.simulator.resources;
-
-public class JobNotFinishedException extends Exception {
-
-	private static final long serialVersionUID = -4258565611596525854L;
-
-}
