47b59847169e40e5265ac37c9c65cb7b9c862cf1
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2007-08-14 18:55:52 +0000

    Refactored the dialogs plugin into several smaller plugins to reduce the dependency hell caused by the old monolithic dialogs plugin
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@3779 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.allocation.helper/src/de/uka/ipd/sdq/pcm/gmf/allocation/helper/AllocationContextEditHelperAdvice.java
+++ Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.allocation.helper/src/de/uka/ipd/sdq/pcm/gmf/allocation/helper/AllocationContextEditHelperAdvice.java
@@ -2,23 +2,18 @@ package de.uka.ipd.sdq.pcm.gmf.allocation.helper;
 
 import java.util.ArrayList;
 
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.gmf.runtime.common.core.command.CompositeCommand;
 import org.eclipse.gmf.runtime.common.core.command.ICommand;
 import org.eclipse.gmf.runtime.emf.type.core.commands.SetValueCommand;
 import org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelperAdvice;
 import org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest;
-import org.eclipse.gmf.runtime.emf.type.core.requests.CreateElementRequest;
 import org.eclipse.gmf.runtime.emf.type.core.requests.SetRequest;
 import org.eclipse.ui.PlatformUI;
 
-import de.uka.ipd.sdq.dialogs.selection.PalladioSelectEObjectDialog;
 import de.uka.ipd.sdq.pcm.allocation.AllocationPackage;
 import de.uka.ipd.sdq.pcm.core.composition.AssemblyContext;
 import de.uka.ipd.sdq.pcm.core.entity.EntityPackage;
-import de.uka.ipd.sdq.pcm.resourceenvironment.ResourceContainer;
+import de.uka.ipd.sdq.pcm.dialogs.selection.PalladioSelectEObjectDialog;
 
 public class AllocationContextEditHelperAdvice
 	extends AbstractEditHelperAdvice {
