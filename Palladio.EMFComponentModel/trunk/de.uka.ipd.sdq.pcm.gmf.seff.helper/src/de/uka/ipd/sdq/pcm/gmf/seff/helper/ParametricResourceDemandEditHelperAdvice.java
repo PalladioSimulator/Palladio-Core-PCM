6a6654436780e8025ccb431ecabbfeed91efba52
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2007-08-23 16:07:49 +0000

    Regenerated probfunction, stoex and pcm Removed all code in probfunction dealing with the old units model
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@3906 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.seff.helper/src/de/uka/ipd/sdq/pcm/gmf/seff/helper/ParametricResourceDemandEditHelperAdvice.java
+++ Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.seff.helper/src/de/uka/ipd/sdq/pcm/gmf/seff/helper/ParametricResourceDemandEditHelperAdvice.java
@@ -4,13 +4,8 @@
 package de.uka.ipd.sdq.pcm.gmf.seff.helper;
 
 import org.eclipse.gmf.runtime.common.core.command.ICommand;
-import org.eclipse.gmf.runtime.emf.type.core.ElementTypeRegistry;
-import org.eclipse.gmf.runtime.emf.type.core.IElementType;
-import org.eclipse.gmf.runtime.emf.type.core.commands.CreateElementCommand;
-import org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelperAdvice;
 import org.eclipse.gmf.runtime.emf.type.core.edithelper.IEditHelperAdvice;
 import org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest;
-import org.eclipse.gmf.runtime.emf.type.core.requests.CreateElementRequest;
 
 
 /** @author roman */
