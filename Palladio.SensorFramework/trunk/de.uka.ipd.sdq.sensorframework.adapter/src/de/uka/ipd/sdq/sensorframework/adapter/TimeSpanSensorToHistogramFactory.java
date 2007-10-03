96cbacee0eeac833da369affd7f8bb4a9f0ed3cc
JensHappe <jens.happe@avenyou.com>
2007-10-03 10:35:25 +0000

    added time series view
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@3996 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.SensorFramework/trunk/de.uka.ipd.sdq.sensorframework.adapter/src/de/uka/ipd/sdq/sensorframework/adapter/TimeSpanSensorToHistogramFactory.java
+++ Palladio.SensorFramework/trunk/de.uka.ipd.sdq.sensorframework.adapter/src/de/uka/ipd/sdq/sensorframework/adapter/TimeSpanSensorToHistogramFactory.java
@@ -1,11 +1,7 @@
 package de.uka.ipd.sdq.sensorframework.adapter;
 
-import java.util.Collection;
-
 import de.uka.ipd.sdq.codegen.simudatavisualisation.datatypes.Histogram;
 import de.uka.ipd.sdq.sensorframework.entities.SensorAndMeasurements;
-import de.uka.ipd.sdq.sensorframework.entities.StateSensor;
-import de.uka.ipd.sdq.sensorframework.entities.TimeSpanMeasurement;
 import de.uka.ipd.sdq.sensorframework.entities.TimeSpanSensor;
 
 public class TimeSpanSensorToHistogramFactory implements IAdapterFactory {
