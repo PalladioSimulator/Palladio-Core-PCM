cf60d91c89deb2a6fe4a56cce025e504c3ce449f
roman <dummy>
2007-10-25 16:29:39 +0000

    fix bug 300 (Anlegen einer ExternalCallAction fixen)
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@4032 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.seff.helper/src/de/uka/ipd/sdq/pcm/gmf/seff/helper/ExternalCallActionEditHelperAdvice.java
+++ Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.seff.helper/src/de/uka/ipd/sdq/pcm/gmf/seff/helper/ExternalCallActionEditHelperAdvice.java
@@ -5,13 +5,12 @@ package de.uka.ipd.sdq.pcm.gmf.seff.helper;
 
 import java.util.ArrayList;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gmf.runtime.common.core.command.ICommand;
-import org.eclipse.gmf.runtime.emf.type.core.commands.SetValueCommand;
 import org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelperAdvice;
 import org.eclipse.gmf.runtime.emf.type.core.edithelper.IEditHelperAdvice;
 import org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest;
-import org.eclipse.gmf.runtime.emf.type.core.requests.SetRequest;
 import org.eclipse.ui.PlatformUI;
 
 import de.uka.ipd.sdq.pcm.dialogs.selection.PalladioSelectEObjectDialog;
@@ -20,11 +19,10 @@ import de.uka.ipd.sdq.pcm.repository.Interface;
 import de.uka.ipd.sdq.pcm.repository.RepositoryPackage;
 import de.uka.ipd.sdq.pcm.repository.RequiredRole;
 import de.uka.ipd.sdq.pcm.repository.Signature;
-import de.uka.ipd.sdq.pcm.seff.SeffPackage;
 
 /**
- * @author admin
- *
+ * @author Roman Andrej
+ * 
  */
 public class ExternalCallActionEditHelperAdvice extends
 		AbstractEditHelperAdvice implements IEditHelperAdvice {
@@ -34,19 +32,19 @@ public class ExternalCallActionEditHelperAdvice extends
 	 */
 	@Override
 	protected ICommand getAfterConfigureCommand(ConfigureRequest request) {
-		EObject eOobject = searchBasicComponent(request.getElementToConfigure());
-		EObject signature = null;
+		EObject eObject = searchBasicComponent(request.getElementToConfigure());
+		Signature signature = null;
+		RequiredRole requiredRole = null;
 		ArrayList<Object> filterList = new ArrayList<Object>();
 		filterList.add(RequiredRole.class);
 		filterList.add(Interface.class);
 		filterList.add(Signature.class);
 		ArrayList<Object> additionalReferences = new ArrayList<Object>();
-		additionalReferences.add(RepositoryPackage.eINSTANCE.getRequiredRole_RequiredInterface__RequiredRole());
+		additionalReferences.add(RepositoryPackage.eINSTANCE
+				.getRequiredRole_RequiredInterface__RequiredRole());
 		PalladioSelectEObjectDialog dialog = new PalladioSelectEObjectDialog(
 				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
-				filterList, 
-				additionalReferences,
-				eOobject);
+				filterList, additionalReferences, eObject);
 		dialog.setProvidedService(Signature.class);
 		dialog.open();
 		if (dialog.getResult() == null)
@@ -54,13 +52,10 @@ public class ExternalCallActionEditHelperAdvice extends
 		if (!(dialog.getResult() instanceof Signature))
 			return new CanceledCommand();
 		signature = (Signature) dialog.getResult();
-		
-		ICommand cmd = new SetValueCommand(
-				new SetRequest(
-						request.getElementToConfigure(), 
-						SeffPackage.eINSTANCE.getExternalCallAction_CalledService_ExternalService(),
-						signature));
-		return cmd;
+		requiredRole = getRequiredRole(eObject, signature);
+
+		return new ExternalCallActionConfigureCommand(request, signature,
+				requiredRole);
 	}
 
 	private EObject searchBasicComponent(EObject elementToConfigure) {
@@ -69,5 +64,27 @@ public class ExternalCallActionEditHelperAdvice extends
 			o = o.eContainer();
 		return o;
 	}
+	
+	/**
+	 * The method supplies back the RequiredRole, from which the service call
+	 * originates.
+	 */
+	public RequiredRole getRequiredRole(EObject input, Signature signature) {
 
+		if (input instanceof BasicComponent) {
+			BasicComponent component = (BasicComponent) input;
+			EList<RequiredRole> roles = component
+					.getRequiredRoles_InterfaceRequiringEntity();
+
+			for (RequiredRole requiredRole : roles) {
+				Interface reqInterface = requiredRole
+						.getRequiredInterface__RequiredRole();
+				if (signature.getInterface_Signature().equals(reqInterface)) {
+					return requiredRole;
+				}
+			}
+		}
+
+		return null;
+	}
 }
