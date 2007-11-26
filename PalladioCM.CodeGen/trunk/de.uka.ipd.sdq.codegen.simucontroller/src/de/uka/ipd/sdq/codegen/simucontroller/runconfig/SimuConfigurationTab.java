1340f6cc56c1f6499f49974b5fb976ec1ef303c9
hkoziolek <heiko.koziolek@gmail.com>
2007-11-26 15:07:25 +0000

    Added Sensitivity Analysis Parameter Selection in RunConfig
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@4078 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- PalladioCM.CodeGen/trunk/de.uka.ipd.sdq.codegen.simucontroller/src/de/uka/ipd/sdq/codegen/simucontroller/runconfig/SimuConfigurationTab.java
+++ PalladioCM.CodeGen/trunk/de.uka.ipd.sdq.codegen.simucontroller/src/de/uka/ipd/sdq/codegen/simucontroller/runconfig/SimuConfigurationTab.java
@@ -14,6 +14,8 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
 import de.uka.ipd.sdq.codegen.runconfig.tabs.ConfigurationTab;
 import de.uka.ipd.sdq.codegen.runconfig.tabs.ConstantsContainer;
@@ -60,6 +62,56 @@ public class SimuConfigurationTab extends ConfigurationTab {
 				SimuConfigurationTab.this.updateLaunchConfigurationDialog();
 			}
 		});
+		
+		final Group sensitivityAnalysisParametersGroup = new Group(container, SWT.NONE);
+		sensitivityAnalysisParametersGroup.setText("Sensitivity Analysis Parameters");
+		final GridData gd_sensitivityAnalysisParametersGroup = new GridData(SWT.FILL, SWT.CENTER, true, false);
+		sensitivityAnalysisParametersGroup.setLayoutData(gd_sensitivityAnalysisParametersGroup);
+		final GridLayout gridLayout_1 = new GridLayout();
+		gridLayout_1.numColumns = 3;
+		sensitivityAnalysisParametersGroup.setLayout(gridLayout_1);
+
+		final Label variableToAdaptLabel = new Label(sensitivityAnalysisParametersGroup, SWT.NONE);
+		final GridData gd_variableToAdaptLabel = new GridData(SWT.RIGHT, SWT.CENTER, false, false);
+		variableToAdaptLabel.setLayoutData(gd_variableToAdaptLabel);
+		variableToAdaptLabel.setText("Variable:");
+
+		final Text text = new Text(sensitivityAnalysisParametersGroup, SWT.BORDER);
+		text.setEnabled(false);
+		text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
+		final Button selectVariableButton = new Button(sensitivityAnalysisParametersGroup, SWT.NONE);
+		selectVariableButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(final SelectionEvent e) {
+			}
+		});
+		selectVariableButton.setText("Select Variable...");
+
+		final Composite composite = new Composite(sensitivityAnalysisParametersGroup, SWT.NONE);
+		final GridData gd_composite = new GridData(SWT.FILL, SWT.CENTER, false, false, 3, 1);
+		composite.setLayoutData(gd_composite);
+		final GridLayout gridLayout_2 = new GridLayout();
+		gridLayout_2.numColumns = 6;
+		composite.setLayout(gridLayout_2);
+
+		final Label minimumLabel = new Label(composite, SWT.NONE);
+		minimumLabel.setText("Minimum:");
+
+		final Text text_1 = new Text(composite, SWT.BORDER);
+		text_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
+		final Label maximumLabel = new Label(composite, SWT.NONE);
+		maximumLabel.setText("Maximum:");
+
+		final Text text_2 = new Text(composite, SWT.BORDER);
+		text_2.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
+		final Label stepWidthLabel = new Label(composite, SWT.NONE);
+		stepWidthLabel.setText("Step Width:");
+
+		final Text text_3 = new Text(composite, SWT.BORDER);
+		text_3.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		
 	}
 
 	/* (non-Javadoc)
