7b10105f49e1aca74a0726506d71bd923d0ddcfc
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2007-12-19 05:57:28 +0000

    Regenerated PCM, corrected some Fork issues
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@4127 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.edit/src/de/uka/ipd/sdq/pcm/seff/provider/ForkedBehaviourItemProvider.java
+++ Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.edit/src/de/uka/ipd/sdq/pcm/seff/provider/ForkedBehaviourItemProvider.java
@@ -73,36 +73,6 @@ public class ForkedBehaviourItemProvider
 	}
 
 	/**
-	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
-	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
-	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object) {
-		if (childrenFeatures == null) {
-			super.getChildrenFeatures(object);
-			childrenFeatures.add(SeffPackage.Literals.FORKED_BEHAVIOUR__NUMBER_OF_REPLICAS_FORKED_BEHAVIOUR);
-		}
-		return childrenFeatures;
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	protected EStructuralFeature getChildFeature(Object object, Object child) {
-		// Check the type of the specified child object and return the proper feature to use for
-		// adding (see {@link AddCommand}) it as a child.
-
-		return super.getChildFeature(object, child);
-	}
-
-	/**
 	 * This returns ForkedBehaviour.gif.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
@@ -134,12 +104,6 @@ public class ForkedBehaviourItemProvider
 	@Override
 	public void notifyChanged(Notification notification) {
 		updateChildren(notification);
-
-		switch (notification.getFeatureID(ForkedBehaviour.class)) {
-			case SeffPackage.FORKED_BEHAVIOUR__NUMBER_OF_REPLICAS_FORKED_BEHAVIOUR:
-				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
-				return;
-		}
 		super.notifyChanged(notification);
 	}
 
@@ -153,11 +117,6 @@ public class ForkedBehaviourItemProvider
 	@Override
 	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
 		super.collectNewChildDescriptors(newChildDescriptors, object);
-
-		newChildDescriptors.add
-			(createChildParameter
-				(SeffPackage.Literals.FORKED_BEHAVIOUR__NUMBER_OF_REPLICAS_FORKED_BEHAVIOUR,
-				 CoreFactory.eINSTANCE.createPCMRandomVariable()));
 	}
 
 	/**
