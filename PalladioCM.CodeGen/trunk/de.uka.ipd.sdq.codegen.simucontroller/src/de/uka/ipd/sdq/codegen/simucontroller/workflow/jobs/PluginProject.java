22d788bd718dda7b2e1057dce27ae231d4a1a31c
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2007-12-21 20:51:58 +0000

    Finally removed old SensorFactory from SimuCom
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@4140 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- PalladioCM.CodeGen/trunk/de.uka.ipd.sdq.codegen.simucontroller/src/de/uka/ipd/sdq/codegen/simucontroller/workflow/jobs/PluginProject.java
+++ PalladioCM.CodeGen/trunk/de.uka.ipd.sdq.codegen.simucontroller/src/de/uka/ipd/sdq/codegen/simucontroller/workflow/jobs/PluginProject.java
@@ -270,6 +270,7 @@ public class PluginProject {
 		out.println("Require-Bundle: org.eclipse.ui,"); //$NON-NLS-1$
 		out.println(" org.eclipse.core.runtime,"); //$NON-NLS-1$
 		out.println(" de.uka.ipd.sdq.simucomframework,"); //$NON-NLS-1$
+		out.println(" de.uka.ipd.sdq.sensorframework,"); //$NON-NLS-1$
 		out.println(" de.uka.ipd.sdq.simucomframework.variables,"); //$NON-NLS-1$
 		out.println(" org.apache.log4j,"); //$NON-NLS-1$
 		out.println(" de.desmoj,"); //$NON-NLS-1$
