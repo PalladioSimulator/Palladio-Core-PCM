25cdb9a39bf02e9d62b8658da5be8e457c080f4b
miku2000 <miku2000@users.noreply.github.com>
2008-04-16 16:25:06 +0000

    cleaned, sorted commented
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@4829 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- FileShare/src/de/uka/ipd/sdq/palladiofileshare/businesslogic/BusinessRunner.java
+++ FileShare/src/de/uka/ipd/sdq/palladiofileshare/businesslogic/BusinessRunner.java
@@ -1,43 +1,69 @@
 package de.uka.ipd.sdq.palladiofileshare.businesslogic;
 
+/**
+ * @author Michael
+ * The class NOT containing the main logic (see BusinessCore for it... WHY so much delegating?)
+ */
 public class BusinessRunner implements Runnable{
 
 	private BusinessCore businessCore;
-	private byte[][] inputFiles;
 	private int[] fileTypes;
+	private byte[][] inputFiles;
+	private boolean measure;
+	private long uploadId;
 	
 	public BusinessRunner() {
 		this.businessCore = new BusinessCore();
+		this.fileTypes = null;
+		this.inputFiles = null;
+		this.measure = false;
+		this.uploadId = -1L;
 	}
 	
-	/**
-	 * delegate
-	 * @param inputStreams
-	 * @param fileType
-	 */
-	public void uploadFiles(byte[][] inputStreams, int[] fileTypes) {
-		this.businessCore.uploadFiles(inputStreams, fileTypes);		
-	}
-
-	@Override
-	public void run() {
-		uploadFiles(inputFiles, fileTypes);
+	public int[] getFileType() {
+		return fileTypes;
 	}
 
 	public byte[][] getInputStreams() {
 		return inputFiles;
 	}
 
-	public void setInputStreams(byte[][] inputFiles) {
-		this.inputFiles = inputFiles;
-	}
-
-	public int[] getFileType() {
-		return fileTypes;
+	@Override
+	public void run() {
+		uploadFiles(inputFiles, fileTypes, this.measure, this.uploadId);
 	}
 
 	public void setFileTypes(int[] fileTypes) {
 		this.fileTypes = fileTypes;
+	}
+
+	public void setInputStreams(byte[][] inputFiles) {
+		this.inputFiles = inputFiles;
+	}
+
+	public void setMeasure(boolean measure) {
+		this.measure = measure;
+	}
+
+	public void setUploadId(long uploadId) {
+		this.uploadId = uploadId;
+	}
+
+	/**
+	 * delegate to BusinessCore
+	 * @param inputStreams
+	 * @param fileType
+	 */
+	public void uploadFiles(
+			byte[][] inputStreams, 
+			int[] fileTypes,
+			boolean measure,
+			long uploadId) {
+		this.businessCore.uploadFiles(
+				inputStreams, 
+				fileTypes,
+				measure,
+				uploadId);		
 	}
 	
 }
