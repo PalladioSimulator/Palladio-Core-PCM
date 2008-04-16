d68dafc57a012837de6d02a45bcfcd2e93d28346
miku2000 <miku2000@users.noreply.github.com>
2008-04-16 16:19:37 +0000

    sorted and cleaned
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@4826 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- FileShare/src/de/uka/ipd/sdq/palladiofileshare/businesslogic/ExistingFilesDatabase.java
+++ FileShare/src/de/uka/ipd/sdq/palladiofileshare/businesslogic/ExistingFilesDatabase.java
@@ -7,25 +7,29 @@ import org.apache.log4j.Logger;
 
 import de.uka.ipd.sdq.palladiofileshare.businesslogic.util.Util;
 
+/**
+ * @author Klaus Krogmann
+ *
+ */
 public class ExistingFilesDatabase {
 
-	private static ExistingFilesDatabase singleton;
+	private static Set<Integer> fileHashes;
 	private static Logger logger = Logger.getLogger(ExistingFilesDatabase.class);
 	private static final int numberOfFilesInDB = 100000;
-	private static Set<Integer> fileHashes;
-	
-	private ExistingFilesDatabase() {	
-		logger.debug("Init ExistingFilesDatabase start");
-		fileHashes = new HashSet<Integer>();
-		fileHashes = Util.initHashSetWithInteger(numberOfFilesInDB);
-		logger.debug("Init ExistingFilesDatabase end");
-	}
+	private static ExistingFilesDatabase singleton;
 	
 	public static ExistingFilesDatabase getSingleton() {
 		if(singleton == null) {
 			singleton = new ExistingFilesDatabase();
 		} 
 		return singleton;		
+	}
+	
+	private ExistingFilesDatabase() {	
+		logger.debug("Init ExistingFilesDatabase start");
+		fileHashes = new HashSet<Integer>();
+		fileHashes = Util.initHashSetWithInteger(numberOfFilesInDB);
+		logger.debug("Init ExistingFilesDatabase end");
 	}
 	
 	public boolean addNewFileHash(byte[] fileHash) {
