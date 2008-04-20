abaa3b3db95593076877ec708d86888c0e2e877f
miku2000 <miku2000@users.noreply.github.com>
2008-04-20 11:46:01 +0000

    added file IDs
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@4861 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- FileShare/src/de/uka/ipd/sdq/palladiofileshare/testdriver/TestDataStruct.java
+++ FileShare/src/de/uka/ipd/sdq/palladiofileshare/testdriver/TestDataStruct.java
@@ -1,25 +1,49 @@
 package de.uka.ipd.sdq.palladiofileshare.testdriver;
 
 public class TestDataStruct {
+	
+	private String[] inputFileIds;
 	private byte[][] inputFiles;
 	private int[] inputFileTypes;
+	private long uploadId;
 	
 	public TestDataStruct() {
+		try {
+			Thread.sleep(50);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		this.uploadId = System.currentTimeMillis();
 		this.inputFiles = new byte[0][];
+		this.inputFileIds = new String[0];
 		this.inputFileTypes = new int[0];
+	}
+	
+	public String[] getInputFileIds() {
+		return inputFileIds;
 	}
 	
 	public byte[][] getInputFiles() {
 		return inputFiles;
 	}
-	public void setInputFiles(byte[][] inputFiles) {
-		this.inputFiles = inputFiles;
-	}
+	
 	public int[] getInputFileTypes() {
 		return inputFileTypes;
 	}
+	
+	public long getUploadId() {
+		return uploadId;
+	}
+	
+	public void setInputFileIds(String[] inputFileIds) {
+		this.inputFileIds = inputFileIds;
+	}
+
+	public void setInputFiles(byte[][] inputFiles) {
+		this.inputFiles = inputFiles;
+	}	
+
 	public void setInputFileTypes(int[] inputFileTypes) {
 		this.inputFileTypes = inputFileTypes;
 	}	
-
 }
