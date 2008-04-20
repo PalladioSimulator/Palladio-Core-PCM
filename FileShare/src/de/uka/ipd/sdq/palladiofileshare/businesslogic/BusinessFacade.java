3ad4acfad3965a5b23c83fb744e05d8a16596dc9
miku2000 <miku2000@users.noreply.github.com>
2008-04-20 12:35:24 +0000

    renamed (and sorted) fields and variables. added file IDs.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@4867 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- FileShare/src/de/uka/ipd/sdq/palladiofileshare/businesslogic/BusinessFacade.java
+++ FileShare/src/de/uka/ipd/sdq/palladiofileshare/businesslogic/BusinessFacade.java
@@ -3,21 +3,27 @@ package de.uka.ipd.sdq.palladiofileshare.businesslogic;
 public class BusinessFacade implements IBusinessFacade {		
 		
 	/**
-	 * 
+	 * Starts a new thread to which the upload is delegated. 
+	 * The thread is executing BusinessRunner logic.
 	 * @param inputFiles
 	 * @param fileTypes type per file in array inputfiles
 	 */
 	public void uploadFiles(
 			long uploadId,
 			byte[][] inputFiles, 
-			int[] fileTypes,
-			boolean measure) {
+			String[] inputFileIds,
+			int[] inputFileTypes,
+			boolean measure,
+			boolean monitor) {
 		
-		BusinessRunner businessFacade = new BusinessRunner();
-		businessFacade.setUploadId(uploadId);
-		businessFacade.setMeasure(measure);
-		businessFacade.setInputStreams(inputFiles);
-		businessFacade.setFileTypes(fileTypes);
+		BusinessRunner businessFacade;
+		businessFacade= new BusinessRunner(
+				uploadId,
+				inputFiles,
+				inputFileIds,
+				inputFileTypes,
+				measure,
+				monitor);
 		
         Thread t = new Thread(businessFacade);        
         t.start();
