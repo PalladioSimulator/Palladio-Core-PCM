c70e93d87a35ae721b504c012a8b1e46f93f9f40
miku2000 <miku2000@users.noreply.github.com>
2008-04-21 08:37:33 +0000

    changed path to have access to input files
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@4892 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- FileShare/runPalladioFileShare_MK_Lenovo_Sun.bat
+++ FileShare/runPalladioFileShare_MK_Lenovo_Sun.bat
@@ -1,2 +1,4 @@
-cd bin
-"C:\Programme\Java\jdk1.6.0_03\bin\java" -server -classpath .;%CLASSPATH%;.\..\lib\JbcParser.jar;.\..\lib\log4j-1.2.15.jar;.\..\lib\opencsv-1.8.jar;.\..\lib\TestMeasuring.jar de.uka.ipd.sdq.palladiofileshare.testdriver.TestDriver
+REM cd bin
+REM"C:\Programme\Java\jdk1.6.0_03\bin\java" -server -classpath .;%CLASSPATH%;.\..\lib\JbcParser.jar;.\..\lib\log4j-1.2.15.jar;.\..\lib\opencsv-1.8.jar;.\..\lib\TestMeasuring.jar de.uka.ipd.sdq.palladiofileshare.testdriver.TestDriver
+REM cd ..
+"C:\Programme\Java\jdk1.6.0_03\bin\java" -server -classpath .;.\bin;%CLASSPATH%;.\lib\JbcParser.jar;.\lib\log4j-1.2.15.jar;.\lib\opencsv-1.8.jar;.\lib\TestMeasuring.jar de.uka.ipd.sdq.palladiofileshare.testdriver.TestDriver
