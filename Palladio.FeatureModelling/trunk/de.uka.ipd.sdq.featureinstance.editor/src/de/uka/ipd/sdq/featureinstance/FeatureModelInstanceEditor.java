11b05f625b91b7f930769b9f5af58d3793e34008
GrischeLiebel <grischa@chalmers.se>
2008-07-07 09:31:17 +0000

    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@5066 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.FeatureModelling/trunk/de.uka.ipd.sdq.featureinstance.editor/src/de/uka/ipd/sdq/featureinstance/FeatureModelInstanceEditor.java
+++ Palladio.FeatureModelling/trunk/de.uka.ipd.sdq.featureinstance.editor/src/de/uka/ipd/sdq/featureinstance/FeatureModelInstanceEditor.java
@@ -1,155 +1,122 @@
 package de.uka.ipd.sdq.featureinstance;
 
-import java.util.Iterator;
-import java.util.Map;
+import java.util.HashMap;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.command.BasicCommandStack;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
+import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
+import org.eclipse.emf.edit.provider.ReflectiveItemProviderAdapterFactory;
+import org.eclipse.emf.edit.provider.resource.ResourceItemProviderAdapterFactory;
+import org.eclipse.emf.edit.ui.util.EditUIUtil;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.CheckboxTreeViewer;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.part.MultiPageEditorPart;
+import org.eclipse.ui.views.properties.PropertySheetPage;
 
 import de.uka.ipd.sdq.featuremodel.Feature;
 import de.uka.ipd.sdq.featuremodel.FeatureDiagram;
-import de.uka.ipd.sdq.featuremodel.Node;
-import de.uka.ipd.sdq.featuremodel.impl.FeatureGroupImpl;
-import de.uka.ipd.sdq.featuremodel.impl.FeatureImpl;
+import de.uka.ipd.sdq.featuremodel.provider.featuremodelItemProviderAdapterFactory;
+import de.uka.ipd.sdq.identifier.provider.IdentifierItemProviderAdapterFactory;
 
 public class FeatureModelInstanceEditor extends MultiPageEditorPart {
+	
+	protected AdapterFactoryEditingDomain editingDomain;
 
-    private int currentXPos = 10;
-    private int currentYPos = 20;
-    private int height = 0;
-    private URI fileUri;
+	protected ComposedAdapterFactory adapterFactory;
+	
+	protected CheckboxTreeViewer treeViewer;
+	protected PropertySheetPage propertySheetPage;
+
+	protected void initializeEditingDomain () {
+		adapterFactory = new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
+
+		adapterFactory.addAdapterFactory(new ResourceItemProviderAdapterFactory());
+		adapterFactory.addAdapterFactory(new featuremodelItemProviderAdapterFactory());
+		adapterFactory.addAdapterFactory(new IdentifierItemProviderAdapterFactory());
+		adapterFactory.addAdapterFactory(new ReflectiveItemProviderAdapterFactory());
+
+		// Create the command stack that will notify this editor as commands are executed.
+		//
+		BasicCommandStack commandStack = new BasicCommandStack();
+
+		editingDomain = new AdapterFactoryEditingDomain(adapterFactory, commandStack, new HashMap<Resource, Boolean>());
+	}
+    private Composite comp;
+
+	public FeatureModelInstanceEditor() {
+		super();
+		initializeEditingDomain();
+	}
 	
 	@Override
 	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
 		if (!(input instanceof IFileEditorInput)) {
             throw new PartInitException("Invalid");
 		}
-		else {
-			IFile selected = ((IFileEditorInput)input).getFile();
-			fileUri = URI.createPlatformResourceURI(selected.getFullPath().toString(), true); 
-		}
         super.init(site, input);
 	}
-
 	
 	@Override
 	protected void createPages() {
-		Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
-		Map<String, Object> m = reg.getExtensionToFactoryMap();
-		m.put("FeatDiag", new XMIResourceFactoryImpl());
+		
+		URI resourceURI = EditUIUtil.getURI(getEditorInput());
+		Resource resource = null;
+		try {
+			// Load the resource through the editing domain.
+			//
+			resource = editingDomain.getResourceSet().getResource(resourceURI, true);
+		}
+		catch (Exception e) {
+			resource = editingDomain.getResourceSet().getResource(resourceURI, false);
+		}
 
-		// Obtain a new resource set
-		ResourceSet resSet = new ResourceSetImpl();
-		
-		// Get the resource
-		Resource resource = resSet.getResource(fileUri, true);
-		
+		FeatureDiagram newDiagram;
+
 		// Get the first model element and cast it to the right type
-		FeatureDiagram newDiagram = (FeatureDiagram) resource.getContents().get(0);
+		newDiagram = (FeatureDiagram) resource.getContents().get(0);
+
+		comp = new Composite(getContainer(), SWT.NONE);
+		comp.setLayout(new FillLayout());
+
+		createViewer(newDiagram);
+
+		int index = addPage(comp);
+		setPageText(index, "");
+        
+        //Hide editor tabs
+		if (getContainer() instanceof CTabFolder) {
+				((CTabFolder)getContainer()).setTabHeight(1);
+				Point point = getContainer().getSize();
+				getContainer().setSize(point.x, point.y + 6);
+		}
+	}
+	
+	public void createViewer (FeatureDiagram root) {
+		treeViewer = new CheckboxTreeViewer(comp);
 		
-		Composite composite = new Composite(getContainer(), SWT.NONE);
-        composite.setLayout(null);
-        
-        composite = paintNodes(composite, (FeatureImpl)newDiagram.getRootFeature());
-        
-		int index = addPage(composite);
-        setPageText(index, "MUH");
-	}
+		treeViewer.setContentProvider(new TreeContentProvider());
+		treeViewer.setLabelProvider(new TreeLabelProvider());		
+		treeViewer.setInput(root);
+		treeViewer.expandAll();
+		getSite().setSelectionProvider(treeViewer);
 
-	public Composite paintNodes (Composite comp, FeatureImpl rootN) {
-		if ((currentXPos-10)/40 > height) {
-			height = (currentXPos-10)/40;
-		}
-		if (!(rootN.getClass().getSimpleName().equals("FeatureGroup") || rootN.getClass().getSimpleName().equals("FeatureGroupImpl"))) {
-			Button currentButton = new Button(comp, SWT.CHECK);
-			currentButton.setText(rootN.getName());
-			currentButton.setSize(currentButton.computeSize(SWT.DEFAULT, SWT.DEFAULT));
-			if (rootN.isIsMandatory()) {
-				currentButton.setSelection(true);
-			}
-			currentButton.setLocation(currentXPos, currentYPos);
-			currentYPos += 20;
-			
-			EList<Node> children = rootN.getChildren();
-			Iterator<Node> iter = children.iterator();
-			currentXPos += 40;
-			while (iter.hasNext()) {
-				Node next = iter.next();
-				if (next.getClass().getSimpleName().equals("FeatureGroupImpl") || next.getClass().getSimpleName().equals("FeatureGroup")) {
-					paintFeatureGroup(comp, next);
-				}
-				else {
-					paintNodes(comp,(FeatureImpl)next);
-				}
-			}
-			currentXPos -= 40;
-			return comp;
-		}
-		else {
-			paintFeatureGroup(comp, rootN);
-		}
-		return comp;
-	}
-	
-	public Composite paintFeatureGroup (Composite comp, Node groupNode) {
-		FeatureGroupImpl group = (FeatureGroupImpl)groupNode;
-		int min = group.getMin();
-		int max = group.getMax();
-		Composite nodeGroup = new Composite(comp,SWT.NONE);
-
-		if (min == 1 && max == 1) {
-			nodeGroup.setLocation(currentXPos, currentYPos);
-			currentXPos = 0;
-			currentYPos = 0;
-			
-			EList<Feature> nodeList = group.getChildren();
-			Iterator<Feature> nodeIter = nodeList.iterator();
-
-			while (nodeIter.hasNext()) {
-				Feature currentFeature = nodeIter.next();
-				Button currentButton = new Button(nodeGroup, SWT.RADIO);
-				currentButton.setText(currentFeature.getName());
-				currentButton.setSize(currentButton.computeSize(SWT.DEFAULT, SWT.DEFAULT));
-				if (currentFeature.isIsMandatory()) {
-					currentButton.setSelection(true);
-				}
-				currentButton.setLocation(currentXPos, currentYPos);
-				currentYPos += 20;
-				
-				EList<Node> children = currentFeature.getChildren();
-				Iterator<Node> iter = children.iterator();
-				currentXPos += 40;
-				while (iter.hasNext()) {
-					Node next = iter.next();
-					if (next.getClass().getSimpleName().equals("FeatureGroupImpl") || next.getClass().getSimpleName().equals("FeatureGroup")) {
-						paintFeatureGroup(nodeGroup, next);
-					}
-					else {
-						paintNodes(nodeGroup,(FeatureImpl)next);
-					}
-				}
-				currentXPos -= 40;
-			}
-			nodeGroup.setSize(nodeGroup.computeSize(SWT.DEFAULT, SWT.DEFAULT));
-		}
-	
-		currentXPos = nodeGroup.getLocation().x;
-		currentYPos = currentYPos + nodeGroup.getLocation().y;
-		return comp;
 	}
 	
 	@Override
@@ -171,9 +138,96 @@ public class FeatureModelInstanceEditor extends MultiPageEditorPart {
 		return false;
 	}
 
+}
 
-	public FeatureModelInstanceEditor() {
-		super();
+class TreeLabelProvider implements ILabelProvider {
+
+	@Override
+	public Image getImage(Object element){
+		ImageDescriptor descriptor = null;
+		descriptor = FeatureModelInstancePlugin.getImageDescriptor("Feature.gif");
+		Image image = descriptor.createImage();
+		return image;
 	}
 
+	@Override
+	public String getText(Object element) {
+		if (element instanceof Feature) {
+			return ((Feature)element).getName();
+		}
+		return null;
+	}
+
+	@Override
+	public void addListener(ILabelProviderListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isLabelProperty(Object element, String property) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removeListener(ILabelProviderListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
+
+class TreeContentProvider implements ITreeContentProvider {
+
+	@Override
+	public Object[] getChildren(Object parentElement) {
+		if (parentElement instanceof Feature) {
+			return ((Feature)parentElement).getChildren().toArray();
+		}
+		else if (parentElement instanceof FeatureDiagram) {
+			Object [] newArray = new Object[1];
+			newArray[0] = ((FeatureDiagram)parentElement).getRootFeature();
+			return newArray;
+		}
+			return null;
+	}
+
+	@Override
+	public Object getParent(Object element) {
+		if (element instanceof Feature) {
+		}
+		return null;
+	}
+
+	@Override
+	public boolean hasChildren(Object element) {
+		if (element instanceof Feature) {
+			return (((Feature) element).getChildren().size() != 0);	
+		}
+		return false;
+	}
+
+	@Override
+	public Object[] getElements(Object inputElement) {
+		return getChildren(inputElement);
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+	@Override
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+				
+	}
+	
+}
+
+
