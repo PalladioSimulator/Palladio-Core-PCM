8e84a2995dc1d577028c2b5960df877d4d8de152
GrischeLiebel <grischa@chalmers.se>
2008-07-10 15:28:56 +0000

    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@5068 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.FeatureModelling/trunk/de.uka.ipd.sdq.featureinstance.editor/src/de/uka/ipd/sdq/featureinstance/FeatureModelInstanceEditor.java
+++ Palladio.FeatureModelling/trunk/de.uka.ipd.sdq.featureinstance.editor/src/de/uka/ipd/sdq/featureinstance/FeatureModelInstanceEditor.java
@@ -1,10 +1,13 @@
 package de.uka.ipd.sdq.featureinstance;
 
 import java.util.HashMap;
+import java.util.Iterator;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.common.command.BasicCommandStack;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
 import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
@@ -30,8 +33,12 @@ import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.part.MultiPageEditorPart;
 import org.eclipse.ui.views.properties.PropertySheetPage;
 
+import de.uka.ipd.sdq.featureconfig.Configuration;
+import de.uka.ipd.sdq.featureconfig.FeatureConfig;
 import de.uka.ipd.sdq.featuremodel.Feature;
 import de.uka.ipd.sdq.featuremodel.FeatureDiagram;
+import de.uka.ipd.sdq.featuremodel.FeatureGroup;
+import de.uka.ipd.sdq.featuremodel.Node;
 import de.uka.ipd.sdq.featuremodel.provider.featuremodelItemProviderAdapterFactory;
 import de.uka.ipd.sdq.identifier.provider.IdentifierItemProviderAdapterFactory;
 
@@ -80,17 +87,37 @@ public class FeatureModelInstanceEditor extends MultiPageEditorPart {
 		Resource resource = null;
 		try {
 			// Load the resource through the editing domain.
-			//
 			resource = editingDomain.getResourceSet().getResource(resourceURI, true);
 		}
 		catch (Exception e) {
 			resource = editingDomain.getResourceSet().getResource(resourceURI, false);
 		}
 
-		FeatureDiagram newDiagram;
-
-		// Get the first model element and cast it to the right type
-		newDiagram = (FeatureDiagram) resource.getContents().get(0);
+		FeatureDiagram newDiagram = null;
+		Object newResource = null;
+		
+		//TODO Returns empty List for *.featureconfig
+		EList<EObject> tempList = resource.getContents();
+		Iterator<EObject> tempIterator = tempList.iterator();
+		if (tempIterator.hasNext()) {
+			newResource = tempIterator.next();
+		}
+		
+		if (newResource instanceof FeatureDiagram) {
+			newDiagram = (FeatureDiagram) newResource;
+		}
+		else if (newResource instanceof Configuration) {
+			Configuration config = (Configuration) newResource;
+			if (config.getConfigOverrides().isEmpty()) {
+				newDiagram = (FeatureDiagram)config.getDefaultConfig().getReferencedObject();
+			}
+			else {
+				Iterator<FeatureConfig> iter = config.getConfigOverrides().iterator();
+				if (iter.hasNext()) {
+					newDiagram = (FeatureDiagram)iter.next().getReferencedObject();
+				}
+			}
+		}
 
 		comp = new Composite(getContainer(), SWT.NONE);
 		comp.setLayout(new FillLayout());
@@ -117,8 +144,37 @@ public class FeatureModelInstanceEditor extends MultiPageEditorPart {
 		treeViewer.expandAll();
 		getSite().setSelectionProvider(treeViewer);
 
+		if (root != null) {
+			treeViewer.setGrayChecked(root.getRootFeature(), true);
+
+			//Gray FeatureGroups
+			Node curRoot = root.getRootFeature();
+			grayFeatureGroups(curRoot);
+		}
+
 	}
 	
+	public void grayFeatureGroups (Node curRoot) {
+		if (curRoot != null) {
+			if (curRoot instanceof FeatureGroup) {
+				treeViewer.setGrayed(curRoot, true);
+				EList<Feature> nodes = ((FeatureGroup)curRoot).getChildren();
+				Iterator<Feature> nodesIter = nodes.iterator();
+				while (nodesIter.hasNext()) {
+					grayFeatureGroups(nodesIter.next());
+				}
+			}
+			else if(curRoot instanceof Feature) {
+				EList<Node> nodes = ((Feature)curRoot).getChildren();
+				Iterator<Node> nodesIter = nodes.iterator();
+				while (nodesIter.hasNext()) {
+					grayFeatureGroups(nodesIter.next());
+				}
+			}
+			
+		
+		}
+	}
 	@Override
 	public void doSave(IProgressMonitor monitor) {
 		// TODO Auto-generated method stub
@@ -145,7 +201,12 @@ class TreeLabelProvider implements ILabelProvider {
 	@Override
 	public Image getImage(Object element){
 		ImageDescriptor descriptor = null;
-		descriptor = FeatureModelInstancePlugin.getImageDescriptor("Feature.gif");
+		if (element instanceof Feature) {
+			descriptor = FeatureModelInstancePlugin.getImageDescriptor("Feature.gif");
+		}
+		else if (element instanceof FeatureGroup) {
+			descriptor = FeatureModelInstancePlugin.getImageDescriptor("FeatureGroup.gif");
+		}
 		Image image = descriptor.createImage();
 		return image;
 	}
@@ -154,6 +215,9 @@ class TreeLabelProvider implements ILabelProvider {
 	public String getText(Object element) {
 		if (element instanceof Feature) {
 			return ((Feature)element).getName();
+		}
+		else if (element instanceof FeatureGroup) {
+			return "FeatureGroup: " + ((FeatureGroup)element).getName();
 		}
 		return null;
 	}
@@ -196,6 +260,9 @@ class TreeContentProvider implements ITreeContentProvider {
 			newArray[0] = ((FeatureDiagram)parentElement).getRootFeature();
 			return newArray;
 		}
+		else if (parentElement instanceof FeatureGroup) {
+			return ((FeatureGroup)parentElement).getChildren().toArray();
+		}
 			return null;
 	}
 
@@ -210,6 +277,9 @@ class TreeContentProvider implements ITreeContentProvider {
 	public boolean hasChildren(Object element) {
 		if (element instanceof Feature) {
 			return (((Feature) element).getChildren().size() != 0);	
+		}
+		else if (element instanceof FeatureGroup) {
+			return (((FeatureGroup) element).getChildren().size() != 0);	
 		}
 		return false;
 	}
