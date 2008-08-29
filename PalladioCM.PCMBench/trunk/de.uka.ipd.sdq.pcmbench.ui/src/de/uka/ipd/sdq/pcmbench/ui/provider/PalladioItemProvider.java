40b6fdf41ceaddc89fed0815a685703c180c25e3
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2008-08-29 12:36:56 +0000

    Changed label provider for create commands, see bug 401
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@5270 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- PalladioCM.PCMBench/trunk/de.uka.ipd.sdq.pcmbench.ui/src/de/uka/ipd/sdq/pcmbench/ui/provider/PalladioItemProvider.java
+++ PalladioCM.PCMBench/trunk/de.uka.ipd.sdq.pcmbench.ui/src/de/uka/ipd/sdq/pcmbench/ui/provider/PalladioItemProvider.java
@@ -3,14 +3,22 @@
  */
 package de.uka.ipd.sdq.pcmbench.ui.provider;
 
+import java.util.Collection;
 import java.util.List;
 
+import org.eclipse.emf.common.EMFPlugin;
+import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.common.notify.Adapter;
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.emf.common.ui.celleditor.ExtendedDialogCellEditor;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.edit.EMFEditPlugin;
+import org.eclipse.emf.edit.command.CommandParameter;
+import org.eclipse.emf.edit.command.CreateChildCommand;
 import org.eclipse.emf.edit.command.SetCommand;
+import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
@@ -34,6 +42,7 @@ import de.uka.ipd.sdq.pcm.resourceenvironment.ProcessingResourceSpecification;
 import de.uka.ipd.sdq.pcm.seff.AbstractResourceDemandingAction;
 import de.uka.ipd.sdq.pcm.seff.ParametricResourceDemand;
 import de.uka.ipd.sdq.pcm.seff.ResourceDemandingSEFF;
+import de.uka.ipd.sdq.pcmbench.ui.PCMBenchUIPlugin;
 import de.uka.ipd.sdq.stoex.RandomVariable;
 import de.uka.ipd.sdq.stoex.StoexPackage;
 
@@ -48,7 +57,8 @@ public class PalladioItemProvider extends ItemProviderDecorator implements
 	ITreeItemContentProvider,	
 	IItemLabelProvider,	
 	IItemPropertySource,
-	Adapter {
+	Adapter,
+	CreateChildCommand.Helper {
 
 	/**
 	 * Default constructor
@@ -127,9 +137,88 @@ public class PalladioItemProvider extends ItemProviderDecorator implements
 	}
 
 	@Override
-	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
-		// TODO Auto-generated method stub
-		return super.getPropertyDescriptors(object);
+	public String getCreateChildDescription(Object owner, Object feature,
+			Object child, Collection<?> selection) {
+		return ((CreateChildCommand.Helper)getDecoratedItemProvider()).getCreateChildDescription(owner, feature, child, selection);
 	}
+
+	@Override
+	public Object getCreateChildImage(Object owner, Object feature,
+			Object child, Collection<?> selection) {
+		return ((CreateChildCommand.Helper)getDecoratedItemProvider()).getCreateChildImage(owner, feature, child, selection);
+	}
+
+	@Override
+	public Collection<?> getCreateChildResult(Object child) {
+		return ((CreateChildCommand.Helper)getDecoratedItemProvider()).getCreateChildResult(child);
+	}
+
+	@Override
+	public String getCreateChildText(Object owner, Object feature,
+			Object child, Collection<?> selection) {
+		if (selection.size() == 1) {
+			if (owner instanceof EObject) {
+				if (feature instanceof EReference) {
+					String a = palladioConvention(((EReference)feature).getName());
+					String b = ((EObject)child).eClass().getName();
+					if (a.replaceAll(" ","").toLowerCase().equals(b.toLowerCase())) {
+						return PCMBenchUIPlugin.INSTANCE.getString(
+								"_UI_CreateChild_palladio_simple", 
+								new Object[] {b}, 
+								true);
+					} else
+						return PCMBenchUIPlugin.INSTANCE.getString(
+							"_UI_CreateChild_palladio", 
+							new Object[] {a,b}, 
+							true);
+				}
+			}
+		}
+		return ((CreateChildCommand.Helper)getDecoratedItemProvider()).getCreateChildText(owner, feature, child, selection);
+	}
+
+	private String palladioConvention(String originalName) {
+		if (originalName.indexOf("_") >= 0) {
+			originalName = originalName.substring(0, originalName.indexOf("_"));
+			if (originalName.length() >= 1) {
+				String newOriginalName = Character.toUpperCase(originalName.charAt(0))+""; 
+				for (int i=1; i < originalName.length(); i++) {
+					if (Character.isUpperCase(originalName.charAt(i))) {
+						newOriginalName += " ";
+					}
+					newOriginalName += originalName.charAt(i);
+				}
+				originalName = newOriginalName;
+			}
+		}
+		return originalName;
+	}
+
+	@Override
+	public String getCreateChildToolTipText(Object owner, Object feature,
+			Object child, Collection<?> selection) {
+		return ((CreateChildCommand.Helper)getDecoratedItemProvider()).getCreateChildToolTipText(owner, feature, child, selection);
+	}
+
+	@Override
+	public Command createCommand(Object object, EditingDomain domain,
+			Class<? extends Command> commandClass,
+			CommandParameter commandParameter) {
+		if (commandClass == CreateChildCommand.class)
+	    {
+	      CommandParameter newChildParameter = (CommandParameter)commandParameter.getValue();
+	      return new CreateChildCommand
+	          (domain,
+	           commandParameter.getEOwner(), 
+	           newChildParameter.getEStructuralFeature(), 
+	           newChildParameter.getValue(),
+	           newChildParameter.getIndex(),
+	           commandParameter.getCollection(),
+	           this);      
+	    }
+		return super.createCommand(object, domain, commandClass, commandParameter);
+	}
+	
+	
 	
 }
