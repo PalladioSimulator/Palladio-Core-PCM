f69a97a8d7045d014128d8b6388daf95e60ede0b
kelsaka <krogmann@fzi.de>
2008-09-15 13:27:33 +0000

    minor update to comform to new feature model (current templates do not support generics any more)
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@5361 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- PalladioCM.CodeGen/trunk/de.uka.ipd.sdq.pcm.codegen.m2m/src/de/uka/sdq/pcm/transformations/FeatureUtils.java
+++ PalladioCM.CodeGen/trunk/de.uka.ipd.sdq.pcm.codegen.m2m/src/de/uka/sdq/pcm/transformations/FeatureUtils.java
@@ -6,6 +6,7 @@ import org.eclipse.emf.common.util.EList;
 import de.uka.ipd.sdq.featureconfig.ConfigNode;
 import de.uka.ipd.sdq.featureconfig.ConfigState;
 import de.uka.ipd.sdq.featureconfig.FeatureConfig;
+import de.uka.ipd.sdq.featuremodel.Feature;
 
 public class FeatureUtils {
 
@@ -14,7 +15,7 @@ public class FeatureUtils {
 		Iterator<ConfigNode> it = nodes.iterator();
 		while (it.hasNext()){
 			ConfigNode configNode = it.next();
-			if (configNode.getOrigin().getName().equals(featureName)) {
+			if ( ((Feature)configNode.getOrigin()).getName().equals(featureName)) {
 				return configNode.getConfigState() == ConfigState.SELECTED;
 			}
 		}
