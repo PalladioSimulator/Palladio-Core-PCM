75dbc088ecde17f5818ea4486da6a146cc0cf204
groenda <groenda@fzi.de>
2008-10-15 16:25:11 +0000

    Documented SensorFrameworkDataset
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@5443 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.SensorFramework/trunk/de.uka.ipd.sdq.sensorframework/src-man/de/uka/ipd/sdq/sensorframework/SensorFrameworkDataset.java
+++ Palladio.SensorFramework/trunk/de.uka.ipd.sdq.sensorframework/src-man/de/uka/ipd/sdq/sensorframework/SensorFrameworkDataset.java
@@ -6,9 +6,10 @@ import java.util.Collections;
 
 import de.uka.ipd.sdq.sensorframework.entities.dao.IDAOFactory;
 
-/**TODO add documentation
+/**Provides access to the single dataset of the SensorFramework.
+ * Used to display all datasources in the SimuBench Perspective.
  * @author Steffen Becker
- *
+ * @author groenda
  */
 public class SensorFrameworkDataset {
 	private static SensorFrameworkDataset singleton = new SensorFrameworkDataset();
@@ -18,37 +19,51 @@ public class SensorFrameworkDataset {
 	private SensorFrameworkDataset() {
 	}
 	
+	/**
+	 * @return the one central instance containing all datasources.
+	 */
 	public static SensorFrameworkDataset singleton() {
 		return singleton;
 	}
 	
-	public Collection<IDAOFactory> getDataSources() {
+	/**Returns all data sources in the dataset.
+	 * @return datasources.
+	 */
+	public synchronized Collection<IDAOFactory> getDataSources() {
 		return Collections.unmodifiableCollection(datasources);
 	}
 	
-	public IDAOFactory getDataSourceByID(long id){
+	/**Retrieve a datasource by its dynamic numeric identifier.
+	 * @param id id of the datasource.
+	 * @return datasource.
+	 */
+	public synchronized IDAOFactory getDataSourceByID(long id){
 		for (IDAOFactory f:datasources)
 			if (f.getID() == id)
 				return f;
 		return null;
 	}
 
+	/**Add a provided datasource.
+	 * @param factory the datasource.
+	 */
 	public synchronized void addDataSource(IDAOFactory factory) {
 		for(IDAOFactory f : datasources) {
 			if (f.getID() == factory.getID())
-				throw new RuntimeException("Attemped to add Datasource with an ID already existing in the Sensorframework Dataset");
-			if (f.getID() == nextID && factory.getID() <= 0)
-				throw new RuntimeException("Attemped to add Datasource with an ID already existing in the Sensorframework Dataset");
+				throw new RuntimeException("Attemped to add Datasource with an ID already existing in the Sensorframework Dataset.");
+			if (f.getID() == nextID && factory.getID() == IDAOFactory.ID_NOT_SET)
+				throw new RuntimeException("Conflict of IDs occurred when attemping to add a new datasource to the Sensorframework Dataset.");
 		}
 		
 		datasources.add(factory);
-		if (factory.getID() <= 0) {
+		if (factory.getID() == IDAOFactory.ID_NOT_SET) {
 			factory.setID(nextID);
 			nextID += 1;
 		}
 		else {
 			if(factory.getID() >= nextID) {
-				this.nextID = 0;
+				// reset nextID completely
+				this.nextID = Long.MIN_VALUE;
 				for (IDAOFactory f : datasources) {
 					if (f.getID() >= nextID)
 						this.nextID = f.getID() + 1;
@@ -57,12 +72,18 @@ public class SensorFrameworkDataset {
 		}
 	}
 
-	public void removeDataSource(IDAOFactory factory) {
+	/**Removes a datasource from the current Dataset.
+	 * @param factory the datasource itself to remove.
+	 */
+	public synchronized void removeDataSource(IDAOFactory factory) {
 		factory.finalizeAndClose();
 		datasources.remove(factory);
 	}
 
-	public void reload() {
+	/** Reloads all known datasources.
+	 *  Throws runtime exeception if some datasources are faulty.
+	 */
+	public synchronized void reload() {
 		String errorMessage = "";
 		boolean failed = false;
 		for (IDAOFactory f : datasources) {
