1db885e75502ddfc25c531fc48a8be919511635f
Glorinka <lucia.happe@icloud.com>
2008-11-18 21:37:17 +0000

    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@5614 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ Palladio.DesignSpaceExploration/de.uka.ipd.sdq.dsexplore/src/de/uka/ipd/sdq/dsexplore/launch/CIPTabGroup.java
@@ -0,0 +1,39 @@
+package de.uka.ipd.sdq.cip.launch;
+
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+
+import de.uka.ipd.sdq.codegen.runconfig.tabs.FeatureOptionsTab;
+import de.uka.ipd.sdq.codegen.runconfig.tabs.FileNamesInputTab;
+import de.uka.ipd.sdq.codegen.simucontroller.runconfig.SimuComConfigurationTab;
+import de.uka.ipd.sdq.codegen.simucontroller.runconfig.SimuConfigurationTab;
+
+/**
+ * Creates a Tab Group for the launch configuration.
+ * @author Lucka
+ *
+ */
+public class CIPTabGroup extends AbstractLaunchConfigurationTabGroup {
+
+	/**
+	 * Actually creates the tabs.
+	 * @param dialog not needed
+	 * @param mode not needed
+	 */
+	@Override
+	public void createTabs(final ILaunchConfigurationDialog dialog, final String mode) {
+		ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] {
+				new FileNamesInputTab(), // Default tab
+				
+				//TODO: Get these depending on available plugins / availaible analyses
+				new SimuComConfigurationTab(),
+				new SimuConfigurationTab(),
+				new FeatureOptionsTab(),
+				new CIPOptionsTab()
+		};
+		setTabs(tabs);
+
+	}
+
+}
