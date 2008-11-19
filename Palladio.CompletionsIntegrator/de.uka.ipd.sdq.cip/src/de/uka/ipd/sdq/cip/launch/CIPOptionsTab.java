6ba6234457f9b029276301b02893ba8c04f0fedc
Glorinka <lucia.happe@icloud.com>
2008-11-19 13:48:55 +0000

    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@5624 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ Palladio.CompletionsIntegrator/de.uka.ipd.sdq.cip/src/de/uka/ipd/sdq/cip/launch/CIPOptionsTab.java
@@ -0,0 +1,124 @@
+package de.uka.ipd.sdq.cip.launch;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Text;
+
+import de.uka.ipd.sdq.codegen.runconfig.RunConfigPlugin;
+import de.uka.ipd.sdq.cip.CIPluginActivator;
+
+public class CIPOptionsTab extends AbstractLaunchConfigurationTab {
+	
+	private Text maximumIterations; 
+	
+	private Image icon;
+
+	@Override
+	public void createControl(Composite parent) {
+		final ModifyListener modifyListener = new ModifyListener() {
+
+			public void modifyText(ModifyEvent e) {
+				CIPOptionsTab.this.setDirty(true);
+				CIPOptionsTab.this.updateLaunchConfigurationDialog();
+			}
+		};
+		
+		Composite container = new Composite(parent, SWT.NONE);
+		setControl(container);
+		container.setLayout(new GridLayout());
+		
+		final Group maximumIterationsGroup = new Group(container, SWT.NONE);
+		final GridLayout glmaximumIterationsGroup = new GridLayout();
+		glmaximumIterationsGroup.numColumns = 3;
+		maximumIterationsGroup.setLayout(glmaximumIterationsGroup);
+		maximumIterationsGroup.setText("Maximum iterations: ");
+		maximumIterationsGroup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER,
+				true, false));
+
+		
+		maximumIterations = new Text(maximumIterationsGroup, SWT.SINGLE	| SWT.BORDER);
+		maximumIterations.setEnabled(true);
+		maximumIterations.addModifyListener(modifyListener);
+
+	}
+
+	@Override
+	public String getName() {
+		return "CIP Options";
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.debug.ui.AbstractLaunchConfigurationTab#getImage()
+	 */
+	public Image getImage() {
+		if (icon == null){
+			icon = CIPluginActivator.getImageDescriptor("icons/completion.gif").createImage(); 
+		}
+		return icon;
+	}
+
+	@Override
+	public void initializeFrom(ILaunchConfiguration configuration) {
+		try {
+			maximumIterations.setText(configuration.getAttribute(
+					CIPConstantsContainer.MAX_ITERATIONS, ""));
+		} catch (CoreException e) {
+			RunConfigPlugin.errorLogger(getName(),"maximum iterations", e.getMessage());
+		}
+
+	}
+
+	@Override
+	public void performApply(ILaunchConfigurationWorkingCopy configuration) {
+		configuration.setAttribute(
+				CIPConstantsContainer.MAX_ITERATIONS,
+				maximumIterations.getText());
+
+	}
+
+	@Override
+	public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
+
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.debug.ui.AbstractLaunchConfigurationTab#isValid(org.eclipse.debug.core.ILaunchConfiguration)
+	 */
+	@Override
+	public boolean isValid(ILaunchConfiguration launchConfig) {
+		setErrorMessage(null);
+
+		// check this.maxIterations
+		if (this.maximumIterations.getText().length() != 0) {
+			try {
+				Integer.parseInt(this.maximumIterations.getText());
+			} catch (NumberFormatException e) {
+				setErrorMessage("Maximum iterations must be an integer value or empty.");
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * Disposes the image for the DSE options tab.
+	 * 
+	 * @see ILaunchConfigurationTab#dispose()
+	 */
+	public void dispose() {
+		icon.dispose();
+	}
+
+}
