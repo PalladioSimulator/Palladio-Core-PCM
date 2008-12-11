4d55ed23e556e3801f18ea98d8d7eabae4371f50
kelsaka <krogmann@fzi.de>
2008-12-11 13:31:01 +0000

    updated featuremodel according to latest RSA version in SVN
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@5791 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ Palladio.FeatureModelling/trunk/de.uka.ipd.sdq.featureconfig.editor/src/de/uka/ipd/sdq/featureconfig/presentation/FeatureconfigEditorPlugin.java
@@ -0,0 +1,97 @@
+/**
+ * (c) 2008 SDQ, Universität Karlsruhe (TH), Germany
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.featureconfig.presentation;
+
+import de.uka.ipd.sdq.featuremodel.provider.FeaturemodelEditPlugin;
+
+import org.eclipse.emf.common.EMFPlugin;
+
+import org.eclipse.emf.common.ui.EclipseUIPlugin;
+
+import org.eclipse.emf.common.util.ResourceLocator;
+
+/**
+ * This is the central singleton for the Featureconfig editor plugin.
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public final class FeatureconfigEditorPlugin extends EMFPlugin {
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final FeatureconfigEditorPlugin INSTANCE = new FeatureconfigEditorPlugin();
+	
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private static Implementation plugin;
+
+	/**
+	 * Create the instance.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public FeatureconfigEditorPlugin() {
+		super
+			(new ResourceLocator [] {
+				FeaturemodelEditPlugin.INSTANCE,
+			});
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	@Override
+	public ResourceLocator getPluginResourceLocator() {
+		return plugin;
+	}
+	
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	public static Implementation getPlugin() {
+		return plugin;
+	}
+	
+	/**
+	 * The actual implementation of the Eclipse <b>Plugin</b>.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static class Implementation extends EclipseUIPlugin {
+		/**
+		 * Creates an instance.
+		 * <!-- begin-user-doc -->
+		 * <!-- end-user-doc -->
+		 * @generated
+		 */
+		public Implementation() {
+			super();
+	
+			// Remember the static instance.
+			//
+			plugin = this;
+		}
+	}
+
+}
