3bb5cdf89c4d07acf2248e3bf7933fa36e4fa8f5
FranzBrosch <FranzBrosch>
2008-12-11 13:53:32 +0000

    (fb) added InternalActionFailedException
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@5796 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ PalladioCM.CodeGen/branches/QoSA2009/de.uka.ipd.sdq.simucomframework/src/de/uka/ipd/sdq/simucomframework/exceptions/InternalActionFailedException.java
@@ -0,0 +1,18 @@
+package de.uka.ipd.sdq.simucomframework.exceptions;
+
+/**
+ * This exception indicates that a failure has occurred during execution of an
+ * InternalAction within a simulation run.
+ * 
+ * @author brosch
+ */
+public class InternalActionFailedException extends RuntimeException {
+
+	/**
+	 * Creates a new InternalActionFailedException. No additional information is
+	 * carried by the exception, so far.
+	 */
+	public InternalActionFailedException() {
+		super();
+	}
+}
