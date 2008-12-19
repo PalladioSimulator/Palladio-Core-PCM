22af50fc757ba27dbfc873d62c0bd097eda8b3e8
FranzBrosch <FranzBrosch>
2008-12-19 15:52:54 +0000

    (fb) For having a first version of a reliability simulation, I adapt the already existing version of the simulation.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@5832 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ PalladioCM.CodeGen/branches/QoSA2009/de.uka.ipd.sdq.codegen.workflow/src/de/uka/ipd/sdq/codegen/workflow/RollbackFailedException.java
@@ -0,0 +1,17 @@
+package de.uka.ipd.sdq.codegen.workflow;
+
+@SuppressWarnings("serial")
+public class RollbackFailedException extends Exception {
+
+	public RollbackFailedException() {
+		super();
+	}
+	
+	public RollbackFailedException(String string) {
+		super(string);
+	}	
+	
+	public RollbackFailedException(String string, Throwable e) {
+		super(string, e);
+	}
+}
