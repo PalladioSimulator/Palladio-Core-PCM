bd1f7d64bdd7c45377e75fbbaf5b410a6d594403
mhauck <hauck@fzi.de>
2008-12-23 15:06:29 +0000

    Branch pcm rel3.0 for resource environment development
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@5839 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ branches/subsystem-resourcemodel/de.uka.ipd.sdq.pcm/src/de/uka/ipd/sdq/pcm/usagemodel/Stop.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2007 by SDQ, IPD, University of Karlsruhe, Germany
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.pcm.usagemodel;
+
+import java.util.Map;
+
+import org.eclipse.emf.common.util.DiagnosticChain;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Stop</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see de.uka.ipd.sdq.pcm.usagemodel.UsagemodelPackage#getStop()
+ * @model
+ * @generated
+ */
+public interface Stop extends AbstractUserAction {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	String copyright = "Copyright 2007 by SDQ, IPD, University of Karlsruhe, Germany";
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * <!-- begin-model-doc -->
+	 * self.successor.oclIsUndefined()
+	 * <!-- end-model-doc -->
+	 * @model
+	 * @generated
+	 */
+	boolean StopHasNoSuccessor(DiagnosticChain diagnostics, Map<Object, Object> context);
+
+} // Stop
---
+++ branches/subsystem-resourcemodel/de.uka.ipd.sdq.pcm/src/de/uka/ipd/sdq/pcm/usagemodel/impl/StartImpl.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright 2007 by SDQ, IPD, University of Karlsruhe, Germany
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.pcm.usagemodel.impl;
+
+import java.util.Map;
+
+import org.eclipse.emf.common.util.BasicDiagnostic;
+import org.eclipse.emf.common.util.Diagnostic;
+import org.eclipse.emf.common.util.DiagnosticChain;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
+import org.eclipse.emf.ecore.util.EObjectValidator;
+import org.eclipse.emf.ocl.expressions.OCLExpression;
+import org.eclipse.emf.ocl.expressions.util.EvalEnvironment;
+import org.eclipse.emf.ocl.expressions.util.ExpressionsUtil;
+import org.eclipse.emf.ocl.parser.Environment;
+import org.eclipse.emf.ocl.parser.ParserException;
+import org.eclipse.emf.ocl.query.Query;
+import org.eclipse.emf.ocl.query.QueryFactory;
+
+import de.uka.ipd.sdq.pcm.usagemodel.Start;
+import de.uka.ipd.sdq.pcm.usagemodel.UsagemodelPackage;
+import de.uka.ipd.sdq.pcm.usagemodel.util.UsagemodelValidator;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Start</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class StartImpl extends AbstractUserActionImpl implements Start {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final String copyright = "Copyright 2007 by SDQ, IPD, University of Karlsruhe, Germany";
+
+	/**
+	 * The parsed OCL expression for the definition of the '{@link #StartHasNoPredecessor <em>Start Has No Predecessor</em>}' invariant constraint.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #StartHasNoPredecessor
+	 * @generated
+	 */
+	private static OCLExpression StartHasNoPredecessorInvOCL;
+	
+	private static final String OCL_ANNOTATION_SOURCE = "http://www.eclipse.org/emf/2002/GenModel";
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected StartImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return UsagemodelPackage.Literals.START;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public boolean StartHasNoPredecessor(DiagnosticChain diagnostics, Map<Object, Object> context) {
+		if (StartHasNoPredecessorInvOCL == null) {
+			Environment env = ExpressionsUtil.createClassifierContext(eClass());
+			
+			
+			String body = "self.predecessor.oclIsUndefined() ";
+			
+			try {
+				StartHasNoPredecessorInvOCL = ExpressionsUtil.createInvariant(env, body, true);
+			} catch (ParserException e) {
+				throw new UnsupportedOperationException(e.getLocalizedMessage());
+			}
+		}
+		
+		Query query = QueryFactory.eINSTANCE.createQuery(StartHasNoPredecessorInvOCL);
+		EvalEnvironment evalEnv = new EvalEnvironment();
+		query.setEvaluationEnvironment(evalEnv);
+		
+		if (!query.check(this)) {
+			if (diagnostics != null) {
+				diagnostics.add
+					(new BasicDiagnostic
+						(Diagnostic.ERROR,
+						 UsagemodelValidator.DIAGNOSTIC_SOURCE,
+						 UsagemodelValidator.START__START_HAS_NO_PREDECESSOR,
+						 EcorePlugin.INSTANCE.getString("_UI_GenericInvariant_diagnostic", new Object[] { "StartHasNoPredecessor", EObjectValidator.getObjectLabel(this, context) }),
+						 new Object [] { this }));
+			}
+			return false;
+		}
+		return true;
+		
+	}
+
+} //StartImpl
