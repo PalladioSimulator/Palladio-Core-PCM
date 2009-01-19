b78fc29430a2d4a7d6514c41e7cc86d0514dee73
kelsaka <krogmann@fzi.de>
2009-01-19 17:33:59 +0000

    Share project "de.uka.ipd.sdq.pcm.resultdecorator" into "svn://klaus@i43pc13.ipd.uka.de/code" Share project "de.uka.ipd.sdq.pcm.resultdecorator.tests" into "svn://klaus@i43pc13.ipd.uka.de/code" Share project "de.uka.ipd.sdq.pcm.resultdecorator.edit" into "svn://klaus@i43pc13.ipd.uka.de/code" Share project "de.uka.ipd.sdq.pcm.resultdecorator.feature" into "svn://klaus@i43pc13.ipd.uka.de/code" Share project "de.uka.ipd.sdq.pcm.resultdecorator.editor" into "svn://klaus@i43pc13.ipd.uka.de/code"
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@5882 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ de.uka.ipd.sdq.pcm.resultdecorator/trunk/de.uka.ipd.sdq.pcm.resultdecorator/src/de/uka/ipd/sdq/pcm/resultdecorator/repository/util/repositorydecoratorResourceFactoryImpl.java
@@ -0,0 +1,56 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.pcm.resultdecorator.repository.util;
+
+import org.eclipse.emf.common.util.URI;
+
+import org.eclipse.emf.ecore.resource.Resource;
+
+import org.eclipse.emf.ecore.resource.impl.ResourceFactoryImpl;
+
+import org.eclipse.emf.ecore.xmi.XMLResource;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Resource Factory</b> associated with the package.
+ * <!-- end-user-doc -->
+ * @see de.uka.ipd.sdq.pcm.resultdecorator.repository.util.repositorydecoratorResourceImpl
+ * @generated
+ */
+public class repositorydecoratorResourceFactoryImpl extends ResourceFactoryImpl {
+	/**
+	 * Creates an instance of the resource factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public repositorydecoratorResourceFactoryImpl() {
+		super();
+	}
+
+	/**
+	 * Creates an instance of the resource.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Resource createResource(URI uri) {
+		XMLResource result = new repositorydecoratorResourceImpl(uri);
+		result.getDefaultSaveOptions().put(XMLResource.OPTION_EXTENDED_META_DATA, Boolean.TRUE);
+		result.getDefaultLoadOptions().put(XMLResource.OPTION_EXTENDED_META_DATA, Boolean.TRUE);
+
+		result.getDefaultSaveOptions().put(XMLResource.OPTION_SCHEMA_LOCATION, Boolean.TRUE);
+
+		result.getDefaultLoadOptions().put(XMLResource.OPTION_USE_ENCODED_ATTRIBUTE_STYLE, Boolean.TRUE);
+		result.getDefaultSaveOptions().put(XMLResource.OPTION_USE_ENCODED_ATTRIBUTE_STYLE, Boolean.TRUE);
+
+		result.getDefaultLoadOptions().put(XMLResource.OPTION_USE_LEXICAL_HANDLER, Boolean.TRUE);
+		return result;
+	}
+
+} //repositorydecoratorResourceFactoryImpl
