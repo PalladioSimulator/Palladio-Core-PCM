54d28073e77581d60603f21af704fe20760e20db
pmerkle <merkle@kit.edu>
2009-01-26 03:01:18 +0000

    Added methods to restore an LQN Ecore model from it's XMI representation
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@5932 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- PalladioCM.PCMSolver/trunk/de.uka.ipd.sdq.pcmsolver/src/de/uka/ipd/sdq/pcmsolver/transformations/pcm2lqn/LqnXmlHandler.java
+++ PalladioCM.PCMSolver/trunk/de.uka.ipd.sdq.pcmsolver/src/de/uka/ipd/sdq/pcmsolver/transformations/pcm2lqn/LqnXmlHandler.java
@@ -49,6 +49,43 @@ public class LqnXmlHandler {
 		fixXMLFile(fileName);
 		
 	}
+	
+	/**
+	 * Restores the corresponding Ecore model, previously serialized via
+	 * {@link #saveModelToXMI(String)}.
+	 * 
+	 * @param fileName
+	 * @return A representation of the model object 'LQN Model Type'; null if
+	 *         the file don't exists or when there were problems reading the
+	 *         file.
+	 */
+	public static LqnModelType loadModelFromXMI(String fileName) {
+		// Revert the Ecore tag names
+		revertXMLFile(fileName);
+		
+		// Create a resource set.
+		ResourceSet resourceSet = new ResourceSetImpl();
+
+		// Register the default resource factory -- only needed for stand-alone!
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap()
+				.put(Resource.Factory.Registry.DEFAULT_EXTENSION,
+						new XMIResourceFactoryImpl());
+
+		URI fileURI = URI.createFileURI(new File(fileName).getAbsolutePath());
+		
+		Resource resource = resourceSet.createResource(fileURI);
+		
+		LqnModelType lqnModel = null;
+		try {
+			resource.load(Collections.EMPTY_MAP);
+			lqnModel = (LqnModelType) resource.getContents().get(0);
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+		}
+		
+		// Can be null if there were problems retrieving the model
+		return lqnModel;
+	}
 
 	/**
 	 * Changes the XML-tag names to their original names. They had
@@ -113,6 +150,67 @@ public class LqnXmlHandler {
 
 	}
 	
-	
+	/**
+	 * Revert the previously changed XML-tag names to their corresponding Ecore
+	 * names. The renaming is required before the model can be restored from an
+	 * XML file.
+	 * 
+	 * @param filename
+	 */
+	private static void revertXMLFile(String filename) {
+		FileInputStream fis = null;
+		byte b[]= null;
+		try {
+			fis = new FileInputStream(filename);
+			int x = 0;
+			x = fis.available();
+			b= new byte[x];
+			fis.read(b);
+			fis.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		String content = new String(b);
+		content = content.replaceAll("lqn-model", "LqnModelType");
+		content = content.replaceAll("xsi:noNamespaceSchemaLocation=\"file:///C:/Program Files/LQN Solvers/lqn.xsd\"", "xmlns=\"file:/C:/Program%20Files/LQN%20Solvers/lqn-core.xsd\"");
+		content = content.replaceAll("xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"", "xmi:version=\"2.0\" xmlns:xmi=\"http://www.omg.org/XMI\"");
+
+		content = content.replaceAll("entry-activity-graph", "entryActivityGraph");
+		content = content.replaceAll("think-time", "thinkTime");
+
+		content = content.replaceAll("entry-phase-activities", "entryPhaseActivities");
+		content = content.replaceAll("solver-params", "solverParams");
+		content = content.replaceAll("synch-call", "synchCall");
+		content = content.replaceAll("conv_val", "convVal");
+		content = content.replaceAll("it_limit", "itLimit");
+		content = content.replaceAll("print_int", "printInt");
+		content = content.replaceAll("underrelax_coeff", "underrelaxCoeff");
+		content = content.replaceAll("host-demand-mean", "hostDemandMean");
+		content = content.replaceAll("calls-mean", "callsMean");
+		content = content.replaceAll("reply-activity", "replyActivity");
+		content = content.replaceAll("post-OR", "postOR");
+		content = content.replaceAll("pre-OR", "preOR");
+		content = content.replaceAll("task-activities", "taskActivities");
+		content = content.replaceAll("bound-to-entry", "boundToEntry");
+		content = content.replaceAll("reply-entry", "replyEntry");
+		content = content.replaceAll("activity-graph", "activityGraph");
+		content = content.replaceAll("speed-factor", "speedFactor");
+		content = content.replaceAll("service-time-distribution", "serviceTimeDistribution");
+		content = content.replaceAll("open-arrival-rate", "openArrivalRate");
+		
+		FileOutputStream fos;
+		try {
+			fos = new FileOutputStream(filename);
+			fos.write(content.getBytes());
+			fos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 }
