f281455f8ab0b232c86602c86abe3f915837b8d7
pmerkle <merkle@kit.edu>
2009-02-13 17:46:43 +0000

    used analysis plugin now depends on the passed configuration object
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@6049 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.DesignSpaceExploration/de.uka.ipd.sdq.dsexplore/src/de/uka/ipd/sdq/dsexplore/analysis/AnalysisProxy.java
+++ Palladio.DesignSpaceExploration/de.uka.ipd.sdq.dsexplore/src/de/uka/ipd/sdq/dsexplore/analysis/AnalysisProxy.java
@@ -1,12 +1,15 @@
 package de.uka.ipd.sdq.dsexplore.analysis;
 
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 
 import de.uka.ipd.sdq.dsexplore.PCMInstance;
 import de.uka.ipd.sdq.dsexplore.helper.ExtensionHelper;
+import de.uka.ipd.sdq.dsexplore.launch.DSEConstantsContainer;
 
 public class AnalysisProxy implements IAnalysis {
 
@@ -31,8 +34,25 @@ public class AnalysisProxy implements IAnalysis {
 
 	private void checkAnalysisExtension() throws CoreException {
 		if (ana == null){
-			ana = (IAnalysis)ExtensionHelper.loadExtension("de.uka.ipd.sdq.dsexplore.analysis").iterator().next();
-			ana.initialise(configuration, mode,launch,monitor);
+//			ana = (IAnalysis)ExtensionHelper.loadExtension("de.uka.ipd.sdq.dsexplore.analysis").iterator().next();
+			
+			// obtain the extension of the choosen analysis method 
+			String methodName = configuration.getAttribute(DSEConstantsContainer.ANALYSIS_METHOD, "");
+			IExtension[] extensions = ExtensionHelper.loadAnalysisExtensions();
+			for (IExtension ext : extensions) {
+				IConfigurationElement[] elements = ext.getConfigurationElements();
+				for (IConfigurationElement element : elements) {
+					if (element.getName().equals("analysis")) { 
+						// if extension fits to analysis method
+						if (element.getAttribute("name").equals(methodName)) { 
+							// obtain an analysis method instance  
+							ana = (IAnalysis)ExtensionHelper.loadExecutableAttribute(ext, "analysis", "delegate");
+							ana.initialise(configuration, mode,launch,monitor);
+							return;
+						}
+					}
+				}
+			}
 		}
 	}
 
