af918a26b06d049c11b338514170d5de891222f6
kelsaka <krogmann@fzi.de>
2009-03-12 10:23:03 +0000

    Share project "de.uka.ipd.sdq.pcm.resultdecorator.tests" into "svn://141.3.52.13/code/Palladio.EMFComponentModel/trunk"
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@6220 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.resultdecorator.tests/src/de/uka/ipd/sdq/pcm/resultdecorator/resourceenvironmentdecorator/tests/ProcessingResourceSpecificationResultTest.java
@@ -0,0 +1,74 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.pcm.resultdecorator.resourceenvironmentdecorator.tests;
+
+import de.uka.ipd.sdq.pcm.resultdecorator.resourceenvironmentdecorator.ProcessingResourceSpecificationResult;
+import de.uka.ipd.sdq.pcm.resultdecorator.resourceenvironmentdecorator.ResourceenvironmentdecoratorFactory;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Processing Resource Specification Result</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class ProcessingResourceSpecificationResultTest extends UtilisationResultTest {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(ProcessingResourceSpecificationResultTest.class);
+	}
+
+	/**
+	 * Constructs a new Processing Resource Specification Result test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ProcessingResourceSpecificationResultTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Returns the fixture for this Processing Resource Specification Result test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected ProcessingResourceSpecificationResult getFixture() {
+		return (ProcessingResourceSpecificationResult)fixture;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#setUp()
+	 * @generated
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		setFixture(ResourceenvironmentdecoratorFactory.eINSTANCE.createProcessingResourceSpecificationResult());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#tearDown()
+	 * @generated
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+} //ProcessingResourceSpecificationResultTest
