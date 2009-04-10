b37568eed2adb17f8cf2ce65d53d80c384801812
GrischeLiebel <grischa@chalmers.se>
2009-04-10 11:37:36 +0000

    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@6404 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- SoMoX/SoMoX Metrics/src/org/somox/metrics/Abstractness.java
+++ SoMoX/SoMoX Metrics/src/org/somox/metrics/Abstractness.java
@@ -1,35 +1,44 @@
 package org.somox.metrics;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.emf.query.conditions.booleans.BooleanCondition;
+import org.eclipse.emf.query.conditions.eobjects.structuralfeatures.EObjectAttributeValueCondition;
+import org.eclipse.emf.query.statements.FROM;
+import org.eclipse.emf.query.statements.IQueryResult;
+import org.eclipse.emf.query.statements.SELECT;
+import org.eclipse.emf.query.statements.WHERE;
+
 import de.fzi.gast.core.ModelElement;
 import de.fzi.gast.core.Root;
 import de.fzi.gast.types.GASTClass;
+import de.fzi.gast.types.typesPackage;
 
 public class Abstractness {
 	public static double compute (Root root, ModelElement [] elements1, ModelElement [] elements2) {
 		double abstractness = 0.0;
-		double numberAbstract = 0.0;
-		double numberTotal = 0.0;
 		
-		for (ModelElement element : elements1) {
-			if (element instanceof GASTClass) {
-				GASTClass gClass = (GASTClass)element;
-				if (gClass.isInterface() || gClass.isAbstract()) {
-					numberAbstract = numberAbstract + 1.0;
-				}
-				numberTotal = numberTotal + 1.0;
+		List<GASTClass> elementList = new LinkedList<GASTClass>();
+		
+		for (ModelElement current : elements1) {
+			if (current instanceof GASTClass) {
+				elementList.add((GASTClass)current);
+
 			}
 		}
-		for (ModelElement element : elements2) {
-			if (element instanceof GASTClass) {
-				GASTClass gClass = (GASTClass)element;
-				if (gClass.isInterface() || gClass.isAbstract()) {
-					numberAbstract = numberAbstract + 1.0;
-				}
-				numberTotal = numberTotal + 1.0;
+		for (ModelElement current : elements2) {
+			if (current instanceof GASTClass) {
+				elementList.add((GASTClass)current);
 			}
 		}
+		EObjectAttributeValueCondition abstractCondition = new EObjectAttributeValueCondition(typesPackage.eINSTANCE.getMember_Abstract(), new BooleanCondition(true));
+		EObjectAttributeValueCondition interfaceCondition = new EObjectAttributeValueCondition(typesPackage.eINSTANCE.getGASTClass_Interface(), new BooleanCondition(true));
+		SELECT abstractnessQuery = new SELECT(new FROM(elementList), new WHERE(abstractCondition.OR(interfaceCondition)));
 		
-		abstractness = numberAbstract/numberTotal;
+		IQueryResult result = abstractnessQuery.execute();
+		
+		abstractness = (double)result.size()/(double)elementList.size();
 		
 		return abstractness;
 	}
