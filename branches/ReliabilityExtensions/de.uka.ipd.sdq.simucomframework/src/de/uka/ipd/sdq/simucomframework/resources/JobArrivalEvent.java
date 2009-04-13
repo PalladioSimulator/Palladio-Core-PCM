890daf3a328d8eef666b9b2e0d10f101c9958918
FranzBrosch <FranzBrosch>
2009-04-13 16:50:02 +0000

    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@6417 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- branches/ReliabilityExtensions/de.uka.ipd.sdq.simucomframework/src/de/uka/ipd/sdq/simucomframework/resources/JobArrivalEvent.java
+++ branches/ReliabilityExtensions/de.uka.ipd.sdq.simucomframework/src/de/uka/ipd/sdq/simucomframework/resources/JobArrivalEvent.java
@@ -33,8 +33,8 @@ public class JobArrivalEvent extends SimEvent {
 		resource.processPassedTime();
 		resource.addJob(demand);
 		double nextEventTime = resource.getTimeWhenNextJobIsDone();
-		if(nextEventTime <= 0){
-			throw new RuntimeException("Next scheduled event time <= 0");
+		if(nextEventTime < 0){
+			throw new RuntimeException("Next scheduled event time < 0");
 		}
 		ISimEventDelegate ev = resource.getJobDoneEvent();
 		ev.removeEvent();
