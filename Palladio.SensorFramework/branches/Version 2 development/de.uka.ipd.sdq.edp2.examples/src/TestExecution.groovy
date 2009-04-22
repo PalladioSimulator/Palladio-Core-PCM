1843196b6258c5c3570fb5cc33a42ac01f595bd1
groenda <groenda@fzi.de>
2009-04-22 17:16:42 +0000

    Added EDP2 storage example (Groovy)
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@6544 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ Palladio.SensorFramework/branches/Version 2 development/de.uka.ipd.sdq.edp2.examples/src/TestExecution.groovy
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+
+
+/**
+ * @author groenda
+ *
+ */
+public class TestExecution{
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(def args){
+		println "execution successful"
+	}
+	
+}
