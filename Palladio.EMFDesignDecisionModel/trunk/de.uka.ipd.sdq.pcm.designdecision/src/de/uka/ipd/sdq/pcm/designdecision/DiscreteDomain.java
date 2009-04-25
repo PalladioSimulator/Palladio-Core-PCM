cd9d4ea17f7544711d972f1f2e4cb13d61f38c5a
AnneKoziolek <anne.koziolek@kit.edu>
2009-04-25 21:08:56 +0000

    added DiscreteDomain.java
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@6599 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ Palladio.EMFDesignDecisionModel/trunk/de.uka.ipd.sdq.pcm.designdecision/src/de/uka/ipd/sdq/pcm/designdecision/DiscreteDomain.java
@@ -0,0 +1,22 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.pcm.designdecision;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Discrete Domain</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see de.uka.ipd.sdq.pcm.designdecision.designdecisionPackage#getDiscreteDomain()
+ * @model interface="true" abstract="true"
+ * @generated
+ */
+public interface DiscreteDomain extends EObject {
+} // DiscreteDomain
