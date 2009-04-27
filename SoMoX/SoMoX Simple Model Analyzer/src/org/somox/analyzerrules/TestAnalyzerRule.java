d60f1c76fafc4f1c70811ada9e486e7dc194952d
kelsaka <krogmann@fzi.de>
2009-04-27 12:29:48 +0000

    prototype
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@6611 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- SoMoX/SoMoX Simple Model Analyzer/src/org/somox/analyzerrules/TestAnalyzerRule.java
+++ SoMoX/SoMoX Simple Model Analyzer/src/org/somox/analyzerrules/TestAnalyzerRule.java
@@ -7,15 +7,24 @@ import org.somox.metrics.Coupling;
 import org.somox.metrics.MetricID;
 import org.somox.metrics.NameResemblance;
 
+/**
+ * Requires the following metrics: Abstrectnees, Coupling, and NameResemblance
+ * @author kelsaka
+ *
+ */
 public class TestAnalyzerRule implements IAnalyzerRule {
 
 	@Override
 	public double computeOverallMetricValue(Map<MetricID, Double> argAB,
 			Map<MetricID, Double> argBA) {
 		
-		if(argAB.get(new Abstractness().getMID()) > argAB.get(new Coupling().getMID())) {
-			if(argAB.get(new Abstractness().getMID()) < argAB.get(new NameResemblance().getMID())) {
-				return argAB.get(new Abstractness().getMID()) * argAB.get(new NameResemblance().getMID());
+		double abstractness = argAB.get(new Abstractness().getMID());
+		double coupling = argAB.get(new Coupling().getMID());
+		double nameResemblance = argAB.get(new NameResemblance().getMID());
+		
+		if(abstractness > coupling) {
+			if(abstractness < nameResemblance) {
+				return abstractness * nameResemblance;
 			}
 			return 0.9;
 		} else {
