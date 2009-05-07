8e39b6cd609b5a53839c37354da48f3af55997d2
Glorinka <lucia.happe@icloud.com>
2009-05-07 14:08:50 +0000

    Removed file/folder
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/branches/HOTCompletions@6687 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- branches/SchedulerIntegration/de.uka.ipd.sdq.simucomframework/src/de/uka/ipd/sdq/simucomframework/abstractSimEngine/ISimEventDelegate.java
+++
@@ -1,7 +0,0 @@
-package de.uka.ipd.sdq.simucomframework.abstractSimEngine;
-
-public interface ISimEventDelegate {
-
-	public abstract void schedule(IEntityDelegate resource, double timeWhenNextJobIsDone);
-
-}
