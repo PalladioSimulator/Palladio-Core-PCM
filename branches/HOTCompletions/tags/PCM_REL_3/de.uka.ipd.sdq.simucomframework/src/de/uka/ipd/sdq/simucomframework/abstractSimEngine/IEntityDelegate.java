d3f600c685fe5142f0b35065c9bc66223ee74bcf
Glorinka <lucia.happe@icloud.com>
2009-05-07 14:11:11 +0000

    Removed file/folder
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@6717 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- branches/HOTCompletions/tags/PCM_REL_3/de.uka.ipd.sdq.simucomframework/src/de/uka/ipd/sdq/simucomframework/abstractSimEngine/IEntityDelegate.java
+++
@@ -1,9 +0,0 @@
-package de.uka.ipd.sdq.simucomframework.abstractSimEngine;
-
-public interface IEntityDelegate {
-
-	public abstract boolean isScheduled();
-
-	public abstract void reschedule(double d);
-
-}
