9c7a5d24859efe31a6623b650906482a69c5a2cc
Glorinka <lucia.happe@icloud.com>
2009-05-07 14:09:09 +0000

    Removed file/folder
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/branches/HOTCompletions@6691 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- FileShare/runHashBenchmarker_MK_Lenovo_Sun_server_JIT_50KB.bat
+++
@@ -1,1 +0,0 @@
-"C:\Programme\Java\jdk1.6.0_03\bin\java" -server -classpath .;.\bin;%CLASSPATH%;.\lib\JbcParser.jar;.\lib\log4j-1.2.15.jar;.\lib\opencsv-1.8.jar;.\lib\TestMeasuring.jar de.uka.ipd.sdq.benchmarking.HashBenchmarker MK_Lenovo -server -default SHA-512 200 true 50000 20
--- FileShare/runTestDriver_KK_Acer_Sun_server_-Xint.bat
+++
@@ -1,1 +0,0 @@
-"C:\Programme\Java\jdk1.6.0_06\bin\java" -server -Xint -classpath .;.\bin;%CLASSPATH%;.\lib\JbcParser.jar;.\lib\log4j-1.2.15.jar;.\lib\opencsv-1.8.jar;.\lib\TestMeasuring.jar de.uka.ipd.sdq.palladiofileshare.testdriver.TestDriver
