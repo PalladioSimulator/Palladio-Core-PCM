c77e34209cb8f082d4784dc52222cc90d2a3fff5
Glorinka <lucia.happe@icloud.com>
2009-05-07 14:10:08 +0000

    Removed file/folder
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@6705 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- branches/HOTCompletions/Palladio.SensorFramework/tags/Experiment2/de.uka.ipd.sdq.sensorframework/src/de/uka/ipd/sdq/sensorframework/adapter/IAdapter.java
+++
@@ -1,9 +0,0 @@
-package de.uka.ipd.sdq.sensorframework.adapter;
-
-import java.util.Properties;
-
-public interface IAdapter {
-	Object getAdaptedObject();
-	Properties getProperties();
-	void setProperties(Properties newProperties);
-}
--- branches/HOTCompletions/Palladio.SensorFramework/tags/Experiment2/de.uka.ipd.sdq.sensorframework/src/de/uka/ipd/sdq/sensorframework/adapter/IAdapterFactory.java
+++
@@ -1,6 +0,0 @@
-package de.uka.ipd.sdq.sensorframework.adapter;
-
-public interface IAdapterFactory {
-	boolean canAdapt(Object adaptee, Class targetClass);
-	IAdapter getAdapter(Object adaptee); 
-}
