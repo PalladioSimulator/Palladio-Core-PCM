3fd4549eb76684006e1a2bb3ad486634e7bf4e7b
Glorinka <lucia.happe@icloud.com>
2009-05-07 14:08:50 +0000

    Removed file/folder
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@6687 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- branches/HOTCompletions/branches/ReliabilityExtensions/de.uka.ipd.sdq.codegen.runconfig/src/de/uka/ipd/sdq/codegen/runconfig/tabs/ComponentLookupEnum.java
+++
@@ -1,6 +0,0 @@
-package de.uka.ipd.sdq.codegen.runconfig.tabs;
-
-public enum ComponentLookupEnum {
-	DEPENDENCY_INJECTION,
-	BROKER
-}
--- branches/HOTCompletions/branches/SchedulerIntegration/de.uka.ipd.sdq.errorhandling/src/de/uka/ipd/sdq/errorhandling/IIssue.java
+++
@@ -1,5 +0,0 @@
-package de.uka.ipd.sdq.errorhandling;
-
-public interface IIssue {
-	String getMessage();
-}
