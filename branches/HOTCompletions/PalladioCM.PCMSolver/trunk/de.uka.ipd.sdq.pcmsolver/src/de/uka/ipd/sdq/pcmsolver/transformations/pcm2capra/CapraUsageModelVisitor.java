221a4fe9903c5cc8f7b9842f3e19289114dea190
Glorinka <lucia.happe@icloud.com>
2009-05-07 14:10:35 +0000

    Removed file/folder
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@6710 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- branches/HOTCompletions/PalladioCM.PCMSolver/trunk/de.uka.ipd.sdq.pcmsolver/src/de/uka/ipd/sdq/pcmsolver/transformations/pcm2capra/CapraUsageModelVisitor.java
+++
@@ -1,9 +0,0 @@
-//package de.uka.ipd.sdq.pcmsolver.transformations.pcm2capra;
-//
-//import de.uka.ipd.sdq.capra.core.CapraExpression;
-//import de.uka.ipd.sdq.pcm.usagemodel.util.UsagemodelSwitch;
-//
-//
-//public class CapraUsageModelVisitor extends UsagemodelSwitch<CapraExpression> {
-//
-//}
