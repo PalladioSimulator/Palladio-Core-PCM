fd930e82281522c6dbc7b45c59330cf30f190d12
GrischeLiebel <grischa@chalmers.se>
2009-06-08 19:43:21 +0000

    BlackboardListener functionality added
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@6904 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- SoMoX/SoMoX Core/src/org/somox/analyzer/ModelAnalyzerTabGroupBlackboard.java
+++ SoMoX/SoMoX Core/src/org/somox/analyzer/ModelAnalyzerTabGroupBlackboard.java
@@ -1,5 +1,8 @@
 package org.somox.analyzer;
 
+import java.util.Enumeration;
+import java.util.Vector;
+
 public class ModelAnalyzerTabGroupBlackboard {
 
 	static int idCounter = 0;
@@ -22,6 +25,48 @@ public class ModelAnalyzerTabGroupBlackboard {
 
 	public void setSomoxAnalyzerInputFile(String somoxAnalyzerInputFile) {
 		this.somoxAnalyzerInputFile = somoxAnalyzerInputFile;
+		fireBlackboardChanged();
+	}
+
+	//
+	// Event handling
+	//
+	private transient Vector<BlackboardListener> blackboardListeners;
+
+	/** Register a listener for Blackboard events */
+	synchronized public void addBlackboardListener(BlackboardListener listener) {
+		if (blackboardListeners== null) {
+			blackboardListeners= new Vector<BlackboardListener>();
+		}
+		blackboardListeners.addElement(listener);
+	}
+
+	synchronized public void removeBlackboardListener(BlackboardListener listener) {
+		if (blackboardListeners == null) {
+			blackboardListeners= new Vector<BlackboardListener>();
+		}
+		blackboardListeners.removeElement(listener);
+	}
+
+	/** Fire to all registered listeners */
+	@SuppressWarnings("unchecked")
+	public void fireBlackboardChanged() {
+		// If we have no listeners, do nothing.
+		if ((blackboardListeners!= null) && !blackboardListeners.isEmpty()) {
+			// Make a copy of the listener list in case anyone adds or removes
+			// listeners.
+			Vector<BlackboardListener> targets;
+			synchronized (blackboardListeners) {
+				targets = (Vector<BlackboardListener>) blackboardListeners.clone();
+			}
+			// Walk through the listener list and call the listener method in
+			// each.
+			Enumeration<BlackboardListener> e = targets.elements();
+			while (e.hasMoreElements()) {
+				BlackboardListener l = (BlackboardListener) e.nextElement();
+				l.blackboardChanged();
+			}
+		}
 	}
 
 }
