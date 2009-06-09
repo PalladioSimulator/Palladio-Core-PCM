f460693415705b668037ce83fb06a0fd51469055
AnneKoziolek <anne.koziolek@kit.edu>
2009-06-09 01:24:43 +0000

    added an "interception" to retrieve results
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@6909 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- branches/SolverWorkflowIntegration/PalladioCM.PCMSolver/trunk/de.uka.ipd.sdq.pcmsolver/src/de/uka/ipd/sdq/pcmsolver/runconfig/PCMSolverReliabilityLaunchConfigurationDelegate.java
+++ branches/SolverWorkflowIntegration/PalladioCM.PCMSolver/trunk/de.uka.ipd.sdq.pcmsolver/src/de/uka/ipd/sdq/pcmsolver/runconfig/PCMSolverReliabilityLaunchConfigurationDelegate.java
@@ -7,6 +7,8 @@ import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
 
 import de.uka.ipd.sdq.pcmsolver.RunPCMAnalysisJob;
+import de.uka.ipd.sdq.pcmsolver.transformations.pcm2markov.Pcm2MarkovStrategy;
+import de.uka.ipd.sdq.workflow.IJob;
 import de.uka.ipd.sdq.workflow.exceptions.JobFailedException;
 import de.uka.ipd.sdq.workflow.exceptions.UserCanceledException;
 
@@ -21,7 +23,22 @@ import de.uka.ipd.sdq.workflow.exceptions.UserCanceledException;
  * 
  */
 public class PCMSolverReliabilityLaunchConfigurationDelegate extends PCMSolverLaunchConfigurationDelegate {
+	
+	private Pcm2MarkovStrategy strategy = null;
 
+	@Override
+	protected IJob createWorkflowJob(PCMSolverWorkflowRunConfiguration config,
+			ILaunch launch) throws CoreException {
+		PCMSolverJob job = (PCMSolverJob) super.createWorkflowJob(config,launch);
+		
+		RunPCMAnalysisJob runJob = (RunPCMAnalysisJob)job.getLast();
+		
+		this.setStrategy((Pcm2MarkovStrategy) runJob.getStrategy());
+		
+		return job;
+		
+	}
+	
 	@Override
 	protected PCMSolverWorkflowRunConfiguration deriveConfiguration(
 			ILaunchConfiguration configuration, String mode)
@@ -31,6 +48,15 @@ public class PCMSolverReliabilityLaunchConfigurationDelegate extends PCMSolverLa
 		
 		return pcmConfig;
 	}
+
+	public void setStrategy(Pcm2MarkovStrategy strategy) {
+		this.strategy = strategy;
+	}
+
+	public Pcm2MarkovStrategy getStrategy() {
+		return strategy;
+	}
+	
 	
 	
 	
