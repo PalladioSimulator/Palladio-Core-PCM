b32be4e240ceb0b89480b5faa52d0aa71505f112
GrischeLiebel <grischa@chalmers.se>
2009-06-11 16:26:54 +0000

    Model extracted in Blackboard
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@6931 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- SoMoX/SoMoX Core/src/org/somox/analyzer/ModelAnalyzerTabGroupBlackboard.java
+++ SoMoX/SoMoX Core/src/org/somox/analyzer/ModelAnalyzerTabGroupBlackboard.java
@@ -1,9 +1,34 @@
 package org.somox.analyzer;
 
+import java.io.File;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Vector;
 
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.emf.common.command.BasicCommandStack;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
+import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
+import org.eclipse.emf.edit.provider.ReflectiveItemProviderAdapterFactory;
+import org.eclipse.emf.edit.provider.resource.ResourceItemProviderAdapterFactory;
+
+import de.fzi.gast.accesses.provider.accessesItemProviderAdapterFactory;
+import de.fzi.gast.annotations.provider.annotationsItemProviderAdapterFactory;
+import de.fzi.gast.core.Root;
+import de.fzi.gast.core.provider.coreItemProviderAdapterFactory;
+import de.fzi.gast.functions.provider.functionsItemProviderAdapterFactory;
+import de.fzi.gast.statements.provider.statementsItemProviderAdapterFactory;
+import de.fzi.gast.types.provider.typesItemProviderAdapterFactory;
+import de.fzi.gast.variables.provider.variablesItemProviderAdapterFactory;
+
 public class ModelAnalyzerTabGroupBlackboard {
+	
+	private Root root;
 
 	static int idCounter = 0;
 	private int myId = 0;
@@ -24,8 +49,72 @@ public class ModelAnalyzerTabGroupBlackboard {
 	}
 
 	public void setSomoxAnalyzerInputFile(String somoxAnalyzerInputFile) {
-		this.somoxAnalyzerInputFile = somoxAnalyzerInputFile;
-		fireBlackboardChanged();
+		if (this.somoxAnalyzerInputFile == null) {
+			this.somoxAnalyzerInputFile = somoxAnalyzerInputFile;
+			if (this.somoxAnalyzerInputFile != null) {
+				if (somoxAnalyzerInputFile.endsWith(".gast")) {
+					loadModel();
+				}
+			}
+			fireBlackboardChanged();
+		} else if (this.somoxAnalyzerInputFile != null && !this.somoxAnalyzerInputFile.equals(somoxAnalyzerInputFile)) {
+			this.somoxAnalyzerInputFile = somoxAnalyzerInputFile;
+			if (this.somoxAnalyzerInputFile != null) {
+				if (somoxAnalyzerInputFile.endsWith(".gast")) {
+					loadModel();
+				}
+			}
+			fireBlackboardChanged();
+		}
+	}
+
+	private void loadModel() {
+		String platformPath = Platform.getInstanceLocation().getURL().getPath();
+		platformPath = platformPath + somoxAnalyzerInputFile;
+		
+		URI fileURI = URI.createFileURI(new File(platformPath).getAbsolutePath());
+		
+		ComposedAdapterFactory adapterFactory = new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
+
+		adapterFactory.addAdapterFactory(new ResourceItemProviderAdapterFactory());
+		adapterFactory.addAdapterFactory(new statementsItemProviderAdapterFactory());
+		adapterFactory.addAdapterFactory(new coreItemProviderAdapterFactory());
+		adapterFactory.addAdapterFactory(new annotationsItemProviderAdapterFactory());
+		adapterFactory.addAdapterFactory(new typesItemProviderAdapterFactory());
+		adapterFactory.addAdapterFactory(new functionsItemProviderAdapterFactory());
+		adapterFactory.addAdapterFactory(new accessesItemProviderAdapterFactory());
+		adapterFactory.addAdapterFactory(new variablesItemProviderAdapterFactory());
+		adapterFactory.addAdapterFactory(new ReflectiveItemProviderAdapterFactory());
+
+		BasicCommandStack commandStack = new BasicCommandStack();
+
+		AdapterFactoryEditingDomain editingDomain = new AdapterFactoryEditingDomain(adapterFactory, commandStack, new HashMap<Resource, Boolean>());
+		
+		// Register the default resource factory -- only needed for stand-alone!
+		editingDomain.getResourceSet().getResourceFactoryRegistry().getExtensionToFactoryMap().put(
+				Resource.Factory.Registry.DEFAULT_EXTENSION, new XMIResourceFactoryImpl());
+
+		//Try to load the resource through the editingDomain.
+		Resource resource = null;
+		try {
+			resource = editingDomain.getResourceSet().getResource(fileURI, true);
+		}
+		catch (Exception e) {
+			resource = editingDomain.getResourceSet().getResource(fileURI, false);
+		}
+		
+		EList<EObject> contents = resource.getContents();
+		
+		for (EObject current : contents) {
+			if (current instanceof Root) {
+				root = (Root)current;
+				break;
+			}
+		}
+	}
+	
+	public Root getRoot () {
+		return root;
 	}
 
 	//
