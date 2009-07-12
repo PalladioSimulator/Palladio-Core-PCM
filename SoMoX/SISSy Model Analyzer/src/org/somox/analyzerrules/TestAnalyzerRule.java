742dd5ecb11a806aac082d10dcf2dc36df69a9c2
GrischeLiebel <grischa@chalmers.se>
2009-07-12 22:18:16 +0000

    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@7043 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- SoMoX/SISSy Model Analyzer/src/org/somox/analyzerrules/TestAnalyzerRule.java
+++ SoMoX/SISSy Model Analyzer/src/org/somox/analyzerrules/TestAnalyzerRule.java
@@ -30,7 +30,7 @@ public class TestAnalyzerRule implements IAnalyzerRule {
 	
 	
 	public TestAnalyzerRule() {
-		//loadProperties();
+		loadProperties();
 	}
 	
 	/**
@@ -42,7 +42,6 @@ public class TestAnalyzerRule implements IAnalyzerRule {
 	 */
 	public double computeOverallMetricValue(Map<Integer, Double> argAB,
 			Map<Integer, Double> argBA) {
-		loadProperties();
 		
 		double nameResemblance = argAB.get((new NameResemblance()).getMID().getID());
 		double subsystemComponent = argAB.get(new SubsystemComponent().getMID().getID());
@@ -103,76 +102,6 @@ public class TestAnalyzerRule implements IAnalyzerRule {
 		
 		return score;
 	}
-//	
-//	/**
-//	 * Computes the overall metric score based on Landrys weighting functions
-//	 * 
-//	 * @param argAB The ID for every metric and its calculated score for the components A B
-//	 * @param argBA The ID for every metric and its calculated score for the components B A
-//	 * @return the overall score of the metric
-//	 */
-//	public double computeOverallMetricValue(Map<Integer, Double> argAB,
-//			Map<Integer, Double> argBA) {
-//		
-//		double nameResemblance = argAB.get((new NameResemblance()).getMID().getID());
-//		double subsystemComponent = argAB.get(new SubsystemComponent().getMID().getID());
-//		double packageMapping = argAB.get(new PackageMapping().getMID().getID());
-//		double dms = argAB.get(new DMS().getMID().getID());
-//		double slaq = argAB.get(new SliceLayerArchitectureQuality().getMID().getID());
-//		
-//		double couplingAB = argAB.get(new Coupling().getMID().getID());
-//		double couplingBA = argBA.get(new Coupling().getMID().getID());
-//		double coupling = Math.max(couplingAB, couplingBA);
-//		
-//		double interfaceViolationAB = argAB.get(new InterfaceViolation().getMID().getID());
-//		double interfaceViolationBA = argBA.get(new InterfaceViolation().getMID().getID());
-//		double interfaceViolation = Math.max(interfaceViolationAB, interfaceViolationBA);
-//		
-//		double w1=0, w2=0, w3=0;
-//		
-//		double w4 = 60.0;
-//		double w5 = 7.0;
-//		
-//		//determine weight of nameResemblance
-//		if (coupling < 0.2) {
-//			w1 = 0.0;
-//		} else if (coupling < 0.7) {
-//			w1 = 10.0;
-//		} else {
-//			if (nameResemblance < 0.5) {
-//				w1 = 15.0;
-//			} else if (nameResemblance < 0.7) {
-//				w1  = 25.0;
-//			} else if (nameResemblance < 0.9) {
-//				w1  = 40.0;
-//			} else {
-//				w1  = 90.0;
-//			}
-//		}
-//		
-//		//determine weight of interfaceViolation
-//		if (coupling >=0.5 && interfaceViolation > 0.0) {
-//			w2  = 60.0;
-//		} else {
-//			w2  = 0.0;
-//		}
-//		
-//		//determine weight of subsystemComponent
-//		if (slaq >= 0.5) {
-//			w3 = 25.0;
-//		} else {
-//			w3 = 0.0;
-//		}
-//		
-//		//compute overall metric score
-//		double sum = w1+w2+w3+w4+w5;
-//		double score = nameResemblance*(double)w1 + interfaceViolation*(double)w2 
-//		               + subsystemComponent*(double)w3 + packageMapping*(double)w4 - dms*(double)w5;
-//		
-//		score = score/sum;
-//		
-//		return score;
-//	}
 
 	private void loadProperties() {
 		this.properties = new Properties();
