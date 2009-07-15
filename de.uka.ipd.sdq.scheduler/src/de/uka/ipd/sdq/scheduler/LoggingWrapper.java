6870905d864b0ff0d466c32439f7959bf81100e9
kelsaka <krogmann@fzi.de>
2009-07-15 12:47:37 +0000

    Stable version of the PCM, version 3.1, based on Eclipse 3.4 SR2 Ganymede Release (part)
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/tags/PCM_REL_3.1@7068 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ de.uka.ipd.sdq.scheduler/src/de/uka/ipd/sdq/scheduler/LoggingWrapper.java
@@ -0,0 +1,25 @@
+package de.uka.ipd.sdq.scheduler;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import de.uka.ipd.sdq.probfunction.math.util.MathTools;
+import de.uka.ipd.sdq.scheduler.factory.SchedulingFactory;
+
+public class LoggingWrapper {
+	
+	public static Logger logger = Logger.getLogger("Scheduler");
+
+	public static void activate(){
+		logger.setLevel(Level.ALL);
+	}
+	
+	public static void deactivate(){
+		logger.setLevel(Level.OFF);
+	}
+	
+	public static void log(String string) {
+		 logger.fatal(MathTools.round(SchedulingFactory.getUsedSimulator().time(), 0.01) + " " + string);
+	}
+
+}
---
+++ de.uka.ipd.sdq.scheduler/src/de/uka/ipd/sdq/scheduler/priority/update/DecayToBaseUpdate.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package de.uka.ipd.sdq.scheduler.priority.update;
+
+import de.uka.ipd.sdq.scheduler.priority.IPriorityUpdateStrategy;
+import de.uka.ipd.sdq.scheduler.processes.impl.ProcessWithPriority;
+
+public class DecayToBaseUpdate implements IPriorityUpdateStrategy {
+
+	public boolean update(ProcessWithPriority process) {
+		if (process.hasBonus() && process.getTimeslice().completelyFinished()){
+			process.decreasePriority();
+			return true;
+		}
+		return false;
+	}
+	
+	public DecayToBaseUpdate cloneFor(ProcessWithPriority process)  {
+		return this;
+	}
+}
