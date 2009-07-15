486e101092e6647e62f24e957b6b40197174c0e9
kelsaka <krogmann@fzi.de>
2009-07-15 12:45:49 +0000

    Stable version of the PCM, version 3.1, based on Eclipse 3.4 SR2 Ganymede Release (part)
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/tags/PCM_REL_3.1@7063 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ de.uka.ipd.sdq.units/src/de/uka/ipd/sdq/units/impl/UnitMultiplicationImpl.java
@@ -0,0 +1,160 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.units.impl;
+
+import de.uka.ipd.sdq.units.Unit;
+import de.uka.ipd.sdq.units.UnitMultiplication;
+import de.uka.ipd.sdq.units.UnitsPackage;
+
+import java.util.Collection;
+
+import org.eclipse.emf.common.notify.NotificationChain;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.util.EObjectContainmentEList;
+import org.eclipse.emf.ecore.util.InternalEList;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Unit Multiplication</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link de.uka.ipd.sdq.units.impl.UnitMultiplicationImpl#getUnits <em>Units</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class UnitMultiplicationImpl extends UnitImpl implements UnitMultiplication {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final String copyright = "Copyright 2007-2009, SDQ, IPD, U Karlsruhe";
+	/**
+	 * The cached value of the '{@link #getUnits() <em>Units</em>}' containment reference list.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getUnits()
+	 * @generated
+	 * @ordered
+	 */
+	protected EList<Unit> units;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected UnitMultiplicationImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return UnitsPackage.Literals.UNIT_MULTIPLICATION;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EList<Unit> getUnits() {
+		if (units == null) {
+			units = new EObjectContainmentEList<Unit>(Unit.class, this, UnitsPackage.UNIT_MULTIPLICATION__UNITS);
+		}
+		return units;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
+		switch (featureID) {
+			case UnitsPackage.UNIT_MULTIPLICATION__UNITS:
+				return ((InternalEList<?>)getUnits()).basicRemove(otherEnd, msgs);
+		}
+		return super.eInverseRemove(otherEnd, featureID, msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case UnitsPackage.UNIT_MULTIPLICATION__UNITS:
+				return getUnits();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case UnitsPackage.UNIT_MULTIPLICATION__UNITS:
+				getUnits().clear();
+				getUnits().addAll((Collection<? extends Unit>)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case UnitsPackage.UNIT_MULTIPLICATION__UNITS:
+				getUnits().clear();
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case UnitsPackage.UNIT_MULTIPLICATION__UNITS:
+				return units != null && !units.isEmpty();
+		}
+		return super.eIsSet(featureID);
+	}
+
+} //UnitMultiplicationImpl
