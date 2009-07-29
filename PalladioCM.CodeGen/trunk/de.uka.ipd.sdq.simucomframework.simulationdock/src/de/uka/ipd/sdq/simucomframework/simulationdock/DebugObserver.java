0f30ba0945b5207d5740d873c66c755134207ccc
kelsaka <krogmann@fzi.de>
2009-07-29 12:48:02 +0000

    fixed error
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@7144 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- PalladioCM.CodeGen/trunk/de.uka.ipd.sdq.simucomframework.simulationdock/src/de/uka/ipd/sdq/simucomframework/simulationdock/DebugObserver.java
+++ PalladioCM.CodeGen/trunk/de.uka.ipd.sdq.simucomframework.simulationdock/src/de/uka/ipd/sdq/simucomframework/simulationdock/DebugObserver.java
@@ -1,6 +1,7 @@
 package de.uka.ipd.sdq.simucomframework.simulationdock;
 
 import java.util.Hashtable;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.osgi.service.event.Event;
@@ -85,8 +86,8 @@ public class DebugObserver implements IStatusObserver {
 		postEvent(topic, new Hashtable<String,Object>());
 	}
 	
-	private void postEvent(String topic, Hashtable<String,Object> newProperties) {
-		Hashtable<String,Object> properties = new Hashtable<String,Object>();
+	private void postEvent(String topic, Map<String,Object> newProperties) {
+		Map<String,Object> properties = new Hashtable<String,Object>();
 		properties.put("DOCK_ID", myDock.getDockId());
 		properties.putAll(newProperties);
 		Event event = new Event(topic, properties);
