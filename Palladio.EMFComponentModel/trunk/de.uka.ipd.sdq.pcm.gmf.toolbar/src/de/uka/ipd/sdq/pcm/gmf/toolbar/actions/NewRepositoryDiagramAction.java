ca81160b0f25988aafc916d4a0f09d31839faed8
kelsaka <krogmann@fzi.de>
2009-10-03 14:44:12 +0000

    temp deactivation of gmf resource editor
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@7438 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.toolbar/src/de/uka/ipd/sdq/pcm/gmf/toolbar/actions/NewRepositoryDiagramAction.java
+++ Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.toolbar/src/de/uka/ipd/sdq/pcm/gmf/toolbar/actions/NewRepositoryDiagramAction.java
@@ -2,13 +2,14 @@ package de.uka.ipd.sdq.pcm.gmf.toolbar.actions;
 
 import org.eclipse.ui.INewWizard;
 
-import de.uka.ipd.sdq.pcm.gmf.repository.part.PalladioComponentModelCreationWizard;
+// import de.uka.ipd.sdq.pcm.gmf.repository.part.PalladioComponentModelCreationWizard; //TODO: re-enable
 import de.uka.ipd.sdq.pcm.gmf.toolbar.BaseNewDiagramAction;
 
 public class NewRepositoryDiagramAction
 	extends BaseNewDiagramAction {
 
 	protected INewWizard getNewWizard() {
-		return new PalladioComponentModelCreationWizard();
+		throw new RuntimeException("not running with eclipse 3.4 / gmf 2.1");
+		//return new PalladioComponentModelCreationWizard(); //TODO: re-enable
 	}
 }
