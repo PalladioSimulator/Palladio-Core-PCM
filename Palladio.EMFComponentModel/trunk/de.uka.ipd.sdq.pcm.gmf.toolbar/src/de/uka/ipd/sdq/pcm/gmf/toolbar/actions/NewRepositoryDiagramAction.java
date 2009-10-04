19027efd4bb14a37698dfb9ac6242df7288247d7
kelsaka <krogmann@fzi.de>
2009-10-04 12:56:50 +0000

    removed wrongly deacticated code / deactivated right code
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@7441 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.toolbar/src/de/uka/ipd/sdq/pcm/gmf/toolbar/actions/NewRepositoryDiagramAction.java
+++ Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.toolbar/src/de/uka/ipd/sdq/pcm/gmf/toolbar/actions/NewRepositoryDiagramAction.java
@@ -2,14 +2,13 @@ package de.uka.ipd.sdq.pcm.gmf.toolbar.actions;
 
 import org.eclipse.ui.INewWizard;
 
-// import de.uka.ipd.sdq.pcm.gmf.repository.part.PalladioComponentModelCreationWizard; //TODO: re-enable
+import de.uka.ipd.sdq.pcm.gmf.repository.part.PalladioComponentModelCreationWizard;
 import de.uka.ipd.sdq.pcm.gmf.toolbar.BaseNewDiagramAction;
 
 public class NewRepositoryDiagramAction
 	extends BaseNewDiagramAction {
 
 	protected INewWizard getNewWizard() {
-		throw new RuntimeException("not running with eclipse 3.4 / gmf 2.1");
-		//return new PalladioComponentModelCreationWizard(); //TODO: re-enable
+		return new PalladioComponentModelCreationWizard();
 	}
 }
--- Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.toolbar/src/de/uka/ipd/sdq/pcm/gmf/toolbar/actions/NewResourceDiagramAction.java
+++ Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.toolbar/src/de/uka/ipd/sdq/pcm/gmf/toolbar/actions/NewResourceDiagramAction.java
@@ -3,12 +3,13 @@ package de.uka.ipd.sdq.pcm.gmf.toolbar.actions;
 import org.eclipse.ui.INewWizard;
 
 import de.uka.ipd.sdq.pcm.gmf.toolbar.BaseNewDiagramAction;
-import de.uka.ipd.sdq.pcm.gmf.resource.part.PalladioComponentModelCreationWizard;
+//import de.uka.ipd.sdq.pcm.gmf.resource.part.PalladioComponentModelCreationWizard;
 
 public class NewResourceDiagramAction
 	extends BaseNewDiagramAction {
 
 	protected INewWizard getNewWizard() {
-		return new PalladioComponentModelCreationWizard();
+		throw new RuntimeException("not running with eclipse 3.4 / gmf 2.1");
+		//return new PalladioComponentModelCreationWizard(); //FIXME: re-enable
 	}
 }
