781e5f81fb7d9f0b0421f685cd7443e5ca8e8493
pmerkle <merkle@kit.edu>
2009-10-14 21:24:36 +0000

    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@7547 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ PalladioCM.CodeGen/branches/Confidence/de.uka.ipd.sdq.codegen.simucontroller/src/de/uka/ipd/sdq/codegen/simucontroller/runconfig/SimuComWorkflowLauncher.java
@@ -0,0 +1,92 @@
+package de.uka.ipd.sdq.codegen.simucontroller.runconfig;
+
+import java.util.ArrayList;
+
+import org.apache.log4j.Level;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+
+import de.uka.ipd.sdq.codegen.simucontroller.debug.IDebugListener;
+import de.uka.ipd.sdq.codegen.simucontroller.debug.SimulationDebugListener;
+import de.uka.ipd.sdq.codegen.simucontroller.workflow.jobs.SimuComJob;
+import de.uka.ipd.sdq.workflow.IJob;
+import de.uka.ipd.sdq.workflow.launchconfig.LoggerAppenderStruct;
+
+/**
+ * The class adapts defined functionality in the AbstractMDSDLaunchConfigurationDelegate for
+ * SimuCom Framework.
+ * 
+ */
+public class SimuComWorkflowLauncher extends
+		AbstractMDSDLaunchConfigurationDelegate<SimuComWorkflowConfiguration> {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seede.uka.ipd.sdq.codegen.runconfig.LaunchConfigurationDelegate#
+	 * creataAttributesGetMethods(org.eclipse.debug.core.ILaunchConfiguration)
+	 */
+	@Override
+	protected SimuComWorkflowConfiguration deriveConfiguration(
+			ILaunchConfiguration configuration, String mode)
+			throws CoreException {
+		SimuComConfigurationBuilder builder = new SimuComLaunchConfigurationBasedConfigBuilder(
+				configuration, mode);
+		return builder.getConfiguration();
+	}
+
+	/* (non-Javadoc)
+	 * @see de.uka.ipd.sdq.codegen.simucontroller.runconfig.AbstractMDSDLaunchConfigurationDelegate#setupLogging(org.apache.log4j.Level)
+	 */
+	@Override
+	protected ArrayList<LoggerAppenderStruct> setupLogging(Level logLevel) throws CoreException {
+		ArrayList<LoggerAppenderStruct> loggerList = super.setupLogging(logLevel);
+		loggerList.add(setupLogger("de.uka.ipd.sdq.codegen", logLevel, Level.DEBUG == logLevel ? DETAILED_LOG_PATTERN : SHORT_LOG_PATTERN));
+		loggerList.add(setupLogger("de.uka.ipd.sdq.simucomframework", logLevel, Level.DEBUG == logLevel ? DETAILED_LOG_PATTERN : SHORT_LOG_PATTERN));
+		
+		return loggerList;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seede.uka.ipd.sdq.codegen.runconfig.LaunchConfigurationDelegate#
+	 * createRunCompositeJob
+	 * (de.uka.ipd.sdq.codegen.runconfig.AttributesGetMethods)
+	 */
+	@Override
+	protected IJob createWorkflowJob(SimuComWorkflowConfiguration config,
+			final ILaunch launch) throws CoreException {
+		IDebugListener listener = null;
+		
+		if (config.isDebug()) {
+			listener = new SimulationDebugListener(launch);
+			/*listener = new IDebugListener() {
+
+				private SimulationDebugTarget target = null;
+
+				public void simulationStartsInDock(DockModel dock) {
+					target   = new SimulationDebugTarget(launch,dock);
+					launch.addDebugTarget(target);
+				}
+
+				public void simulationStoppedInDock() {
+					if (target != null) {
+						// Wait for termination, needed as termination is reported via async events by the dock
+						while (!target.isTerminated()) {
+							try {
+								Thread.sleep(100);
+							} catch (InterruptedException e) {
+							}
+						}
+						launch.removeDebugTarget(target);
+						target.dispose();
+					}
+				}
+
+			};*/
+		}
+		return new SimuComJob(config,listener);
+	}	
+}
---
+++ PalladioCM.CodeGen/branches/Confidence/de.uka.ipd.sdq.codegen.simucontroller/src/de/uka/ipd/sdq/codegen/simucontroller/workflow/jobs/TransformPCMForSensitivityAnalysisJob.java
@@ -0,0 +1,108 @@
+package de.uka.ipd.sdq.codegen.simucontroller.workflow.jobs;
+
+import java.util.Iterator;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+
+import de.uka.ipd.sdq.codegen.simucontroller.workflow.blackboard.PCMResourceSetPartition;
+import de.uka.ipd.sdq.pcm.core.PCMRandomVariable;
+import de.uka.ipd.sdq.pcm.usagemodel.ClosedWorkload;
+import de.uka.ipd.sdq.workflow.IBlackboardInteractingJob;
+import de.uka.ipd.sdq.workflow.exceptions.JobFailedException;
+import de.uka.ipd.sdq.workflow.exceptions.RollbackFailedException;
+import de.uka.ipd.sdq.workflow.exceptions.UserCanceledException;
+import de.uka.ipd.sdq.workflow.mdsd.blackboard.MDSDBlackboard;
+
+public class TransformPCMForSensitivityAnalysisJob implements
+		IBlackboardInteractingJob<MDSDBlackboard> {
+
+	private MDSDBlackboard blackboard;
+	private int runNo;
+	private double step;
+	private String variable;
+	private double min;
+	private Logger logger = Logger.getLogger(TransformPCMForSensitivityAnalysisJob.class);
+	
+	public TransformPCMForSensitivityAnalysisJob(
+			String variable, int runNo, double step, double min) throws JobFailedException {
+		super();
+		this.runNo = runNo;
+		this.step = step;
+		this.variable = variable;
+		this.min = min;
+		if (variable != null && !variable.equals(""))
+			throw new JobFailedException("Bad Sensitivity Analysis Job Configuration");
+	}
+
+	/* (non-Javadoc)
+	 * @see de.uka.ipd.sdq.codegen.workflow.IBlackboardInteractingJob#setBlackbard(de.uka.ipd.sdq.codegen.workflow.Blackboard)
+	 */
+	public void setBlackboard(MDSDBlackboard blackboard) {
+		this.blackboard = blackboard;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.uka.ipd.sdq.codegen.workflow.IJob#execute(org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	public void execute(IProgressMonitor monitor) throws JobFailedException,
+			UserCanceledException {
+		PCMResourceSetPartition pcmPartition = (PCMResourceSetPartition) this.blackboard.getPartition(LoadPCMModelsIntoBlackboardJob.PCM_MODELS_PARTITION_ID);
+		
+		EObject o = null;
+		for(Resource r : pcmPartition.getResourceSet().getResources()) {
+			if (o == null) {
+				o = findEObject(r.getContents().get(0));
+				if ( o != null ) {
+					logger.info("Altering PCM variable for sensitivity analysis");
+					alterObject(o);
+				}
+			}
+		}
+		if (o == null)
+			throw new JobFailedException("Sensistivity analaysis transformation was unable to find the sensitivity variable");
+		
+	}
+
+	public void rollback(IProgressMonitor monitor)
+			throws RollbackFailedException {
+	}
+
+	public String getName() {
+		return "Inplace Transformation job for Sensitivity Analysis";
+	}
+	
+	// ---
+
+	private void alterObject(EObject o) {
+		int modifier = (int)(min + runNo * step);
+		if (o instanceof PCMRandomVariable) {
+			PCMRandomVariable pcmRandVar = (PCMRandomVariable) o;
+			String spec = pcmRandVar.getSpecification();
+			spec += " " + (modifier < 0 ? " - " + Math.abs(modifier) : " + " + Math.abs(modifier));
+			pcmRandVar.setSpecification(
+				spec	
+				);
+		}
+		if (o instanceof ClosedWorkload) {
+			ClosedWorkload cw = (ClosedWorkload) o;
+			int number = cw.getPopulation();
+			number += modifier;
+			cw.setPopulation(number);
+		}
+	}
+
+	private EObject findEObject(EObject object) {
+		Iterator<EObject> it = object.eAllContents();
+		while (it.hasNext()) {
+			EObject eo = it.next();
+			if (EcoreUtil.getURI(eo).toString().equals(variable))
+				return eo;
+		}
+		return null;
+	}
+
+}
