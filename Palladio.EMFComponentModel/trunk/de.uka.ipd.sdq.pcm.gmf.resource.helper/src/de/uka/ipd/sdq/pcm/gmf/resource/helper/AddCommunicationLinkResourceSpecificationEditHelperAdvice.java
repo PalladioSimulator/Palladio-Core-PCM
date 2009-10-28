97397ba66dddf2622957cccb7775b4883ee87a9e
tom.beyer <dummy>
2009-10-28 15:08:05 +0000

    Resource Helper
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@7593 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.resource.helper/src/de/uka/ipd/sdq/pcm/gmf/resource/helper/AddCommunicationLinkResourceSpecificationEditHelperAdvice.java
@@ -0,0 +1,7 @@
+package de.uka.ipd.sdq.pcm.gmf.resource.helper;
+
+import org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelper;
+
+public class AddCommunicationLinkResourceSpecificationEditHelperAdvice extends AbstractEditHelper {
+
+}
