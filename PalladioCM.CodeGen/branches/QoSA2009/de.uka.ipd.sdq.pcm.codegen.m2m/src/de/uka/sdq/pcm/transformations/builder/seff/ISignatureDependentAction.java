84f4ea14e41877531f762985d64484ce5462d3ee
mhauck <hauck@fzi.de>
2009-10-29 11:55:45 +0000

    Has been merged with head
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@7617 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- PalladioCM.CodeGen/branches/QoSA2009/de.uka.ipd.sdq.pcm.codegen.m2m/src/de/uka/sdq/pcm/transformations/builder/seff/ISignatureDependentAction.java
+++
@@ -1,9 +0,0 @@
-package de.uka.sdq.pcm.transformations.builder.seff;
-
-import de.uka.ipd.sdq.pcm.repository.Signature;
-
-public interface ISignatureDependentAction {
-
-	void setCurrentSignature(Signature sig);
-
-}
