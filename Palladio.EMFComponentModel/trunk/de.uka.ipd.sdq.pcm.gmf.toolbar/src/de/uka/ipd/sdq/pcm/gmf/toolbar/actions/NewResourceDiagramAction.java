233c580054f164f1f299d83fb4f046e5fab1924c
tom.beyer <dummy>
2009-10-29 21:43:24 +0000

    Fixed NewResourceDiagram-Button in Toolbar
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@7623 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.toolbar/src/de/uka/ipd/sdq/pcm/gmf/toolbar/actions/NewResourceDiagramAction.java
+++ Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.toolbar/src/de/uka/ipd/sdq/pcm/gmf/toolbar/actions/NewResourceDiagramAction.java
@@ -3,13 +3,13 @@ package de.uka.ipd.sdq.pcm.gmf.toolbar.actions;
 import org.eclipse.ui.INewWizard;
 
 import de.uka.ipd.sdq.pcm.gmf.toolbar.BaseNewDiagramAction;
-//import de.uka.ipd.sdq.pcm.gmf.resource.part.PalladioComponentModelCreationWizard;
+import de.uka.ipd.sdq.pcm.gmf.resource.part.PalladioComponentModelCreationWizard;
 
 public class NewResourceDiagramAction
 	extends BaseNewDiagramAction {
 
 	protected INewWizard getNewWizard() {
-		throw new RuntimeException("not running with eclipse 3.4 / gmf 2.1");
-		//return new PalladioComponentModelCreationWizard(); //FIXME: re-enable
+//		throw new RuntimeException("not running with eclipse 3.4 / gmf 2.1");
+		return new PalladioComponentModelCreationWizard(); //FIXME: re-enable
 	}
 }
