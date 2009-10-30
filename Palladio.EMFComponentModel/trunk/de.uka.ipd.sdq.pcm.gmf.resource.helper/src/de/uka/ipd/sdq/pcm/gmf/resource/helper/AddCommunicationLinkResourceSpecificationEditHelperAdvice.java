d693754a0d2e12a82385a478d5447afc047b2db8
tom.beyer <dummy>
2009-10-30 16:13:41 +0000

    Updated Resource Helper
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@7631 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.resource.helper/src/de/uka/ipd/sdq/pcm/gmf/resource/helper/AddCommunicationLinkResourceSpecificationEditHelperAdvice.java
+++ Palladio.EMFComponentModel/trunk/de.uka.ipd.sdq.pcm.gmf.resource.helper/src/de/uka/ipd/sdq/pcm/gmf/resource/helper/AddCommunicationLinkResourceSpecificationEditHelperAdvice.java
@@ -1,8 +1,13 @@
 package de.uka.ipd.sdq.pcm.gmf.resource.helper;
 
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gmf.runtime.common.core.command.ICommand;
 import org.eclipse.gmf.runtime.emf.type.core.commands.SetValueCommand;
+import org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelperAdvice;
 import org.eclipse.gmf.runtime.emf.type.core.edithelper.IEditHelperAdvice;
+import org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest;
 import org.eclipse.gmf.runtime.emf.type.core.requests.IEditCommandRequest;
 import org.eclipse.gmf.runtime.emf.type.core.requests.SetRequest;
 
@@ -16,35 +21,44 @@ import de.uka.ipd.sdq.pcm.seff.LoopAction;
 import de.uka.ipd.sdq.pcm.seff.SeffPackage;
 
 public class AddCommunicationLinkResourceSpecificationEditHelperAdvice extends
-		AbstractCreateCommunicationLinkResourceSpecificationEditHelperAdvice implements IEditHelperAdvice {
+		AbstractEditHelperAdvice implements IEditHelperAdvice {
 
 	@Override
 	public ICommand getAfterEditCommand(IEditCommandRequest request) {
-		CommunicationLinkResourceSpecification spec = ResourceenvironmentFactory.eINSTANCE.createCommunicationLinkResourceSpecification();
-		
-		SetRequest setRequest = new SetRequest((LinkingResource) request.getElementsToEdit()
-				, ResourceenvironmentPackage.eINSTANCE
-				.getLinkingResource_CommunicationLinkResourceSpecifications_LinkingResource(), spec);
+		CommunicationLinkResourceSpecification spec = ResourceenvironmentFactory.eINSTANCE
+				.createCommunicationLinkResourceSpecification();
+		SetRequest setRequest = new SetRequest(
+				(EObject) request.getElementsToEdit().get(0),
+				ResourceenvironmentPackage.eINSTANCE
+						.getLinkingResource_CommunicationLinkResourceSpecifications_LinkingResource(),
+				spec);
 
 		return new SetValueCommand(setRequest);
 	}
 
-	@Override
-	public ICommand getBeforeEditCommand(IEditCommandRequest request) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+//	@Override
+//	public ICommand getBeforeEditCommand(IEditCommandRequest request) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public void configureRequest(IEditCommandRequest request) {
+//		//
+//	}
 
-	@Override
-	public boolean approveRequest(IEditCommandRequest request) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void configureRequest(IEditCommandRequest request) {
-		// TODO Auto-generated method stub
-
-	}
+//	@Override
+//	protected ICommand getAfterConfigureCommand(ConfigureRequest request) {
+//		CommunicationLinkResourceSpecification spec = ResourceenvironmentFactory.eINSTANCE
+//				.createCommunicationLinkResourceSpecification();
+//
+//		SetRequest setRequest = new SetRequest(
+//				(LinkingResource) request.getElementsToEdit(),
+//				ResourceenvironmentPackage.eINSTANCE
+//						.getLinkingResource_CommunicationLinkResourceSpecifications_LinkingResource(),
+//				spec);
+//
+//		return new SetValueCommand(setRequest);
+//	}
 
 }
