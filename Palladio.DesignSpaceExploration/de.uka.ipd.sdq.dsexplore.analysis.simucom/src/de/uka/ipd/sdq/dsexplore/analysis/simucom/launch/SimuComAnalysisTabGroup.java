f0e82958e7f1b52a06282458d87147bba8cd57cf
AnneKoziolek <anne.koziolek@kit.edu>
2009-11-03 15:43:09 +0000

    Reverted that SimuComAnalysisTabGroup inherits from SimuTabGroup, because that includes he FileNameInputTab twice and causes problems when applying changes. Now SimuComAnalysisTabGroup has again its own implementation of  createTabs()
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@7686 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Palladio.DesignSpaceExploration/de.uka.ipd.sdq.dsexplore.analysis.simucom/src/de/uka/ipd/sdq/dsexplore/analysis/simucom/launch/SimuComAnalysisTabGroup.java
+++ Palladio.DesignSpaceExploration/de.uka.ipd.sdq.dsexplore.analysis.simucom/src/de/uka/ipd/sdq/dsexplore/analysis/simucom/launch/SimuComAnalysisTabGroup.java
@@ -1,13 +1,30 @@
 package de.uka.ipd.sdq.dsexplore.analysis.simucom.launch;
 
-import de.uka.ipd.sdq.codegen.simucontroller.runconfig.SimuTabGroup;
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import org.eclipse.debug.ui.CommonTab;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
 
-/**
- * Provides a configuration tab group for the SimuCom analysis method.
- * 
- * @author pmerkle
- * 
- */
-public class SimuComAnalysisTabGroup extends SimuTabGroup {
-	
+import de.uka.ipd.sdq.codegen.simucontroller.runconfig.FeatureOptionsTab;
+import de.uka.ipd.sdq.codegen.simucontroller.runconfig.SimuComConfigurationTab;
+import de.uka.ipd.sdq.codegen.simucontroller.runconfig.SimuConfigurationTab;
+
+ /**
+  * Provides a configuration tab group for the SimuCom analysis method.
+  * 
+  * @author pmerkle
+  * 
+  */
+public class SimuComAnalysisTabGroup extends AbstractLaunchConfigurationTabGroup {
+
+	@Override
+	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
+		ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] {
+				new SimuComConfigurationTab(), 
+				new SimuConfigurationTab(),
+				new FeatureOptionsTab(),
+				new CommonTab()
+		};
+		setTabs(tabs);
+	}
 }
