173f981938bed80d802827d441b7a05860a19240
AnneKoziolek <anne.koziolek@kit.edu>
2009-11-03 09:45:00 +0000

    deleted PCMResourceSetPartition.java from this plugin, because it was moved to de.uka.ipd.sdq.workflow.pcm
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@7681 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- PalladioCM.CodeGen/trunk/de.uka.ipd.sdq.codegen.simucontroller/src/de/uka/ipd/sdq/pcm/transformations/ApplyConnectorCompletionsJob.java
+++ PalladioCM.CodeGen/trunk/de.uka.ipd.sdq.codegen.simucontroller/src/de/uka/ipd/sdq/pcm/transformations/ApplyConnectorCompletionsJob.java
@@ -7,8 +7,8 @@ import org.eclipse.emf.ecore.resource.Resource;
 
 import de.uka.ipd.sdq.codegen.simucontroller.SimuControllerPlugin;
 import de.uka.ipd.sdq.codegen.simucontroller.runconfig.SimuComWorkflowConfiguration;
-import de.uka.ipd.sdq.codegen.simucontroller.workflow.blackboard.PCMResourceSetPartition;
 import de.uka.ipd.sdq.codegen.simucontroller.workflow.jobs.LoadMiddlewareConfigurationIntoBlackboardJob;
+import de.uka.ipd.sdq.workflow.pcm.blackboard.PCMResourceSetPartition;
 import de.uka.ipd.sdq.workflow.pcm.jobs.LoadPCMModelsIntoBlackboardJob;
 import de.uka.ipd.sdq.featureconfig.Configuration;
 import de.uka.ipd.sdq.pcm.allocation.AllocationContext;
