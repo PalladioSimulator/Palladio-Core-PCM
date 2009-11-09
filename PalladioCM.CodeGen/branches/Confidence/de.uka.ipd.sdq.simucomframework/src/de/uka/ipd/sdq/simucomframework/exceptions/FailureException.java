182df0204b4c4e633755ee1e74851cd3652165f7
AnneKoziolek <anne.koziolek@kit.edu>
2009-11-09 09:43:28 +0000

    branch has been merged to the trunk
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code@7880 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- PalladioCM.CodeGen/branches/Confidence/de.uka.ipd.sdq.simucomframework/src/de/uka/ipd/sdq/simucomframework/exceptions/FailureException.java
+++
@@ -1,9 +0,0 @@
-package de.uka.ipd.sdq.simucomframework.exceptions;
-
-/**
- * Represents a failure inside the simulated system. 
- */
-public class FailureException extends RuntimeException {
-	private static final long serialVersionUID = -6074335938145682592L;
-
-}
