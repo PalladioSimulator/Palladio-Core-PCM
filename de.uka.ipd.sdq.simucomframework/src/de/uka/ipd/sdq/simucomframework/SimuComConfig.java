8d720fb610f8a4a8e31215aa245eae06333963a2
hubern <niko.huber@gmail.com>
2010-03-15 13:06:48 +0000

    Branch of latest trunk to finally integrate the scheduler
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/branches/SchedulerIntegrationNew@8624 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ de.uka.ipd.sdq.simucomframework/src/de/uka/ipd/sdq/simucomframework/SimuComConfig.java
@@ -0,0 +1,153 @@
+package de.uka.ipd.sdq.simucomframework;
+
+import java.io.Serializable;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.URI;
+
+/**
+ * @author roman
+ * 
+ * The class encapsulates all configuration options for SimuCom.
+ */
+public class SimuComConfig implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3364130550065874984L;
+	
+	public static final String DATASOURCE_ID = "datasourceID";
+	public static final String SHOULD_THROW_EXCEPTION = "shouldThrowException";
+	public static final String MAXIMUM_MEASUREMENT_COUNT = "maximumMeasurementCount";
+	
+	/** SimuCom configuration tab */
+	public static String EXPERIMENT_RUN = "experimentRun";
+	public static String SIMULATION_TIME = "simTime";
+	public static String VERBOSE_LOGGING = "verboseLogging";
+	public static String SIMULATE_FAILURES = "simulateFailures";
+	public static String USE_CONFIDENCE = "useConfidenceStopCondition";
+	public static String CONFIDENCE_LEVEL = "confidenceLevel";
+	public static String CONFIDENCE_HALFWIDTH = "confidenceHalfWidth";
+	public static String CONFIDENCE_MODELELEMENT_URI = "confidenceModelElementURI";
+	public static String CONFIDENCE_MODELELEMENT_NAME = "confidenceModelElementName";
+	
+	/** configuration options */
+	private String nameExperimentRun;
+	private long simuTime;
+	private boolean verboseLogging;
+	private boolean simulateFailures = false;
+	private long datasourceID;
+	private Integer runNumber;
+	private Long maxMeasurementsCount;
+	private boolean isDebug;
+	private boolean useConfidence = false;
+	private int confidenceLevel = 0;
+	private int confidenceHalfWidth = 0;
+	private URI confidenceModelElementURI;
+	private String confidenceModelElementName;
+
+	/**
+	 * @param configuration a map which maps configuration option IDs to their values. 
+	 * The required keys are SimuComConfig.EXPERIMENT_RUN, SimuComConfig.SIMULATION_TIME 
+	 * SimuComConfig.MAXIMUM_MEASUREMENT_COUNT SimuComConfig.VERBOSE_LOGGING, 
+	 * SimuComConfig.DATASOURCE_ID. Optional keys are SimuComConfig.SIMULATE_FAILURES and
+	 * USE_CONFIDENCE. If USE_CONFIDENCE is set to true, you also need to set 
+	 * SimuComConfig.CONFIDENCE_LEVEL, SimuComConfig.CONFIDENCE_HALFWIDTH, 
+	 * SimuComConfig.CONFIDENCE_MODELELEMENT_NAME, SimuComConfig.CONFIDENCE_MODELELEMENT_URI 
+	 * 
+	 */
+	public SimuComConfig(Map<String,Object> configuration, int runNo, boolean debug){
+		try {
+			this.nameExperimentRun = (String) configuration.get(
+					EXPERIMENT_RUN);
+			this.simuTime = Long.valueOf((String)configuration.get(
+					SIMULATION_TIME));
+			this.maxMeasurementsCount = Long.valueOf((String)configuration.get(
+					MAXIMUM_MEASUREMENT_COUNT));
+			this.verboseLogging = (Boolean)configuration.get(
+					VERBOSE_LOGGING);
+			this.datasourceID = (Integer)configuration.get(
+					DATASOURCE_ID);
+			this.runNumber = runNo;
+			this.isDebug = debug;
+			
+			if (configuration.containsKey(SIMULATE_FAILURES)){
+				this.simulateFailures = (Boolean)configuration.get(
+						SIMULATE_FAILURES);
+			}
+			
+			// confidence information is optional in the map. It this.useConfidence defaults to false.
+			if (configuration.containsKey(USE_CONFIDENCE)) {
+				this.useConfidence = (Boolean) configuration
+						.get(USE_CONFIDENCE);
+				this.confidenceLevel = Integer.valueOf((String) configuration
+						.get(CONFIDENCE_LEVEL));
+				this.confidenceHalfWidth = Integer
+						.valueOf((String) configuration
+								.get(CONFIDENCE_HALFWIDTH));
+				this.confidenceModelElementName = (String) configuration
+						.get(CONFIDENCE_MODELELEMENT_NAME);
+				this.confidenceModelElementURI = URI
+						.createURI((String) configuration
+								.get(CONFIDENCE_MODELELEMENT_URI));
+
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("Setting up properties failed, please check launch config (check all tabs).", e);
+		}
+	}
+
+	public String getNameExperimentRun() {
+		return nameExperimentRun + " RunNo. "+runNumber;
+	}
+
+	public long getSimuTime() {
+		return simuTime;
+	}
+
+	public long getMaxMeasurementsCount() {
+		return maxMeasurementsCount;
+	}
+	
+	public boolean getVerboseLogging() {
+		return verboseLogging || isDebug;
+	}
+	
+	public boolean getSimulateFailures() {
+		return simulateFailures;
+	}
+
+	public long getDatasourceID() {
+		return this.datasourceID;
+	}
+
+	public boolean isDebug() {
+		return this.isDebug;
+	}
+	
+	public boolean isUseConfidence() {
+		return useConfidence;
+	}
+
+	public int getConfidenceLevel() {
+		return confidenceLevel;
+	}
+
+	public int getConfidenceHalfWidth() {
+		return confidenceHalfWidth;
+	}
+	
+	public String getConfidenceModelElementName() {
+		return confidenceModelElementName;
+	}
+	
+	public URI getConfidenceModelElementURI() {
+		return confidenceModelElementURI;
+	}
+
+	public String getEngine() {
+		return "de.uka.ipd.sdq.simucomframework.ssj.SSJSimEngineFactory";
+	}
+
+}
---
+++ de.uka.ipd.sdq.simucomframework/src/de/uka/ipd/sdq/simucomframework/abstractSimEngine/SimProcess.java
@@ -0,0 +1,117 @@
+package de.uka.ipd.sdq.simucomframework.abstractSimEngine;
+
+import org.apache.log4j.Logger;
+
+import de.uka.ipd.sdq.scheduler.ISchedulableProcess;
+import de.uka.ipd.sdq.simucomframework.SimuComResult;
+import de.uka.ipd.sdq.simucomframework.model.SimuComModel;
+import de.uka.ipd.sdq.simucomframework.simucomstatus.Process;
+import de.uka.ipd.sdq.simucomframework.simucomstatus.SimucomstatusFactory;
+import de.uka.ipd.sdq.simucomframework.simucomstatus.WaitForDelay;
+
+public abstract class SimProcess extends SimulationElement 
+implements ISimProcessDelegate, ISchedulableProcess {
+
+	protected static Logger logger = 
+		Logger.getLogger(SimProcess.class.getName());
+	
+	ISimProcessDelegate delegate = null;
+	private Process processStatus = null;
+
+	private boolean isDebug;
+	
+	protected SimProcess(SimuComModel model, String name) {
+		super(model, name);
+		delegate = model.getSimEngineFactory().createSimProcess(this,model,name);
+		isDebug = model.getConfig().isDebug();
+	}
+
+	/* (non-Javadoc)
+	 * @see de.uka.ipd.sdq.simucomframework.abstractSimEngine.ISimProcessDelegate#lifeCycle()
+	 */
+	public final void lifeCycle() {
+		addProcessToSimStatus();
+		try {
+			this.internalLifeCycle();
+		} catch (Exception e) {
+			logger.warn("Simulation caused an exception. Caught it in SimProcess Lifecycle Method",e);
+			((SimuComModel)getModel()).setStatus(SimuComResult.ERROR,
+					e);
+			logger.debug("Trying to stop simulation now...");
+			this.getModel().getSimulationControl().stop();
+		}
+		removeProcessFromSimStatus();
+	}
+
+	/**
+	 * 
+	 */
+	protected void removeProcessFromSimStatus() {
+		logger.debug("Terminating SimProcess "+this.getName());
+		if (isDebug) {
+			this.getModel().getSimulationStatus().getProcessStatus().getProcesses().remove(processStatus);
+		}
+	}
+
+	/**
+	 * Clients may override default behaviour, e.g., PassiveResource
+	 */
+	protected void addProcessToSimStatus() {
+		logger.debug("Starting simulation process "+this.getName());
+
+		if (isDebug) {
+			processStatus = SimucomstatusFactory.eINSTANCE.createProcess();
+			this.getModel().getSimulationStatus().getProcessStatus().getProcesses().add(processStatus);
+			processStatus.setId(this.getName());
+			processStatus.setProcessStartTime(this.getModel().getSimulationControl().getCurrentSimulationTime());
+		}
+	}
+	
+	protected abstract void internalLifeCycle();
+
+	// Strategy dependent methods
+
+	public void hold(double d) {
+		if (isDebug) {
+			WaitForDelay action = SimucomstatusFactory.eINSTANCE.createWaitForDelay();
+			action.setActionStartTime(this.getModel().getSimulationControl().getCurrentSimulationTime());
+			action.setDelay(d);
+			this.getSimProcessStatus().setCurrentAction(action);
+		}
+		delegate.hold(d);
+	}
+
+	public boolean isTerminated() {
+		return delegate.isTerminated();
+	}
+
+	public void passivate() {
+		delegate.passivate();
+	}
+	
+	public void activate() {
+		this.scheduleAt(0);
+	}
+
+	public void scheduleAt(double d) {
+		delegate.scheduleAt(d);
+	}
+	
+	public String getId() {
+		return Thread.currentThread().getName();
+	}
+	
+	public ISchedulableProcess getRootProcess(){
+		// TODO: What is expected here?
+		return null;
+	}
+	
+	public boolean isFinished() {
+		return isTerminated();
+	}
+
+	public Process getSimProcessStatus() {
+		return this.processStatus;
+	}
+	
+}
