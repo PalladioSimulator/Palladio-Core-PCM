d5dc5ff29d31bef24b20bdd71ffe5c1f4378d684
kelsaka <krogmann@fzi.de>
2011-03-07 17:15:43 +0000

    trunk shouldn't be here
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/PalladioCM.CodeGen/tags/PCM_REL_3.2@11185 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- trunk/de.uka.ipd.sdq.pcm.codegen.m2m/src/de/uka/sdq/pcm/transformations/Helper.java
+++
@@ -1,7 +0,0 @@
-package de.uka.sdq.pcm.transformations;
-
-public class Helper {
-	public static String removeAllSpecialChars(String s) {
-		return s.replace('\n',' ').replace('\t', ' ').replace('\r', ' ');
-	}
-}
--- trunk/de.uka.ipd.sdq.simucomframework/src/de/uka/ipd/sdq/simucomframework/resources/IDemandListener.java
+++
@@ -1,7 +0,0 @@
-package de.uka.ipd.sdq.simucomframework.resources;
-
-public interface IDemandListener {
-
-	public void demand(double demand);
-	
-}
