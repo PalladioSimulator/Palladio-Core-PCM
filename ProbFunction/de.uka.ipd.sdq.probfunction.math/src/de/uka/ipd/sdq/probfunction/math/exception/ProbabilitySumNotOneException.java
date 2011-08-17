72f7dee0b3e70635d4904b022cddee2e88b366ff
mhauck <hauck@fzi.de>
2011-08-17 12:33:30 +0000

    Move probfunction projects to Palladio/Core/tags/Experiment2/ProbFunction
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/tags/Experiment2@12732 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ ProbFunction/de.uka.ipd.sdq.probfunction.math/src/de/uka/ipd/sdq/probfunction/math/exception/ProbabilitySumNotOneException.java
@@ -0,0 +1,10 @@
+package de.uka.ipd.sdq.probfunction.math.exception;
+
+public class ProbabilitySumNotOneException extends ProbabilityFunctionException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8883581276507990208L;
+
+}
