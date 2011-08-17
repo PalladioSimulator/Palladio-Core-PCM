8668853b4d06e0e2cd81768b93a5963ff9feab41
mhauck <hauck@fzi.de>
2011-08-17 16:03:07 +0000

    Move PalladioCM.EMFAnalyticalModel/trunk/* to Palladio/Core/trunk/Solver
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@13043 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ Solver/de.uka.ipd.sdq.spa.resourcemodel/src/de/uka/ipd/sdq/spa/resourcemodel/impl/PassiveResourceImpl.java
@@ -0,0 +1,42 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.spa.resourcemodel.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import de.uka.ipd.sdq.spa.resourcemodel.PassiveResource;
+import de.uka.ipd.sdq.spa.resourcemodel.ResourceModelPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Passive Resource</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class PassiveResourceImpl extends ContentionResourceImpl implements PassiveResource {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected PassiveResourceImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected EClass eStaticClass() {
+		return ResourceModelPackage.Literals.PASSIVE_RESOURCE;
+	}
+
+} //PassiveResourceImpl
