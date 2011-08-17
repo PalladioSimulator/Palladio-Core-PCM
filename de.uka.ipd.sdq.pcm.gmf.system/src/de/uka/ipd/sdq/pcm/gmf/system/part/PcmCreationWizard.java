07809eae0df4bff86858aa51b823e56800f7ee5d
mhauck <hauck@fzi.de>
2011-08-17 09:47:53 +0000

    Move editor projects to Palladio/Core/tags/BeforeNewSystemGMFEditor/Editors
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/tags/BeforeNewSystemGMFEditor@12554 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ de.uka.ipd.sdq.pcm.gmf.system/src/de/uka/ipd/sdq/pcm/gmf/system/part/PcmCreationWizard.java
@@ -0,0 +1,36 @@
+/*
+ *by SDQ, IPD, U Karlrsuhe (TH), 2006
+ */
+package de.uka.ipd.sdq.pcm.gmf.system.part;
+
+import org.eclipse.gmf.runtime.diagram.ui.resources.editor.ide.wizards.EditorCreationWizard;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbench;
+
+/**
+ * @generated
+ */
+public class PcmCreationWizard extends EditorCreationWizard {
+
+	/**
+	 * @generated
+	 */
+	public void addPages() {
+		super.addPages();
+		if (page == null) {
+			page = new PcmCreationWizardPage(getWorkbench(), getSelection());
+		}
+		addPage(page);
+	}
+
+	/**
+	 * @generated
+	 */
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		super.init(workbench, selection);
+		setWindowTitle("New system Diagram"); //$NON-NLS-1$
+		setDefaultPageImageDescriptor(SystemDiagramEditorPlugin
+				.getBundledImageDescriptor("icons/wizban/NewSystemWizard.gif")); //$NON-NLS-1$
+		setNeedsProgressMonitor(true);
+	}
+}
---
+++ de.uka.ipd.sdq.pcm.gmf.system/src/de/uka/ipd/sdq/pcm/gmf/system/providers/PcmViewProvider.java
@@ -0,0 +1,161 @@
+/*
+ *by SDQ, IPD, U Karlrsuhe (TH), 2006
+ */
+package de.uka.ipd.sdq.pcm.gmf.system.providers;
+
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.diagram.core.providers.AbstractViewProvider;
+import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.gmf.runtime.emf.type.core.IElementType;
+import de.uka.ipd.sdq.pcm.gmf.system.edit.parts.AssemblyConnectorEditPart;
+import de.uka.ipd.sdq.pcm.gmf.system.edit.parts.AssemblyContextComponent_Context_CompartmentEditPart;
+import de.uka.ipd.sdq.pcm.gmf.system.edit.parts.AssemblyContextEditPart;
+import de.uka.ipd.sdq.pcm.gmf.system.edit.parts.AssemblyContextEntityNameEditPart;
+import de.uka.ipd.sdq.pcm.gmf.system.edit.parts.ComponentEditPart;
+import de.uka.ipd.sdq.pcm.gmf.system.edit.parts.ProvidedDelegationConnectorEditPart;
+import de.uka.ipd.sdq.pcm.gmf.system.edit.parts.ReqRoleEditPart;
+import de.uka.ipd.sdq.pcm.gmf.system.edit.parts.ReqRoleLabelEditPart;
+import de.uka.ipd.sdq.pcm.gmf.system.edit.parts.RequiredDelegationConnectorEditPart;
+import de.uka.ipd.sdq.pcm.gmf.system.edit.parts.RoleEditPart;
+import de.uka.ipd.sdq.pcm.gmf.system.edit.parts.RoleLabel2EditPart;
+import de.uka.ipd.sdq.pcm.gmf.system.edit.parts.RoleLabelEditPart;
+import de.uka.ipd.sdq.pcm.gmf.system.edit.parts.System2EditPart;
+import de.uka.ipd.sdq.pcm.gmf.system.edit.parts.SystemEditPart;
+import de.uka.ipd.sdq.pcm.gmf.system.edit.parts.SystemProvidedRoleEditPart;
+import de.uka.ipd.sdq.pcm.gmf.system.edit.parts.SystemProvidedRoleEntityNameEditPart;
+import de.uka.ipd.sdq.pcm.gmf.system.edit.parts.SystemRequiredRoleEditPart;
+
+import de.uka.ipd.sdq.pcm.gmf.system.edit.parts.SystemRequiredRoleEntityNameEditPart;
+
+import de.uka.ipd.sdq.pcm.gmf.system.part.PcmVisualIDRegistry;
+
+import de.uka.ipd.sdq.pcm.gmf.system.view.factories.AssemblyConnectorViewFactory;
+import de.uka.ipd.sdq.pcm.gmf.system.view.factories.AssemblyContextComponent_Context_CompartmentViewFactory;
+import de.uka.ipd.sdq.pcm.gmf.system.view.factories.AssemblyContextEntityNameViewFactory;
+import de.uka.ipd.sdq.pcm.gmf.system.view.factories.AssemblyContextViewFactory;
+import de.uka.ipd.sdq.pcm.gmf.system.view.factories.ComponentViewFactory;
+import de.uka.ipd.sdq.pcm.gmf.system.view.factories.ProvidedDelegationConnectorViewFactory;
+import de.uka.ipd.sdq.pcm.gmf.system.view.factories.ReqRoleLabelViewFactory;
+import de.uka.ipd.sdq.pcm.gmf.system.view.factories.ReqRoleViewFactory;
+import de.uka.ipd.sdq.pcm.gmf.system.view.factories.RequiredDelegationConnectorViewFactory;
+import de.uka.ipd.sdq.pcm.gmf.system.view.factories.RoleLabel2ViewFactory;
+import de.uka.ipd.sdq.pcm.gmf.system.view.factories.RoleLabelViewFactory;
+import de.uka.ipd.sdq.pcm.gmf.system.view.factories.RoleViewFactory;
+import de.uka.ipd.sdq.pcm.gmf.system.view.factories.System2ViewFactory;
+import de.uka.ipd.sdq.pcm.gmf.system.view.factories.SystemProvidedRoleEntityNameViewFactory;
+import de.uka.ipd.sdq.pcm.gmf.system.view.factories.SystemProvidedRoleViewFactory;
+import de.uka.ipd.sdq.pcm.gmf.system.view.factories.SystemRequiredRoleEntityNameViewFactory;
+import de.uka.ipd.sdq.pcm.gmf.system.view.factories.SystemRequiredRoleViewFactory;
+import de.uka.ipd.sdq.pcm.gmf.system.view.factories.SystemViewFactory;
+
+/**
+ * @generated
+ */
+public class PcmViewProvider extends AbstractViewProvider {
+
+	/**
+	 * @generated
+	 */
+	protected Class getDiagramViewClass(IAdaptable semanticAdapter,
+			String diagramKind) {
+		EObject semanticElement = getSemanticElement(semanticAdapter);
+		if (SystemEditPart.MODEL_ID.equals(diagramKind)
+				&& PcmVisualIDRegistry.getDiagramVisualID(semanticElement) != -1) {
+			return SystemViewFactory.class;
+		}
+		return null;
+	}
+
+	/**
+	 * @generated
+	 */
+	protected Class getNodeViewClass(IAdaptable semanticAdapter,
+			View containerView, String semanticHint) {
+		if (containerView == null) {
+			return null;
+		}
+		IElementType elementType = getSemanticElementType(semanticAdapter);
+		if (elementType != null
+				&& !PcmElementTypes.isKnownElementType(elementType)) {
+			return null;
+		}
+		EClass semanticType = getSemanticEClass(semanticAdapter);
+		EObject semanticElement = getSemanticElement(semanticAdapter);
+		int nodeVID = PcmVisualIDRegistry.getNodeVisualID(containerView,
+				semanticElement, semanticType, semanticHint);
+		switch (nodeVID) {
+		case AssemblyContextEditPart.VISUAL_ID:
+			return AssemblyContextViewFactory.class;
+		case AssemblyContextEntityNameEditPart.VISUAL_ID:
+			return AssemblyContextEntityNameViewFactory.class;
+		case SystemProvidedRoleEditPart.VISUAL_ID:
+			return SystemProvidedRoleViewFactory.class;
+		case SystemProvidedRoleEntityNameEditPart.VISUAL_ID:
+			return SystemProvidedRoleEntityNameViewFactory.class;
+		case SystemRequiredRoleEditPart.VISUAL_ID:
+			return SystemRequiredRoleViewFactory.class;
+		case SystemRequiredRoleEntityNameEditPart.VISUAL_ID:
+			return SystemRequiredRoleEntityNameViewFactory.class;
+		case RoleEditPart.VISUAL_ID:
+			return RoleViewFactory.class;
+		case RoleLabelEditPart.VISUAL_ID:
+			return RoleLabelViewFactory.class;
+		case ReqRoleEditPart.VISUAL_ID:
+			return ReqRoleViewFactory.class;
+		case ReqRoleLabelEditPart.VISUAL_ID:
+			return ReqRoleLabelViewFactory.class;
+		}
+		return null;
+	}
+
+	/**
+	 * @generated
+	 */
+	protected Class getEdgeViewClass(IAdaptable semanticAdapter,
+			View containerView, String semanticHint) {
+		IElementType elementType = getSemanticElementType(semanticAdapter);
+		if (elementType != null
+				&& !PcmElementTypes.isKnownElementType(elementType)) {
+			return null;
+		}
+		EClass semanticType = getSemanticEClass(semanticAdapter);
+		if (semanticType == null) {
+			return null;
+		}
+		EObject semanticElement = getSemanticElement(semanticAdapter);
+		int linkVID = PcmVisualIDRegistry.getLinkWithClassVisualID(
+				semanticElement, semanticType);
+		switch (linkVID) {
+		case AssemblyConnectorEditPart.VISUAL_ID:
+			return AssemblyConnectorViewFactory.class;
+		case ProvidedDelegationConnectorEditPart.VISUAL_ID:
+			return ProvidedDelegationConnectorViewFactory.class;
+		case RequiredDelegationConnectorEditPart.VISUAL_ID:
+			return RequiredDelegationConnectorViewFactory.class;
+		}
+		return getUnrecognizedConnectorViewClass(semanticAdapter,
+				containerView, semanticHint);
+	}
+
+	/**
+	 * @generated
+	 */
+	private IElementType getSemanticElementType(IAdaptable semanticAdapter) {
+		if (semanticAdapter == null) {
+			return null;
+		}
+		return (IElementType) semanticAdapter.getAdapter(IElementType.class);
+	}
+
+	/**
+	 * @generated
+	 */
+	private Class getUnrecognizedConnectorViewClass(IAdaptable semanticAdapter,
+			View containerView, String semanticHint) {
+		// Handle unrecognized child node classes here
+		return null;
+	}
+
+}
