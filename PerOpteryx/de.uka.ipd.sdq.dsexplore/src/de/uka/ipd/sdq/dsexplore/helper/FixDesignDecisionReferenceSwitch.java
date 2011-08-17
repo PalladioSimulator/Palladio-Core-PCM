0a3aeb10b4e10ffbfc0d4bf07439aa40a1e73570
mhauck <hauck@fzi.de>
2011-08-17 13:13:46 +0000

    Move design space exploration projects to Palladio/Core/tags/PCM_3.2_RC1/PerOpteryx
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/tags/PCM_3.2_RC1@12822 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ PerOpteryx/de.uka.ipd.sdq.dsexplore/src/de/uka/ipd/sdq/dsexplore/helper/FixDesignDecisionReferenceSwitch.java
@@ -0,0 +1,252 @@
+package de.uka.ipd.sdq.dsexplore.helper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil.EqualityHelper;
+
+import de.uka.ipd.sdq.dsexplore.opt4j.start.Opt4JStarter;
+import de.uka.ipd.sdq.pcm.allocation.AllocationContext;
+import de.uka.ipd.sdq.pcm.core.composition.AssemblyContext;
+import de.uka.ipd.sdq.pcm.core.entity.Entity;
+import de.uka.ipd.sdq.pcm.designdecision.AllocationDegree;
+import de.uka.ipd.sdq.pcm.designdecision.AssembledComponentDegree;
+import de.uka.ipd.sdq.pcm.designdecision.Candidate;
+import de.uka.ipd.sdq.pcm.designdecision.Candidates;
+import de.uka.ipd.sdq.pcm.designdecision.CapacityDegree;
+import de.uka.ipd.sdq.pcm.designdecision.Choice;
+import de.uka.ipd.sdq.pcm.designdecision.DegreeOfFreedom;
+import de.uka.ipd.sdq.pcm.designdecision.EnumerationChoice;
+import de.uka.ipd.sdq.pcm.designdecision.EnumerationDegree;
+import de.uka.ipd.sdq.pcm.designdecision.Problem;
+import de.uka.ipd.sdq.pcm.designdecision.ProcessingResourceDegree;
+import de.uka.ipd.sdq.pcm.designdecision.ResourceContainerReplicationDegree;
+import de.uka.ipd.sdq.pcm.designdecision.util.designdecisionSwitch;
+import de.uka.ipd.sdq.pcm.repository.BasicComponent;
+import de.uka.ipd.sdq.pcm.repository.PassiveResource;
+import de.uka.ipd.sdq.pcm.repository.Repository;
+import de.uka.ipd.sdq.pcm.repository.RepositoryComponent;
+import de.uka.ipd.sdq.pcm.resourceenvironment.ProcessingResourceSpecification;
+import de.uka.ipd.sdq.pcm.resourceenvironment.ResourceContainer;
+import de.uka.ipd.sdq.pcm.resourcetype.ProcessingResourceType;
+import de.uka.ipd.sdq.pcmsolver.models.PCMInstance;
+
+/**
+ * This class fixes the references of designdecision models. The problem 
+ * addressed is, that if you separately load a PCM model and a designdecision
+ * model, the object identities of the loaded elements are not the same. For 
+ * example, a basic component A may be represented by a Java object A1 when 
+ * loaded directly from the repository file. When a degree of freedom model is 
+ * loaded, and the PCM instance is accessed usingthe references in that model,
+ * the PCM model is loaded again and the component A is represented by a second
+ * Java object A2. Calling equals on A1 and A2 results in false. Thus,
+ * collection.contains(A1) will fail even if A2 is contained. 
+ * 
+ * Thus, this class fixes the references by replacing each reference to A2 to 
+ * a reference to A1, so that only one consistent object tree represents the 
+ * models at the end. 
+ * 
+ * XXX: Maybe this could be done more generically based on EObject and the structural 
+ * features, without being meta model dependent. 
+ * Solution: Just use the same EMF resource set when loading more models and then resolve proxies using ECoreUtil#resolveAll.
+ * @author martens
+ *
+ * @deprecated
+ */
+public class FixDesignDecisionReferenceSwitch extends designdecisionSwitch<EObject> {
+	
+	protected static Logger logger = Logger
+		.getLogger(FixDesignDecisionReferenceSwitch.class.getName());
+
+	public void fixEntitiesForDomain(EnumerationDegree degree, List<Entity> memoryEntityList) {
+
+		List<Entity> newList = new ArrayList<Entity>();
+		
+		for (Entity entity : degree.getDomainOfEntities()) {
+			Entity rightOne = EMFHelper.retrieveEntityByID(memoryEntityList, entity.getId());
+			newList.add(rightOne);
+		}
+
+		degree.getDomainOfEntities().clear();
+		degree.getDomainOfEntities().addAll(newList);
+	}
+
+	private PCMInstance initialInstance;
+
+	public FixDesignDecisionReferenceSwitch(PCMInstance initialInstance2) {
+		this.initialInstance = initialInstance2;
+	}
+
+	@Override
+	public EObject caseAllocationDegree(AllocationDegree object) {
+		String id = object.getChangeableEntity().getId();
+		List<AllocationContext> acs = this.initialInstance.getAllocation().getAllocationContexts_Allocation();
+		AllocationContext rightOne = (AllocationContext)EMFHelper.retrieveEntityByID(acs,id);
+		object.setChangeableEntity(rightOne);
+		
+		
+		List<Entity> allCurrentServers = new ArrayList<Entity>();
+		allCurrentServers.addAll(this.initialInstance.getResourceEnvironment().getResourceContainer_ResourceEnvironment());
+		
+		fixEntitiesForDomain(object, allCurrentServers);
+		
+		return object;
+	}
+
+	@Override
+	public EObject caseCandidate(Candidate object) {
+		List<Choice> choices = object.getChoice();
+		for (Choice choice : choices) {
+			doSwitch(choice);
+		}
+		
+		return object;
+	}
+
+	@Override
+	public EObject caseCandidates(Candidates object) {
+		
+		object.setProblem(Opt4JStarter.getProblem().getEMFProblem());
+		
+		for (Candidate candidate: object.getCandidate()) {
+			doSwitch(candidate);
+		}
+		return object;
+	}
+
+	@Override
+	public EObject caseChoice(Choice object) {
+		
+		Problem inMemoryProblem = Opt4JStarter.getProblem().getEMFProblem();
+				
+		DegreeOfFreedom originalDegree = object.getDegreeOfFreedom();
+		doSwitch(originalDegree);
+		
+		boolean foundDegree = false;
+		
+		for (DegreeOfFreedom inMemoryDegree : inMemoryProblem.getDesigndecision()) {
+			// Need to initialize this every time anew, because otherwise it will remember old comparisons and compares with previous matches. 
+			EqualityHelper equalityHelper = new EqualityHelper();
+			if (equalityHelper.equals(inMemoryDegree, originalDegree)){
+				object.setDegreeOfFreedom(inMemoryDegree);
+				foundDegree = true;
+				break;
+			}
+		}
+		
+		if (!foundDegree){
+			throw new RuntimeException("Fixing design decision references failed, could not find in memory degree for "+object.getDegreeOfFreedom());
+		}
+		return object;
+	}
+
+	@Override
+	public EObject caseEnumerationChoice(EnumerationChoice object) {
+		
+		//First need to fix the references of the degrees of freedom  
+		caseChoice(object);
+	
+		// Fix the chosen entity. Is one of the degree of freedoms domain (which must have been fixed before). 
+		DegreeOfFreedom degree = object.getDegreeOfFreedom();
+		if (degree instanceof EnumerationDegree){
+			EnumerationDegree enumDegree = (EnumerationDegree)degree;
+			Entity inMemoryEntity = EMFHelper.retrieveEntityByID(enumDegree.getDomainOfEntities(), object.getEntity().getId());
+			object.setEntity(inMemoryEntity);
+		} else {
+			throw new RuntimeException("Invalid enumeration choice encountered: Referenced degree of freedom must be of type EnumerationDegree.");
+		}
+		
+		return object;
+	}
+
+	@Override
+	public EObject caseAssembledComponentDegree(
+			AssembledComponentDegree object) {
+		
+		String id = object.getChangeableEntity().getId();
+		List<AssemblyContext> acs = this.initialInstance.getSystem().getAssemblyContexts_ComposedStructure();
+		AssemblyContext rightOne = (AssemblyContext)EMFHelper.retrieveEntityByID(acs, id);
+		object.setChangeableEntity(rightOne);
+		
+		List<Entity> allCurrentComponents = new ArrayList<Entity>();
+		List<Repository> repositories = this.initialInstance.getRepositories();
+		for (Repository repository : repositories) {
+			allCurrentComponents.addAll(repository.getComponents__Repository());
+		}
+		
+		fixEntitiesForDomain(object, allCurrentComponents);
+
+		return object;
+	}
+
+
+	@Override
+	public EObject caseProblem(Problem object) {
+		for (DegreeOfFreedom dd : object.getDesigndecision()) {
+			doSwitch(dd);
+		};
+		return object;
+	}
+
+	@Override
+	public EObject caseProcessingResourceDegree(ProcessingResourceDegree object) {
+		String id = ((ResourceContainer)object.getChangeableEntity()).getId();
+			
+		List<ResourceContainer> rcs = this.initialInstance.getResourceEnvironment().getResourceContainer_ResourceEnvironment();
+		ResourceContainer rightOne = (ResourceContainer)EMFHelper.retrieveEntityByID(rcs, id);
+		object.setChangeableEntity(rightOne);
+		
+		ProcessingResourceType originalType = object.getProcessingresourcetype();
+		List<ProcessingResourceSpecification> resources = rightOne.getActiveResourceSpecifications_ResourceContainer();
+		boolean foundResourceType = false;
+		for (ProcessingResourceSpecification resource : resources) {
+			if (EMFHelper.checkIdentity(resource.getActiveResourceType_ActiveResourceSpecification(), originalType)){
+				object.setProcessingresourcetype(resource.getActiveResourceType_ActiveResourceSpecification());
+				foundResourceType = true;
+				break;
+			}
+		}
+		if (!foundResourceType){
+			throw new RuntimeException("Fixing design decision references failed, could not find in memory resource type for "+object.getProcessingresourcetype());
+		}
+		
+		return object;
+	}
+
+	@Override
+	public EObject caseCapacityDegree(CapacityDegree object) {
+		
+		String id = (object.getChangeableEntity()).getId();
+		
+		List<Repository> repos = this.initialInstance.getRepositories();
+		for (Repository repository : repos) {
+			List<RepositoryComponent> components = repository.getComponents__Repository();
+			for (RepositoryComponent component : components) {
+				if (component instanceof BasicComponent){
+					List<PassiveResource> prs = ((BasicComponent)component).getPassiveResource_BasicComponent();
+					PassiveResource rightOne = (PassiveResource)EMFHelper.retrieveEntityByID(prs, id);
+					if (rightOne != null){
+						object.setChangeableEntity(rightOne);
+						return object;
+					}
+				}
+			}
+		}
+		logger.warn("Could not find "+object.getChangeableEntity().getEntityName()+" in design decision reference switch, errors may occur.");
+		return object;
+	}
+
+	@Override
+	public EObject caseResourceContainerReplicationDegree(
+			ResourceContainerReplicationDegree object) {
+		// TODO Auto-generated method stub
+		return super.caseResourceContainerReplicationDegree(object);
+	}
+	
+	
+
+	
+	
+}
---
+++ PerOpteryx/de.uka.ipd.sdq.dsexplore/src/de/uka/ipd/sdq/dsexplore/opt4j/optimizer/heuristic/operators/impl/ServerExpansionImpl.java
@@ -0,0 +1,182 @@
+package de.uka.ipd.sdq.dsexplore.opt4j.optimizer.heuristic.operators.impl;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+import org.opt4j.core.problem.Genotype;
+import org.opt4j.operator.copy.Copy;
+
+import de.uka.ipd.sdq.dsexplore.launch.DSEWorkflowConfiguration;
+import de.uka.ipd.sdq.dsexplore.opt4j.optimizer.heuristic.operators.AbstractTactic;
+import de.uka.ipd.sdq.dsexplore.opt4j.optimizer.heuristic.operators.TacticsResultCandidate;
+import de.uka.ipd.sdq.dsexplore.opt4j.optimizer.heuristic.operators.UtilisationResultCacheAndHelper;
+import de.uka.ipd.sdq.dsexplore.opt4j.representation.DSEIndividual;
+import de.uka.ipd.sdq.dsexplore.opt4j.representation.DSEIndividualBuilder;
+import de.uka.ipd.sdq.dsexplore.qml.handling.QMLConstantsContainer;
+import de.uka.ipd.sdq.pcm.designdecision.AllocationDegree;
+import de.uka.ipd.sdq.pcm.designdecision.Choice;
+import de.uka.ipd.sdq.pcm.designdecision.EnumerationChoice;
+import de.uka.ipd.sdq.pcm.resourceenvironment.ProcessingResourceSpecification;
+import de.uka.ipd.sdq.pcm.resourceenvironment.ResourceContainer;
+import de.uka.ipd.sdq.pcm.resultdecorator.resourceenvironmentdecorator.ProcessingResourceSpecificationResult;
+import de.uka.ipd.sdq.pcm.resultdecorator.resourceenvironmentdecorator.UtilisationResult;
+import de.uka.ipd.sdq.pcm.resultdecorator.resourceenvironmentdecorator.impl.ProcessingResourceSpecificationResultImpl;
+
+/**
+ * Implements a server expansion heuristic. This heuristic finds a
+ * highly utilised resource container, creates a new resource container and reallocates 
+ * components to this new resource container
+ * @author martens, Tom Beyer
+ */
+public class ServerExpansionImpl extends AbstractTactic {
+	/**
+	 * Maximum number of components reallocated from selected resource container
+	 * to a new resource container
+	 */
+	private int maxNumberOfReplacements;
+	/**
+	 * If utilisation is larger than this double it will be considered
+	 * a high utilisation
+	 */
+	private double thresholdHighUtilisation;
+	
+	private Random generator = new Random();
+	
+	
+	private UtilisationResultCacheAndHelper resultInterpretationHelper = new UtilisationResultCacheAndHelper();
+	
+	/**
+	 * @param copy Used to copy genotype
+	 * @param individualBuilder Used to build individual
+	 */
+	public ServerExpansionImpl(Copy<Genotype> copy,
+			DSEIndividualBuilder individualBuilder, DSEWorkflowConfiguration configuration) {
+		super(copy, individualBuilder, configuration, new String[] {QMLConstantsContainer.QUALITY_ATTRIBUTE_DIMENSION_RESPONSETIME_DEFINITION_PATH});
+		// set config
+		setHeuristicWeight(configuration.getServerExpansionWeight());
+		maxNumberOfReplacements = configuration.getServerExpansionMaxNumberOfReplacements();
+		thresholdHighUtilisation = configuration.getServerExpansionThresholdHighUtilisation();
+	}
+	
+	/**
+	 * Check whether there is a resource container that has a 
+	 * utilisation >= THRESHOLD_HIGH_UTILISATION and is not null
+	 * @param individual
+	 * @return
+	 */
+	private boolean doesMatchHighUtilisation(DSEIndividual individual) {
+		ProcessingResourceSpecificationResult maxUtilisationResult = this.resultInterpretationHelper
+			.getMaxProcUtilisationResult(individual);
+		return maxUtilisationResult != null && 
+			maxUtilisationResult.getResourceUtilisation() >= thresholdHighUtilisation;
+	}
+	
+	/**
+	 * Check whether there is a resource container that has a 
+	 * utilisation >= THRESHOLD_HIGH_UTILISATION and there is a least one
+	 * unused resource container (unused means no allocated components)
+	 * @param individual
+	 * @return
+	 */
+	public boolean doesMatchPrecondition(DSEIndividual individual) {
+		return doesMatchHighUtilisation(individual) && !resultInterpretationHelper.getUnusedResourceContainers(individual).isEmpty();
+	}
+	
+	/**
+	 * Generates collection of candidates by applying the server consolidation heuristic
+	 * @param individual Individual which the heuristic should be applied to
+	 */
+	public List<TacticsResultCandidate> getHeuristicCandidates(DSEIndividual individual, UtilisationResultCacheAndHelper resultsHelper) {
+		this.resultInterpretationHelper = resultsHelper;
+		
+		List<TacticsResultCandidate> candidates = new ArrayList<TacticsResultCandidate>();
+		if (doesMatchPrecondition(individual)) {
+			/*
+			 * 1. Find max utilised resource
+			 * 2. copy genotype
+			 * 3. Get a new (unused) target resource container
+			 * 4. Reallocate components to target resource container
+			 */
+			// 1. Find max utilised resource
+			ProcessingResourceSpecificationResult maxUtilisationResult = this.resultInterpretationHelper.getMaxProcUtilisationResult(individual);
+			// 2. copy genotype
+			TacticsResultCandidate candidate = individualBuilder.buildCandidate(copy.copy(individual.getGenotype()), individual);
+			ProcessingResourceSpecification maxProcessingResourceSpec = ((ProcessingResourceSpecificationResultImpl) maxUtilisationResult)
+					.getProcessingResourceSpecification_ProcessingResourceSpecificationResult();
+			ResourceContainer resourceContainer = ((ResourceContainer) maxProcessingResourceSpec.eContainer());
+
+			// 3. Get a new (unused) target resource container
+			ResourceContainer targetResourceContainer = getRandomUnusedResourceContainer(individual);
+			
+			// 4. reallocate components
+			// 4a Iterate through AllocationDegrees and find the choices for this container
+			List<EnumerationChoice> componentsAllocatedHere = new LinkedList<EnumerationChoice>();
+			for (Choice choice : candidate.getGenotype()) {
+				if (choice instanceof EnumerationChoice) {
+					EnumerationChoice enumerationChoice = (EnumerationChoice)choice;
+					if (enumerationChoice.getDegreeOfFreedom() instanceof AllocationDegree) {
+						if (enumerationChoice.getEntity() == resourceContainer) {
+							componentsAllocatedHere.add(enumerationChoice);
+
+						}
+					}
+				}
+			}
+			
+			// 4b reallocate half of the found components, but not more than this.maxNumberOfReplacements
+			int halfOfFoundComponents = componentsAllocatedHere.size() / 2;
+			int reallocateAtMost = halfOfFoundComponents > maxNumberOfReplacements ? maxNumberOfReplacements : halfOfFoundComponents;
+			for (int i = 0; i < reallocateAtMost; i++){ 
+				EnumerationChoice enumerationChoice = componentsAllocatedHere.get(this.generator.nextInt(componentsAllocatedHere.size()));
+				// Reallocate this component to target resource container
+				enumerationChoice.setEntity(targetResourceContainer);
+				componentsAllocatedHere.remove(enumerationChoice);
+			}
+			
+			candidate.setCandidateWeight(getCandidateWeight(maxUtilisationResult));
+			candidate.setHeuristic(this);
+			candidates.add(candidate);
+			increaseCounterOfGeneratedCandidates();
+		}
+		return candidates;
+	}
+	
+
+	/**
+	 * Calculates weight based on the following scheme: if utilisation equals THRESHOLD_HIGH_UTLISATION
+	 * then it will return 0, if utilisation equals 1 it will return 1. Values in between are linearly 
+	 * extrapolated. Return values will always be >= 0.
+	 * @param utilisationResult
+	 * @return Weight based on utilisationResult's utilisation
+	 */
+	private double getCandidateWeight(UtilisationResult utilisationResult) {
+		return Math.max(utilisationResult.getResourceUtilisation()/(1.0 - thresholdHighUtilisation) - thresholdHighUtilisation/(1.0 - thresholdHighUtilisation),
+				0);
+	}
+	
+	/**
+	 * Get a random resource containers that has no allocated components
+	 * @param individual
+	 * @return
+	 */
+	private ResourceContainer getRandomUnusedResourceContainer(DSEIndividual individual) {
+		Random random = new Random();
+		Collection<ResourceContainer> resourceContainers = resultInterpretationHelper.getUnusedResourceContainers(individual);
+		int randomInt = random.nextInt(resourceContainers.size());
+		int n = 0;
+		for (ResourceContainer container : resourceContainers ) {
+			if (n == randomInt) {
+				return container;
+			}
+			n++;
+		}
+		// wont be executed unless resourceContainers is empty
+		return null;
+	}
+	
+
+
+}
