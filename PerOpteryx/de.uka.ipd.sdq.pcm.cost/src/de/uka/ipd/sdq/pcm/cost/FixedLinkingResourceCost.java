1696e6c355db39a472ee727a0e0964ba6f601ed0
mhauck <hauck@fzi.de>
2011-08-17 13:29:01 +0000

    Move cost model projects to Palladio/Core/trunk/PerOpteryx
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@12850 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ PerOpteryx/de.uka.ipd.sdq.pcm.cost/src/de/uka/ipd/sdq/pcm/cost/FixedLinkingResourceCost.java
@@ -0,0 +1,23 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.pcm.cost;
+
+import de.uka.ipd.sdq.pcm.resourceenvironment.LinkingResource;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Fixed Linking Resource Cost</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see de.uka.ipd.sdq.pcm.cost.costPackage#getFixedLinkingResourceCost()
+ * @model
+ * @generated
+ */
+public interface FixedLinkingResourceCost extends LinkingResourceCost, Cost {
+
+} // FixedLinkingResourceCost
