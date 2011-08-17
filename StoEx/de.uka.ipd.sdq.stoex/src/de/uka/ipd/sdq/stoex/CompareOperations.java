9756d4dc2d08c153c648ee6e91a817904ad79cea
mhauck <hauck@fzi.de>
2011-08-17 10:09:14 +0000

    Move stoex.* projects to Palladio/Core/tags/Experiment2/Stoex
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/tags/Experiment2@12587 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ StoEx/de.uka.ipd.sdq.stoex/src/de/uka/ipd/sdq/stoex/CompareOperations.java
@@ -0,0 +1,320 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.stoex;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.emf.common.util.Enumerator;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the literals of the enumeration '<em><b>Compare Operations</b></em>',
+ * and utility methods for working with them.
+ * <!-- end-user-doc -->
+ * @see de.uka.ipd.sdq.stoex.StoexPackage#getCompareOperations()
+ * @model
+ * @generated
+ */
+public enum CompareOperations implements Enumerator {
+	/**
+	 * The '<em><b>GREATER</b></em>' literal object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #GREATER_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	GREATER(0, "GREATER", "GREATER"),
+
+	/**
+	 * The '<em><b>EQUALS</b></em>' literal object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #EQUALS_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	EQUALS(1, "EQUALS", "EQUALS"),
+
+	/**
+	 * The '<em><b>LESS</b></em>' literal object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #LESS_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	LESS(2, "LESS", "LESS"),
+
+	/**
+	 * The '<em><b>NOTEQUAL</b></em>' literal object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #NOTEQUAL_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	NOTEQUAL(3, "NOTEQUAL", "NOTEQUAL"),
+
+	/**
+	 * The '<em><b>LESSEQUAL</b></em>' literal object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #LESSEQUAL_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	LESSEQUAL(4, "LESSEQUAL", "LESSEQUAL"),
+
+	/**
+	 * The '<em><b>GREATEREQUAL</b></em>' literal object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #GREATEREQUAL_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	GREATEREQUAL(5, "GREATEREQUAL", "GREATEREQUAL");
+
+	/**
+	 * The '<em><b>GREATER</b></em>' literal value.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>GREATER</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @see #GREATER
+	 * @model
+	 * @generated
+	 * @ordered
+	 */
+	public static final int GREATER_VALUE = 0;
+
+	/**
+	 * The '<em><b>EQUALS</b></em>' literal value.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>EQUALS</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @see #EQUALS
+	 * @model
+	 * @generated
+	 * @ordered
+	 */
+	public static final int EQUALS_VALUE = 1;
+
+	/**
+	 * The '<em><b>LESS</b></em>' literal value.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>LESS</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @see #LESS
+	 * @model
+	 * @generated
+	 * @ordered
+	 */
+	public static final int LESS_VALUE = 2;
+
+	/**
+	 * The '<em><b>NOTEQUAL</b></em>' literal value.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>NOTEQUAL</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @see #NOTEQUAL
+	 * @model
+	 * @generated
+	 * @ordered
+	 */
+	public static final int NOTEQUAL_VALUE = 3;
+
+	/**
+	 * The '<em><b>LESSEQUAL</b></em>' literal value.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>LESSEQUAL</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @see #LESSEQUAL
+	 * @model
+	 * @generated
+	 * @ordered
+	 */
+	public static final int LESSEQUAL_VALUE = 4;
+
+	/**
+	 * The '<em><b>GREATEREQUAL</b></em>' literal value.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>GREATEREQUAL</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @see #GREATEREQUAL
+	 * @model
+	 * @generated
+	 * @ordered
+	 */
+	public static final int GREATEREQUAL_VALUE = 5;
+
+	/**
+	 * An array of all the '<em><b>Compare Operations</b></em>' enumerators.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private static final CompareOperations[] VALUES_ARRAY =
+		new CompareOperations[] {
+			GREATER,
+			EQUALS,
+			LESS,
+			NOTEQUAL,
+			LESSEQUAL,
+			GREATEREQUAL,
+		};
+
+	/**
+	 * A public read-only list of all the '<em><b>Compare Operations</b></em>' enumerators.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final List<CompareOperations> VALUES = Collections.unmodifiableList(Arrays.asList(VALUES_ARRAY));
+
+	/**
+	 * Returns the '<em><b>Compare Operations</b></em>' literal with the specified literal value.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static CompareOperations get(String literal) {
+		for (int i = 0; i < VALUES_ARRAY.length; ++i) {
+			CompareOperations result = VALUES_ARRAY[i];
+			if (result.toString().equals(literal)) {
+				return result;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Returns the '<em><b>Compare Operations</b></em>' literal with the specified name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static CompareOperations getByName(String name) {
+		for (int i = 0; i < VALUES_ARRAY.length; ++i) {
+			CompareOperations result = VALUES_ARRAY[i];
+			if (result.getName().equals(name)) {
+				return result;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Returns the '<em><b>Compare Operations</b></em>' literal with the specified integer value.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static CompareOperations get(int value) {
+		switch (value) {
+			case GREATER_VALUE: return GREATER;
+			case EQUALS_VALUE: return EQUALS;
+			case LESS_VALUE: return LESS;
+			case NOTEQUAL_VALUE: return NOTEQUAL;
+			case LESSEQUAL_VALUE: return LESSEQUAL;
+			case GREATEREQUAL_VALUE: return GREATEREQUAL;
+		}
+		return null;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private final int value;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private final String name;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private final String literal;
+
+	/**
+	 * Only this class can construct instances.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private CompareOperations(int value, String name, String literal) {
+		this.value = value;
+		this.name = name;
+		this.literal = literal;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public int getValue() {
+	  return value;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getName() {
+	  return name;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getLiteral() {
+	  return literal;
+	}
+
+	/**
+	 * Returns the literal value of the enumerator, which is its string representation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String toString() {
+		return literal;
+	}
+	
+} //CompareOperations
