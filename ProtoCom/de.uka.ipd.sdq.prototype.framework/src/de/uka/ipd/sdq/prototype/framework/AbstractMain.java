d4a62274d8be3ea3506f3de4d8b49364ac7141bb
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2011-10-10 18:37:20 +0000

    ProtoCom: Refactored large parts of abstract main. Now, the command line stuff is handled by the CommandLine class. The UserMenu class displays the user menu. The handling of selected items is still in the abstractMain and needs to be refactored next.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@13440 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- ProtoCom/de.uka.ipd.sdq.prototype.framework/src/de/uka/ipd/sdq/prototype/framework/AbstractMain.java
+++ ProtoCom/de.uka.ipd.sdq.prototype.framework/src/de/uka/ipd/sdq/prototype/framework/AbstractMain.java
@@ -5,15 +5,8 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Scanner;
 
-import org.apache.commons.cli.GnuParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.OptionBuilder;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Level;
 import org.apache.log4j.PatternLayout;
@@ -60,8 +53,7 @@ public abstract class AbstractMain {
 	 * @param args Command line arguments given for the prototype according to Apache CLI's configuration
 	 */
 	protected void run(String[] args) {
-
-		parseCommandLine(args);
+		runProps = CommandLineParser.parseCommandLine(args);
 		setupLogging();
 		logger.info("Command line read. Logging initialised. Protocom starts its workflow now...");
 	
@@ -78,15 +70,25 @@ public abstract class AbstractMain {
 	
 		StoExCache.initialiseStoExCache(randomGen);
 	
-		if (runProps.hasOption('M')) // main class option set
+		handleStartOption();
+	}
+
+	/**
+	 * Handles the start option set via the M-parameter.
+	 * - If M has no option value, the user menu is displayed
+	 * - If M has a option value, the corresponding main method is invoked (currently this is an experimental feature)
+	 * - If M is not set, the default main method is invoked 
+	 */
+	private void handleStartOption() 
+	{
+		if (runProps.hasOption('M'))
 		{
 			String mainClass = runProps.getOptionValue('M');
 			Method mainMethod = getMain(mainClass);
 	
 			if (!mainMethodFound(mainMethod))
 			{
-				// show menu & invoke selected method
-				List<Integer> itemIds = showUserMenu();
+				List<Integer> itemIds = UserMenu.showUserMenu(getSystems());
 				for (int itemId : itemIds)
 				{
 					handleMenuItem(itemId);
@@ -96,11 +98,105 @@ public abstract class AbstractMain {
 			{
 				invokeMethod(mainMethod, new String[]{});
 			}
-		} else {
+		} 
+		else
+		{
 			startDefaultMain();
 		}
 	}
 
+	/**
+	 * Executes the action that corresponds to itemId
+	 * @param itemId The id of a menu item to execute
+	 */
+	private void handleMenuItem(int itemId)
+	{
+		if(itemId == 1)
+		{
+			// Start everything in local mode
+			logger.debug("Start: Start everything in local mode");
+			RmiRegistry.startRegistry();
+			AbstractAllocationStorage.setLocalMode(true);
+			setupResources();
+			startDefaultMain();
+		}
+		else if(itemId == 2)
+		{
+			// RmiRegistry
+			logger.debug("Start: RmiRegistry");
+			RmiRegistry.main(null);
+		}
+		else if(itemId == 3)
+		{
+			// Usage Scenarios
+			logger.debug("Start: Usage Scenarios");
+			
+			createExperiment();
+			initMeasurement();
+		}
+		else
+		{
+			int i = 4;
+
+			// This data source is only used temporary by components for inner sensors and NOT for the usage scenario.
+			// This should be refactored into a better place, just like all in this class...
+			datasource = prepareDatasource();
+			ExperimentManager.setExperiment(datasource.createExperimentDAO().addExperiment(runProps.getOptionValue('n')));
+
+			
+			// systems
+			String[][] systems = getSystems();
+			for(String[] system : systems)
+			{
+				if(itemId == i)
+				{
+					logger.debug("Start: System "+system[0]);
+					invokeMethod(getMain(system[0]), getRMIRegistry());
+				}
+				i++;
+			}
+			
+			// container
+			Collection<String> containers = AbstractAllocationStorage.getContainerIds();
+			for(String containerId : containers)
+			{
+				if(itemId == i)
+				{
+					logger.debug("Start: Container "+AbstractAllocationStorage.getContainerName(containerId));
+					Collection<Class<?>> components = AbstractAllocationStorage.getComponents(containerId);
+					AbstractAllocationStorage.setActiveContainer(containerId);
+					setupResources();
+					
+					for(Class<?> component : components)
+					{
+						logger.debug("Start: Component "+component.getName());
+						invokeMethod(getMain(component), getRMIRegistry());
+					}
+				}
+				i++;
+			}
+		}
+	}
+
+	
+	private String[] getRMIRegistry() {
+		if (runProps.hasOption("R")) {
+			return new String[]{ runProps.getOptionValue("R") };
+		}
+		return new String[]{};
+	}
+	
+	private Method getMain(Class<?> mainClass) {
+		if (mainClass == null)
+			return null;
+		try {
+			return mainClass.getMethod("main", String[].class);
+		} catch (Throwable e) {
+		}
+
+		return null;
+	}
+	
 	private boolean mainMethodFound(Method mainMethod) {
 		logger.debug(mainMethod == null ? "Main method not found. Falling back" : "Main method passed on command line.");
 		return mainMethod != null;
@@ -180,142 +276,6 @@ public abstract class AbstractMain {
 		}
 		return null;
 	}
-	
-	private Method getMain(Class<?> mainClass) {
-		if (mainClass == null)
-			return null;
-		try {
-			return mainClass.getMethod("main", String[].class);
-		} catch (Throwable e) {
-		}
-
-		return null;
-	}
-	
-	private List<Integer> showUserMenu() {
-		System.out.println("Select starting option:");
-		showMenuItems();
-		System.out.print("Option: ");
-		Scanner sc = new Scanner(System.in);
-		String st = null;
-		if (sc.hasNextLine()) {
-			st = sc.nextLine();
-		}
-		sc.close();
-
-		List<Integer> ids = new LinkedList<Integer>();
-		if (st != null) {
-			sc = new Scanner(st);
-
-			while (sc.hasNextInt()) {
-				ids.add(sc.nextInt());
-			}
-		}
-		sc.close();
-
-		return ids;
-	}
-	
-	private void showMenuItems()
-	{
-		System.out.println("1: Start everything in local mode (hardware container #1 is used, only for testing purpose!)");
-		System.out.println("2: RmiRegistry");
-		System.out.println("3: Usage Scenarios");
-		
-		int i = 4;
-
-		// systems
-		String[][] systems = getSystems();
-		for(String[] system : systems)
-		{
-			System.out.println(i+": System "+system[1]);
-			i++;
-		}
-		
-		// container
-		Collection<String> containers = AbstractAllocationStorage.getContainerNames();
-		for(String container : containers)
-		{
-			System.out.println(i+": Container "+container);
-			i++;
-		}
-	}
-
-	private void handleMenuItem(int itemId)
-	{
-		if(itemId == 1)
-		{
-			// Start everything in local mode
-			logger.debug("Start: Start everything in local mode");
-			RmiRegistry.startRegistry();
-			AbstractAllocationStorage.setLocalMode(true);
-			setupResources();
-			startDefaultMain();
-		}
-		else if(itemId == 2)
-		{
-			// RmiRegistry
-			logger.debug("Start: RmiRegistry");
-			RmiRegistry.main(null);
-		}
-		else if(itemId == 3)
-		{
-			// Usage Scenarios
-			logger.debug("Start: Usage Scenarios");
-			
-			createExperiment();
-			initMeasurement();
-		}
-		else
-		{
-			int i = 4;
-
-			// This data source is only used temporary by components for inner sensors and NOT for the usage scenario.
-			// This should be refactored into a better place, just like all in this class...
-			datasource = prepareDatasource();
-			ExperimentManager.setExperiment(datasource.createExperimentDAO().addExperiment(runProps.getOptionValue('n')));
-
-			
-			// systems
-			String[][] systems = getSystems();
-			for(String[] system : systems)
-			{
-				if(itemId == i)
-				{
-					logger.debug("Start: System "+system[0]);
-					invokeMethod(getMain(system[0]), getRMIRegistry());
-				}
-				i++;
-			}
-			
-			// container
-			Collection<String> containers = AbstractAllocationStorage.getContainerIds();
-			for(String containerId : containers)
-			{
-				if(itemId == i)
-				{
-					logger.debug("Start: Container "+AbstractAllocationStorage.getContainerName(containerId));
-					Collection<Class<?>> components = AbstractAllocationStorage.getComponents(containerId);
-					AbstractAllocationStorage.setActiveContainer(containerId);
-					setupResources();
-					
-					for(Class<?> component : components)
-					{
-						logger.debug("Start: Component "+component.getName());
-						invokeMethod(getMain(component), getRMIRegistry());
-					}
-				}
-				i++;
-			}
-		}
-	}
-
-	private String[] getRMIRegistry() {
-		if (runProps.hasOption("R")) {
-			return new String[]{ runProps.getOptionValue("R") };
-		}
-		return new String[]{};
-	}
 
 	private static void writeResultsAndClose(IDAOFactory datasource) {
 		logger.info("Storing results...");
@@ -351,66 +311,6 @@ public abstract class AbstractMain {
 			logger.setLevel(Level.DEBUG);
 		else
 			logger.setLevel(Level.INFO);
-	}
-
-	@SuppressWarnings("static-access")
-	private RunProperties parseCommandLine(String[] arguments) {
-		Options o = new Options();
-		
-		o.addOption("w", "wait", false, "Suspend program execution before starting to pin program to a single processor");
-		o.addOption("h", "help", false, "Show this help");
-		o.addOption("D", "debug", false, "Print debug information. Turn off for real experiments!");
-		o.addOption("P", "passive", false, "Make server passive, no load drivers are started. No warmup runs are executed, either.");
-		o.addOption("W", "warmup only", false, "Execute a number of runs equal to the warmup runs configuration. Depending on configuration, the inner times might be measured.");
-		
-		o.addOption(OptionBuilder.hasArg().withArgName("propertyFile").withLongOpt("propFile").withDescription("Property file used to set default commandline parameters. Defaults can be overwritten by additional parameters.")
-				.create("f"));
-		o.addOption(OptionBuilder.hasArg().withArgName("dataDirectory").withLongOpt("dataDir").withDescription("Datadirectory used by the FileStorage Sensor DAO to store the measured times [mandatory]")
-				.create('d'));
-		o.addOption(OptionBuilder.hasArg().withArgName("user count").withLongOpt("threadcount").withDescription("Override usage scenario population (only for closed workloads)")
-				.create("c"));
-		o.addOption(OptionBuilder.hasArg().withArgName("max measurements").withLongOpt("maxmeasurement").withDescription("Maximum measurements to take")
-				.create("m"));
-		o.addOption(OptionBuilder.hasArg().withArgName("warmup runs").withLongOpt("warmup").withDescription("Warmup runs before measuring (default: 1000)")
-				.create("u"));
-		o.addOption(OptionBuilder.hasArg().withArgName("calibrationPath").withLongOpt("storeCalPath").withDescription("Path to the directory where the calibration is stored [mandatory]")
-				.create("s"));
-		o.addOption(OptionBuilder.hasArg().withArgName("ip").withLongOpt("remoteAddr").withDescription("Remote address to the system component.")
-				.create("R"));
-		o.addOption(OptionBuilder.hasArg().withArgName("seed").withLongOpt("randomGenerator").withDescription("Seed for the StoEx random generator")
-				.create("E"));
-		o.addOption(OptionBuilder.hasArg().withLongOpt("name").withDescription("Name of the experiment for use in the stored data [mandatory]")
-				.create('n'));
-		
-		o.addOption(OptionBuilder.hasArgs().withLongOpt("cpuStrategy").withArgName("CPU Strategy").withDescription("CPU calibration strategy: primes, count_numbers, fft, fibonacci, mandelbrot, sortarray, void, wait (default: Fibonacci).")
-				.create('p'));
-		o.addOption(OptionBuilder.hasArgs().withLongOpt("hddStrategy").withArgName("HDD Strategy").withDescription("HDD calibration strategy: largeChunks (default: LargeChunks).")
-				.create('H'));
-		o.addOption(OptionBuilder.hasArgs().withLongOpt("accuracy").withArgName("Accuracy").withDescription("Accuracy of CPU and HDD calibration: LOW, MEDIUM, or HIGH (default: MEDIUM). ")
-				.create('a'));
-		
-		o.addOption(OptionBuilder.hasOptionalArg().withArgName("main").withLongOpt("mainClass").withDescription("Remote address to the system component.")
-				.create("M"));
-		
-		getAdditionalOptions(o);
-
-		boolean parserError = false;
-		try {
-			runProps = new RunProperties(new GnuParser().parse(o, arguments), o);
-		} catch (ParseException e) {
-			logger.error("Unable to start execution\n" + e.getMessage() + "\n");
-			parserError = true;
-		}
-
-		if (parserError || runProps.hasOption('h') || !runProps.hasOption('d') || !runProps.hasOption('n') || !runProps.hasOption('s') || !runProps.hasOption('m')) {
-			new HelpFormatter().printHelp("Main", "Prototype runner, copyright 2007-2011, SDQ, IPD, Uni Karlsruhe & MDE, Uni Paderborn\n", o,
-					"\nInitiated by Steffen Becker (steffen.becker@upb.de)");
-			System.exit(-1);
-		}
-		return runProps;
-	}
-
-	protected void getAdditionalOptions(Options o) {
 	}
 
 	private void waitForKey() {
