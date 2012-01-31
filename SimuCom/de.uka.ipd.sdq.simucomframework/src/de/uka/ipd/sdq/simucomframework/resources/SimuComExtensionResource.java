953345561b3d397c16f9697e4dd411175911df53
mhauck <hauck@fzi.de>
2012-01-31 17:40:15 +0000

    Adapted scheduler extension to latest simucomframework changes
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@14041 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- SimuCom/de.uka.ipd.sdq.simucomframework/src/de/uka/ipd/sdq/simucomframework/resources/SimuComExtensionResource.java
+++ SimuCom/de.uka.ipd.sdq.simucomframework/src/de/uka/ipd/sdq/simucomframework/resources/SimuComExtensionResource.java
@@ -1,11 +1,12 @@
 package de.uka.ipd.sdq.simucomframework.resources;
 
+import de.uka.ipd.sdq.scheduler.SchedulerModel;
 import de.uka.ipd.sdq.scheduler.resources.active.AbstractActiveResource;
 import de.uka.ipd.sdq.simucomframework.model.SimuComModel;
 
 public abstract class SimuComExtensionResource extends AbstractActiveResource {
 	
-	public SimuComExtensionResource(SimuComModel model, int capacity, String name, String id) {
+	public SimuComExtensionResource(SchedulerModel model, int capacity, String name, String id) {
 		super(model, capacity, name, id);
 	}
 
