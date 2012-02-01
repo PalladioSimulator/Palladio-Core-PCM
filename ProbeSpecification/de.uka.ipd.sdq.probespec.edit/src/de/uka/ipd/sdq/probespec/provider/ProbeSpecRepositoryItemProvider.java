8b5f4035fa9bc8369b1b21e2b7b62b0d2e708c62
mhauck <hauck@fzi.de>
2012-02-01 15:13:28 +0000

    Adapter ProbespecRepository label provider for EMF tree editor
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@14048 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- ProbeSpecification/de.uka.ipd.sdq.probespec.edit/src/de/uka/ipd/sdq/probespec/provider/ProbeSpecRepositoryItemProvider.java
+++ ProbeSpecification/de.uka.ipd.sdq.probespec.edit/src/de/uka/ipd/sdq/probespec/provider/ProbeSpecRepositoryItemProvider.java
@@ -7,20 +7,13 @@
 package de.uka.ipd.sdq.probespec.provider;
 
 
-import de.uka.ipd.sdq.probespec.ProbeSpecRepository;
-import de.uka.ipd.sdq.probespec.probespecFactory;
-import de.uka.ipd.sdq.probespec.probespecPackage;
-
 import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-
 import org.eclipse.emf.common.util.ResourceLocator;
-
 import org.eclipse.emf.ecore.EStructuralFeature;
-
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
@@ -29,6 +22,13 @@ import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
 import org.eclipse.emf.edit.provider.ItemProviderAdapter;
 import org.eclipse.emf.edit.provider.ViewerNotification;
+
+import de.uka.ipd.sdq.probespec.BinaryCalculator;
+import de.uka.ipd.sdq.probespec.Calculator;
+import de.uka.ipd.sdq.probespec.ProbeSpecRepository;
+import de.uka.ipd.sdq.probespec.UnaryCalculator;
+import de.uka.ipd.sdq.probespec.probespecFactory;
+import de.uka.ipd.sdq.probespec.probespecPackage;
 
 /**
  * This is the item provider adapter for a {@link de.uka.ipd.sdq.probespec.ProbeSpecRepository} object.
@@ -114,11 +114,74 @@ public class ProbeSpecRepositoryItemProvider
 	 * This returns the label text for the adapted class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated not
 	 */
 	@Override
 	public String getText(Object object) {
-		return getString("_UI_ProbeSpecRepository_type");
+		//return getString("_UI_ProbeSpecRepository_type");
+		ProbeSpecRepository probeSpecRepository = (ProbeSpecRepository)object;
+		String label = "Sensor Repository";
+		if ((probeSpecRepository.getCalculator() == null) || (probeSpecRepository.getCalculator().size() == 0)) {
+			label = label + " (no sensors specified)";
+		} else {
+			if (probeSpecRepository.getCalculator().size() == 1) {
+				label = label + " (1 sensor specified";
+			} else {
+				label = label + " (" + probeSpecRepository.getCalculator().size() + " sensors specified";
+			}
+			int numberOfInvalidSensors = getNumberOfInvalidCalculators(probeSpecRepository);
+			if (numberOfInvalidSensors > 0) {
+				label = label + ", " + numberOfInvalidSensors + " invalid";
+			}
+			label = label + ")";
+		}
+		return label;
+	}
+	
+	private int getNumberOfInvalidCalculators(ProbeSpecRepository probeSpecRepository) {
+		int result = 0;
+		if ((probeSpecRepository.getCalculator() == null) || (probeSpecRepository.getCalculator().size() == 0)) {
+			return result;
+		}
+		for (int i=0; i<probeSpecRepository.getCalculator().size(); i++) {
+			if (probeSpecRepository.getCalculator().get(i) instanceof BinaryCalculator) {
+				if (!isValid((BinaryCalculator)probeSpecRepository.getCalculator().get(i))) {
+					result++;
+				}
+			} else if (probeSpecRepository.getCalculator().get(i) instanceof UnaryCalculator) {
+				if (!isValid((UnaryCalculator)probeSpecRepository.getCalculator().get(i))) {
+					result++;
+				}
+			} else {
+				if (!isValid(probeSpecRepository.getCalculator().get(i))) {
+					result++;
+				}
+			}
+		}
+		return result;
+	}
+	
+	private boolean isValid(BinaryCalculator calculator) {
+		if ((calculator.getProbeSet() == null) || (calculator.getProbeSet().size() == 0)) { 
+			return false;
+		}
+		for (int i=0; i<calculator.getProbeSet().size(); i++) {
+			if (calculator.getProbeSet().get(i).getAnnotatedElement() == null) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private boolean isValid(UnaryCalculator calculator) {
+		if ((calculator.getProbeSet() == null) || (calculator.getProbeSet().getAnnotatedElement() == null)) {
+			return false;
+		}
+		return true;
+	}
+	
+	private boolean isValid(Calculator calculator) {
+		return false;
 	}
 
 	/**
