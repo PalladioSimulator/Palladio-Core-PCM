2b639e1b0b2595885b741dc734acf3f304be66f4
groenda <groenda@fzi.de>
2012-02-08 08:36:22 +0000

    Added documentation. Added ceiling function.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@14102 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ Accuracy/de.fzi.se.validation.effort.blackbox/src/de/fzi/se/validation/effort/blackbox/MathFunctionsLibrary.java
@@ -0,0 +1,43 @@
+package de.fzi.se.validation.effort.blackbox;
+
+import org.eclipse.m2m.qvt.oml.blackbox.java.Operation;
+import org.eclipse.m2m.qvt.oml.blackbox.java.Operation.Kind;
+
+/**
+ * This QVT Operational black-box library implements utility functions for mathematical operations,
+ * which are not included in OCL. Examples are the logarithm or the ceiling function.
+ *
+ * @author Martin Krogmann
+ * @author Henning Groenda
+ *
+ */
+public class MathFunctionsLibrary {
+
+	/**Required default constructor.
+	 */
+	public MathFunctionsLibrary() {
+	}
+
+	/**
+	 * Logarithm function from the {@link Math} library.
+	 * Named mathLog, as log is already reserved for the logging function.
+	 * @see Math#log(double)
+	 * @param r Real value.
+	 * @return Math.log(r)
+	 */
+	@Operation(kind=Kind.QUERY, contextual=false)
+	public Double mathLog(Double r) {
+		return Math.log(r);
+	}
+
+	/**
+	 * Logarithm function from the {@link Math} library.
+	 * @see Math#ceil(double)
+	 * @param value Real value.
+	 * @return Math.ceil(r)
+	 */
+	@Operation(kind=Kind.QUERY, contextual=false)
+	public Long ceiling(Double value) {
+		return new Double(Math.ceil(value)).longValue();
+	}
+}
