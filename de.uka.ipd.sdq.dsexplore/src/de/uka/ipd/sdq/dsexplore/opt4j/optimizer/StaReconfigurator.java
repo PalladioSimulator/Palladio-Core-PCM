5c6405bca3a4e5ea85036129b6a0e244af0325d8
hubern <niko.huber@gmail.com>
2012-02-28 10:01:24 +0000

    first iteration with hard-coded thresholds and two tactics (add_resources and migrate_component)
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/branches/StrategyTacticAction@14397 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- de.uka.ipd.sdq.dsexplore/src/de/uka/ipd/sdq/dsexplore/opt4j/optimizer/StaReconfigurator.java
+++ de.uka.ipd.sdq.dsexplore/src/de/uka/ipd/sdq/dsexplore/opt4j/optimizer/StaReconfigurator.java
@@ -1,5 +1,6 @@
 package de.uka.ipd.sdq.dsexplore.opt4j.optimizer;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -31,116 +32,168 @@ import edu.kit.ipd.descartes.reconfiguration.opt4j.optimizer.TacticsManager;
 
 public class StaReconfigurator extends AbstractOptimizer {
 
+	private static final double TARGET_RESP_TIME = 4.0;
+	private static final double RESP_TIME_DELTA = 0.1;
+	private List<Double> prevRespTimes = new ArrayList<Double>();
+	private boolean strategySargetAchieved = false;
 	TacticsManager staManager;
-	
+
 	/** Logger for log4j. */
-	private static Logger logger = 
-		Logger.getLogger("de.uka.ipd.sdq.dsexplore.opt4j.optimizer.RuleBasedSearch");
-	
+	private static Logger logger = Logger
+			.getLogger("de.uka.ipd.sdq.dsexplore.opt4j.optimizer.RuleBasedSearch");
+
 	@Inject
 	public StaReconfigurator(Population population, Archive archive,
 			IndividualBuilder individualBuilder, Completer completer,
 			Control control, Copy<Genotype> copy) {
 		super(population, archive, individualBuilder, completer, control);
-		staManager = new TacticsManager(copy, (DSEIndividualBuilder) individualBuilder);
+		staManager = new TacticsManager(copy,
+				(DSEIndividualBuilder) individualBuilder);
 	}
 
 	@Override
 	public void optimize() throws StopException, TerminationException {
 		// TODO Auto-generated method stub
-		System.out.println("CURRENTLY NOTHING IS IMPLEMENTED! ADD THE TACTICS HERE!");
+		System.out
+				.println("CURRENTLY NOTHING IS IMPLEMENTED! ADD THE TACTICS HERE!");
 		DSEIndividual currentCandidate = null;
-		
+
 		staManager.setActiveTacticNumber(TacticsManager.INCREASE_CPU);
-		//1. Apply a tactic. Tactic is chosen according to the weights. 
-		//   Applying a tactic returns a new "candidate".
+
 		Individual ind = individualBuilder.build();
 		this.population.add(ind);
 		nextIteration();
 		
-		List<Individual> nextGeneration = new LinkedList<Individual>();
-		assert(this.population.size() == 1);
+		assert (this.population.size() == 1);
 		for (Individual individual : this.population) {
-			if (individual instanceof DSEIndividual){
+			if (individual instanceof DSEIndividual) {
 				currentCandidate = (DSEIndividual) individual;
-				List<TacticsResultCandidate> candidateResults = staManager.applyTactic(currentCandidate);
-				
-				nextGeneration.addAll(candidateResults);
-			} else {
-				throw new RuntimeException("Encountered a non-DSEIndividual in rule based search that I cannot handle. Aborting.");
+				evaluateCandidate(currentCandidate);
 			}
-			
 		}
+
+		while (!isStrategyTargetAchieved()) {
+
+			List<Individual> nextGeneration = new LinkedList<Individual>();
 			
-		//2. Evaluate new candidate according to the target of the strategy
-		
-		double respTime = getResponseTime(currentCandidate);
-		System.out.println(respTime);
+			for (Individual individual : this.population) {
+				if (individual instanceof DSEIndividual) {
+					currentCandidate = (DSEIndividual) individual;
+					// 1. Apply a tactic. Tactic is chosen according to the
+					// weights. Applying a tactic returns a new "candidate".
+					List<TacticsResultCandidate> candidateResults = staManager
+							.applyTactic(currentCandidate);
+					nextGeneration.addAll(candidateResults);
+				} else {
+					throw new RuntimeException(
+							"Encountered a non-DSEIndividual in rule based search that I cannot handle. Aborting.");
+				}
 
-		
-		//3. Reassign weights to tactics depending on the history and the evaluation result
+			}
+
+			int nextGenerationSize = nextGeneration.size();
+			nextGeneration.removeAll(population);
+			nextGeneration.removeAll(archive);
+			logger
+					.info("Removed "
+							+ (nextGeneration.size() - nextGenerationSize)
+							+ " duplicate candidate (that were in the previous population or in the archive).");
+
+			completer.complete(nextGeneration);
+
+			this.population.clear();
+			this.population.addAll(nextGeneration);
+
+			// if (!this.fullSearch){
+			// List<Individual> archiveAndNewUnion = new
+			// ArrayList<Individual>(this.archive.size() +
+			// nextGeneration.size());
+			// archiveAndNewUnion.addAll(nextGeneration);
+			// archiveAndNewUnion.addAll(archive);
+			// List<Individual> optimalCandidates =
+			// FilterParetoOptimalIndividuals.filterPareto(archiveAndNewUnion);
+			// this.population.retainAll(optimalCandidates);
+			// }
+
+			if (this.population.size() == 0) {
+				// logger.warn("No more individuals in population, aborting after iteration "+i);
+				// break;
+			}
+
+			// 2. Evaluate new candidate according to the target of the strategy
+			evaluateCandidate(currentCandidate);
+			nextIteration();
+			
 
 
-		int nextGenerationSize = nextGeneration.size();
-		nextGeneration.removeAll(population);
-		nextGeneration.removeAll(archive);
-		logger.info("Removed "+(nextGeneration.size() - nextGenerationSize)+" duplicate candidate (that were in the previous population or in the archive).");
-		
-		completer.complete(nextGeneration);
-		
-		this.population.clear();
-		this.population.addAll(nextGeneration);
-		
-//		if (!this.fullSearch){
-//			List<Individual> archiveAndNewUnion = new ArrayList<Individual>(this.archive.size() + nextGeneration.size());
-//			archiveAndNewUnion.addAll(nextGeneration);
-//			archiveAndNewUnion.addAll(archive);
-//			List<Individual> optimalCandidates = FilterParetoOptimalIndividuals.filterPareto(archiveAndNewUnion);
-//			this.population.retainAll(optimalCandidates);
-//		}
-		
-		if (this.population.size() == 0){
-//			logger.warn("No more individuals in population, aborting after iteration "+i);
-//			break;
+			// 3. Reassign weights to tactics depending on the history and the
+			// evaluation result
+
 		}
-		
-		nextIteration();
-		
-		
-		
+
 		// teardown
-		
-		ResultsWriter writer = new ResultsWriter(Opt4JStarter.getDSEWorkflowConfig().getResultFolder()+"rule-based search results");
+
+		ResultsWriter writer = new ResultsWriter(Opt4JStarter
+				.getDSEWorkflowConfig().getResultFolder()
+				+ "rule-based search results");
 		for (Individual individual : this.archive) {
-			if (individual instanceof DSEIndividual){
-				DSEIndividual dseIndiv = (DSEIndividual)individual;
+			if (individual instanceof DSEIndividual) {
+				DSEIndividual dseIndiv = (DSEIndividual) individual;
 				writer.writeIndividual(dseIndiv);
 			} else {
-				logger.error("Encountered a non DSE-Individual in the rule based search, aborting to write results.");
+				logger
+						.error("Encountered a non DSE-Individual in the rule based search, aborting to write results.");
 			}
 		}
-		writer.flush();		
+		writer.flush();
+	}
+
+	private boolean isStrategyTargetAchieved() {
+		return strategySargetAchieved;
+	}
+	
+	private void evaluateCandidate(DSEIndividual currentCandidate)
+	{
+		double respTime = getResponseTime(currentCandidate);
+		prevRespTimes.add(respTime);
+		logger.info("Current candidate response time " + respTime);
+		
+		strategySargetAchieved = respTime < TARGET_RESP_TIME;
+		double respTimeImprovement = calcRespTimeImprovement();
+		if(respTimeImprovement != Double.NaN && respTimeImprovement < RESP_TIME_DELTA)
+		{
+			staManager.setActiveTacticNumber(TacticsManager.MIGRATE_COMPONENT);
+		}
+	}
+
+	private double calcRespTimeImprovement() {
+		int lastEntry = prevRespTimes.size();
+		if (lastEntry == 0 || lastEntry == 1)
+			return Double.NaN;
+		else
+			return prevRespTimes.get(lastEntry - 1) - prevRespTimes.get(lastEntry - 2);
 	}
 
 	private double getResponseTime(DSEIndividual currentCandidate) {
-		
+
 		DSEObjectives objs = currentCandidate.getObjectives();
-		List<Criterion> criteriaToSave = ResultsWriter.determineCriterionsToSave(currentCandidate);
+		List<Criterion> criteriaToSave = ResultsWriter
+				.determineCriterionsToSave(currentCandidate);
 		return formatValue(objs.getValueForCriterion(criteriaToSave.get(1)));
-		
+
 	}
 
 	private static double formatValue(Value<?> value) {
-		
-		if (value instanceof IntegerValue){
-			IntegerValue intValue = (IntegerValue)value;
+
+		if (value instanceof IntegerValue) {
+			IntegerValue intValue = (IntegerValue) value;
 			return intValue.getDouble();
-			
+
 		} else {
 			double d = value.getDouble();
 			return d;
 		}
-		
+
 	}
 
 }
