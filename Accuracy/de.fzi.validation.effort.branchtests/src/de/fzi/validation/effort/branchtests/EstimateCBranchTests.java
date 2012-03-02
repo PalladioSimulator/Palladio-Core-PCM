3fb695ed14849538130086195b26be46318e1c6c
MartinKrogmann <MartinKrogmann>
2012-03-02 17:23:04 +0000

    initial checkin
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@14418 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ Accuracy/de.fzi.validation.effort.branchtests/src/de/fzi/validation/effort/branchtests/EstimateCBranchTests.java
@@ -0,0 +1,35 @@
+package de.fzi.validation.effort.branchtests;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunchConfiguration;
+
+import de.fzi.se.validation.effort.workflow.AbstractEstimateQVTO;
+import de.uka.ipd.sdq.pcm.seff.ResourceDemandingBehaviour;
+import de.uka.ipd.sdq.workflow.exceptions.JobFailedException;
+import de.uka.ipd.sdq.workflow.exceptions.UserCanceledException;
+import de.uka.ipd.sdq.workflow.mdsd.blackboard.ModelLocation;
+
+/**
+ * Validation effort estimation algorithm CBranchTest.
+ * @author Martin Krogmann
+ *
+ */
+public class EstimateCBranchTests  extends AbstractEstimateQVTO {
+
+	/** Operational QVT transformation script URI which transforms {@link ResourceDemandingBehaviour} to {@link Graph}. */
+	protected static final String CBRANCHTEST_TRANSFORMATION_SCRIPT = "platform:/plugin/de.fzi.se.validation.effort.qvtoscripts/transforms/behaviour2CBranchTestsEffortEstimationResult.qvto";
+
+	protected ILaunchConfiguration configuration = null;
+	
+	/* (non-Javadoc)
+	 * @see de.fzi.se.validation.effort.jjnpaths.AbstractEstimateQVTO#executeScript(org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	protected void executeScript(IProgressMonitor monitor) throws JobFailedException, UserCanceledException {
+		executeStep(monitor, new ModelLocation[] {targetBehaviour, resultModelLocation}, CBRANCHTEST_TRANSFORMATION_SCRIPT);
+	}
+
+	@Override
+	public void buildAndSetCustomConfiguration(
+			ILaunchConfiguration configuration) {
+	}
+}
