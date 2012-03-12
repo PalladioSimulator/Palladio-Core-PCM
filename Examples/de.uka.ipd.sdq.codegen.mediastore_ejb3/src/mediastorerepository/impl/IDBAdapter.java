e6b0c3bfc42ff6933a425d076f403ee3807476d3
AnneKoziolek <anne.koziolek@kit.edu>
2012-03-12 16:44:53 +0000

    Moved a media store implementation(?) to CaseStudies
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@14514 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Examples/de.uka.ipd.sdq.codegen.mediastore_ejb3/src/mediastorerepository/impl/IDBAdapter.java
+++
@@ -1,7 +0,0 @@
-package mediastorerepository.impl;
-
-public interface IDBAdapter {
-    byte[][] iAudioDB_queryDB(int[] desiredFiles);
-
-    byte[][] iAudioDB_queryAndGetFromDB(String queryString, int fieldID);
-}
