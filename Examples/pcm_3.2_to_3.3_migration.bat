1d6f370f70774a5feb16d4b5c8d187a16fbf86eb
AnneKoziolek <anne.koziolek@kit.edu>
2012-04-20 14:19:31 +0000

    PCM 3.2 to supposed 3.3 migration script, see https://sdqweb.ipd.kit.edu/wiki/PCM_Model_Migration
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@15042 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ Examples/pcm_3.2_to_3.3_migration.bat
@@ -0,0 +1,47 @@
+rem the replacement commands are executed for all PCM files in the current folder. #
+rem Possibly not all metamodel changes have been accounted for, so some may have to be done manually. 
+
+for /f %%a IN ('dir /b *.repository') do (
+
+  sed -i "s/assemblyContexts_ComposedStructure/assemblyContexts__ComposedStructure/g" %%a
+
+  sed -i "s/encapsulatedComponent_AssemblyContext/encapsulatedComponent__AssemblyContext/g" %%a
+  sed -i "s/datatypes_Repository/dataTypes__Repository/g" %%a
+
+  sed -i "s/datatype__Parameter/dataType__Parameter/g" %%a
+
+)
+
+for /f %%a IN ('dir /b *.usagemodel') do (
+ sed -i "s/sdq.ipd.uka.de\/PalladioComponentModel\/UsageModel\/4.0/sdq.ipd.uka.de\/PalladioComponentModel\/UsageModel\/5.0/g" %%a
+)
+
+for /f %%a IN ('dir /b *.allocation') do (
+ sed -i "s/sdq.ipd.uka.de\/PalladioComponentModel\/Allocation\/4.0/sdq.ipd.uka.de\/PalladioComponentModel\/Allocation\/5.0/g" %%a
+)
+
+for /f %%a IN ('dir /b *.repository *.system') do (
+
+  sed -i "s/sdq.ipd.uka.de\/PalladioComponentModel\/Core\/Composition\/4.0/sdq.ipd.uka.de\/PalladioComponentModel\/Core\/Composition\/5.0/g" %%a
+  sed -i "s/sdq.ipd.uka.de\/PalladioComponentModel\/Repository\/4.0/sdq.ipd.uka.de\/PalladioComponentModel\/Repository\/5.0/g" %%a
+  sed -i "s/sdq.ipd.uka.de\/PalladioComponentModel\/SEFF\/4.0/sdq.ipd.uka.de\/PalladioComponentModel\/SEFF\/5.0/g" %%a
+  sed -i "s/sdq.ipd.uka.de\/PalladioComponentModel\/System\/4.0/sdq.ipd.uka.de\/PalladioComponentModel\/System\/5.0/g" %%a
+
+)
+
+for /f %%a IN ('dir /b *.resourceenvironment') do (
+ 
+sed -i "s/name=\"[a-zA-Z_]*\">/><!-- deleted name -->/g" %%a
+rem \">/><!-- deleted name \1 -->
+
+sed -i "s/sdq.ipd.uka.de\/PalladioComponentModel\/ResourceEnvironment\/4.0/sdq.ipd.uka.de\/PalladioComponentModel\/ResourceEnvironment\/5.0/g" %%a
+sed -i "s/schedulingPolicy=\"PROCESSOR_SHARING\">/> <schedulingPolicy href=\"pathmap:\/\/PCM_MODELS\/Palladio.resourcetype#ProcessorSharing\"\/>/g" %%a
+sed -i "s/schedulingPolicy=\"DELAY\">/> <schedulingPolicy href=\"pathmap:\/\/PCM_MODELS\/Palladio.resourcetype#Delay\"\/>/g" %%a 
+sed -i "s/schedulingPolicy=\"FCFS\">/> <schedulingPolicy href=\"pathmap:\/\/PCM_MODELS\/Palladio.resourcetype#FCFS\"\/>/g" %%a
+sed -i "s/schedulingPolicy=\"EXACT\">/> <schedulingPolicy href=\"pathmap:\/\/PCM_MODELS\/Palladio.resourcetype#EXACT\"\/>/g" %%a
+sed -i "s/<activeResourceSpecifications_ResourceContainer>/<activeResourceSpecifications_ResourceContainer> <schedulingPolicy href=\"pathmap:\/\/PCM_MODELS\/Palladio.resourcetype#ProcessorSharing\"\/>/g" %%a
+ 
+)
+
+@echo ********
+@echo Try whether your models are valid now. If not, you may have to change more things manually. Check out https://sdqweb.ipd.kit.edu/wiki/PCM_Model_Migration and https://sdqweb.ipd.kit.edu/wiki/PCM_Changelog
