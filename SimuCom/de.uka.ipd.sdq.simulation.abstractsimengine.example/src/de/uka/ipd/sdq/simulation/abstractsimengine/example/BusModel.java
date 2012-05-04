5f9c96d46990f914f92996aa4faa2c616074737f
pmerkle <merkle@kit.edu>
2012-05-04 12:36:49 +0000

    Provided a simulation example which illustrates the use of the abstract simulation engine
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@15173 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ SimuCom/de.uka.ipd.sdq.simulation.abstractsimengine.example/src/de/uka/ipd/sdq/simulation/abstractsimengine/example/BusModel.java
@@ -0,0 +1,46 @@
+package de.uka.ipd.sdq.simulation.abstractsimengine.example;
+
+import org.osgi.framework.BundleContext;
+
+import de.uka.ipd.sdq.simulation.abstractsimengine.AbstractSimulationModel;
+import de.uka.ipd.sdq.simulation.abstractsimengine.ISimEngineFactory;
+import de.uka.ipd.sdq.simulation.preferences.SimulationPreferencesHelper;
+
+public class BusModel extends AbstractSimulationModel {
+
+    public BusModel(BusSimConfig config, ISimEngineFactory factory) {
+        super(config, factory);
+    }
+
+    @Override
+    public void init() {
+        // nothing to do here
+    }
+
+    @Override
+    public void finalise() {
+        // nothing to do here
+    }
+
+    /**
+     * Creates the simulation model for the specified configuration.
+     * 
+     * @param config
+     *            the simulation configuration
+     * @param bundleContext
+     * @return the created simulation model
+     */
+    public static BusModel create(final BusSimConfig config, final BundleContext bundleContext) {
+        // load factory for the preferred simulation engine
+        ISimEngineFactory factory = SimulationPreferencesHelper.getPreferredSimulationEngine();
+        if (factory == null) {
+            throw new RuntimeException("There is no simulation engine available. Install at least one engine.");
+        }
+
+        // create and return simulation model
+        final BusModel model = new BusModel(config, factory);
+
+        return model;
+    }
+
+}
---
+++ SimuCom/de.uka.ipd.sdq.simulation.abstractsimengine.example/src/de/uka/ipd/sdq/simulation/abstractsimengine/example/BusSimulationExample.java
@@ -0,0 +1,48 @@
+package de.uka.ipd.sdq.simulation.abstractsimengine.example;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+import de.uka.ipd.sdq.simulation.abstractsimengine.ISimulationControl;
+
+public class BusSimulationExample implements IApplication {
+
+    private static final int MAX_SIMULATION_TIME = 1000;
+    
+    private BusSimConfig config;
+    private BusModel model;
+    private ISimulationControl simControl;
+
+    public BusSimulationExample() {
+        this.config = new BusSimConfig();
+        this.model = BusModel.create(config, null);
+        this.simControl = model.getSimulationControl();
+        this.simControl.setMaxSimTime(MAX_SIMULATION_TIME);
+    }
+
+    @Override
+    public Object start(IApplicationContext context) throws Exception {
+        prepareSimulation();
+        
+        // run the simulation
+        model.getSimulationControl().start();
+        
+        return EXIT_OK;
+    }
+
+    @Override
+    public void stop() {
+        // nothing to do
+    }
+    
+    private void prepareSimulation() {
+        Bus transitBusYellowLine = new Bus(60, model, "Transit Bus (Yellow Line)");
+        Bus transitBusBlueLine = new Bus(60, model, "Transit Bus (Blue Line)");
+        Bus tourBus = new Bus(40, model, "Tour Bus");
+        
+        transitBusYellowLine.scheduleAt(0);
+        transitBusBlueLine.scheduleAt(10);
+        tourBus.scheduleAt(100);   
+    }
+    
+}
