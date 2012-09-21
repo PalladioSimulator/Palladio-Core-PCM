3fefe2599966119257e7253ae426a8f348ca41ab
mhauck <hauck@fzi.de>
2012-09-21 11:52:00 +0000

    Moved Completion & CIP plugins that have been factored out from Palladio Core to Palladio/Addons/Completions
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@16500 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Completions/de.uka.ipd.sdq.cip/src/de/uka/ipd/sdq/cip/configuration/CompletionConfigurationProvider.java
+++
@@ -1,7 +0,0 @@
-package de.uka.ipd.sdq.cip.configuration;
-
-
-public interface CompletionConfigurationProvider {
-	public CompletionConfiguration getCompletionConfiguration();
-	public void setCompletionConfiguration(CompletionConfiguration configuration);
-}
