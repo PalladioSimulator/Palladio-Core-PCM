e45eb353a2204461944a505941c58cfa3734ade4
BenjaminKlatt <klatt@fzi.de>
2012-10-16 16:25:29 +0000

    PALLADIO-176 Enable automated SimuCom Model Tests
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@16670 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- SimuCom/de.uka.ipd.sdq.codegen.simucontroller.tests/src/de/uka/ipd/sdq/codegen/simucontroller/tests/SimuControllerTestSuite.java
+++ SimuCom/de.uka.ipd.sdq.codegen.simucontroller.tests/src/de/uka/ipd/sdq/codegen/simucontroller/tests/SimuControllerTestSuite.java
@@ -22,9 +22,9 @@ import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.core.ILaunchManager;
 import org.junit.Test;
 
-import de.uka.ipd.sdq.workflow.launchconfig.ConstantsContainer;
 import de.uka.ipd.sdq.pipesandfilters.framework.recorder.sensorframework.launch.SensorFrameworkConfig;
 import de.uka.ipd.sdq.simucomframework.SimuComConfig;
+import de.uka.ipd.sdq.workflow.pcm.ConstantsContainer;
 
 public class SimuControllerTestSuite {
 
@@ -36,7 +36,8 @@ public class SimuControllerTestSuite {
 	 */
 	@Test
 	public void RunSimuComLaunchConfigs() throws CoreException {
-		ILaunchManager manager = DebugPlugin.getDefault().getLaunchManager();
+		DebugPlugin debugPlugin = DebugPlugin.getDefault();
+		ILaunchManager manager = debugPlugin.getLaunchManager();
 		ILaunchConfigurationType type = manager
 				.getLaunchConfigurationType(SimuControllerTestSuite.ID_SIMUCOM_LAUNCH);
 		ILaunchConfiguration[] configurations = manager
@@ -59,35 +60,42 @@ public class SimuControllerTestSuite {
 	}
 	
 	
-	/** A test of example SimuCom LaunchConfigs -> QoSA2007_MediaStore */ 
+	/** A test of example MediaStore */ 
 	@Test
-	public void RunSimuComLaunch_QoSA2007_MediaStore() throws CoreException {
-		runSimuComLaunch("QoSA2007_MediaStore");
+	public void RunSimuComLaunch_MediaStore() throws CoreException {
+		runSimuComLaunch("MediaStore");
 	}
 	
-	/** A test of example SimuCom LaunchConfigs -> QoSA2006_WebAudioStore */ 
-	@Test
-	public void RunSimuComLaunch_QoSA2006_WebAudioStore() throws CoreException {
-		runSimuComLaunch("QoSA2006_WebAudioStore");
-	}
 	
-	/** A test of example SimuCom LaunchConfigs -> ModelExample_QoSA06 */ 
-	@Test
-	public void RunSimuComLaunch_ModelExample_QoSA06() throws CoreException {
-		runSimuComLaunch("ModelExample_QoSA06");
-	}
-	
-	/** A test of example SimuCom LaunchConfigs -> PCMForkExample */ 
-	@Test
-	public void RunSimuComLaunch_PCMForkExample() throws CoreException {
-		runSimuComLaunch("PCMForkExample");
-	}
-	
-	/** A test of example SimuCom LaunchConfigs -> PCMSetVariableExample */ 
-	@Test
-	public void RunSimuComLaunch_PCMSetVariableExample() throws CoreException {
-		runSimuComLaunch("PCMSetVariableExample");
-	}
+//	/** A test of example SimuCom LaunchConfigs -> QoSA2007_MediaStore */ 
+//	@Test
+//	public void RunSimuComLaunch_QoSA2007_MediaStore() throws CoreException {
+//		runSimuComLaunch("QoSA2007_MediaStore");
+//	}
+//	
+//	/** A test of example SimuCom LaunchConfigs -> QoSA2006_WebAudioStore */ 
+//	@Test
+//	public void RunSimuComLaunch_QoSA2006_WebAudioStore() throws CoreException {
+//		runSimuComLaunch("QoSA2006_WebAudioStore");
+//	}
+//	
+//	/** A test of example SimuCom LaunchConfigs -> ModelExample_QoSA06 */ 
+//	@Test
+//	public void RunSimuComLaunch_ModelExample_QoSA06() throws CoreException {
+//		runSimuComLaunch("ModelExample_QoSA06");
+//	}
+//	
+//	/** A test of example SimuCom LaunchConfigs -> PCMForkExample */ 
+//	@Test
+//	public void RunSimuComLaunch_PCMForkExample() throws CoreException {
+//		runSimuComLaunch("PCMForkExample");
+//	}
+//	
+//	/** A test of example SimuCom LaunchConfigs -> PCMSetVariableExample */ 
+//	@Test
+//	public void RunSimuComLaunch_PCMSetVariableExample() throws CoreException {
+//		runSimuComLaunch("PCMSetVariableExample");
+//	}
 	
 	/**
 	 * A test of example SimuCom LaunchConfigs in the workspace. We are not
@@ -125,12 +133,31 @@ public class SimuControllerTestSuite {
 			copy.setAttribute(ConstantsContainer.SYSTEM_FILE,
 					getModelsFilePath(modelName,
 							ConstantsContainer.SYSTEM_EXTENSION[0]));
+			
+			copy.setAttribute(ConstantsContainer.MWREPOSITORY_FILE,
+					ConstantsContainer.DEFAULT_MIDDLEWARE_REPOSITORY_FILE);
+			
+			copy.setAttribute(ConstantsContainer.EVENT_MIDDLEWARE_REPOSITORY_FILE,
+					ConstantsContainer.DEFAULT_EVENT_MIDDLEWARE_FILE);
+			
+			copy.setAttribute(ConstantsContainer.TEMPORARY_DATA_LOCATION,
+					ConstantsContainer.DEFAULT_TEMPORARY_DATA_LOCATION);
+			
+			copy.setAttribute(ConstantsContainer.ACCURACY_QUALITY_ANNOTATION_FILE,
+					ConstantsContainer.DEFAULT_ACCURACY_QUALITY_ANNOTATION_FILE);
+			
+			copy.setAttribute(ConstantsContainer.FEATURE_CONFIG,
+					ConstantsContainer.DEFAULT_FEATURE_CONFIGURATION_FILE);
 
-			copy.setAttribute(SensorFrameworkConfig.DATASOURCE_ID, 1);
+			copy.setAttribute(SimuComConfig.PERSISTENCE_RECORDER_NAME, SensorFrameworkConfig.PERSISTENCE_RECORDER_ID);
+			copy.setAttribute(SensorFrameworkConfig.DATASOURCE_ID, 0);
+			copy.setAttribute(SimuComConfig.MAXIMUM_MEASUREMENT_COUNT,"100");
+			copy.setAttribute(SimuComConfig.USE_FIXED_SEED,Boolean.FALSE);
 			copy.setAttribute(SimuComConfig.SIMULATION_TIME, "1500");
 			copy.setAttribute(SimuComConfig.EXPERIMENT_RUN, "MyRun");
 			copy.setAttribute(SimuComConfig.SHOULD_THROW_EXCEPTION, true);
 			copy.setAttribute(SimuComConfig.VERBOSE_LOGGING, false);
+			copy.setAttribute(SimuComConfig.SIMULATOR_ID, SimuComConfig.DEFAULT_SIMULATOR_ID);
 			copy.launch(ILaunchManager.RUN_MODE, new NullProgressMonitor());
 		} catch (CoreException ex) {
 			StringWriter sw = new StringWriter();
@@ -152,7 +179,6 @@ public class SimuControllerTestSuite {
 				dir = FileLocator.resolve(dir);
 				dirPath = dir.getPath().replaceFirst("/", "");
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
