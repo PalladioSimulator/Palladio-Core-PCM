d4dc9693ca9526372c8c5e796adfa6a3f3ceaedb
BenjaminKlatt <klatt@fzi.de>
2012-10-17 09:40:44 +0000

    PALLADIO-176 Enable automated SimuCom Model Tests
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@16690 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- SimuCom/de.uka.ipd.sdq.codegen.simucontroller.tests/src/de/uka/ipd/sdq/codegen/simucontroller/tests/SimuControllerTestSuite.java
+++ SimuCom/de.uka.ipd.sdq.codegen.simucontroller.tests/src/de/uka/ipd/sdq/codegen/simucontroller/tests/SimuControllerTestSuite.java
@@ -20,6 +20,7 @@ import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationType;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.jdt.internal.launching.LaunchingPlugin;
 import org.junit.Test;
 
 import de.uka.ipd.sdq.pipesandfilters.framework.recorder.sensorframework.launch.SensorFrameworkConfig;
@@ -30,34 +31,34 @@ public class SimuControllerTestSuite {
 
 	public static final String ID_SIMUCOM_LAUNCH = "de.uka.ipd.sdq.simucontroller.SimuLaunching";
 	
-	/**
-	 * A test of all SimuCom Launch Configs in the workspace. We are not
-	 * expecting any exceptions during the execution
-	 */
-	@Test
-	public void RunSimuComLaunchConfigs() throws CoreException {
-		DebugPlugin debugPlugin = DebugPlugin.getDefault();
-		ILaunchManager manager = debugPlugin.getLaunchManager();
-		ILaunchConfigurationType type = manager
-				.getLaunchConfigurationType(SimuControllerTestSuite.ID_SIMUCOM_LAUNCH);
-		ILaunchConfiguration[] configurations = manager
-				.getLaunchConfigurations(type);
-
-		for (ILaunchConfiguration config : configurations) {
-			try {
-				ILaunchConfigurationWorkingCopy copy = config
-						.copy("Test Suite Launcher");
-				copy.setAttribute(SimuComConfig.SHOULD_THROW_EXCEPTION, false);
-				copy.launch(ILaunchManager.RUN_MODE, new NullProgressMonitor());
-			} catch (CoreException ex) {
-				StringWriter sw = new StringWriter();
-				PrintWriter pw = new PrintWriter(sw);
-				ex.printStackTrace(pw);
-				Assert.fail(config.getName() + "failed: " + ex.getMessage()
-						+ "\n" + sw.toString());
-			}
-		}
-	}
+//	/**
+//	 * A test of all SimuCom Launch Configs in the workspace. We are not
+//	 * expecting any exceptions during the execution
+//	 */
+//	@Test
+//	public void RunSimuComLaunchConfigs() throws CoreException {
+//		DebugPlugin debugPlugin = DebugPlugin.getDefault();
+//		ILaunchManager manager = debugPlugin.getLaunchManager();
+//		ILaunchConfigurationType type = manager
+//				.getLaunchConfigurationType(SimuControllerTestSuite.ID_SIMUCOM_LAUNCH);
+//		ILaunchConfiguration[] configurations = manager
+//				.getLaunchConfigurations(type);
+//
+//		for (ILaunchConfiguration config : configurations) {
+//			try {
+//				ILaunchConfigurationWorkingCopy copy = config
+//						.copy("Test Suite Launcher");
+//				copy.setAttribute(SimuComConfig.SHOULD_THROW_EXCEPTION, false);
+//				copy.launch(ILaunchManager.RUN_MODE, new NullProgressMonitor());
+//			} catch (CoreException ex) {
+//				StringWriter sw = new StringWriter();
+//				PrintWriter pw = new PrintWriter(sw);
+//				ex.printStackTrace(pw);
+//				Assert.fail(config.getName() + "failed: " + ex.getMessage()
+//						+ "\n" + sw.toString());
+//			}
+//		}
+//	}
 	
 	
 	/** A test of example MediaStore */ 
