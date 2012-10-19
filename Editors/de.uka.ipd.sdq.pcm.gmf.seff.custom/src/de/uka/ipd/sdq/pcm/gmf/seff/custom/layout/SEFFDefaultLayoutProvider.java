71a3e8384164078a803f8f74f90873f1805c1130
joehen <joerghe@gmail.com>
2012-10-19 08:55:55 +0000

    changed style to match CheckStyle
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@16770 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Editors/de.uka.ipd.sdq.pcm.gmf.seff.custom/src/de/uka/ipd/sdq/pcm/gmf/seff/custom/layout/SEFFDefaultLayoutProvider.java
+++ Editors/de.uka.ipd.sdq.pcm.gmf.seff.custom/src/de/uka/ipd/sdq/pcm/gmf/seff/custom/layout/SEFFDefaultLayoutProvider.java
@@ -11,43 +11,39 @@ import org.eclipse.gmf.runtime.diagram.ui.services.layout.ILayoutNodeOperation;
 import de.uka.ipd.sdq.pcm.gmf.seff.edit.parts.ResourceDemandingSEFFEditPart;
 
 /**
- * Enable topdown layout for SEFFs
+ * Enable topdown layout for SEFFs.
  * @author joerg henss
- *
  */
-public class SEFFDefaultLayoutProvider extends TopDownProvider
-{
-	
-	/**
-	 * Check if we have a SEFF
-	 */
-    public boolean provides(IOperation operation) {
-        
-        if (operation instanceof ILayoutNodeOperation) 
-        {
-        	ILayoutNodeOperation op = (ILayoutNodeOperation) operation;
-        	IAdaptable hint = op.getLayoutHint();
-        	
-			if(hint instanceof ObjectAdapter)
-			{
-				GraphicalEditPart part = (GraphicalEditPart)hint.getAdapter(GraphicalEditPart.class);
-        		if(part != null)
-        		{
-        			 EditPart root = part.getRoot().getContents();
-        			if(part instanceof ResourceDemandingSEFFEditPart || root instanceof ResourceDemandingSEFFEditPart)
-        				return true;
-        		}
-			}	
-        	        	
-            
-        } 
-        
+public class SEFFDefaultLayoutProvider extends TopDownProvider {
+
+    /**
+     * Check if we have a SEFF.
+     * @param operation The layout operation to support
+     * @return true if we are in a SEFF diagram
+     */
+    @Override
+    public final boolean provides(final IOperation operation) {
+
+        if (operation instanceof ILayoutNodeOperation) {
+            ILayoutNodeOperation op = (ILayoutNodeOperation) operation;
+            IAdaptable hint = op.getLayoutHint();
+
+            if (hint instanceof ObjectAdapter) {
+                GraphicalEditPart part = (GraphicalEditPart) hint
+                        .getAdapter(GraphicalEditPart.class);
+                if (part != null) {
+                    EditPart root = part.getRoot().getContents();
+                    if (part instanceof ResourceDemandingSEFFEditPart
+                            || root instanceof ResourceDemandingSEFFEditPart) {
+                        return true;
+                    }
+                }
+            }
+
+        }
+
         return false;
-        
+
     }
 
-   
-
-    
-    
 }
