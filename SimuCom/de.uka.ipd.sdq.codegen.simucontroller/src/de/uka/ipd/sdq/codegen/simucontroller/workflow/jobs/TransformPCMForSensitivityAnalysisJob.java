24fa090ae0df1d8304ca4ce203b36b1a802de05d
AnneKoziolek <anne.koziolek@kit.edu>
2013-06-19 08:46:36 +0000

    reverted revision 17064 (which was a suggested fix for Palladio-217) because it limits the flexibility of the sensitivity analysis: It only allowed to change ResourceEnvironment parameters. But it should be also possible to change other parameters, such as resource demands, component parameters, or workload parameters.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@18942 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- SimuCom/de.uka.ipd.sdq.codegen.simucontroller/src/de/uka/ipd/sdq/codegen/simucontroller/workflow/jobs/TransformPCMForSensitivityAnalysisJob.java
+++ SimuCom/de.uka.ipd.sdq.codegen.simucontroller/src/de/uka/ipd/sdq/codegen/simucontroller/workflow/jobs/TransformPCMForSensitivityAnalysisJob.java
@@ -7,12 +7,16 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 
 import de.uka.ipd.sdq.codegen.simucontroller.runconfig.AbstractSimulationWorkflowConfiguration;
 import de.uka.ipd.sdq.pcm.core.PCMRandomVariable;
-import de.uka.ipd.sdq.pcm.resourceenvironment.ResourceEnvironment;
+import de.uka.ipd.sdq.pcm.core.entity.Entity;
 import de.uka.ipd.sdq.pcm.usagemodel.ClosedWorkload;
 import de.uka.ipd.sdq.pcm.usagemodel.OpenWorkload;
 import de.uka.ipd.sdq.workflow.IBlackboardInteractingJob;
@@ -73,23 +77,28 @@ public class TransformPCMForSensitivityAnalysisJob implements
 			throw new JobFailedException(
 					"Bad Sensitivity Analysis Job Configuration, no variable specified.");
 
+		URI uri = URI.createURI(sconf.getVariable());
+
+		ResourceSet rs = new ResourceSetImpl();
+		Resource resource = rs.getResource(uri, true);
 		String uriString = sconf.getVariable();
 		String varUri = uriString.substring(uriString.indexOf("#")+1);
+		EObject variableToChange = resource.getEObject(varUri);
 
-		EObject o = null; Resource r; Iterator<Resource> it = pcmPartition.getResourceSet().getResources().iterator();
-		while ( (r = it.next()) != null && o == null) {
-			if (r.getContents().get(0) instanceof ResourceEnvironment) {
-				o = r.getEObject(varUri);
+		EObject o = null;
+		for (Resource r : pcmPartition.getResourceSet().getResources()) {
+			if (o == null) {
+				o = findEObject(r.getContents().get(0), variableToChange);
+				if (o != null) {
+					if(logger.isEnabledFor(Level.INFO)) 
+						logger.info("Altering PCM variable for sensitivity analysis");
+					alterObject(o, sconf);
+				}
 			}
 		}
 		if (o == null)
 			throw new JobFailedException(
 					"Sensistivity analaysis transformation was unable to find the sensitivity variable");
-		else {
-			if(logger.isEnabledFor(Level.INFO)) 
-				logger.info("Altering PCM variable for sensitivity analysis");
-			alterObject(o, sconf);
-		}
 	}
 
 	public void rollback(IProgressMonitor monitor)
@@ -119,11 +128,80 @@ public class TransformPCMForSensitivityAnalysisJob implements
 		}
 		if (o instanceof ClosedWorkload) {
 			ClosedWorkload cw = (ClosedWorkload) o;
-			cw.setPopulation((int) value);
+			cw.setPopulation((int)sconf.getCurrent());
 		}
 		if (o instanceof OpenWorkload) {
 			OpenWorkload ow = (OpenWorkload) o;
 			ow.getInterArrivalTime_OpenWorkload().setSpecification("Exp(" + value + ")");
 		}
 	}
+
+	/**
+	 * Finds object in tree based on the {@link #entityBasedEquals(EObject, EObject)} equality definition.
+	 *
+	 * @param objectTreeToSearchIn
+	 * @param eObjectToFind
+	 * @return
+	 */
+	private EObject findEObject(EObject objectTreeToSearchIn, EObject eObjectToFind) {
+		Iterator<EObject> it = objectTreeToSearchIn.eAllContents();
+
+		while (it.hasNext()) {
+			EObject eo = it.next();
+
+			if (entityBasedEquals(eo, eObjectToFind)) {
+					return eo;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Finds object based on the following recursive equality definition:
+	 *
+	 * Two objects are equal iff (1) they have both instance of {@link Entity}
+	 * and have the same id, or (2) they are not both instances of
+	 * {@link Entity} and they are instances of the same {@link EClass} and
+	 * their parents are of equal {@link EClass} and their parents are equal
+	 * based on this definition.
+	 */
+	private boolean entityBasedEquals(EObject eObject1, EObject eObject2) {
+
+		if (eObject1 == eObject2){
+			return true;
+		}
+
+		if (eObject1 != null && eObject2 != null){
+
+			if (eObject1 instanceof Entity && eObject2 instanceof Entity) {
+				Entity entity1 = (Entity) eObject1;
+				Entity entity2 = (Entity) eObject2;
+
+				if (entity1.getId().equals(entity2.getId())) {
+					return true;
+				}
+
+			} else {
+
+				//they must have the same class.
+				EClass eClass1 = eObject1.eClass();
+			    if (eClass1 != eObject2.eClass()){
+			    	return false;
+			    }
+
+				// objects could be the same, check parents
+			    EObject parent1 = eObject1.eContainer();
+			    EObject parent2 = eObject2.eContainer();
+
+				return entityBasedEquals(parent1, parent2);
+
+			}
+
+
+		}
+
+		return false;
+
+	}
+
 }
