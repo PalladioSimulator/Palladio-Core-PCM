b40b5bf0c5aa303152b8c5ec4536f4df7a393da6
s_foehrd <s_foehrd>
2013-10-07 11:19:46 +0000

    Moved simulation preference page to abstractsimengine plugin
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/branches/DeclarativeServices@19525 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ de.uka.ipd.sdq.simulation.abstractsimengine/src/de/uka/ipd/sdq/simulation/preferences/SimulationPreferencePage.java
@@ -0,0 +1,59 @@
+package de.uka.ipd.sdq.simulation.preferences;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.preference.ComboFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+import de.uka.ipd.sdq.simulation.abstractsimengine.Activator;
+import de.uka.ipd.sdq.simulation.abstractsimengine.util.AbstractSimEngineExtensionHelper;
+
+/**
+ * The preference page for the abstract simulation engine.
+ * 
+ * @author Philipp Merkle
+ * 
+ */
+public class SimulationPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+
+    private static final Logger logger = Logger.getLogger(SimulationPreferencePage.class);
+
+    /** the id for the simulation engine preference **/
+    public static final String PREFERENCE_SIMULATION_ENGINE_ID = "simulationEngineField";
+
+    /**
+     * Default constructor.
+     */
+    public SimulationPreferencePage() {
+        // use grid layout
+        super(GRID);
+    }
+
+    @Override
+    public void init(IWorkbench workbench) {
+        this.setDescription("Select the simulation engine to be used for simulation runs.");
+        this.setPreferenceStore(Activator.getDefault().getPreferenceStore());
+    }
+
+    @Override
+    protected void createFieldEditors() {
+        // retrieve simulation engine names and ids
+        String[][] engineNamesAndIds = null;
+        try {
+            engineNamesAndIds = AbstractSimEngineExtensionHelper.getEngineNamesAndIds();
+        } catch (CoreException e) {
+            if (logger.isEnabledFor(Level.WARN))
+                logger.warn("Could not retrieve simulation engine names and ids.", e);
+        }
+
+        // create field editor
+        ComboFieldEditor simulationEngineEditor = new ComboFieldEditor(PREFERENCE_SIMULATION_ENGINE_ID,
+                "Simulation Engine", engineNamesAndIds, getFieldEditorParent());
+        addField(simulationEngineEditor);
+
+    }
+
+}
---
+++ de.uka.ipd.sdq.simulation.abstractsimengine/src/de/uka/ipd/sdq/simulation/preferences/SimulationPreferencesHelper.java
@@ -0,0 +1,58 @@
+package de.uka.ipd.sdq.simulation.preferences;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Platform;
+
+import de.uka.ipd.sdq.simulation.abstractsimengine.Activator;
+import de.uka.ipd.sdq.simulation.abstractsimengine.ISimEngineFactory;
+import de.uka.ipd.sdq.simulation.abstractsimengine.util.AbstractSimEngineExtensionHelper;
+
+public class SimulationPreferencesHelper {
+
+    private static final Logger logger = Logger.getLogger(SimulationPreferencesHelper.class);
+
+    /**
+     * Returns the default simulation engine. The default engine is the first entry in the list of
+     * extensions.
+     */
+    public static String getDefaultEngineId() {
+        // retrieve all available simulation engines and set the first engine as default.
+        String firstEngineId = null;
+        try {
+            String[][] engineNamesAndIds = AbstractSimEngineExtensionHelper.getEngineNamesAndIds();
+            if (engineNamesAndIds.length > 0) {
+                firstEngineId = engineNamesAndIds[0][1];
+            }
+        } catch (CoreException e) {
+            if (logger.isEnabledFor(Level.WARN))
+                logger.warn("Could not retrieve simulation engine names and ids.", e);
+        }
+        return firstEngineId;
+    }
+
+    public static ISimEngineFactory getPreferredSimulationEngine() {
+        // retrieve the id of the preferred engine
+        String preferredEngineId = Platform.getPreferencesService().getString(Activator.PLUGIN_ID,
+                SimulationPreferencePage.PREFERENCE_SIMULATION_ENGINE_ID, getDefaultEngineId(), null);
+
+        // retrieve simulation engine factory for the preferred engine
+        ISimEngineFactory engineFactory = null;
+        try {
+            engineFactory = AbstractSimEngineExtensionHelper.getEngineFactory(preferredEngineId);
+        } catch (CoreException e) {
+            logger.warn("Could not load preferred simulation engine " + preferredEngineId + ".", e);
+        }
+
+        // if no factory has been found for the preferred engine, use the default engine
+        try {
+            engineFactory = AbstractSimEngineExtensionHelper.getEngineFactory(getDefaultEngineId());
+        } catch (CoreException e) {
+            logger.warn("Could not load default simulation engine " + getDefaultEngineId() + ".", e);
+        }
+
+        return engineFactory;
+    }
+
+}
