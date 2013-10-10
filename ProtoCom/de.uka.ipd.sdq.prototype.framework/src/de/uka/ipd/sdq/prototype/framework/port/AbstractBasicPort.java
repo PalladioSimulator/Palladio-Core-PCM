19bcbf6632029b6576b98357f0701548d703a7f5
zolynski <thomas.zolynski@comsysto.com>
2013-10-10 16:40:43 +0000

    ProtoCom: added call visitor
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@19546 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- ProtoCom/de.uka.ipd.sdq.prototype.framework/src/de/uka/ipd/sdq/prototype/framework/port/AbstractBasicPort.java
+++ ProtoCom/de.uka.ipd.sdq.prototype.framework/src/de/uka/ipd/sdq/prototype/framework/port/AbstractBasicPort.java
@@ -1,8 +1,12 @@
 package de.uka.ipd.sdq.prototype.framework.port;
 
 import java.rmi.RemoteException;
+import java.util.LinkedList;
+import java.util.List;
 
 import de.uka.ipd.sdq.prototype.framework.IComponent;
+import de.uka.ipd.sdq.prototype.framework.visitor.ICallVisitor;
+import de.uka.ipd.sdq.simucomframework.variables.StackContext;
 
 /**
  * Port class for basic components.
@@ -18,6 +22,8 @@ public class AbstractBasicPort<AComponent extends IComponent> extends java.rmi.s
 
 	private static final long serialVersionUID = -7620141651057731901L;
 
+	protected List<ICallVisitor> visitors = new LinkedList<ICallVisitor>();
+	
 	protected AComponent myComponent = null;
 
 	protected AbstractBasicPort() throws RemoteException {
@@ -38,6 +44,22 @@ public class AbstractBasicPort<AComponent extends IComponent> extends java.rmi.s
 	public void setComponentFrame(de.uka.ipd.sdq.simucomframework.variables.stackframe.SimulatedStackframe<Object> myComponentStackFrame)
 			throws java.rmi.RemoteException {
 		myComponent.setComponentFrame(myComponentStackFrame);
+	}
+	
+	public void addVisitor(ICallVisitor visitor) {
+		visitors.add(visitor);
+	}
+	
+	protected void preCallVisitor(StackContext ctx, String callIdentifier) {
+		for (ICallVisitor visitor : visitors) {
+			visitor.preCallVisit(ctx, callIdentifier);
+		}
+	}
+	
+	protected void postCallVisitor(StackContext ctx, String callIdentifier) {
+		for (ICallVisitor visitor : visitors) {
+			visitor.postCallVisit(ctx, callIdentifier);
+		}
 	}
 
 }
