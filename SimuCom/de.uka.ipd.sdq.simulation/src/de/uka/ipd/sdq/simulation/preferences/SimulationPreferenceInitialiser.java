12fae7ad842623db763963ea5bcd8477d7b1f8f4
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2013-11-10 08:48:26 +0000

    Removed warnings
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@19767 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- SimuCom/de.uka.ipd.sdq.simulation/src/de/uka/ipd/sdq/simulation/preferences/SimulationPreferenceInitialiser.java
+++ SimuCom/de.uka.ipd.sdq.simulation/src/de/uka/ipd/sdq/simulation/preferences/SimulationPreferenceInitialiser.java
@@ -1,6 +1,5 @@
 package de.uka.ipd.sdq.simulation.preferences;
 
-import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.core.runtime.preferences.DefaultScope;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
@@ -15,15 +14,13 @@ import de.uka.ipd.sdq.simulation.Activator;
  */
 public class SimulationPreferenceInitialiser extends AbstractPreferenceInitializer {
 
-    private static final Logger logger = Logger.getLogger(SimulationPreferenceInitialiser.class);
-
     @Override
     public void initializeDefaultPreferences() {
         // retrieve all available simulation engines and set the first engine as default.
         String firstEngineId = SimulationPreferencesHelper.getDefaultEngineId();
 
         // set the default simulation engine
-        IEclipsePreferences preferences = new DefaultScope().getNode(Activator.PLUGIN_ID);
+        IEclipsePreferences preferences = DefaultScope.INSTANCE.getNode(Activator.PLUGIN_ID);
         preferences.put(SimulationPreferencePage.PREFERENCE_SIMULATION_ENGINE_ID, firstEngineId);
     }
 
