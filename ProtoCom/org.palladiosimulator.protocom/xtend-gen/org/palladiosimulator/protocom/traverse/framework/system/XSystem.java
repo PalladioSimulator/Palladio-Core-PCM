e5a8d0c96947006e98dc5a27d6711c27e38d65dc
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-01-09 10:54:18 +0000

    preparation for regeneration
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@20346 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- ProtoCom/org.palladiosimulator.protocom/xtend-gen/org/palladiosimulator/protocom/traverse/framework/system/XSystem.java
+++
@@ -1,7 +0,0 @@
-package org.palladiosimulator.protocom.traverse.framework.system;
-
-import org.palladiosimulator.protocom.traverse.framework.PcmRepresentative;
-
-@SuppressWarnings("all")
-public class XSystem extends PcmRepresentative<System> {
-}
