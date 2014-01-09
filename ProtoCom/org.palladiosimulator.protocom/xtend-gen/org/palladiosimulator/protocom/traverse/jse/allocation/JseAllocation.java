d94321284a1e971637367f0561948e6e0e3bb429
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-01-09 06:24:37 +0000

    Update to new palladio URL: regenerated xtend files
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@20324 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ ProtoCom/org.palladiosimulator.protocom/xtend-gen/org/palladiosimulator/protocom/traverse/jse/allocation/JseAllocation.java
@@ -0,0 +1,22 @@
+package org.palladiosimulator.protocom.traverse.jse.allocation;
+
+import org.palladiosimulator.protocom.lang.GeneratedFile;
+import org.palladiosimulator.protocom.lang.java.IJClass;
+import org.palladiosimulator.protocom.lang.java.impl.JClass;
+import org.palladiosimulator.protocom.tech.rmi.PojoAllocationStorage;
+import org.palladiosimulator.protocom.traverse.framework.allocation.XAllocation;
+
+/**
+ * An Allocation for JSE translates into one additional container information file.
+ * 
+ * @author Thomas Zolynski
+ */
+@SuppressWarnings("all")
+public class JseAllocation extends XAllocation {
+  public void generate() {
+    JClass _instance = this.injector.<JClass>getInstance(JClass.class);
+    PojoAllocationStorage _pojoAllocationStorage = new PojoAllocationStorage(this.entity);
+    GeneratedFile<IJClass> _createFor = _instance.createFor(_pojoAllocationStorage);
+    this.generatedFiles.add(_createFor);
+  }
+}
