e3679f6f953a44c4714a7cf1e0f97d18bd6cc66a
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2014-01-10 14:49:29 +0000

    Removed warnings
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@20368 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- StoEx/de.uka.ipd.sdq.pcm.stochasticexpressions.tests/src/de/uka/ipd/sdq/pcm/stochasticexpressions/tests/TypeInferTests.java
+++ StoEx/de.uka.ipd.sdq.pcm.stochasticexpressions.tests/src/de/uka/ipd/sdq/pcm/stochasticexpressions/tests/TypeInferTests.java
@@ -1,6 +1,5 @@
 package de.uka.ipd.sdq.pcm.stochasticexpressions.tests;
 
-import junit.framework.Assert;
 import junit.framework.TestCase;
 import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.runtime.CommonTokenStream;
@@ -20,25 +19,25 @@ public class TypeInferTests extends TestCase {
 		ExpressionInferTypeVisitor visitor = new ExpressionInferTypeVisitor();
 		Expression expression = parser("\"blah\"");
 		infer(expression,visitor);
-		Assert.assertEquals(TypeEnum.ENUM, visitor.getType(expression));
+		assertEquals(TypeEnum.ENUM, visitor.getType(expression));
 		visitor = new ExpressionInferTypeVisitor();
 		expression = parser("EnumPMF[(\"blah\";0.4)(\"blub\";0.6)]");
 		infer(expression,visitor);
-		Assert.assertEquals(TypeEnum.ENUM_PMF, visitor.getType(expression));
+		assertEquals(TypeEnum.ENUM_PMF, visitor.getType(expression));
 	}
 	
 	public void testIntPMF() throws RecognitionException{
 		ExpressionInferTypeVisitor visitor = new ExpressionInferTypeVisitor();
 		Expression expression = parser("IntPMF[(1;0.2)(2;0.4)]");
 		infer(expression,visitor);
-		Assert.assertEquals(TypeEnum.INT_PMF, visitor.getType(expression));
+		assertEquals(TypeEnum.INT_PMF, visitor.getType(expression));
 	}
 
 	public void testNegativeDoublePDF() throws RecognitionException{
 		ExpressionInferTypeVisitor visitor = new ExpressionInferTypeVisitor();
 		Expression expression = parser("DoublePDF[(-1.0;0.2)(2.0;0.4)]");
 		infer(expression,visitor);
-		Assert.assertEquals(TypeEnum.DOUBLE_PDF, visitor.getType(expression));
+		assertEquals(TypeEnum.DOUBLE_PDF, visitor.getType(expression));
 	}
 
 	
@@ -46,54 +45,54 @@ public class TypeInferTests extends TestCase {
 		ExpressionInferTypeVisitor visitor = new ExpressionInferTypeVisitor();
 		Expression expression = parser("192.0 / file.STRUCTURE < 1");
 		infer(expression,visitor);
-		Assert.assertEquals(TypeEnum.BOOL_PMF, visitor.getType(expression));
+		assertEquals(TypeEnum.BOOL_PMF, visitor.getType(expression));
 	}
 
 	public void testDoubleAnyCompareIfElse() throws RecognitionException{
 		ExpressionInferTypeVisitor visitor = new ExpressionInferTypeVisitor();
 		IfElseExpression expression = (IfElseExpression) parser("192.0 / file.STRUCTURE < 1 ? 5 : 5");
 		infer(expression,visitor);
-		Assert.assertEquals(TypeEnum.ANY, visitor.getType(expression));
-		Assert.assertEquals(TypeEnum.BOOL_PMF, visitor.getType(expression.getConditionExpression()));
+		assertEquals(TypeEnum.ANY, visitor.getType(expression));
+		assertEquals(TypeEnum.BOOL_PMF, visitor.getType(expression.getConditionExpression()));
 	}
 
 	public void testTypeInfererBool() throws RecognitionException {
-		Assert.assertTrue(infer("true AND false") == TypeEnum.BOOL);
-		Assert.assertTrue(infer("true OR false") == TypeEnum.BOOL);
-		Assert.assertTrue(infer("128.0 < 192 OR false") == TypeEnum.BOOL);
-		Assert.assertTrue(infer("NOT(128.0 < 192 OR false)") == TypeEnum.BOOL);
-		Assert.assertTrue(infer("true ? 1 : 5") == TypeEnum.ANY);
+		assertTrue(infer("true AND false") == TypeEnum.BOOL);
+		assertTrue(infer("true OR false") == TypeEnum.BOOL);
+		assertTrue(infer("128.0 < 192 OR false") == TypeEnum.BOOL);
+		assertTrue(infer("NOT(128.0 < 192 OR false)") == TypeEnum.BOOL);
+		assertTrue(infer("true ? 1 : 5") == TypeEnum.ANY);
 	}
 	
 	public void testsubInfer() throws RecognitionException {
 		ExpressionInferTypeVisitor visitor = new ExpressionInferTypeVisitor();
 		IfElseExpression expression = (IfElseExpression) parser("true ? 1 : 5.5");
 		infer(expression,visitor);
-		Assert.assertEquals(visitor.getType(expression.getConditionExpression()),TypeEnum.BOOL);
-		Assert.assertEquals(TypeEnum.INT,visitor.getType(expression.getIfExpression()));
-		Assert.assertEquals(TypeEnum.DOUBLE,visitor.getType(expression.getElseExpression()));
+		assertEquals(visitor.getType(expression.getConditionExpression()),TypeEnum.BOOL);
+		assertEquals(TypeEnum.INT,visitor.getType(expression.getIfExpression()));
+		assertEquals(TypeEnum.DOUBLE,visitor.getType(expression.getElseExpression()));
 	}
 
 	public void testSubFuncInfer() throws RecognitionException {
 		ExpressionInferTypeVisitor visitor = new ExpressionInferTypeVisitor();
 		FunctionLiteral expression = (FunctionLiteral) parser("Trunc(2.5)");
 		infer(expression,visitor);
-		Assert.assertEquals(TypeEnum.INT_PMF,visitor.getType(expression));
-		Assert.assertEquals(TypeEnum.DOUBLE,visitor.getType(expression.getParameters_FunctionLiteral().get(0)));
+		assertEquals(TypeEnum.INT_PMF,visitor.getType(expression));
+		assertEquals(TypeEnum.DOUBLE,visitor.getType(expression.getParameters_FunctionLiteral().get(0)));
 	}	
 
 	public void testVariables() throws RecognitionException {
 		ExpressionInferTypeVisitor visitor = new ExpressionInferTypeVisitor();
 		Expression expression = parser("file.TYPE");
 		infer(expression,visitor);
-		Assert.assertEquals(TypeEnum.ANY_PMF,visitor.getType(expression));
+		assertEquals(TypeEnum.ANY_PMF,visitor.getType(expression));
 	}
 
 	public void testParenthesis() throws RecognitionException {
 		ExpressionInferTypeVisitor visitor = new ExpressionInferTypeVisitor();
 		Expression expression = parser("file.BYTESIZE * ( file.TYPE / 192 )");
 		infer(expression,visitor);
-		Assert.assertEquals(TypeEnum.ANY_PMF,visitor.getType(expression));
+		assertEquals(TypeEnum.ANY_PMF,visitor.getType(expression));
 	}
 	
 	public void testTenaryOp() throws RecognitionException {
