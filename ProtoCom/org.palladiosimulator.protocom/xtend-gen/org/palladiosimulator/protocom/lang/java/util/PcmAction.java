3dfedb67171fc0ab31b15b427a9cce8e04ea17f8
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-01-13 11:46:37 +0000

    Regenerated xtend code
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@20400 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ ProtoCom/org.palladiosimulator.protocom/xtend-gen/org/palladiosimulator/protocom/lang/java/util/PcmAction.java
@@ -0,0 +1,402 @@
+package org.palladiosimulator.protocom.lang.java.util;
+
+import de.uka.ipd.sdq.pcm.repository.PassiveResource;
+import de.uka.ipd.sdq.pcm.seff.AbstractAction;
+import de.uka.ipd.sdq.pcm.seff.AbstractBranchTransition;
+import de.uka.ipd.sdq.pcm.seff.AcquireAction;
+import de.uka.ipd.sdq.pcm.seff.BranchAction;
+import de.uka.ipd.sdq.pcm.seff.CollectionIteratorAction;
+import de.uka.ipd.sdq.pcm.seff.ExternalCallAction;
+import de.uka.ipd.sdq.pcm.seff.ForkAction;
+import de.uka.ipd.sdq.pcm.seff.GuardedBranchTransition;
+import de.uka.ipd.sdq.pcm.seff.InternalAction;
+import de.uka.ipd.sdq.pcm.seff.LoopAction;
+import de.uka.ipd.sdq.pcm.seff.ProbabilisticBranchTransition;
+import de.uka.ipd.sdq.pcm.seff.ReleaseAction;
+import de.uka.ipd.sdq.pcm.seff.SetVariableAction;
+import de.uka.ipd.sdq.pcm.seff.StartAction;
+import de.uka.ipd.sdq.pcm.seff.StopAction;
+import de.uka.ipd.sdq.pcm.usagemodel.AbstractUserAction;
+import de.uka.ipd.sdq.pcm.usagemodel.Branch;
+import de.uka.ipd.sdq.pcm.usagemodel.Delay;
+import de.uka.ipd.sdq.pcm.usagemodel.EntryLevelSystemCall;
+import de.uka.ipd.sdq.pcm.usagemodel.Loop;
+import de.uka.ipd.sdq.pcm.usagemodel.Start;
+import de.uka.ipd.sdq.pcm.usagemodel.Stop;
+import java.util.Arrays;
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.xbase.lib.Functions.Function1;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import org.palladiosimulator.protocom.lang.java.util.JavaNames;
+
+/**
+ * Abstract class for implementing PCM actions, i.e., the behavior of components as specified
+ * via SEFFs. Refinements of this class could, for instance, add simulate stack frames for performance
+ * prototypes or provide code stub generation.
+ * 
+ * @author Sebastian Lehrig
+ */
+@SuppressWarnings("all")
+public abstract class PcmAction {
+  /**
+   * Follows the action path and calls "action" for each action in it.
+   * Note that actions do not branch! Branching is solved by a BranchAction, therefore
+   * at most one successor is given at any time.
+   */
+  public String actions(final AbstractAction action) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("/*");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("* ");
+    Class<? extends AbstractAction> _class = action.getClass();
+    String _simpleName = _class.getSimpleName();
+    _builder.append(_simpleName, " ");
+    _builder.append(" (");
+    _builder.append(action, " ");
+    _builder.append(")");
+    _builder.newLineIfNotEmpty();
+    _builder.append(" ");
+    _builder.append("*/");
+    _builder.newLine();
+    String _action = this.action(action);
+    _builder.append(_action, "");
+    _builder.newLineIfNotEmpty();
+    _builder.newLine();
+    {
+      boolean _isInstance = StopAction.class.isInstance(action);
+      boolean _not = (!_isInstance);
+      if (_not) {
+        AbstractAction _successor_AbstractAction = action.getSuccessor_AbstractAction();
+        String _actions = this.actions(_successor_AbstractAction);
+        _builder.append(_actions, "");
+        _builder.newLineIfNotEmpty();
+      }
+    }
+    return _builder.toString();
+  }
+  
+  /**
+   * StartAction. Should be empty, I guess.
+   */
+  protected String _action(final StartAction action) {
+    StringConcatenation _builder = new StringConcatenation();
+    return _builder.toString();
+  }
+  
+  /**
+   * StopAction. Nothing to see here either.
+   */
+  protected String _action(final StopAction action) {
+    StringConcatenation _builder = new StringConcatenation();
+    return _builder.toString();
+  }
+  
+  /**
+   * No idea. We didn't implement this for the last ProtoCom either.
+   * FIXME Implement this as it is a crucial part of the bahavior (lehrig)
+   */
+  protected String _action(final CollectionIteratorAction action) {
+    StringConcatenation _builder = new StringConcatenation();
+    return _builder.toString();
+  }
+  
+  /**
+   * LoopAction is transformed into a simple FOR statement.
+   */
+  protected String _action(final LoopAction action) {
+    StringConcatenation _builder = new StringConcatenation();
+    return _builder.toString();
+  }
+  
+  /**
+   * ExternalCallAction calls a remote service.
+   */
+  protected String _action(final ExternalCallAction action) {
+    StringConcatenation _builder = new StringConcatenation();
+    return _builder.toString();
+  }
+  
+  /**
+   * InteralAction uses a load generator to simulate CPU/HDD usage.
+   * Note that ProtoCom does NOT use InfrastructureCalls from the PCM model, because these
+   * should be reflected by the underlying middleware and OS!
+   */
+  protected String _action(final InternalAction action) {
+    StringConcatenation _builder = new StringConcatenation();
+    return _builder.toString();
+  }
+  
+  /**
+   * BranchAction is implemented as an IF condition. A BranchAction may have two different transition types:
+   * ProbabilisticBranchTransition and GuardedBranchTransition.
+   */
+  protected String _action(final BranchAction action) {
+    StringConcatenation _builder = new StringConcatenation();
+    return _builder.toString();
+  }
+  
+  /**
+   * Branch transition for ProbabilisticBranchTransition entities.
+   */
+  protected CharSequence _branchTransition(final BranchAction action, final ProbabilisticBranchTransition transition) {
+    StringConcatenation _builder = new StringConcatenation();
+    return _builder;
+  }
+  
+  /**
+   * Branch transition for GuardedBranchTransition.
+   */
+  protected CharSequence _branchTransition(final BranchAction action, final GuardedBranchTransition transition) {
+    StringConcatenation _builder = new StringConcatenation();
+    return _builder;
+  }
+  
+  /**
+   * AcquireAction is mapped to the acquire method of Java Collection's semaphore implementation.
+   */
+  protected String _action(final AcquireAction action) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("try {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("//logger.debug(\"Acquiring passive resource ");
+    PassiveResource _passiveresource_AcquireAction = action.getPassiveresource_AcquireAction();
+    String _entityName = _passiveresource_AcquireAction.getEntityName();
+    String _javaString = JavaNames.javaString(_entityName);
+    _builder.append(_javaString, "	");
+    _builder.append("\");");
+    _builder.newLineIfNotEmpty();
+    _builder.append("\t");
+    _builder.append("passive_resource_");
+    PassiveResource _passiveresource_AcquireAction_1 = action.getPassiveresource_AcquireAction();
+    String _entityName_1 = _passiveresource_AcquireAction_1.getEntityName();
+    String _javaVariableName = JavaNames.javaVariableName(_entityName_1);
+    _builder.append(_javaVariableName, "	");
+    _builder.append(".acquire();");
+    _builder.newLineIfNotEmpty();
+    _builder.append("} catch (InterruptedException e) {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("//logger.error(\"Should never happen: Acquire of semaphore ");
+    PassiveResource _passiveresource_AcquireAction_2 = action.getPassiveresource_AcquireAction();
+    String _entityName_2 = _passiveresource_AcquireAction_2.getEntityName();
+    String _javaString_1 = JavaNames.javaString(_entityName_2);
+    _builder.append(_javaString_1, "	");
+    _builder.append(" interrupted\");");
+    _builder.newLineIfNotEmpty();
+    _builder.append("\t");
+    _builder.append("java.lang.System.exit(-1);");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    return _builder.toString();
+  }
+  
+  /**
+   * ReleaseAction is mapped to the release method of Java Collection's semaphore implementation.
+   */
+  protected String _action(final ReleaseAction action) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("// Release ");
+    PassiveResource _passiveResource_ReleaseAction = action.getPassiveResource_ReleaseAction();
+    _builder.append(_passiveResource_ReleaseAction, "");
+    _builder.newLineIfNotEmpty();
+    _builder.append("//logger.debug(\"Releasing passive resource ");
+    PassiveResource _passiveResource_ReleaseAction_1 = action.getPassiveResource_ReleaseAction();
+    String _entityName = _passiveResource_ReleaseAction_1.getEntityName();
+    String _javaString = JavaNames.javaString(_entityName);
+    _builder.append(_javaString, "");
+    _builder.append("\");");
+    _builder.newLineIfNotEmpty();
+    _builder.append("passive_resource_");
+    PassiveResource _passiveResource_ReleaseAction_2 = action.getPassiveResource_ReleaseAction();
+    String _entityName_1 = _passiveResource_ReleaseAction_2.getEntityName();
+    String _javaVariableName = JavaNames.javaVariableName(_entityName_1);
+    _builder.append(_javaVariableName, "");
+    _builder.append(".release();");
+    _builder.newLineIfNotEmpty();
+    return _builder.toString();
+  }
+  
+  protected String _action(final SetVariableAction action) {
+    return null;
+  }
+  
+  /**
+   * A ForkAction spawns a new thread for each defined behavior. These should be processed asynchronously in parallel.
+   * Please note that manually spawning new threads is discouraged on certain middlewares (like JavaEE)!
+   */
+  protected String _action(final ForkAction action) {
+    StringConcatenation _builder = new StringConcatenation();
+    return _builder.toString();
+  }
+  
+  /**
+   * Follows the user action path and calls "userAction" for each action in it.
+   * Note that actions do not branch! Branching is solved by a Branch action, therefore
+   * at most one successor is given at any time.
+   */
+  public String userActions(final AbstractUserAction userAction) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("/*");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("* ");
+    Class<? extends AbstractUserAction> _class = userAction.getClass();
+    String _simpleName = _class.getSimpleName();
+    _builder.append(_simpleName, " ");
+    _builder.append(" (");
+    _builder.append(userAction, " ");
+    _builder.append(")");
+    _builder.newLineIfNotEmpty();
+    _builder.append(" ");
+    _builder.append("*/");
+    _builder.newLine();
+    String _userAction = this.userAction(userAction);
+    _builder.append(_userAction, "");
+    _builder.newLineIfNotEmpty();
+    _builder.newLine();
+    {
+      boolean _isInstance = Stop.class.isInstance(userAction);
+      boolean _not = (!_isInstance);
+      if (_not) {
+        AbstractUserAction _successor = userAction.getSuccessor();
+        String _userActions = this.userActions(_successor);
+        _builder.append(_userActions, "");
+        _builder.newLineIfNotEmpty();
+      }
+    }
+    return _builder.toString();
+  }
+  
+  /**
+   * EntryLevelSystemCall is an user action which calls a system service from an usage scenario.
+   */
+  protected String _userAction(final EntryLevelSystemCall userAction) {
+    StringConcatenation _builder = new StringConcatenation();
+    return _builder.toString();
+  }
+  
+  /**
+   * FIXME Implement and test this action with Thread.sleep
+   */
+  protected String _userAction(final Delay userAction) {
+    StringConcatenation _builder = new StringConcatenation();
+    return _builder.toString();
+  }
+  
+  protected String _userAction(final Start userAction) {
+    StringConcatenation _builder = new StringConcatenation();
+    return _builder.toString();
+  }
+  
+  protected String _userAction(final Stop userAction) {
+    StringConcatenation _builder = new StringConcatenation();
+    return _builder.toString();
+  }
+  
+  /**
+   * Loop actions are transformed into a simple FOR statement.
+   */
+  protected String _userAction(final Loop userAction) {
+    StringConcatenation _builder = new StringConcatenation();
+    return _builder.toString();
+  }
+  
+  /**
+   * UserActions only have probabilistic transitions.
+   */
+  protected String _userAction(final Branch userAction) {
+    StringConcatenation _builder = new StringConcatenation();
+    return _builder.toString();
+  }
+  
+  /**
+   * Helper method to find the first StartAction in a list of actions.
+   */
+  public static StartAction findStart(final Iterable<AbstractAction> actions) {
+    final Function1<AbstractAction,Boolean> _function = new Function1<AbstractAction,Boolean>() {
+      public Boolean apply(final AbstractAction it) {
+        boolean _isInstance = StartAction.class.isInstance(it);
+        return Boolean.valueOf(_isInstance);
+      }
+    };
+    AbstractAction _findFirst = IterableExtensions.<AbstractAction>findFirst(actions, _function);
+    return ((StartAction) _findFirst);
+  }
+  
+  /**
+   * And another helper method, since Actions and UserActions are *obviously* so
+   * different that they cannot have a common supertype...
+   */
+  public static Start findUserStart(final Iterable<AbstractUserAction> actions) {
+    final Function1<AbstractUserAction,Boolean> _function = new Function1<AbstractUserAction,Boolean>() {
+      public Boolean apply(final AbstractUserAction it) {
+        boolean _isInstance = Start.class.isInstance(it);
+        return Boolean.valueOf(_isInstance);
+      }
+    };
+    AbstractUserAction _findFirst = IterableExtensions.<AbstractUserAction>findFirst(actions, _function);
+    return ((Start) _findFirst);
+  }
+  
+  public String action(final AbstractAction action) {
+    if (action instanceof CollectionIteratorAction) {
+      return _action((CollectionIteratorAction)action);
+    } else if (action instanceof LoopAction) {
+      return _action((LoopAction)action);
+    } else if (action instanceof AcquireAction) {
+      return _action((AcquireAction)action);
+    } else if (action instanceof BranchAction) {
+      return _action((BranchAction)action);
+    } else if (action instanceof ForkAction) {
+      return _action((ForkAction)action);
+    } else if (action instanceof InternalAction) {
+      return _action((InternalAction)action);
+    } else if (action instanceof ReleaseAction) {
+      return _action((ReleaseAction)action);
+    } else if (action instanceof SetVariableAction) {
+      return _action((SetVariableAction)action);
+    } else if (action instanceof StartAction) {
+      return _action((StartAction)action);
+    } else if (action instanceof StopAction) {
+      return _action((StopAction)action);
+    } else if (action instanceof ExternalCallAction) {
+      return _action((ExternalCallAction)action);
+    } else {
+      throw new IllegalArgumentException("Unhandled parameter types: " +
+        Arrays.<Object>asList(action).toString());
+    }
+  }
+  
+  public CharSequence branchTransition(final BranchAction action, final AbstractBranchTransition transition) {
+    if (transition instanceof GuardedBranchTransition) {
+      return _branchTransition(action, (GuardedBranchTransition)transition);
+    } else if (transition instanceof ProbabilisticBranchTransition) {
+      return _branchTransition(action, (ProbabilisticBranchTransition)transition);
+    } else {
+      throw new IllegalArgumentException("Unhandled parameter types: " +
+        Arrays.<Object>asList(action, transition).toString());
+    }
+  }
+  
+  public String userAction(final AbstractUserAction userAction) {
+    if (userAction instanceof Branch) {
+      return _userAction((Branch)userAction);
+    } else if (userAction instanceof Delay) {
+      return _userAction((Delay)userAction);
+    } else if (userAction instanceof EntryLevelSystemCall) {
+      return _userAction((EntryLevelSystemCall)userAction);
+    } else if (userAction instanceof Loop) {
+      return _userAction((Loop)userAction);
+    } else if (userAction instanceof Start) {
+      return _userAction((Start)userAction);
+    } else if (userAction instanceof Stop) {
+      return _userAction((Stop)userAction);
+    } else {
+      throw new IllegalArgumentException("Unhandled parameter types: " +
+        Arrays.<Object>asList(userAction).toString());
+    }
+  }
+}
