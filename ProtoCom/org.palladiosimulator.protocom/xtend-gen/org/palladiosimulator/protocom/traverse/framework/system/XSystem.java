394c7b113df1e9a5d3768161c6f652b4386be4ff
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-01-21 08:38:26 +0000

    Preparing for xtend regeneration
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@20498 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- ProtoCom/org.palladiosimulator.protocom/xtend-gen/org/palladiosimulator/protocom/traverse/framework/system/XSystem.java
+++
@@ -1,7 +0,0 @@
-package org.palladiosimulator.protocom.traverse.framework.system;
-
-import org.palladiosimulator.protocom.traverse.framework.PcmRepresentative;
-
-@SuppressWarnings("all")
-public class XSystem extends PcmRepresentative<de.uka.ipd.sdq.pcm.system.System> {
-}
