4b993cad9258e326253a3bb635f8e1eb6ab9a4c3
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-01-21 13:31:01 +0000

    Regenerated xtend code
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@20503 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ ProtoCom/org.palladiosimulator.protocom/xtend-gen/org/palladiosimulator/protocom/lang/java/IJField.java
@@ -0,0 +1,21 @@
+package org.palladiosimulator.protocom.lang.java;
+
+@SuppressWarnings("all")
+public interface IJField {
+  /**
+   * The name of the field.
+   */
+  public abstract String name();
+  
+  /**
+   * Type of the field.
+   */
+  public abstract String type();
+  
+  /**
+   * The visibility modifier of this field.
+   * 
+   * Default value is PROTECTED.
+   */
+  public abstract String visibility();
+}
