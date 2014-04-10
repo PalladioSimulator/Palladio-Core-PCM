e9678e77ff2a2478f48a81f988246fb9cb4692d7
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-04-10 09:41:29 +0000

    Preparing code regeneration
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@21770 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- ProtoCom/org.palladiosimulator.protocom/xtend-gen/org/palladiosimulator/protocom/lang/java/IJeeInterface.java
+++
@@ -1,8 +0,0 @@
-package org.palladiosimulator.protocom.lang.java;
-
-import org.palladiosimulator.protocom.lang.java.IJInterface;
-
-@SuppressWarnings("all")
-public interface IJeeInterface extends IJInterface {
-  public abstract String jeeInterfaceRemoteAnnotation();
-}
