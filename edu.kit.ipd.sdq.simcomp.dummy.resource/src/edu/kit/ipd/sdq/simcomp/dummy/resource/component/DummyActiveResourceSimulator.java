2ea2fab70207bf0c2757b9e57722f8c16fb10038
cfoehrdes <christoph.foehrdes@leaphub.de>
2014-06-02 12:17:03 +0000

    Added simulation middleware reference to dummy resource
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/branches/SimulationComponents@22743 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- edu.kit.ipd.sdq.simcomp.dummy.resource/src/edu/kit/ipd/sdq/simcomp/dummy/resource/component/DummyActiveResourceSimulator.java
+++ edu.kit.ipd.sdq.simcomp.dummy.resource/src/edu/kit/ipd/sdq/simcomp/dummy/resource/component/DummyActiveResourceSimulator.java
@@ -3,6 +3,7 @@ package edu.kit.ipd.sdq.simcomp.dummy.resource.component;
 import de.uka.ipd.sdq.pcm.resourceenvironment.ResourceContainer;
 import de.uka.ipd.sdq.pcm.resourcetype.ResourceType;
 import edu.kit.ipd.sdq.simcomp.component.IRequest;
+import edu.kit.ipd.sdq.simcomp.component.ISimulationMiddleware;
 import edu.kit.ipd.sdq.simcomp.resource.active.component.IActiveResource;
 
 /**
@@ -14,9 +15,33 @@ import edu.kit.ipd.sdq.simcomp.resource.active.component.IActiveResource;
  */
 public class DummyActiveResourceSimulator implements IActiveResource {
 
+	private ISimulationMiddleware middleware;
+
 	@Override
 	public void consume(IRequest request, ResourceContainer resourceContainer, ResourceType resourceType, double absoluteDemand) {
 		// we do nothing as this is a dummy implementation
+	}
+
+	/**
+	 * Binds a simulation middleware instance to the simulation component.
+	 * Called by the declarative service framework.
+	 * 
+	 * @param middleware
+	 */
+	public void bindSimulationMiddleware(ISimulationMiddleware middleware) {
+		this.middleware = middleware;
+	}
+
+	/**
+	 * Unbind a simulation middleware instance from the simulation component
+	 * when it is deactivated. Called by the declarative service framework.
+	 * 
+	 * @param middleware
+	 */
+	public void unbindSimulationMiddleware(ISimulationMiddleware middleware) {
+		if (this.middleware.equals(middleware)) {
+			this.middleware = null;
+		}
 	}
 
 }
