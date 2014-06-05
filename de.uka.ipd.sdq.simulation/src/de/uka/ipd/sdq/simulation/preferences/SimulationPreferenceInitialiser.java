5dcaaa32e52b0ef6d46a384c00d7a7492038b938
pmerkle <merkle@kit.edu>
2014-06-05 11:54:48 +0000

    Share project "de.uka.ipd.sdq.simulation" into "https://svnserver.informatik.kit.edu/i43/svn/code" (branched from rev. 19767)
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/branches/SimulationComponents@22848 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ de.uka.ipd.sdq.simulation/src/de/uka/ipd/sdq/simulation/preferences/SimulationPreferenceInitialiser.java
@@ -0,0 +1,27 @@
+package de.uka.ipd.sdq.simulation.preferences;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.core.runtime.preferences.DefaultScope;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+
+import de.uka.ipd.sdq.simulation.Activator;
+
+/**
+ * Initialises the preferences for {@link SimulationPreferencePage}.
+ * 
+ * @author Philipp Merkle
+ * 
+ */
+public class SimulationPreferenceInitialiser extends AbstractPreferenceInitializer {
+
+    @Override
+    public void initializeDefaultPreferences() {
+        // retrieve all available simulation engines and set the first engine as default.
+        String firstEngineId = SimulationPreferencesHelper.getDefaultEngineId();
+
+        // set the default simulation engine
+        IEclipsePreferences preferences = DefaultScope.INSTANCE.getNode(Activator.PLUGIN_ID);
+        preferences.put(SimulationPreferencePage.PREFERENCE_SIMULATION_ENGINE_ID, firstEngineId);
+    }
+
+}
