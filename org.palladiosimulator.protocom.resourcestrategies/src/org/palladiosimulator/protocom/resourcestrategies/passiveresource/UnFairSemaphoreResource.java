f0f002a57465c02a0a7a9a87cfeede897ae167db
klaussner <christian@klaussner.me>
2014-06-13 17:38:26 +0000

    Added branch for ProtoCom WebUI
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/branches/ProtoCom-WebUI@23003 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ org.palladiosimulator.protocom.resourcestrategies/src/org/palladiosimulator/protocom/resourcestrategies/passiveresource/UnFairSemaphoreResource.java
@@ -0,0 +1,18 @@
+package org.palladiosimulator.protocom.resourcestrategies.passiveresource;
+
+
+public class UnFairSemaphoreResource implements IPassiveResourceStrategy {
+
+	//@Override
+	public void acquire(int num) {
+		// TODO Auto-generated method stub
+
+	}
+
+	//@Override
+	public void release(int num) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
