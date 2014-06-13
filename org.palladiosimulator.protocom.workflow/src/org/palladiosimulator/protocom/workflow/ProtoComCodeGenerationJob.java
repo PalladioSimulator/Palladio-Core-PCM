3f36c7c299ccbd55b226cb9bbbce83b4bae95dee
klaussner <christian@klaussner.me>
2014-06-13 17:39:18 +0000

    Added branch for ProtoCom WebUI
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/branches/ProtoCom-WebUI@23007 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ org.palladiosimulator.protocom.workflow/src/org/palladiosimulator/protocom/workflow/ProtoComCodeGenerationJob.java
@@ -0,0 +1,52 @@
+package org.palladiosimulator.protocom.workflow;
+
+import org.eclipse.core.runtime.CoreException;
+
+import de.uka.ipd.sdq.codegen.simucontroller.debug.IDebugListener;
+import de.uka.ipd.sdq.workflow.jobs.IBlackboardInteractingJob;
+import de.uka.ipd.sdq.workflow.jobs.SequentialBlackboardInteractingJob;
+import de.uka.ipd.sdq.workflow.mdsd.blackboard.MDSDBlackboard;
+import de.uka.ipd.sdq.workflow.pcm.jobs.LoadPCMModelsIntoBlackboardJob;
+import de.uka.ipd.sdq.workflow.pcm.jobs.ValidatePCMModelsJob;
+
+/**
+ * Main job for the SDQ workflow engine which will run a ProtoCom generation
+ * @author Steffen Becker, Thomas Zolynski, Sebastian Lehrig
+ */
+public class ProtoComCodeGenerationJob 
+extends SequentialBlackboardInteractingJob<MDSDBlackboard>
+implements IBlackboardInteractingJob<MDSDBlackboard> {
+
+	public ProtoComCodeGenerationJob(ProtoComGenerationConfiguration configuration) throws CoreException {
+		this(configuration, null);
+	}
+	
+	public ProtoComCodeGenerationJob(ProtoComGenerationConfiguration configuration, IDebugListener listener) throws CoreException {
+		super(false);
+		
+		if (listener == null && configuration.isDebug())
+			throw new IllegalArgumentException("Debug listener has to be non-null for debug runs");
+		
+		// 1. Load PCM models into memory		
+		this.addJob(new LoadPCMModelsIntoBlackboardJob(configuration));
+
+		// 2. Validate PCM models in memory
+		this.addJob(new ValidatePCMModelsJob(configuration));
+
+		// TODO Daria: Remove the following job and integrate project creation into transformation
+		// Create empty project
+	//	this.add(new CreatePluginProjectJob(configuration));
+        
+		// 3. Generate code into projects using Xtend
+		this.addJob(new TransformPCMToCodeXtendJob(configuration));
+
+		// TODO Daria: Remove step 4, delete CreateProtoComMetaDataFilesJob, and integrate manifest generation into transformation
+		// 4. Generate MANIFEST.MF file
+	//	this.addJob(new CreateProtoComMetaDataFilesJob(configuration));			
+		
+		// TODO Check the role of the following step once Daria's tasks are done
+		// 5. Compile the plugin (otherwise the source files are not properly found)
+	// this.addJob(new CompilePluginCodeJob(configuration));
+
+	}
+}
