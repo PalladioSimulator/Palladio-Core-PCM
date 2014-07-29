5c712fb3d676b6df6a9446db473283d63af889d0
jonasheinisch <jonas@heinisch-web.de>
2014-07-29 16:39:51 +0000

    added better ability for finding the TypeMismatchIssue in AST
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk/StoEx@23742 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- de.uka.ipd.sdq.stoex.analyser/src/de/uka/ipd/sdq/stoex/analyser/exceptions/ExpectedTypeMismatchIssue.java
+++ de.uka.ipd.sdq.stoex.analyser/src/de/uka/ipd/sdq/stoex/analyser/exceptions/ExpectedTypeMismatchIssue.java
@@ -1,5 +1,8 @@
 package de.uka.ipd.sdq.stoex.analyser.exceptions;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+
 import de.uka.ipd.sdq.errorhandling.IIssue;
 import de.uka.ipd.sdq.stoex.analyser.visitors.TypeEnum;
 
@@ -7,6 +10,8 @@ public class ExpectedTypeMismatchIssue implements IIssue {
 
 	private String expectedType;
 	private TypeEnum foundType;
+	private EObject object;
+    private EStructuralFeature feature;
 
 	public ExpectedTypeMismatchIssue(TypeEnum expectedType, TypeEnum type) {
 		this.expectedType = expectedType.name();
@@ -17,12 +22,40 @@ public class ExpectedTypeMismatchIssue implements IIssue {
 		this.expectedType = expectedType;
 		this.foundType = type;
 	}
+	
+	public ExpectedTypeMismatchIssue(String expectedType, TypeEnum type, EObject object, EStructuralFeature feature) {
+        this(expectedType, type);
+        this.object = object;
+        this.feature = feature;
+    }
+	
+	public ExpectedTypeMismatchIssue(TypeEnum expectedType, TypeEnum type, EObject object, EStructuralFeature feature) {
+        this(expectedType, type);
+        this.object = object;
+        this.feature = feature;
+    }
 
 	public String getMessage() {
 		return 
 			"Expected Type and Actual Type mismatch.\nInfered Type: " +
 			( foundType == null ? "unknown" : foundType.name() ) +
 			", Expected Type: "+expectedType;
+	}
+	
+	public TypeEnum getFoundType(){
+	    return foundType;
+	}
+	
+	public String getExpectedType(){
+	    return expectedType;
+	}
+	
+	public EObject getObject(){
+	    return object;
+	}
+	
+	public EStructuralFeature getFeature(){
+	    return feature;
 	}
 
 }
