c09eed379c19f64dba2d609e132a80daccbfd102
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-08-18 10:54:01 +0000

    PCM Launch config actually did not include a lot of PCM-specific concepts. Therefore, moved some methods to super class.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@24116 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Workflow/de.uka.ipd.sdq.workflow.pcm/src/de/uka/ipd/sdq/workflow/pcm/configurations/AbstractPCMLaunchConfigurationDelegate.java
+++ Workflow/de.uka.ipd.sdq.workflow.pcm/src/de/uka/ipd/sdq/workflow/pcm/configurations/AbstractPCMLaunchConfigurationDelegate.java
@@ -1,16 +1,6 @@
 package de.uka.ipd.sdq.workflow.pcm.configurations;
 
-import java.util.ArrayList;
-
-import org.apache.log4j.Level;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.debug.core.ILaunch;
-
-import de.uka.ipd.sdq.workflow.logging.console.LoggerAppenderStruct;
 import de.uka.ipd.sdq.workflow.mdsd.AbstractWorkflowBasedMDSDLaunchConfigurationDelegate;
-import de.uka.ipd.sdq.workflow.mdsd.blackboard.MDSDBlackboard;
-import de.uka.ipd.sdq.workflow.ui.UIBasedWorkflow;
 
 /**
  * Base class for workflow based launch configurations (i.e., Eclipse Run and Debug tasks) which sets up a blackboard
@@ -34,39 +24,4 @@ extends
 		super();
 	}
 
-	/* (non-Javadoc)
-	 * @see de.uka.ipd.sdq.workflow.mdsd.AbstractWorkflowBasedMDSDLaunchConfigurationDelegate#createBlackboard()
-	 */
-	@Override
-	protected MDSDBlackboard createBlackboard() {
-		return new MDSDBlackboard();
-	}
-
-	/* (non-Javadoc)
-	 * @see de.uka.ipd.sdq.workflow.launchconfig.AbstractWorkflowBasedLaunchConfigurationDelegate#createWorkflow(de.uka.ipd.sdq.workflow.launchconfig.AbstractWorkflowBasedRunConfiguration, org.eclipse.core.runtime.IProgressMonitor, org.eclipse.debug.core.ILaunch)
-	 */
-	@Override
-	protected UIBasedWorkflow<MDSDBlackboard> createWorkflow(
-			WorkflowConfigurationType workflowConfiguration,
-			IProgressMonitor monitor,
-			ILaunch launch) throws CoreException {
-		return new UIBasedWorkflow<MDSDBlackboard>(
-				createWorkflowJob(workflowConfiguration, launch),
-				monitor,
-				createExcpetionHandler(workflowConfiguration.isInteractive()),
-				createBlackboard());
-	}
-
-	/* (non-Javadoc)
-	 * @see de.uka.ipd.sdq.workflow.launchconfig.AbstractWorkflowBasedLaunchConfigurationDelegate#setupLogging(org.apache.log4j.Level)
-	 */
-	@Override
-	protected ArrayList<LoggerAppenderStruct> setupLogging(Level logLevel) throws CoreException {
-		ArrayList<LoggerAppenderStruct> loggerList = super.setupLogging(logLevel);
-
-		// Setup openArchitectureWare Logging
-		loggerList.add(setupLogger("org.openarchitectureware", logLevel, SHORT_LOG_PATTERN));
-
-		return loggerList;
-	}
 }
