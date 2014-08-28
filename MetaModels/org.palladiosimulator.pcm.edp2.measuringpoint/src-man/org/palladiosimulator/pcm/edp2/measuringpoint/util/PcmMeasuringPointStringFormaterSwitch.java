b8fe5ea7c45548f32646ba93b5dbeff6bfc64258
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-08-28 06:28:29 +0000

    Added support for MeasuringPoints for EntryLevelSystemCalls and ExternalCallActions
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@24205 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- MetaModels/org.palladiosimulator.pcm.edp2.measuringpoint/src-man/org/palladiosimulator/pcm/edp2/measuringpoint/util/PcmMeasuringPointStringFormaterSwitch.java
+++ MetaModels/org.palladiosimulator.pcm.edp2.measuringpoint/src-man/org/palladiosimulator/pcm/edp2/measuringpoint/util/PcmMeasuringPointStringFormaterSwitch.java
@@ -3,6 +3,8 @@ package org.palladiosimulator.pcm.edp2.measuringpoint.util;
 import org.palladiosimulator.pcmmeasuringpoint.ActiveResourceMeasuringPoint;
 import org.palladiosimulator.pcmmeasuringpoint.AssemblyOperationMeasuringPoint;
 import org.palladiosimulator.pcmmeasuringpoint.AssemblyPassiveResourceMeasuringPoint;
+import org.palladiosimulator.pcmmeasuringpoint.EntryLevelSystemCallMeasuringPoint;
+import org.palladiosimulator.pcmmeasuringpoint.ExternalCallActionMeasuringPoint;
 import org.palladiosimulator.pcmmeasuringpoint.LinkingResourceMeasuringPoint;
 import org.palladiosimulator.pcmmeasuringpoint.SubSystemOperationMeasuringPoint;
 import org.palladiosimulator.pcmmeasuringpoint.SystemOperationMeasuringPoint;
@@ -10,13 +12,19 @@ import org.palladiosimulator.pcmmeasuringpoint.UsageScenarioMeasuringPoint;
 import org.palladiosimulator.pcmmeasuringpoint.util.PcmmeasuringpointSwitch;
 
 import de.uka.ipd.sdq.pcm.resourceenvironment.ProcessingResourceSpecification;
+import de.uka.ipd.sdq.pcm.seff.ExternalCallAction;
+import de.uka.ipd.sdq.pcm.usagemodel.EntryLevelSystemCall;
 
 public class PcmMeasuringPointStringFormaterSwitch extends PcmmeasuringpointSwitch<String> {
 
     private final StringBuilder result = new StringBuilder();
 
-    /* (non-Javadoc)
-     * @see org.palladiosimulator.pcmmeasuringpoint.util.PcmmeasuringpointSwitch#caseAssemblyOperationMeasuringPoint(org.palladiosimulator.pcmmeasuringpoint.AssemblyOperationMeasuringPoint)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.palladiosimulator.pcmmeasuringpoint.util.PcmmeasuringpointSwitch#
+     * caseAssemblyOperationMeasuringPoint
+     * (org.palladiosimulator.pcmmeasuringpoint.AssemblyOperationMeasuringPoint)
      */
     @Override
     public String caseAssemblyOperationMeasuringPoint(final AssemblyOperationMeasuringPoint object) {
@@ -30,8 +38,12 @@ public class PcmMeasuringPointStringFormaterSwitch extends PcmmeasuringpointSwit
         return result.toString();
     }
 
-    /* (non-Javadoc)
-     * @see org.palladiosimulator.pcmmeasuringpoint.util.PcmmeasuringpointSwitch#caseAssemblyPassiveResourceMeasuringPoint(org.palladiosimulator.pcmmeasuringpoint.AssemblyPassiveResourceMeasuringPoint)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.palladiosimulator.pcmmeasuringpoint.util.PcmmeasuringpointSwitch#
+     * caseAssemblyPassiveResourceMeasuringPoint
+     * (org.palladiosimulator.pcmmeasuringpoint.AssemblyPassiveResourceMeasuringPoint)
      */
     @Override
     public String caseAssemblyPassiveResourceMeasuringPoint(final AssemblyPassiveResourceMeasuringPoint object) {
@@ -43,24 +55,65 @@ public class PcmMeasuringPointStringFormaterSwitch extends PcmmeasuringpointSwit
         return result.toString();
     }
 
-    /* (non-Javadoc)
-     * @see org.palladiosimulator.pcmmeasuringpoint.util.PcmmeasuringpointSwitch#caseActiveResourceMeasuringPoint(org.palladiosimulator.pcmmeasuringpoint.ActiveResourceMeasuringPoint)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.palladiosimulator.pcmmeasuringpoint.util.PcmmeasuringpointSwitch#
+     * caseActiveResourceMeasuringPoint
+     * (org.palladiosimulator.pcmmeasuringpoint.ActiveResourceMeasuringPoint)
      */
     @Override
     public String caseActiveResourceMeasuringPoint(final ActiveResourceMeasuringPoint object) {
-        final ProcessingResourceSpecification activeResource = object
-                .getActiveResource();
+        final ProcessingResourceSpecification activeResource = object.getActiveResource();
         result.append(activeResource.getActiveResourceType_ActiveResourceSpecification().getEntityName());
         result.append(" [");
         result.append(String.valueOf(object.getReplicaID()));
         result.append("] in ");
-        result.append(object.getActiveResource()
-                .getResourceContainer_ProcessingResourceSpecification().getEntityName());
+        result.append(object.getActiveResource().getResourceContainer_ProcessingResourceSpecification().getEntityName());
         return result.toString();
     }
 
-    /* (non-Javadoc)
-     * @see org.palladiosimulator.pcmmeasuringpoint.util.PcmmeasuringpointSwitch#caseSystemOperationMeasuringPoint(org.palladiosimulator.pcmmeasuringpoint.SystemOperationMeasuringPoint)
+    @Override
+    public String caseEntryLevelSystemCallMeasuringPoint(EntryLevelSystemCallMeasuringPoint object) {
+        final EntryLevelSystemCall entryLevelSystemCall = object.getEntryLevelSystemCall();
+        result.append("SystemCall ");
+        result.append(entryLevelSystemCall.getEntityName());
+        result.append(" to ");
+        result.append(entryLevelSystemCall.getProvidedRole_EntryLevelSystemCall().getProvidingEntity_ProvidedRole()
+                .getEntityName());
+        result.append("->");
+        result.append(entryLevelSystemCall.getProvidedRole_EntryLevelSystemCall().getEntityName());
+        result.append("->");
+        result.append(entryLevelSystemCall.getOperationSignature__EntryLevelSystemCall().getEntityName());
+        result.append(" [");
+        result.append(entryLevelSystemCall.getId());
+        result.append("]");
+        return result.toString();
+    }
+
+    @Override
+    public String caseExternalCallActionMeasuringPoint(ExternalCallActionMeasuringPoint object) {
+        final ExternalCallAction externalCall = object.getExternalCall();
+        result.append("ExternalCall ");
+        result.append(externalCall.getEntityName());
+        result.append(" from ");
+        result.append(externalCall.getRole_ExternalService().getRequiringEntity_RequiredRole().getEntityName());
+        result.append(" to ");
+        result.append(externalCall.getRole_ExternalService().getEntityName());
+        result.append("->");
+        result.append(externalCall.getCalledService_ExternalService().getEntityName());
+        result.append(" [");
+        result.append(externalCall.getId());
+        result.append("]");
+        return result.toString();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.palladiosimulator.pcmmeasuringpoint.util.PcmmeasuringpointSwitch#
+     * caseSystemOperationMeasuringPoint
+     * (org.palladiosimulator.pcmmeasuringpoint.SystemOperationMeasuringPoint)
      */
     @Override
     public String caseSystemOperationMeasuringPoint(final SystemOperationMeasuringPoint object) {
@@ -73,8 +126,12 @@ public class PcmMeasuringPointStringFormaterSwitch extends PcmmeasuringpointSwit
         return result.toString();
     }
 
-    /* (non-Javadoc)
-     * @see org.palladiosimulator.pcmmeasuringpoint.util.PcmmeasuringpointSwitch#caseLinkingResourceMeasuringPoint(org.palladiosimulator.pcmmeasuringpoint.LinkingResourceMeasuringPoint)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.palladiosimulator.pcmmeasuringpoint.util.PcmmeasuringpointSwitch#
+     * caseLinkingResourceMeasuringPoint
+     * (org.palladiosimulator.pcmmeasuringpoint.LinkingResourceMeasuringPoint)
      */
     @Override
     public String caseLinkingResourceMeasuringPoint(final LinkingResourceMeasuringPoint object) {
@@ -84,8 +141,12 @@ public class PcmMeasuringPointStringFormaterSwitch extends PcmmeasuringpointSwit
         return result.toString();
     }
 
-    /* (non-Javadoc)
-     * @see org.palladiosimulator.pcmmeasuringpoint.util.PcmmeasuringpointSwitch#caseSubSystemOperationMeasuringPoint(org.palladiosimulator.pcmmeasuringpoint.SubSystemOperationMeasuringPoint)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.palladiosimulator.pcmmeasuringpoint.util.PcmmeasuringpointSwitch#
+     * caseSubSystemOperationMeasuringPoint
+     * (org.palladiosimulator.pcmmeasuringpoint.SubSystemOperationMeasuringPoint)
      */
     @Override
     public String caseSubSystemOperationMeasuringPoint(final SubSystemOperationMeasuringPoint object) {
@@ -99,8 +160,12 @@ public class PcmMeasuringPointStringFormaterSwitch extends PcmmeasuringpointSwit
         return result.toString();
     }
 
-    /* (non-Javadoc)
-     * @see org.palladiosimulator.pcmmeasuringpoint.util.PcmmeasuringpointSwitch#caseUsageScenarioMeasuringPoint(org.palladiosimulator.pcmmeasuringpoint.UsageScenarioMeasuringPoint)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.palladiosimulator.pcmmeasuringpoint.util.PcmmeasuringpointSwitch#
+     * caseUsageScenarioMeasuringPoint
+     * (org.palladiosimulator.pcmmeasuringpoint.UsageScenarioMeasuringPoint)
      */
     @Override
     public String caseUsageScenarioMeasuringPoint(final UsageScenarioMeasuringPoint object) {
