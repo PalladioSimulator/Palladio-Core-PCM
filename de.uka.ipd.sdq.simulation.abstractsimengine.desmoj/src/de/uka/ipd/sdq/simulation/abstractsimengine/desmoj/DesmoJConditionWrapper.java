985ee6d4d77bceea563c66089682300050d2f3aa
jonasheinisch <jonas@heinisch-web.de>
2014-08-30 20:41:55 +0000

    started palladio core's branch for stoex migration with xtext
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/branches/stoex-xtext-migration@24350 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ de.uka.ipd.sdq.simulation.abstractsimengine.desmoj/src/de/uka/ipd/sdq/simulation/abstractsimengine/desmoj/DesmoJConditionWrapper.java
@@ -0,0 +1,30 @@
+package de.uka.ipd.sdq.simulation.abstractsimengine.desmoj;
+
+import de.uka.ipd.sdq.simulation.abstractsimengine.SimCondition;
+import desmoj.core.simulator.Condition;
+import desmoj.core.simulator.Entity;
+
+/**
+ * @author Steffen Becker
+ * @author Philipp Merkle
+ */
+public class DesmoJConditionWrapper extends Condition<Entity> {
+
+    private SimCondition abstractCondition;
+
+    public DesmoJConditionWrapper(SimCondition abstractCondition, DesmoJModel owner) {
+        super(owner, abstractCondition.getClass().getName(), false);
+        this.abstractCondition = abstractCondition;
+    }
+
+    @Override
+    public boolean check() {
+        return abstractCondition.check();
+    }
+
+    @Override
+    public boolean check(Entity arg0) {
+        return false;
+    }
+
+}
