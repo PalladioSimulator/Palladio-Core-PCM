8c0ea0d98e5d29cc067d4fd75832d4672587905f
jonasheinisch <jonas@heinisch-web.de>
2014-08-30 20:39:38 +0000

    started palladio core's branch for stoex migration with xtext
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/branches/stoex-xtext-migration@24317 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ de.uka.ipd.sdq.simucomframework.simucomstatus/src/de/uka/ipd/sdq/simucomframework/simucomstatus/Action.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright 2008, sdq.ipd.uka.de, U KA
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.simucomframework.simucomstatus;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc --> A representation of the model object '<em><b>Action</b></em>'. <!--
+ * end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ * <li>{@link de.uka.ipd.sdq.simucomframework.simucomstatus.Action#getActionStartTime <em>Action
+ * Start Time</em>}</li>
+ * <li>{@link de.uka.ipd.sdq.simucomframework.simucomstatus.Action#getActionDuration <em>Action
+ * Duration</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see de.uka.ipd.sdq.simucomframework.simucomstatus.SimucomstatusPackage#getAction()
+ * @model abstract="true"
+ * @generated
+ */
+public interface Action extends EObject {
+    /**
+     * Returns the value of the '<em><b>Action Start Time</b></em>' attribute. <!-- begin-user-doc
+     * -->
+     * <p>
+     * If the meaning of the '<em>Action Start Time</em>' attribute isn't clear, there really should
+     * be more of a description here...
+     * </p>
+     * <!-- end-user-doc -->
+     * 
+     * @return the value of the '<em>Action Start Time</em>' attribute.
+     * @see #setActionStartTime(double)
+     * @see de.uka.ipd.sdq.simucomframework.simucomstatus.SimucomstatusPackage#getAction_ActionStartTime()
+     * @model required="true" ordered="false"
+     * @generated
+     */
+    double getActionStartTime();
+
+    /**
+     * Sets the value of the '
+     * {@link de.uka.ipd.sdq.simucomframework.simucomstatus.Action#getActionStartTime
+     * <em>Action Start Time</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
+     * @param value
+     *            the new value of the '<em>Action Start Time</em>' attribute.
+     * @see #getActionStartTime()
+     * @generated
+     */
+    void setActionStartTime(double value);
+
+    /**
+     * Returns the value of the '<em><b>Action Duration</b></em>' attribute. <!-- begin-user-doc -->
+     * <p>
+     * If the meaning of the '<em>Action Duration</em>' attribute isn't clear, there really should
+     * be more of a description here...
+     * </p>
+     * <!-- end-user-doc -->
+     * 
+     * @return the value of the '<em>Action Duration</em>' attribute.
+     * @see #setActionDuration(double)
+     * @see de.uka.ipd.sdq.simucomframework.simucomstatus.SimucomstatusPackage#getAction_ActionDuration()
+     * @model required="true" transient="true" volatile="true" ordered="false"
+     * @generated
+     */
+    double getActionDuration();
+
+    /**
+     * Sets the value of the '
+     * {@link de.uka.ipd.sdq.simucomframework.simucomstatus.Action#getActionDuration
+     * <em>Action Duration</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
+     * @param value
+     *            the new value of the '<em>Action Duration</em>' attribute.
+     * @see #getActionDuration()
+     * @generated
+     */
+    void setActionDuration(double value);
+
+} // Action
---
+++ de.uka.ipd.sdq.simucomframework.simucomstatus/src/de/uka/ipd/sdq/simucomframework/simucomstatus/WaitForAcquire.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2008, sdq.ipd.uka.de, U KA
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.simucomframework.simucomstatus;
+
+/**
+ * <!-- begin-user-doc --> A representation of the model object '<em><b>Wait For Acquire</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ * <li>{@link de.uka.ipd.sdq.simucomframework.simucomstatus.WaitForAcquire#getResource <em>Resource
+ * </em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see de.uka.ipd.sdq.simucomframework.simucomstatus.SimucomstatusPackage#getWaitForAcquire()
+ * @model
+ * @generated
+ */
+public interface WaitForAcquire extends Action {
+    /**
+     * Returns the value of the '<em><b>Resource</b></em>' reference. It is bidirectional and its
+     * opposite is '
+     * {@link de.uka.ipd.sdq.simucomframework.simucomstatus.PassiveResource#getWaitingProcesses
+     * <em>Waiting Processes</em>}'. <!-- begin-user-doc -->
+     * <p>
+     * If the meaning of the '<em>Resource</em>' reference isn't clear, there really should be more
+     * of a description here...
+     * </p>
+     * <!-- end-user-doc -->
+     * 
+     * @return the value of the '<em>Resource</em>' reference.
+     * @see #setResource(PassiveResource)
+     * @see de.uka.ipd.sdq.simucomframework.simucomstatus.SimucomstatusPackage#getWaitForAcquire_Resource()
+     * @see de.uka.ipd.sdq.simucomframework.simucomstatus.PassiveResource#getWaitingProcesses
+     * @model opposite="waitingProcesses" required="true" ordered="false"
+     * @generated
+     */
+    PassiveResource getResource();
+
+    /**
+     * Sets the value of the '
+     * {@link de.uka.ipd.sdq.simucomframework.simucomstatus.WaitForAcquire#getResource
+     * <em>Resource</em>}' reference. <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
+     * @param value
+     *            the new value of the '<em>Resource</em>' reference.
+     * @see #getResource()
+     * @generated
+     */
+    void setResource(PassiveResource value);
+
+} // WaitForAcquire
