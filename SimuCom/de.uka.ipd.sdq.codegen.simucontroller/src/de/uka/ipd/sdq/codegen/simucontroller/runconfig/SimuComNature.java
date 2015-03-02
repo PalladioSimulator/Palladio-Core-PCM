50fbb514f07851642554fa26ee18fe0824c8430b
MichaelJunker <MichaelJunker>
2015-03-02 14:38:42 +0000

    - further refactorings on the documentation of simucontroller and rerunsimulation plug in
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@25532 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- SimuCom/de.uka.ipd.sdq.codegen.simucontroller/src/de/uka/ipd/sdq/codegen/simucontroller/runconfig/SimuComNature.java
+++ SimuCom/de.uka.ipd.sdq.codegen.simucontroller/src/de/uka/ipd/sdq/codegen/simucontroller/runconfig/SimuComNature.java
@@ -4,22 +4,31 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectNature;
 import org.eclipse.core.runtime.CoreException;
 
+/**
+ * Class for the SimuCom Project Nature. This project nature is attached to every project that is
+ * generated before the SimuCom Simulation starts.
+ * 
+ * This is an implementation of org.eclipse.core.resources.natures extension point and was only
+ * intended to support de.uka.ipd.sdq.simucom.rerunsimulation plug-in
+ * 
+ * @author Michael Junker
+ *
+ */
 public class SimuComNature implements IProjectNature {
-    
+
+    /**
+     * The id of this project nature. Can be used to search for projects containing this nature
+     */
     public static final String NATURE_ID = "de.uka.ipd.sdq.codegen.simucontroller.runconfig.simucomnature";
-    
+
     private IProject project;
 
     @Override
     public void configure() throws CoreException {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void deconfigure() throws CoreException {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
