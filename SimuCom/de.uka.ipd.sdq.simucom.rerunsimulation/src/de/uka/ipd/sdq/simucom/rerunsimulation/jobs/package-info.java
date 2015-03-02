2ea4d2f202607b4274507d081f698818ae66c976
MichaelJunker <MichaelJunker>
2015-03-02 09:28:53 +0000

    - added doc folder to the plugin
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@25528 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ SimuCom/de.uka.ipd.sdq.simucom.rerunsimulation/src/de/uka/ipd/sdq/simucom/rerunsimulation/jobs/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contains the jobs needed for rerunning a simulation. Those jobs include the main process for
+ * rerunning a simulation and a code generation job when allocation, usage or system should be
+ * regenerated.
+ * 
+ * @author Michael Junker
+ */
+package de.uka.ipd.sdq.simucom.rerunsimulation.jobs;
