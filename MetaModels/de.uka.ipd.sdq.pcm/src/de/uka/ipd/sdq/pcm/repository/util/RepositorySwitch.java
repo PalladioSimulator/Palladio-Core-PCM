dc2f3f75b6d05cf4c7a141d8b86eac574a84aa4d
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2015-04-10 09:03:52 +0000

    Added debug message
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@25987 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- MetaModels/de.uka.ipd.sdq.pcm/src/de/uka/ipd/sdq/pcm/repository/util/RepositorySwitch.java
+++ MetaModels/de.uka.ipd.sdq.pcm/src/de/uka/ipd/sdq/pcm/repository/util/RepositorySwitch.java
@@ -98,6 +98,9 @@ public class RepositorySwitch<T> {
      * @generated
      */
     public T doSwitch(final EObject theEObject) {
+        if (theEObject == null) {
+            throw new IllegalArgumentException("Passed eObject was null!");
+        }
         return this.doSwitch(theEObject.eClass(), theEObject);
     }
 
