e933f99361fdc11dc138acbbb801cd3fd69a3434
matzebecker <matthias.becker@iem.fraunhofer.de>
2015-04-13 08:03:45 +0000

    Marked generated files as not derived
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk/StoEx@26080 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.pcm.pcmstoex.tests/src/org/palladiosimulator/pcm/pcmstoex/tests/ParserTest.java
+++ org.palladiosimulator.pcm.pcmstoex.tests/src/org/palladiosimulator/pcm/pcmstoex/tests/ParserTest.java
@@ -51,20 +51,20 @@ public class ParserTest {
         parse("\"Dies ist ein Text!\"", PCMPARSER);
         parse("true", PCMPARSER);
         parse("false", PCMPARSER);
-        //this one is not supported by PCMStoExParser, as this is a not type variable
+        // this one is not supported by PCMStoExParser, as this is a not type variable
         parse("a.package.variable", STOEXPARSER);
-        //this one is not supported by PCMStoExParser, as func is not a valid function
+        // this one is not supported by PCMStoExParser, as func is not a valid function
         parse("func(true, 42)", STOEXPARSER);
         parse("40+2", PCMPARSER);
         parse("44-2", PCMPARSER);
         parse("3*3", PCMPARSER);
         parse("84/2", PCMPARSER);
         parse("4%3", PCMPARSER);
-        //this one is not supported by PCMStoExParser, as this is a not type variable
+        // this one is not supported by PCMStoExParser, as this is a not type variable
         parse("my.variable >= 5", STOEXPARSER);
-        //this one is not supported by PCMStoExParser, as this is a not type variable
+        // this one is not supported by PCMStoExParser, as this is a not type variable
         parse("your.variable == 0", STOEXPARSER);
-        //this one is not supported by PCMStoExParser, as this is a not type variable
+        // this one is not supported by PCMStoExParser, as this is a not type variable
         parse("eine.bedingung ? true : false", STOEXPARSER);
         parse("( 2 + 4 ) * 3", PCMPARSER);
         parse("IntPMF[(1;0.2)(2;0.5)(5;0.3)] * Exp(4) + 2 % 4 >= 42 OR 2 == 0", PCMPARSER);
@@ -79,20 +79,20 @@ public class ParserTest {
         parseFail("a$JFfuRKrp23r32#", PCMPARSER);
         parseFail("", PCMPARSER);
     }
-    
+
     @Test
-    public void testValidator(){
-    	parseFail("true AND 42", PCMPARSER_ONLY);
+    public void testValidator() {
+        parseFail("false AND 42", PCMPARSER_ONLY);
     }
 
     private void parseFail(String string, int type) {
         boolean crashed = false;
-        try{
+        try {
             parse(string, type);
-        } catch(RuntimeException e){
+        } catch (RuntimeException e) {
             crashed = true;
         }
-        if(!crashed){
+        if (!crashed) {
             throw new RuntimeException("Error expected but there is no :(");
         }
     }
@@ -100,7 +100,7 @@ public class ParserTest {
     private void parse(final String s, int type) {
         final StoExParser parser;
         final StoExSerializer seri;
-        if(type == STOEXPARSER){
+        if (type == STOEXPARSER) {
             parser = new StoExParser(s, true);
             seri = new StoExSerializer();
         } else {
@@ -112,11 +112,11 @@ public class ParserTest {
         if (parser.hasErrors()) {
             throw new RuntimeException("Xtext Syntax Error");
         }
-        
+
         Assert.assertEquals(removespaces(s), removespaces(seri.valueOf(o)));
-        
-        //things parsed with PCMStoExParser should be parsable by the StoExParser
-        if(type == PCMPARSER){
+
+        // things parsed with PCMStoExParser should be parsable by the StoExParser
+        if (type == PCMPARSER) {
             parse(s, STOEXPARSER);
         }
 
