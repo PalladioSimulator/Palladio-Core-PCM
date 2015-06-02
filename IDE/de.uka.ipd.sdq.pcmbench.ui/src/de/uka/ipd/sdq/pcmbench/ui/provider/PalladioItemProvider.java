28e0fecd32f6abf36996b81e27d683cc4188aefc
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2015-06-02 18:22:04 +0000

    Refactored Adapter Factory
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@26829 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- IDE/de.uka.ipd.sdq.pcmbench.ui/src/de/uka/ipd/sdq/pcmbench/ui/provider/PalladioItemProvider.java
+++ IDE/de.uka.ipd.sdq.pcmbench.ui/src/de/uka/ipd/sdq/pcmbench/ui/provider/PalladioItemProvider.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package de.uka.ipd.sdq.pcmbench.ui.provider;
 
@@ -35,174 +35,187 @@ import de.uka.ipd.sdq.pcmbench.ui.PCMBenchUIPlugin;
  *
  */
 public class PalladioItemProvider extends ItemProviderDecorator implements
-	IEditingDomainItemProvider,	
-	IStructuredItemContentProvider,	
-	ITreeItemContentProvider,	
-	IItemLabelProvider,	
-	IItemPropertySource,
-	Adapter,
-	CreateChildCommand.Helper {
+IEditingDomainItemProvider,
+IStructuredItemContentProvider,
+ITreeItemContentProvider,
+IItemLabelProvider,
+IItemPropertySource,
+Adapter,
+CreateChildCommand.Helper {
 
-	/**
-	 * Default constructor
-	 * @param adapterFactory Decorated adapter factory
-	 */
-	public PalladioItemProvider(AdapterFactory adapterFactory) {
-		super(adapterFactory);
-	}
+    /**
+     * Default constructor
+     * @param adapterFactory Decorated adapter factory
+     */
+    public PalladioItemProvider(final AdapterFactory adapterFactory) {
+        super(adapterFactory);
+    }
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.emf.edit.provider.ItemProviderDecorator#getText(java.lang.Object)
-	 * A label getter which is aware of the PCM
-	 */
-	@Override
-	public String getText(Object object) {
-		String result = "";
-		if (object instanceof PCMRandomVariable) {
-			String label = ((PCMRandomVariable)object).getSpecification();
-			if (label == null) label = "";
+    /* (non-Javadoc)
+     * @see org.eclipse.emf.edit.provider.ItemProviderDecorator#getText(java.lang.Object)
+     * A label getter which is aware of the PCM
+     */
+    @Override
+    public String getText(final Object object) {
+        String result = "";
+        if (object instanceof PCMRandomVariable) {
+            String label = ((PCMRandomVariable)object).getSpecification();
+            if (label == null) {
+                label = "";
+            }
 
-			String containingFeature = ((PCMRandomVariable)object).eContainingFeature().getName();
-			containingFeature = containingFeature.split("_")[0];
-			containingFeature = containingFeature.substring(0, 1).toUpperCase() + containingFeature.substring(1);
-			return containingFeature + ": " + label + " <PCM Random Variable>";
-		}
-		if (object instanceof AssemblyContext) {
-			AssemblyContext ctx = (AssemblyContext)object;
-			result = ctx.getEntityName();
-			result += ctx.getEncapsulatedComponent__AssemblyContext() == null ? "" : " <Component: "+ctx.getEncapsulatedComponent__AssemblyContext().getEntityName()+">";
-		} else if (object instanceof ProcessingResourceSpecification) { 
-			ProcessingResourceSpecification spec = (ProcessingResourceSpecification) object;
-			result += "Processing Resource ";
-			if (spec.getActiveResourceType_ActiveResourceSpecification() != null)
-				result += spec.getActiveResourceType_ActiveResourceSpecification().getEntityName() + ": ";
-			else
-				result += "<unset>: ";
-			result += "Cores: ";
-			result += spec.getNumberOfReplicas();
-			result += " Rate: ";
-			if (spec.getProcessingRate_ProcessingResourceSpecification() != null)
-				if (spec.getProcessingRate_ProcessingResourceSpecification().getSpecification() != null) {
-					result += spec.getProcessingRate_ProcessingResourceSpecification().getSpecification() + " ";
-				}
-				else {
-					result += "N/A ";
-				}
-			else
-				result += "N/A ";
-			if (spec.getSchedulingPolicy() != null) {
-				result += "Scheduling: "+spec.getSchedulingPolicy().getEntityName();
-			} else {
-				result += "Scheduling: N/A";
-			}
-		} else if (object instanceof Entity) {
-			result = ((Entity)object).getEntityName();
-		} else if (object instanceof ResourceDemandingSEFF){
-			ResourceDemandingSEFF seff = (ResourceDemandingSEFF) object;
-			result = "SEFF " + (seff.getDescribedService__SEFF() == null ? "" : seff.getDescribedService__SEFF().getEntityName());
-		} else {
-			if (object instanceof EObject && new RepositoryPrinter().doSwitch((EObject)object)!=null)
-				result = (String) new RepositoryPrinter().doSwitch((EObject)object);
-			else
-				result = super.getText(object); 
-		}
-		if (object instanceof EObject)
-		{
-			result += " <"+((EObject)object).eClass().getName()+"> ";
-		}
-		if (object instanceof Identifier)
-		{
-			result += " [ID: " + ((Identifier)object).getId() + "]";
-		}
-		return result; 
-	}
+            String containingFeature = ((PCMRandomVariable)object).eContainingFeature().getName();
+            containingFeature = containingFeature.split("_")[0];
+            containingFeature = containingFeature.substring(0, 1).toUpperCase() + containingFeature.substring(1);
+            return containingFeature + ": " + label + " <PCM Random Variable>";
+        }
+        if (object instanceof AssemblyContext) {
+            final AssemblyContext ctx = (AssemblyContext)object;
+            result = ctx.getEntityName();
+            result += ctx.getEncapsulatedComponent__AssemblyContext() == null ? "" : " <Component: "+ctx.getEncapsulatedComponent__AssemblyContext().getEntityName()+">";
+        } else if (object instanceof ProcessingResourceSpecification) {
+            final ProcessingResourceSpecification spec = (ProcessingResourceSpecification) object;
+            result += "Processing Resource ";
+            if (spec.getActiveResourceType_ActiveResourceSpecification() != null) {
+                result += spec.getActiveResourceType_ActiveResourceSpecification().getEntityName() + ": ";
+            } else {
+                result += "<unset>: ";
+            }
+            result += "Cores: ";
+            result += spec.getNumberOfReplicas();
+            result += " Rate: ";
+            if (spec.getProcessingRate_ProcessingResourceSpecification() != null) {
+                if (spec.getProcessingRate_ProcessingResourceSpecification().getSpecification() != null) {
+                    result += spec.getProcessingRate_ProcessingResourceSpecification().getSpecification() + " ";
+                }
+                else {
+                    result += "N/A ";
+                }
+            } else {
+                result += "N/A ";
+            }
+            if (spec.getSchedulingPolicy() != null) {
+                result += "Scheduling: "+spec.getSchedulingPolicy().getEntityName();
+            } else {
+                result += "Scheduling: N/A";
+            }
+        } else if (object instanceof Entity) {
+            result = ((Entity)object).getEntityName();
+        } else if (object instanceof ResourceDemandingSEFF){
+            final ResourceDemandingSEFF seff = (ResourceDemandingSEFF) object;
+            result = "SEFF " + (seff.getDescribedService__SEFF() == null ? "" : seff.getDescribedService__SEFF().getEntityName());
+        } else {
+            if (object instanceof EObject && new RepositoryPrinter().doSwitch((EObject)object)!=null) {
+                result = (String) new RepositoryPrinter().doSwitch((EObject)object);
+            } else {
+                result = super.getText(object);
+            }
+        }
+        if (object instanceof EObject)
+        {
+            result += " <"+((EObject)object).eClass().getName()+"> ";
+        }
+        if (object instanceof Identifier)
+        {
+            result += " [ID: " + ((Identifier)object).getId() + "]";
+        }
+        return result;
+    }
 
-	public Notifier getTarget() {
-		return ((Adapter)getDecoratedItemProvider()).getTarget();
-	}
+    @Override
+    public Notifier getTarget() {
+        return ((Adapter)getDecoratedItemProvider()).getTarget();
+    }
 
-	public void setTarget(Notifier newTarget) {
-		((Adapter)getDecoratedItemProvider()).setTarget(newTarget);
-	}
+    @Override
+    public void setTarget(final Notifier newTarget) {
+        ((Adapter)getDecoratedItemProvider()).setTarget(newTarget);
+    }
 
-	public String getCreateChildDescription(Object owner, Object feature,
-			Object child, Collection<?> selection) {
-		return ((CreateChildCommand.Helper)getDecoratedItemProvider()).getCreateChildDescription(owner, feature, child, selection);
-	}
+    @Override
+    public String getCreateChildDescription(final Object owner, final Object feature,
+            final Object child, final Collection<?> selection) {
+        return ((CreateChildCommand.Helper)getDecoratedItemProvider()).getCreateChildDescription(owner, feature, child, selection);
+    }
 
-	public Object getCreateChildImage(Object owner, Object feature,
-			Object child, Collection<?> selection) {
-		return ((CreateChildCommand.Helper)getDecoratedItemProvider()).getCreateChildImage(owner, feature, child, selection);
-	}
+    @Override
+    public Object getCreateChildImage(final Object owner, final Object feature,
+            final Object child, final Collection<?> selection) {
+        return ((CreateChildCommand.Helper)getDecoratedItemProvider()).getCreateChildImage(owner, feature, child, selection);
+    }
 
-	public Collection<?> getCreateChildResult(Object child) {
-		return ((CreateChildCommand.Helper)getDecoratedItemProvider()).getCreateChildResult(child);
-	}
+    @Override
+    public Collection<?> getCreateChildResult(final Object child) {
+        return ((CreateChildCommand.Helper)getDecoratedItemProvider()).getCreateChildResult(child);
+    }
 
-	public String getCreateChildText(Object owner, Object feature,
-			Object child, Collection<?> selection) {
-		if (selection.size() == 1) {
-			if (owner instanceof EObject) {
-				if (feature instanceof EReference) {
-					String a = palladioConvention(((EReference)feature).getName());
-					String b = ((EObject)child).eClass().getName();
-					if (a.replaceAll(" ","").toLowerCase().equals(b.toLowerCase())) {
-						return PCMBenchUIPlugin.INSTANCE.getString(
-								"_UI_CreateChild_palladio_simple", 
-								new Object[] {b}, 
-								true);
-					} else
-						return PCMBenchUIPlugin.INSTANCE.getString(
-							"_UI_CreateChild_palladio", 
-							new Object[] {a,b}, 
-							true);
-				}
-			}
-		}
-		return ((CreateChildCommand.Helper)getDecoratedItemProvider()).getCreateChildText(owner, feature, child, selection);
-	}
+    @Override
+    public String getCreateChildText(final Object owner, final Object feature,
+            final Object child, final Collection<?> selection) {
+        if (selection.size() == 1) {
+            if (owner instanceof EObject) {
+                if (feature instanceof EReference) {
+                    final String a = palladioConvention(((EReference)feature).getName());
+                    final String b = ((EObject)child).eClass().getName();
+                    if (a.replaceAll(" ","").toLowerCase().equals(b.toLowerCase())) {
+                        return PCMBenchUIPlugin.INSTANCE.getString(
+                                "_UI_CreateChild_palladio_simple",
+                                new Object[] {b},
+                                true);
+                    } else {
+                        return PCMBenchUIPlugin.INSTANCE.getString(
+                                "_UI_CreateChild_palladio",
+                                new Object[] {a,b},
+                                true);
+                    }
+                }
+            }
+        }
+        return ((CreateChildCommand.Helper)getDecoratedItemProvider()).getCreateChildText(owner, feature, child, selection);
+    }
 
-	private String palladioConvention(String originalName) {
-		if (originalName.indexOf("_") >= 0) {
-			originalName = originalName.substring(0, originalName.indexOf("_"));
-			if (originalName.length() >= 1) {
-				String newOriginalName = Character.toUpperCase(originalName.charAt(0))+""; 
-				for (int i=1; i < originalName.length(); i++) {
-					if (Character.isUpperCase(originalName.charAt(i))) {
-						newOriginalName += " ";
-					}
-					newOriginalName += originalName.charAt(i);
-				}
-				originalName = newOriginalName;
-			}
-		}
-		return originalName;
-	}
+    private String palladioConvention(String originalName) {
+        if (originalName.indexOf("_") >= 0) {
+            originalName = originalName.substring(0, originalName.indexOf("_"));
+            if (originalName.length() >= 1) {
+                String newOriginalName = Character.toUpperCase(originalName.charAt(0))+"";
+                for (int i=1; i < originalName.length(); i++) {
+                    if (Character.isUpperCase(originalName.charAt(i))) {
+                        newOriginalName += " ";
+                    }
+                    newOriginalName += originalName.charAt(i);
+                }
+                originalName = newOriginalName;
+            }
+        }
+        return originalName;
+    }
 
-	public String getCreateChildToolTipText(Object owner, Object feature,
-			Object child, Collection<?> selection) {
-		return ((CreateChildCommand.Helper)getDecoratedItemProvider()).getCreateChildToolTipText(owner, feature, child, selection);
-	}
+    @Override
+    public String getCreateChildToolTipText(final Object owner, final Object feature,
+            final Object child, final Collection<?> selection) {
+        return ((CreateChildCommand.Helper)getDecoratedItemProvider()).getCreateChildToolTipText(owner, feature, child, selection);
+    }
 
-	@Override
-	public Command createCommand(Object object, EditingDomain domain,
-			Class<? extends Command> commandClass,
-			CommandParameter commandParameter) {
-		if (commandClass == CreateChildCommand.class)
-	    {
-	      CommandParameter newChildParameter = (CommandParameter)commandParameter.getValue();
-	      return new CreateChildCommand
-	          (domain,
-	           commandParameter.getEOwner(), 
-	           newChildParameter.getEStructuralFeature(), 
-	           newChildParameter.getValue(),
-	           newChildParameter.getIndex(),
-	           commandParameter.getCollection(),
-	           this);      
-	    }
-		return super.createCommand(object, domain, commandClass, commandParameter);
-	}
-	
-	
-	
+    @Override
+    public Command createCommand(final Object object, final EditingDomain domain,
+            final Class<? extends Command> commandClass,
+            final CommandParameter commandParameter) {
+        if (commandClass == CreateChildCommand.class)
+        {
+            final CommandParameter newChildParameter = (CommandParameter)commandParameter.getValue();
+            return new CreateChildCommand
+                    (domain,
+                            commandParameter.getEOwner(),
+                            newChildParameter.getEStructuralFeature(),
+                            newChildParameter.getValue(),
+                            newChildParameter.getIndex(),
+                            commandParameter.getCollection(),
+                            this);
+        }
+        return super.createCommand(object, domain, commandClass, commandParameter);
+    }
+
+
+
 }
--- IDE/de.uka.ipd.sdq.pcmbench.ui/src/de/uka/ipd/sdq/pcmbench/ui/provider/PalladioItemProviderAdapterFactory.java
+++ IDE/de.uka.ipd.sdq.pcmbench.ui/src/de/uka/ipd/sdq/pcmbench/ui/provider/PalladioItemProviderAdapterFactory.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package de.uka.ipd.sdq.pcmbench.ui.provider;
 
@@ -13,47 +13,48 @@ import org.eclipse.emf.edit.provider.INotifyChangedListener;
 
 /**
  * @author Snowball
- * This factory generates the default ItemProvider adapter which can be used 
- * to display elements in Palladio related GUI elements, e.g., trees or list boxes 
+ * This factory generates the default ItemProvider adapter which can be used
+ * to display elements in Palladio related GUI elements, e.g., trees or list boxes
  *
  */
-public class PalladioItemProviderAdapterFactory 
-	extends DecoratorAdapterFactory 
-	implements INotifyChangedListener, ComposeableAdapterFactory
+public class PalladioItemProviderAdapterFactory
+extends DecoratorAdapterFactory
+implements INotifyChangedListener, ComposeableAdapterFactory
 {
 
-	/**
-	 * @param decoratedAdapterFactory The factory which gets decorated
-	 */
-	public PalladioItemProviderAdapterFactory(
-			AdapterFactory decoratedAdapterFactory) {
-		super(decoratedAdapterFactory);
-	}
+    /**
+     * @param decoratedAdapterFactory The factory which gets decorated
+     */
+    public PalladioItemProviderAdapterFactory(
+            final AdapterFactory decoratedAdapterFactory) {
+        super(decoratedAdapterFactory);
+    }
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.emf.edit.provider.DecoratorAdapterFactory#createItemProviderDecorator(java.lang.Object, java.lang.Object)
-	 */
-	@Override
-	protected IItemProviderDecorator createItemProviderDecorator(Object target,
-			Object Type) {
-		PalladioItemProvider result = new PalladioItemProvider(this);
-		if (((Class)Type).isInstance(result)) {
-			result.addListener(this);
-			return result;
-		}
-		return null;
-	}
+    /* (non-Javadoc)
+     * @see org.eclipse.emf.edit.provider.DecoratorAdapterFactory#createItemProviderDecorator(java.lang.Object, java.lang.Object)
+     */
+    @Override
+    protected IItemProviderDecorator createItemProviderDecorator(final Object target,
+            final Object type) {
+        final PalladioItemProvider result = new PalladioItemProvider(this);
+        if (((Class<?>)type).isInstance(result)) {
+            result.addListener(this);
+            return result;
+        }
+        return null;
+    }
 
-	public void notifyChanged(Notification notification) {
-		fireNotifyChanged(notification);
-	}
+    @Override
+    public void notifyChanged(final Notification notification) {
+        fireNotifyChanged(notification);
+    }
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.emf.edit.provider.DecoratorAdapterFactory#getRootAdapterFactory()
-	 */
-	@Override
-	public ComposeableAdapterFactory getRootAdapterFactory() {
-		return this;
-	}
+    /* (non-Javadoc)
+     * @see org.eclipse.emf.edit.provider.DecoratorAdapterFactory#getRootAdapterFactory()
+     */
+    @Override
+    public ComposeableAdapterFactory getRootAdapterFactory() {
+        return this;
+    }
 
 }
