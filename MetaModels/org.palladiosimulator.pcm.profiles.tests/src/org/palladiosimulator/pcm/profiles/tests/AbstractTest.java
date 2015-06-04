d862c28d01ab7767d97e48e6aab4b442aa4a23c2
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2015-06-04 14:32:25 +0000

    waiting for UI tests in buckminster now...
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@26858 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- MetaModels/org.palladiosimulator.pcm.profiles.tests/src/org/palladiosimulator/pcm/profiles/tests/AbstractTest.java
+++ MetaModels/org.palladiosimulator.pcm.profiles.tests/src/org/palladiosimulator/pcm/profiles/tests/AbstractTest.java
@@ -2,12 +2,10 @@ package org.palladiosimulator.pcm.profiles.tests;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.Map;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.modelversioning.emfprofile.Profile;
@@ -15,8 +13,6 @@ import org.modelversioning.emfprofile.registry.IProfileRegistry;
 import org.palladiosimulator.commons.emfutils.EMFLoadHelper;
 
 import de.uka.ipd.sdq.pcm.repository.Repository;
-import de.uka.ipd.sdq.pcm.repository.RepositoryFactory;
-import de.uka.ipd.sdq.pcm.repository.RepositoryPackage;
 
 /**
  * @author Sebastian Lehrig
@@ -25,7 +21,7 @@ public abstract class AbstractTest {
 
     private static final String SYSTEM_TEMP = System.getProperty("java.io.tmpdir");
     private static final String FILE_URI = "file://" + SYSTEM_TEMP + "/Palladio/ProfileTest.repository";
-    private static final String REPOSITORY_URI = "platform:/resource/org.palladiosimulator.pcm.profiles.tests/model/default.repository";
+    private static final String REPOSITORY_URI = "platform:/plugin/org.palladiosimulator.pcm.profiles.tests/model/default.repository";
     private static final String REPOSITORY_ID = "_Mh6NQAOdEeW7RJF7nLqWPA";
     private static final String ORIGINAL_REPOSITORY_URI = REPOSITORY_URI + "#" + REPOSITORY_ID;
     private static final String STORAGE_REPOSITORY_URI = FILE_URI + "#" + REPOSITORY_ID;
@@ -69,14 +65,6 @@ public abstract class AbstractTest {
     }
 
     protected static void reset() {
-        /** EMF factory is needed to load repository resources. */
-        RepositoryPackage.eINSTANCE.eClass();
-        final RepositoryFactory factory = RepositoryFactory.eINSTANCE;
-
-        final Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
-        final Map<String, Object> m = reg.getExtensionToFactoryMap();
-        m.put("repository", new XMIResourceFactoryImpl());
-
         final Resource resource = (new ResourceSetImpl()).createResource(URI.createURI(FILE_URI));
         resource.getContents().add(EMFLoadHelper.loadAndResolveEObject(ORIGINAL_REPOSITORY_URI));
         try {
