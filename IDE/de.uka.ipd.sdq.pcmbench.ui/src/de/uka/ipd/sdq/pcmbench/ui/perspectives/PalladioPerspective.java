bd9f4170297db586ba69d490216707f83d00b50b
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2015-06-10 19:08:09 +0000

    EDP2 now replaces the experiments navigator in the Palladio perspective
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@27029 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- IDE/de.uka.ipd.sdq.pcmbench.ui/src/de/uka/ipd/sdq/pcmbench/ui/perspectives/PalladioPerspective.java
+++ IDE/de.uka.ipd.sdq.pcmbench.ui/src/de/uka/ipd/sdq/pcmbench/ui/perspectives/PalladioPerspective.java
@@ -16,44 +16,46 @@ import org.eclipse.ui.console.IConsoleConstants;
 public class PalladioPerspective implements IPerspectiveFactory {
 
     private final String SIMULATION_VIEW_ID = "de.uka.ipd.sdq.codegen.simucontroller.gui.DockStatusViewPart";
-    private final String EXPERIMENTS_VIEW = "de.uka.ipd.sdq.codegen.simudatavisualization.ExperimentsView";
+    private final String EXPERIMENTS_VIEW = "org.palladiosimulator.edp2.ui.views.navigator";
 
     @Override
-    public void createInitialLayout(IPageLayout layout) {
+    public void createInitialLayout(final IPageLayout layout) {
         defineActions(layout);
         defineLayout(layout);
     }
 
-    private void defineLayout(IPageLayout layout) {
+    private void defineLayout(final IPageLayout layout) {
         layout.setEditorAreaVisible(true);
         layout.setFixed(false);
 
-        String editorAreaId = layout.getEditorArea();
+        final String editorAreaId = layout.getEditorArea();
 
         // Top left: Project view and Experiments view
-        IFolderLayout topLeftFolder = layout.createFolder("topLeft", IPageLayout.LEFT, (float) 0.25, editorAreaId);
+        final IFolderLayout topLeftFolder = layout
+                .createFolder("topLeft", IPageLayout.LEFT, (float) 0.25, editorAreaId);
         topLeftFolder.addView(IPageLayout.ID_PROJECT_EXPLORER);
-        topLeftFolder.addView(EXPERIMENTS_VIEW);
+        topLeftFolder.addView(this.EXPERIMENTS_VIEW);
 
         // Bottom left: Outline view.
-        IFolderLayout bottomLeftFolder = layout.createFolder("bottomLeft", IPageLayout.BOTTOM, 0.618f, "topLeft");
+        final IFolderLayout bottomLeftFolder = layout.createFolder("bottomLeft", IPageLayout.BOTTOM, 0.618f, "topLeft");
         bottomLeftFolder.addView(IPageLayout.ID_OUTLINE);
 
         // Place properties and console view to bottom of the editor area.
-        IFolderLayout bottomFolder = layout.createFolder("bottom", IPageLayout.BOTTOM, (float) 0.75, editorAreaId);
+        final IFolderLayout bottomFolder = layout
+                .createFolder("bottom", IPageLayout.BOTTOM, (float) 0.75, editorAreaId);
         bottomFolder.addView(IPageLayout.ID_PROP_SHEET);
         bottomFolder.addView(IConsoleConstants.ID_CONSOLE_VIEW);
 
         // Place Simulation Dock Status next to properties.
-        IFolderLayout bottomRightFolder = layout
-                .createFolder("bottomRight", IPageLayout.RIGHT, (float) 0.618, "bottom");
-        bottomRightFolder.addView(SIMULATION_VIEW_ID);
+        final IFolderLayout bottomRightFolder = layout.createFolder("bottomRight", IPageLayout.RIGHT, (float) 0.618,
+                "bottom");
+        bottomRightFolder.addView(this.SIMULATION_VIEW_ID);
     }
 
-    private void defineActions(IPageLayout layout) {
+    private void defineActions(final IPageLayout layout) {
         // add "Run <configuration>"-button (along with additional buttons residing in the same
         // action set)
         layout.addActionSet(IDebugUIConstants.LAUNCH_ACTION_SET);
     }
-    
+
 }
