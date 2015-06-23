1040e662516f33c7f7efafc6144326cd707e0c18
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2015-06-23 13:02:13 +0000

    Extracted GMF Editors to addons
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@27493 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- Editors/de.uka.ipd.sdq.pcm.gmf.composite.custom/src/de/uka/ipd/sdq/pcm/gmf/composite/custom/edit/parts/package-info.java
+++
@@ -1,7 +0,0 @@
-/** Edit part package. */
-package de.uka.ipd.sdq.pcm.gmf.composite.custom.edit.parts;
-
-/**
- *Missing Package info.
- */
-
--- Editors/de.uka.ipd.sdq.pcm.gmf.seff.custom/src/de/uka/ipd/sdq/pcm/gmf/seff/custom/edit/commands/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Provides customized edit commands for the seff editor.
- */
-package de.uka.ipd.sdq.pcm.gmf.seff.custom.edit.commands;
-
