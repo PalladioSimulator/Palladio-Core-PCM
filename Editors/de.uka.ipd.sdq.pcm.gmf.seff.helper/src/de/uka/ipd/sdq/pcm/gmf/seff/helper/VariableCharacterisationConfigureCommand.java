799e5e89aa2aebdd50688ede2cdd1a9439ca154d
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2015-07-10 08:47:18 +0000

    fixed PALLADIO-345
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Core/trunk@28316 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ Editors/de.uka.ipd.sdq.pcm.gmf.seff.helper/src/de/uka/ipd/sdq/pcm/gmf/seff/helper/VariableCharacterisationConfigureCommand.java
@@ -0,0 +1,111 @@
+package de.uka.ipd.sdq.pcm.gmf.seff.helper;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.gmf.runtime.common.core.command.CommandResult;
+import org.eclipse.gmf.runtime.emf.type.core.commands.ConfigureElementCommand;
+import org.eclipse.gmf.runtime.emf.type.core.commands.SetValueCommand;
+import org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest;
+import org.eclipse.gmf.runtime.emf.type.core.requests.SetRequest;
+import org.eclipse.jface.window.Window;
+import org.eclipse.ui.PlatformUI;
+import org.palladiosimulator.editors.dialogs.stoex.StochasticExpressionEditDialog;
+import org.palladiosimulator.pcm.core.CoreFactory;
+import org.palladiosimulator.pcm.core.PCMRandomVariable;
+import org.palladiosimulator.pcm.parameter.ParameterPackage;
+import org.palladiosimulator.pcm.parameter.VariableCharacterisation;
+
+import de.uka.ipd.sdq.stoex.StoexPackage;
+import de.uka.ipd.sdq.stoex.analyser.visitors.TypeEnum;
+
+/**
+ * The Class ParametricResourceDemandConfigureCommand allows the selection of a parametric resource
+ * and specification of the amout used.
+ *
+ * @author roman
+ */
+public class VariableCharacterisationConfigureCommand extends ConfigureElementCommand {
+
+    /** The request. */
+    private ConfigureRequest request = null;
+
+    /**
+     * Instantiates a new parametric resource demand configure command.
+     *
+     * @param request
+     *            the request
+     */
+    public VariableCharacterisationConfigureCommand(final ConfigureRequest request) {
+        super(request);
+        this.request = request;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand#
+     * doExecuteWithResult(org.eclipse.core.runtime.IProgressMonitor,
+     * org.eclipse.core.runtime.IAdaptable)
+     */
+    /**
+     * Do execute with result.
+     *
+     * @param monitor
+     *            the monitor
+     * @param info
+     *            the info
+     * @return the command result
+     * @throws ExecutionException
+     *             the execution exception
+     * @see org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand#doExecuteWithResult(org.eclipse.core.runtime.IProgressMonitor,
+     *      org.eclipse.core.runtime.IAdaptable)
+     */
+    @Override
+    protected CommandResult doExecuteWithResult(final IProgressMonitor monitor, final IAdaptable info)
+            throws ExecutionException {
+        final CommandResult commandResult = this.setSpecification_ParametricResourceDemand(monitor, info);
+        if (!this.isOK(commandResult)) {
+            return CommandResult.newErrorCommandResult("Set Action for the Usage Characterisation failed!");
+        }
+        return CommandResult.newOKCommandResult();
+    }
+
+    /**
+     * Sets the specification_ parametric resource demand.
+     *
+     * @param monitor
+     *            the monitor
+     * @param info
+     *            the info
+     * @return the command result
+     * @throws ExecutionException
+     *             the execution exception
+     */
+    private CommandResult setSpecification_ParametricResourceDemand(final IProgressMonitor monitor,
+            final IAdaptable info) throws ExecutionException {
+
+        final StochasticExpressionEditDialog dialog = new StochasticExpressionEditDialog(PlatformUI.getWorkbench()
+                .getActiveWorkbenchWindow().getShell(), TypeEnum.DOUBLE, this.request.getElementToConfigure());
+        dialog.open();
+
+        if (dialog.getReturnCode() == Window.CANCEL) {
+            return CommandResult.newCancelledCommandResult();
+        }
+
+        final PCMRandomVariable randomVariable = CoreFactory.eINSTANCE.createPCMRandomVariable();
+
+        final SetRequest setRequest = new SetRequest(request.getElementToConfigure(),
+                ParameterPackage.eINSTANCE.getVariableCharacterisation_Specification_VariableCharacterisation(), randomVariable);
+
+        SetValueCommand cmd = new SetValueCommand(setRequest);
+        cmd.execute(monitor, info);
+
+        cmd = new SetValueCommand(
+                new SetRequest(((VariableCharacterisation) this.request.getElementToConfigure()).getSpecification_VariableCharacterisation(),
+                        StoexPackage.eINSTANCE.getRandomVariable_Specification(), dialog.getResultText()));
+        cmd.execute(monitor, info);
+
+        return cmd.getCommandResult();
+    }
+}
--- Editors/de.uka.ipd.sdq.pcm.gmf.seff.helper/src/de/uka/ipd/sdq/pcm/gmf/seff/helper/VariableCharacterisationEditHelperAdvice.java
+++ Editors/de.uka.ipd.sdq.pcm.gmf.seff.helper/src/de/uka/ipd/sdq/pcm/gmf/seff/helper/VariableCharacterisationEditHelperAdvice.java
@@ -1,36 +1,30 @@
 /**
- * 
+ *
  */
 package de.uka.ipd.sdq.pcm.gmf.seff.helper;
 
 import org.eclipse.gmf.runtime.common.core.command.ICommand;
-import org.eclipse.gmf.runtime.emf.type.core.ElementTypeRegistry;
-import org.eclipse.gmf.runtime.emf.type.core.IElementType;
-import org.eclipse.gmf.runtime.emf.type.core.commands.CreateElementCommand;
 import org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelperAdvice;
 import org.eclipse.gmf.runtime.emf.type.core.edithelper.IEditHelperAdvice;
 import org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest;
-import org.eclipse.gmf.runtime.emf.type.core.requests.CreateElementRequest;
-
-import org.palladiosimulator.pcm.parameter.ParameterPackage;
 
 /**
  * The Class VariableCharacterisationEditHelperAdvice is responsible for automatically creation of a
  * PCM random variable.
- * 
+ *
  * @author Roman Andrej
  */
 public class VariableCharacterisationEditHelperAdvice extends AbstractEditHelperAdvice implements IEditHelperAdvice {
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelperAdvice#
      * getAfterConfigureCommand(org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest)
      */
     /**
      * Gets the after configure command.
-     * 
+     *
      * @param request
      *            the request
      * @return the after configure command
@@ -38,30 +32,7 @@ public class VariableCharacterisationEditHelperAdvice extends AbstractEditHelper
      */
     @Override
     protected ICommand getAfterConfigureCommand(final ConfigureRequest request) {
-
-        final IElementType elementType = ElementTypeRegistry.getInstance().getType(
-                "de.uka.ipd.sdq.pcm.gmf.seff.PCMRandomVariable_9000");
-
-        final CreateElementRequest createElementRequest = new CreateElementRequest(request.getElementToConfigure(),
-                elementType,
-                ParameterPackage.eINSTANCE.getVariableCharacterisation_Specification_VariableCharacterisation());
-        return new CreateElementCommand(createElementRequest);
-
-        // FIXME: These commented out lines are more than 4 years commented out, do we really need
-        // them?
-        // PCMRandomVariable randomVariable = CoreFactory.eINSTANCE
-        // .createPCMRandomVariable();
-        //
-        // randomVariable.setSpecification("1");
-        //
-        // SetRequest setRequest = new SetRequest(
-        // (VariableCharacterisation) request.getElementToConfigure(),
-        // ParameterPackage.eINSTANCE
-        // .getVariableCharacterisation_Specification_VariableCharacterisation(),
-        // randomVariable);
-        //
-        // // TODO Auto-generated method stub
-        // return new SetValueCommand(setRequest);
+        return new VariableCharacterisationConfigureCommand(request);
     }
 
 }
