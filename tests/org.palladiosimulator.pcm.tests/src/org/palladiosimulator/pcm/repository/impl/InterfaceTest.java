b9157c299e8f1a596abf94b44ce257d9c28eddb9
Stephan Seifermann <stephan.seifermann@kit.edu>
2021-09-23 10:46:52 +0200

    PALLADIO-539 Test Interface::isAssignableFrom with diamond and list

--- tests/org.palladiosimulator.pcm.tests/src/org/palladiosimulator/pcm/repository/impl/InterfaceTest.java
+++ tests/org.palladiosimulator.pcm.tests/src/org/palladiosimulator/pcm/repository/impl/InterfaceTest.java
@@ -42,9 +42,22 @@ public class InterfaceTest {
     @Test
     public void testIsAssignedFromDirectParent() {
         var i0 = createInterface();
+        var i0_0 = createInterface();
+        i0.getParentInterfaces__Interface().add(i0_0);
+        assertTrue(i0.isAssignableFrom(i0_0));
+        assertFalse(i0_0.isAssignableFrom(i0));
+    }
+    
+    @Test
+    public void testIsAssignedFromDirectParentFromList() {
+        var i0 = createInterface();
+        var i0_0 = createInterface();
         var i0_1 = createInterface();
+        i0.getParentInterfaces__Interface().add(i0_0);
         i0.getParentInterfaces__Interface().add(i0_1);
+        assertTrue(i0.isAssignableFrom(i0_0));
         assertTrue(i0.isAssignableFrom(i0_1));
+        assertFalse(i0_0.isAssignableFrom(i0));
         assertFalse(i0_1.isAssignableFrom(i0));
     }
     
@@ -58,6 +71,20 @@ public class InterfaceTest {
         assertTrue(i0.isAssignableFrom(i0_0_0));
         assertFalse(i0_0_0.isAssignableFrom(i0));
         assertFalse(i0_0_0.isAssignableFrom(i0_0));
+    }
+    
+    @Test
+    public void testIsAssignedFromIndirectParentInDiamond() {
+        var i0 = createInterface();
+        var i0_0 = createInterface();
+        var i0_1 = createInterface();
+        var i0_01_0 = createInterface();
+        i0.getParentInterfaces__Interface().add(i0_0);
+        i0.getParentInterfaces__Interface().add(i0_1);
+        i0_0.getParentInterfaces__Interface().add(i0_01_0);
+        i0_1.getParentInterfaces__Interface().add(i0_01_0);
+        assertTrue(i0.isAssignableFrom(i0_01_0));
+        assertFalse(i0_01_0.isAssignableFrom(i0));
     }
 
     protected static Interface createInterface() {
