22a4d0d7cbd2a8ddd8e4d3571781499128accf70
Stephan Seifermann <stephan.seifermann@kit.edu>
2021-09-23 10:28:53 +0200

    PALLADIO-539 Add method isAssignableFrom to Interface

---
+++ tests/org.palladiosimulator.pcm.tests/src/org/palladiosimulator/pcm/repository/impl/InterfaceTest.java
@@ -0,0 +1,67 @@
+package org.palladiosimulator.pcm.repository.impl;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.palladiosimulator.pcm.repository.Interface;
+import org.palladiosimulator.pcm.repository.RepositoryFactory;
+
+import tools.mdsd.library.standalone.initialization.StandaloneInitializationException;
+import tools.mdsd.library.standalone.initialization.StandaloneInitializerBuilder;
+
+public class InterfaceTest {
+
+    @BeforeAll
+    public static void init() throws StandaloneInitializationException {
+        StandaloneInitializerBuilder.builder()
+            .build()
+            .init();
+    }
+
+    @Test
+    public void testIsAssignedFromNull() {
+        var i0 = createInterface();
+        assertFalse(i0.isAssignableFrom(null));
+    }
+    
+    @Test
+    public void testIsAssignedFromUnrelatedInterface() {
+        var i0 = createInterface();
+        var i1 = createInterface();
+        assertFalse(i0.isAssignableFrom(i1));
+    }
+    
+    @Test
+    public void testIsAssignedFromSameInterface() {
+        var i0 = createInterface();
+        assertTrue(i0.isAssignableFrom(i0));
+    }
+    
+    @Test
+    public void testIsAssignedFromDirectParent() {
+        var i0 = createInterface();
+        var i0_1 = createInterface();
+        i0.getParentInterfaces__Interface().add(i0_1);
+        assertTrue(i0.isAssignableFrom(i0_1));
+        assertFalse(i0_1.isAssignableFrom(i0));
+    }
+    
+    @Test
+    public void testIsAssignedFromIndirectParent() {
+        var i0 = createInterface();
+        var i0_0 = createInterface();
+        i0.getParentInterfaces__Interface().add(i0_0);
+        var i0_0_0 = createInterface();
+        i0_0.getParentInterfaces__Interface().add(i0_0_0);
+        assertTrue(i0.isAssignableFrom(i0_0_0));
+        assertFalse(i0_0_0.isAssignableFrom(i0));
+        assertFalse(i0_0_0.isAssignableFrom(i0_0));
+    }
+
+    protected static Interface createInterface() {
+        return RepositoryFactory.eINSTANCE.createOperationInterface();
+    }
+
+}
